660505647cf9f0abed9ab8984eff06251fea8faa
kevin123 <kevinlekiller@gmail.com>
2013-04-11 23:31:41 -0400

    Syntax

--- www/lib/releases.php
+++ www/lib/releases.php
@@ -1183,7 +1183,7 @@ class Releases
 				{
 					$colID = $rowcol['ID'];
 					//get the filecount
-					$binfiles = $db->queryOneRow(sprintf("SELECT count(ID) from binaries where collectionID = %d", $colID))
+					$binfiles = $db->queryOneRow(sprintf("SELECT count(ID) from binaries where collectionID = %d", $colID));
 					$binfiles = array_shift($binfiles);
 					$db->queryDirect(sprintf("UPDATE collections set filecheck = 2 and totalFiles = %d where ID = %d", $binfiles, $colID));
 				}
