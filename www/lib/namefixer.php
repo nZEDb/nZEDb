14997c4120be7c09cb1b29827c16e4e59f70541f
kevin123 <kevinlekiller@gmail.com>
2013-04-12 23:34:37 -0400

    Update the status of this file.

--- www/lib/namefixer.php
+++ www/lib/namefixer.php
@@ -8,23 +8,29 @@ class Namefixer
 	//
 	//Attempts to fix release names using the release name.
 	//
-	public function fixNamesWithNames($type)
+	public function fixNamesWithNames($type, $time)
 	{
-		//Fix releases using the release name, added in the past 24 hours.
+		//Fix releases which have never been fixed using the release name.
 		if ($type == 1)
 		{
-			
+			$query = "SELECT name, ID from releases where relnamestatus = 1"
 		}
 		
-		//Fix all releases using the release name.
+		//Fix all releases using the release name ignoring relnamestatus.
 		if ($type == 2)
 		{
-			
+			$query = "SELECT name, ID from releases"
 		}
 		
-		if ($type > 2 || $type == 0 || !is_numeric($type))
+		//Do 24 hours or full DB.
+		if ($time == 1)
 		{
-			exit("ERROR: Wrong argument");
+			$nameres = $db->queryDirect(sprintf($query,"where adddate > (now() - interval 1 day)"));
+		}
+		
+		if ($time == 2)
+		{
+			$nameres = $db->queryDirect($query);
 		}
 	}
 	
@@ -36,17 +42,23 @@ class Namefixer
 		//fix releases using the NFO, added in the past 24 hours.
 		if ($type == 1)
 		{
-			
+			$query = "SELECT name, ID from releases where relnamestatus = 1"
 		}
 		//Fix all releases using the NFO.
 		if ($type == 2)
 		{
-			
+			$query = "SELECT name, ID from releases"
 		}
 		
-		if ($type > 2 || $type == 0 || !is_numeric($type))
+		//Do 24 hours or full DB.
+		if ($time == 1)
 		{
-			exit("ERROR: Wrong argument");
+			$nameres = $db->queryDirect(sprintf($query,"where adddate > (now() - interval 1 day)"));
+		}
+		
+		if ($time == 2)
+		{
+			$nameres = $db->queryDirect($query);
 		}
 	}
 	
@@ -58,17 +70,48 @@ class Namefixer
 		//fix releases using the release file, added in the past 24 hours.
 		if ($type == 1)
 		{
-			
+			$query = "SELECT name, ID from releases where relnamestatus = 1"
 		}
 		//Fix all releases using the file name.
 		if ($type == 2)
+		if ($type == 2)
 		{
-			
+			$query = "SELECT name, ID from releases"
+		}
+		
+		//Do 24 hours or full DB.
+		if ($time == 1)
+		{
+			$nameres = $db->queryDirect(sprintf($query,"where adddate > (now() - interval 1 day)"));
+		}
+		
+		if ($time == 2)
+		{
+			$nameres = $db->queryDirect($query);
+		}
+	}
+	
+	//
+	//Reset the relnamestatus to 1.
+	//
+	public function resetRelname($type)
+	{
+		//Reset relnamestatus to 1 for releases added in the past 24 hours.
+		if ($type == 1)
+		{
+			$db = new DB();
+			$db->queryDirect("UPDATE releases set relnamestatus = 1 where adddate > (now() - interval 1 day)")
+		}
+		//Reset relnamestatus to 1 for all releases.
+		if ($type == 2)
+		{
+			$db = new DB();
+			$db->queryDirect("UPDATE releases set relnamestatus = 1")
 		}
 		
 		if ($type > 2 || $type == 0 || !is_numeric($type))
 		{
-			exit("ERROR: Wrong argument");
+			exit("ERROR: Wrong argument.");
 		}
-	}	
+	}
 }
