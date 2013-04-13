5a5a79add2e11df075d1297a1de0057578583ad9
kevin123 <kevinlekiller@gmail.com>
2013-04-13 03:10:16 -0400

    Better grouping of collections. sleep ..

--- www/lib/binaries.php
+++ www/lib/binaries.php
@@ -252,9 +252,11 @@ class Binaries
 					//Anything between the quotes. Too much variance within the quotes, so remove it completely.
 					$cleansubject = preg_replace('/\".+\"/i', '', $cleansubject);
 					//File extensions - If it was not quotes.
-					$cleansubject = preg_replace('/(\.part(\d{1,5})?)?\.(7z|\d{3}(?=(\s|"))|avi|idx|jpg|mp4|nfo|nzb|par\s?2|pdf|rar|rev|r\d\d|sfv|srs|srr|sub|vol.+(par2)|zip)"?|\d{2,3}\.pdf|\d{2,3}\s\-\s.+\.mp3/i', '', $cleansubject);
+					$cleansubject = preg_replace('/(\.part(\d{1,5})?)?\.(7z|\d{3}(?=(\s|"))|avi|idx|jpg|mp4|nfo|nzb|par\s?2|pdf|rar|rev|r\d\d|sfv|srs|srr|sub|txt|vol.+(par2)|zip)"?|\d{2,3}\.pdf|\d{2,3}\s\-\s.+\.mp3|\.part\d{1,4}\./i', '', $cleansubject);
 					//File Sizes - Non unique ones.
-					$cleansubject = preg_replace('/\-\s\d{1,3}\.\d{1,3}\s(M|K)B\s(?=\-\s\d{1,3}\.\d{1,3}\s(G|M)B\s)|><\s\d{1,3}\/\d{1,3}\s\(.+><\s\d{1,3},\d{1,3}\s(G|M)B\s>|\]\s\d{1,}KB\s/i', '', $cleansubject);
+					$cleansubject = preg_replace('/\-\s\d{1,3}\.\d{1,3}\s(M|K)B\s(?=\-\s\d{1,3}\.\d{1,3}\s(G|M)B\s)|><\s\d{1,3}\/\d{1,3}\s\(.+><\s\d{1,3},\d{1,3}\s(G|M)B\s>|(\])?\s\d{1,}KB\s(yENC)?|"?\s\d{1,}\sbytes|(\-\s)?\d{1,}(\.|,)?\d{1,}\s(g|k|m)?B\s\-(\syenc)?/i', '', $cleansubject);
+					//Random stuff.
+					$cleansubject = preg_replace('/AutoRarPar\d{1,5}/i', '', $cleansubject);
 					$cleansubject = utf8_encode(trim($cleansubject));
 					
 					if(!isset($this->message[$subject]))
@@ -309,12 +311,12 @@ class Binaries
 						{
 							//File and part count.
 							$cleanerName = preg_replace('/\[\d+(\/|(\s|_)of(\s|_)|\-)\d+\]|\(\d+(\/|\sof\s|\-)\d+\)|File\s\d+\sof\s\d{1,4}|\-\s\d{1,3}\/\d{1,3}\s\-|\d{1,3}\/\d{1,3}\]\s\-/i', '', $subject);
-							//Extensions.
-							$cleanerName = preg_replace('/(\.part(\d{1,5})?)?\.(7z|\d{3}(?=(\s|"))|avi|idx|jpg|mp4|nfo|nzb|par\s?2|pdf|rar|rev|r\d\d|sfv|srs|srr|sub|vol.+(par2)|zip)"?|\d{2,3}\.pdf|yEnc/i', '', $cleanerName);
 							//Size.
-							$cleanerName = preg_replace('/\d{1,3}(\.|,)\d{1,3}\s(K|M|G)B|\d{1,}(K|M|G)B/i', '', $cleanerName);
+							$cleanerName = preg_replace('/\d{1,3}(\.|,)\d{1,3}\s(K|M|G)B|\d{1,}(K|M|G)B|\d{1,}\sbytes|(\-\s)?\d{1,}(\.|,)?\d{1,}\s(g|k|m)?B\s\-(\syenc)?/i', '', $cleanerName);
+							//Extensions.
+							$cleanerName = preg_replace('/(\.part(\d{1,5})?)?\.(7z|\d{3}(?=(\s|"))|avi|idx|jpg|mp4|nfo|nzb|par\s?2|pdf|rar|rev|r\d\d|sfv|srs|srr|sub|txt|vol.+(par2)|zip)"?|\d{2,3}\.pdf|yEnc|\.part\d{1,4}\./i', '', $cleanerName);
 							//Unwanted stuff.
-							$cleanerName = preg_replace('/SECTIONED brings you|usenet-space-cowboys.info|<.+https:\/\/secretusenet\.com>|> USC <|\[\d{1,}\]\-\[FULL\].+#a\.b[\w.#!@$%^&*\(\){}\|\\:"\';<>,?~` ]+\]|brothers-of-usenet.info(\/\.net)?|Partner von SSL-News.info/i', '', $cleanerName);
+							$cleanerName = preg_replace('/SECTIONED brings you|usenet\-space\-cowboys\.info|<.+https:\/\/secretusenet\.com>|> USC <|\[\d{1,}\]\-\[FULL\].+#a\.b[\w.#!@$%^&*\(\){}\|\\:"\';<>,?~` ]+\]|brothers\-of\-usenet\.info(\/\.net)?|Partner von SSL-News\.info|AutoRarPar\d{1,5}/i', '', $cleanerName);
 							//Removes some characters.
 							$cleanerName = preg_replace('/<|>|"|=|\[|\]|\(|\)|\{|\}/i', '', $cleanerName);
 							//Replaces some characters with 1 space.
