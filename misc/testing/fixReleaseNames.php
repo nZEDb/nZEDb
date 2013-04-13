62419df554174ba2b21d018ec27a02ffb3f1275d
kevin123 <kevinlekiller@gmail.com>
2013-04-13 01:10:31 -0400

    Submitting what I have now.

--- misc/testing/fixReleaseNames.php
+++ misc/testing/fixReleaseNames.php
@@ -1,8 +1,9 @@
 <?php
 
-require("config.php");
-require_once(WWW_DIR."/lib/framework/db.php");
-require_once(WWW_DIR."/lib/framework/namefixer.php");
+define('FS_ROOT', realpath(dirname(__FILE__)));
+require_once(FS_ROOT."/../../www/config.php");
+require_once(FS_ROOT."/../../www/lib/framework/db.php");
+require_once(FS_ROOT."/../../www/lib/namefixer.php");
 
 $n = "\n";
 $namefixer = new Namefixer;
@@ -11,27 +12,27 @@ if (isset($argv[1]))
 {
 	if ($argv[1] == 1)
 	{
-		$namerfixer->fixNamesWithNames(1);
+		$namefixer->fixNamesWithNames(1);
 	}
 	else if ($argv[1] == 2)
 	{
-		$namerfixer->fixNamesWithNames(2);
+		$namefixer->fixNamesWithNames(2);
 	}
 	else if ($argv[1] == 3)
 	{
-		$namerfixer->fixNamesWithNfo(1);
+		$namefixer->fixNamesWithNfo(1);
 	}
 	else if ($argv[1] == 4)
 	{
-		$namerfixer->fixNamesWithNfo(2);
+		$namefixer->fixNamesWithNfo(2);
 	}
 	else if ($argv[1] == 5)
 	{
-		$namerfixer->fixNamesWithFiles(1);
+		$namefixer->fixNamesWithFiles(1);
 	}
 	else if ($argv[1] == 6)
 	{
-		$namerfixer->fixNamesWithFiles(2);
+		$namefixer->fixNamesWithFiles(2);
 	}
 	else
 	{
@@ -42,11 +43,11 @@ if (isset($argv[1]))
 else
 {
 	echo "ERROR: You must supply an argument.\n".
-			"php fixReleaseNames.php 1 ...: Fix release names, using the usenet subject in the past 24 hours - on all categories.".$n
+			"php fixReleaseNames.php 1 ...: Fix release names, using the usenet subject in the past 24 hours - on all categories.".$n.
 			"php fixReleaseNames.php 2 ...: Fix release names, using the usenet subject - on all categories.".$n.
 			"php fixReleaseNames.php 3 ...: Placeholder - fix release names in misc categories using NFO in the past 24 hours.".$n.
 			"php fixReleaseNames.php 4 ...: Placeholder - fix release names in misc categories using NFO.".$n.
-			"php fixReleaseNames.php 5 ...: Placeholder - fix release names in misc categories using File Name in the past 24 hours.".$n;
+			"php fixReleaseNames.php 5 ...: Placeholder - fix release names in misc categories using File Name in the past 24 hours.".$n.
 			"php fixReleaseNames.php 6 ...: Placeholder - fix release names in misc categories using File Name.".$n;
 }
 
--- www/lib/namefixer.php
+++ www/lib/namefixer.php
@@ -5,6 +5,7 @@ require_once(WWW_DIR."/lib/categorizer.php");
 
 class Namefixer
 {
+	private $tmpName = array();
 	//
 	//Attempts to fix release names using the release name.
 	//
@@ -12,17 +13,23 @@ class Namefixer
 	{
 		//Fix releases which have never been fixed using the release name.
 		
-		$query = "SELECT name, ID from releases"
+		$db = new DB();
+		$query = "SELECT name, ID from releases";
 		
 		//Do 24 hours or full DB.
 		if ($time == 1)
 		{
-			$nameres = $db->queryDirect(sprintf($query,"where adddate > (now() - interval 1 day)"));
+			$relres = $db->queryDirect(sprintf($query,"where adddate > (now() - interval 1 day)"));
 		}
 		
 		if ($time == 2)
 		{
-			$nameres = $db->queryDirect($query);
+			$relres = $db->queryDirect($query);
+		}
+		
+		while ($relrow = mysql_fetch_array($relres))
+		{
+			$this->checkName($relrow);
 		}
 	}
 	
@@ -33,7 +40,8 @@ class Namefixer
 	{
 		//Fix releases in misc -> other using the NFO.
 		
-		$query = "SELECT name, ID from releases where categoryID = 7010 and categoryID = 2020 and categoryID = 5050"
+		$db = new DB();
+		$query = "SELECT ID from releases where categoryID = 7010 and categoryID = 2020 and categoryID = 5050";
 		
 		//Do 24 hours or full DB.
 		if ($time == 1)
@@ -45,6 +53,9 @@ class Namefixer
 		{
 			$nameres = $db->queryDirect($query);
 		}
+		
+		$nfo = $db->queryOneRow(sprintf("SELECT uncompress(nfo) as NFO from releasenfo where releaseID = %d", $nameres['ID']));
+		$this->checkName($nfo);
 	}
 	
 	//
@@ -54,7 +65,8 @@ class Namefixer
 	{
 		//Fix releases in misc -> other using the file names.
 		
-		$query = "SELECT name, ID from releases where categoryID = 7010 and categoryID = 2020 and categoryID = 5050"
+		$db = new DB();
+		$query = "SELECT ID from releases where categoryID = 7010 and categoryID = 2020 and categoryID = 5050";
 		
 		//Do 24 hours or full DB.
 		if ($time == 1)
@@ -66,5 +78,48 @@ class Namefixer
 		{
 			$nameres = $db->queryDirect($query);
 		}
+		
+		$filename = $db->queryOneRow(sprintf("SELECT name as filename from releasefiles where releaseID = %d", $nameres['ID']));
+		$this->checkName($filename);
+	}
+	
+	//
+	//Update the release with the new information.
+	//
+	public function updateRelease($ID, $name)
+	{
+		$db = new DB();
+		$db->queryDirect(sprintf("UPDATE releases set searchname = %s where ID = %d", $ID, $name));
+	}
+	
+	//
+	//Check the array using regex for a clean name.
+	//
+	public function checkName($array)
+	{                          
+		if($this->tvCheck($array))
+		{ 
+			$this->updateRelease($ID, $name);
+		}
+		if($this->movieCheck($array))
+		{ 
+			$this->updateRelease($ID, $name);
+		}
+	}
+	
+	//
+	//Look for a TV name.
+	//
+	public function tvCheck($array)
+	{
+		print_r($array);
+	}
+	
+	//
+	//Look for a movie name.
+	//
+	public function movieCheck($array)
+	{
+		
 	}
 }
