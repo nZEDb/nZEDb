21059974bb8a37479f46f23d6a2f57636d52c0f1
kevin123 <kevinlekiller@gmail.com>
2013-04-14 11:29:21 -0400

    Adding better shell scripts.

--- misc/update_scripts/nix_scripts/start.sh
+++ misc/update_scripts/nix_scripts/start.sh
@@ -1,37 +1,22 @@
 #!/bin/sh
-# call this script from within screen to get binaries, processes releases and 
-# every half day get tv/theatre info and optimise the database
-
-set -e
 
 export NEWZNAB_PATH="/var/www/newznab/misc/update_scripts"
-export NEWZNAB_SLEEP_TIME="60" # in seconds
-LASTOPTIMIZE=`date +%s`
-LASTOPTIMIZE1=`date +%s`
+export NIX_PATH="/var/www/newznab/misc/update_scripts/nix_scripts"
+export NEWZNAB_SLEEP_TIME="60"
+export PHP="$(which php5)"
+export SCREEN="$(which screen)"
 
 while :
 
- do
-CURRTIME=`date +%s`
+do
+
+cd ${NIX_PATH}
+if ! $SCREEN -list | grep -q "binaries"; then
+	cd $NIX_PATH && $SCREEN -dmS binaries $SCREEN sh $NIX_PATH/update.sh
+fi
+
 cd ${NEWZNAB_PATH}
-/usr/bin/php5 ${NEWZNAB_PATH}/update_binaries.php
-/usr/bin/php5 ${NEWZNAB_PATH}/update_releases.php 3
-
-DIFF=$(($CURRTIME-$LASTOPTIMIZE))
-if [ "$DIFF" -gt 7200 ] || [ "$DIFF" -lt 1 ]
-then
-	LASTOPTIMIZE=`date +%s`
-	echo "Optimizing DB..."
-	/usr/bin/php5 ${NEWZNAB_PATH}/optimise_db.php
-fi
-
-DIFF1=$(($CURRTIME-$LASTOPTIMIZE1))
-if [ "$DIFF1" -gt 43200 ] || [ "$DIFF1" -lt 1 ]
-then
-	LASTOPTIMIZE1=`date +%s`
-	/usr/bin/php5 ${NEWZNAB_PATH}/update_tvschedule.php
-	/usr/bin/php5 ${NEWZNAB_PATH}/update_theaters.php
-fi
+$PHP ${NEWZNAB_PATH}/update_releases.php 3
 
 echo "waiting ${NEWZNAB_SLEEP_TIME} seconds..."
 sleep ${NEWZNAB_SLEEP_TIME}
