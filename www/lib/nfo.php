8a74b2cb6560cc34fe461bea00a2a2707c87b936
kevin123 <kevinlekiller@gmail.com>
2013-04-15 11:32:32 -0400

    Fix nfo processing, postprocess_releases only doing additional.

--- www/lib/nfo.php
+++ www/lib/nfo.php
@@ -13,7 +13,20 @@ class Nfo
 		$this->echooutput = $echooutput;
 	}
 	
-	public function addReleaseNfo($relid, $binid)
+	public function determineReleaseNfo($relid)
+	{
+		$nfos = array();
+		$db = new DB();
+		$result = $db->queryDirect(sprintf("select binaries.* from binaries where releaseID = %d order by relpart", $relid));		
+		while ($row = mysql_fetch_assoc($result)) 
+			if (preg_match('/.*\.nfo[ "\)\]\-]?/i', $row['name'])) 
+				$nfos[$row['name']] = $row;
+
+		ksort($nfos);
+		return (is_array($nfos) && !empty($nfos)) ? array_shift($nfos) : false;
+	}
+	
+	public function addReleaseNfo($relid)
 	{
 		$db = new DB();
 		return $db->queryInsert(sprintf("INSERT INTO releasenfo (releaseID) VALUE (%d)", $relid));		
@@ -53,8 +66,114 @@ class Nfo
 		$groups = new Groups();
 		$nzbcontents = new NZBcontents();
 		
-		$res = $db->queryDirect("SELECT rn.ID, r.guid, r.groupID, rn.releaseID FROM releasenfo rn left outer join releases r ON r.ID = rn.releaseID WHERE rn.nfo IS NULL AND r.nfostatus between -5 and 0 order by adddate desc limit 0,50");
-		if (mysql_num_rows($res) > 0)
+		$res = $db->queryDirect("SELECT ID, guid, groupID FROM releases WHERE nfostatus between -6 and -1 order by adddate desc limit 0,50");
+		if (mysql_num_rows($res) >= 0)
+		{	
+			if ($this->echooutput)
+				echo "Processing ".mysql_num_rows($res)." NFO's.\n";
+		
+			$nntp->doConnect();
+			while ($arr = mysql_fetch_assoc($res))
+			{
+				$guid = $arr['guid'];
+				$relID = $arr['ID'];
+				$groupID = $arr['groupID'];
+				if ($messageid = $nzbcontents->getNFOfromNZB($guid))
+				{
+					$this->addReleaseNfo($relID);
+					$groupName = $groups->getByNameByID($groupID);
+					$fetchedBinary = $nntp->getMessage($groupName, $messageid);
+				
+					if ($fetchedBinary !== false) 
+					{
+						//insert nfo into database
+						$db->query(sprintf("UPDATE releasenfo SET nfo = compress(%s) WHERE releaseID = %d", $db->escapeString($fetchedBinary), $arr["ID"]));
+						$db->query(sprintf("UPDATE releases SET nfostatus = 1 WHERE ID = %d", $arr["ID"]));
+						$ret++;
+					
+						$imdbId = $this->parseImdb($fetchedBinary);
+						if ($imdbId !== false) 
+						{
+							//update release with imdb id
+							$db->query(sprintf("UPDATE releases SET imdbID = %s WHERE ID = %d", $db->escapeString($imdbId), $arr["ID"]));
+						
+							//if set scan for imdb info
+							if ($processImdb == 1)
+							{
+								$movie = new Movie($this->echooutput);
+								//check for existing movie entry
+								$movCheck = $movie->getMovieInfo($imdbId);
+								if ($movCheck === false || (isset($movCheck['updateddate']) && (time() - strtotime($movCheck['updateddate'])) > 2592000))
+								{
+									$movieId = $movie->updateMovieInfo($imdbId);
+								}
+							}
+						}
+					
+						$rageId = $this->parseRageId($fetchedBinary);
+						if ($rageId !== false)
+						{	
+							//if set scan for tvrage info
+							if ($processTvrage == 1)
+							{
+								$tvrage = new Tvrage($this->echooutput);
+								$show = $tvrage->parseNameEpSeason($arr['searchname']);	
+								if (is_array($show) && $show['name'] != '')
+								{	
+									// update release with season, ep, and airdate info (if available) from releasetitle
+									$tvrage->updateEpInfo($show, $arr['ID']);
+								
+									$rid = $tvrage->getByRageID($rageId);
+									if (!$rid)
+									{
+										$tvrShow = $tvrage->getRageInfoFromService($rageId);
+										$tvrage->updateRageInfo($rageId, $show, $tvrShow, $arr['ID']);
+									}
+								}
+							}
+						}
+					} 
+					else 
+					{
+						//nfo download failed, increment attempts
+						$db->query(sprintf("UPDATE releases SET nfostatus = nfostatus-1 WHERE ID = %d", $arr["ID"]));
+					}
+				
+					if ($ret != 0 && $this->echooutput && ($ret % 5 == 0))
+						echo $ret."..";
+					}
+				else
+				{	//No .nfo file in the NZB.
+					$db->queryOneRow("update releases set nfostatus = 0 where ID = %d", $relID);
+					break;
+				}
+			}
+			$nntp->doQuit();
+		}
+		
+		//remove nfo that we cant fetch after 5 attempts
+		$relres = $db->queryDirect("Select ID from releases where nfostatus <= -6");
+		while ($relrow = mysql_fetch_assoc($relres))
+		{
+			$db->query(sprintf("DELETE FROM releasenfo WHERE nfo IS NULL and releaseID = %d", $relrow['ID']));
+		}
+		
+		if ($this->echooutput)
+			echo "\n".$ret." NFO files processed\n";
+		
+		return $ret;
+	}
+	
+	public function processNfoFiles2($processImdb=1, $processTvrage=1)
+	{
+		$ret = 0;
+		$db = new DB();
+		$nntp = new Nntp();
+		$groups = new Groups();
+		$nzbcontents = new NZBcontents();
+		
+		$res = $db->queryDirect("SELECT rn.ID, r.guid, r.groupID, rn.releaseID FROM releasenfo rn left outer join releases r ON r.ID = rn.releaseID WHERE r.nfostatus between -5 and 0 order by adddate desc limit 0,50");
+		if (mysql_num_rows($res) >= 0)
 		{	
 			if ($this->echooutput)
 				echo "Processing ".mysql_num_rows($res)." NFO's.\n";
