7f749a4d01f20ab1e1c0d175683e5608afe4a3c9
jonnyboy <jonnyboy@jbmatrix.net>
2013-04-18 18:08:11 -0400

    renamed nzb-import with bulk handler to nzb-import-bulk.php, added original method nzb-import.php

---
+++ misc/testing/convert_all_tables_to_innodb_compressed.php
@@ -0,0 +1,19 @@
+<?php
+require(dirname(__FILE__)."/../../www/config.php");
+require_once(WWW_DIR."/lib/framework/db.php");
+
+$sql = "SHOW tables";
+$db = new DB();
+
+$tables = $db->query($sql);
+foreach($tables as $row)
+    {
+	$tbl = $row['Tables_in_newznab'];
+	printf("Converting $tbl\n");
+        $sql = "ALTER TABLE $tbl ENGINE=INNODB ROW_FORMAT=COMPRESSED";
+        $db->query($sql);
+    }
+
+
+?>
+
