64b88e65c232ee6ca11f0b0f1fbb502a66152b0d
kevin123 <kevinlekiller@gmail.com>
2013-04-18 15:33:59 -0400

    Add more file count cleaning.

--- www/lib/namecleaning.php
+++ www/lib/namecleaning.php
@@ -11,7 +11,7 @@
 		public function collectionsCleaner($subject)
 		{
 			//Parts/files
-			$cleansubject = preg_replace('/\[\d+(\/|(\s|_)of(\s|_)|\-)\d+\]|\(\d+(\/|\sof\s|\-)\d+\)|File\s\d+\sof\s\d{1,4}|\-\s\d{1,3}\/\d{1,3}\s\-|\d{1,3}\/\d{1,3}\]\s\-|\s\d{2,3}(\\|\/)\d{2,3}|^\[\d{1,3}\/\d{1,3}\s\s|\(\d{1,3}\|\d{1,3}\)/i', '', $subject);
+			$cleansubject = preg_replace('/\[\d+(\/|(\s|_)of(\s|_)|\-)\d+\]|\(\d+(\/|\sof\s|\-)\d+\)|File\s\d+\sof\s\d{1,4}|\-\s\d{1,3}\/\d{1,3}\s\-|\d{1,3}\/\d{1,3}\]\s\-|\s\d{2,3}(\\|\/)\d{2,3}|^\[\d{1,3}\/\d{1,3}\s\s|\(\d{1,3}\|\d{1,3}\)|\-\s\[\s\d{1,3}\/\d{1,3}\s\]\s\-|\s\d{1,3}\sof\s\d{1,3}/i', '', $subject);
 			//Anything between the quotes. Too much variance within the quotes, so remove it completely.
 			$cleansubject = preg_replace('/\".+\"/i', '', $cleansubject);
 			//File extensions - If it was not quotes.
@@ -31,7 +31,7 @@
 		public function releaseCleaner($subject)
 		{
 			//File and part count.
-			$cleanerName = preg_replace('/\[\d+(\/|(\s|_)of(\s|_)|\-)\d+\]|\(\d+(\/|\sof\s|\-)\d+\)|File\s\d+\sof\s\d{1,4}|\-\s\d{1,3}\/\d{1,3}\s\-|\d{1,3}\/\d{1,3}\]\s\-|\s\d{2,3}(\\|\/)\d{2,3}\s|^\[\d{1,3}\/\d{1,3}\s|\(\d{1,3}\|\d{1,3}\)/i', '', $subject);
+			$cleanerName = preg_replace('/\[\d+(\/|(\s|_)of(\s|_)|\-)\d+\]|\(\d+(\/|\sof\s|\-)\d+\)|File\s\d+\sof\s\d{1,4}|\-\s\d{1,3}\/\d{1,3}\s\-|\d{1,3}\/\d{1,3}\]\s\-|\s\d{2,3}(\\|\/)\d{2,3}\s|^\[\d{1,3}\/\d{1,3}\s|\(\d{1,3}\|\d{1,3}\)|\-\s\[\s\d{1,3}\/\d{1,3}\s\]\s\-|\s\d{1,3}\sof\s\d{1,3}/i', '', $subject);
 			//Size.
 			$cleanerName = preg_replace('/\d{1,3}(\.|,)\d{1,3}\s(K|M|G)B|\d{1,}(K|M|G)B|\d{1,}\sbytes|(\-\s)?\d{1,}(\.|,)?\d{1,}\s(g|k|m)?B\s\-(\syenc)?|\s\(d{1,3},\d{1,3}\s{K,M,G}B\)\s/i', '', $cleanerName);
 			//Extensions.
