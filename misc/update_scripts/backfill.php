d05801cdb92ce38d2ae222c90a1ccb9446654cae
kevin123 <kevinlekiller@gmail.com>
2013-04-21 20:33:22 -0400

    Fixed a typod

--- misc/update_scripts/backfill.php
+++ misc/update_scripts/backfill.php
@@ -43,7 +43,7 @@ else
 	exit("ERROR: Wrong set of arguments.".$n.$n.
 		 "php backfill.php safe 200000		 ...: Backfill an active group alphabetically, x articles, the script stops, ".$n.
 		 "					 ...: if the group has reached reached 2012-06-24, the next group will backfill.".$n.$n.
-		 "php backfill.php alph 200000 		 ...: Backfills all groups (sorted alphabetically_ by number of articles".$n.
+		 "php backfill.php alph 200000 		 ...: Backfills all groups (sorted alphabetically) by number of articles".$n.
 		 "php backfill.php date 200000 		 ...: Backfills all groups (sorted by least backfilled in time) by number of articles".$n.
 		 "php backfill.php alt.binaries.ath 200000 ...: Backfills a group by name by number of articles".$n.
 		 "php backfill.php all			 ...: Backfills all groups 1 at a time, by date (set in admin-view groups)".$n.
