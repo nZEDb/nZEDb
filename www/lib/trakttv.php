17c7e267c7475a8e4ab650e96f9afd04ef5ecc49
kevin123 <kevinlekiller@gmail.com>
2013-04-21 20:20:31 -0400

    Clean up the trakttv script

--- www/lib/trakttv.php
+++ www/lib/trakttv.php
@@ -1,9 +1,8 @@
 <?php
-require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/site.php");
 
 /* 
-*	Lookup information from trak.tv using their API.
+*	Lookup information from trakt.tv using their API.
 */
 Class Trakttv
 {
@@ -12,62 +11,6 @@ Class Trakttv
 		$s = new Sites();
 		$site = $s->get();
 		$this->APIKEY = $site->trakttvkey;
-	}
-	
-	public function traktSearchShow($title='')
-	{
-		$title = str_replace(array("_", " ", '.'), '-', $title);
-		$url = 'http://api.trakt.tv/search/shows.json/'.$this->APIKEY.'/'.$title;
-		$json = @file_get_contents($url, 0, null, null);
-		
-		if($json === false)
-		{
-			// We failed getting the URL. Maybe the API key is not set, or the release is not on the site?
-			return false;
-		}
-		else
-		{
-			$tarray = json_decode($json, true);
-			$preg_title = str_replace('-', ' ', $title);
-			// Check if the name matches.
-			if(preg_match("/".$tarray["0"]["title"]."/i", $preg_title))
-			{	
-				print_r($tarray);
-			}
-			// Try the second name as a last ditched effort.
-			else if(preg_match("/".$tarray["1"]["title"]."/i", $preg_title))
-			{
-				print_r($tarray);
-			}
-		}
-	}
-	
-	public function traktSearchMovie($title='')
-	{
-		$title = str_replace(array("_", " ", '.'), '-', $title);
-		$url = 'http://api.trakt.tv/search/movies.json/'.$this->APIKEY.'/'.$title;
-		$json = @file_get_contents($TVurl, 0, null, null);
-		
-		if($json === false)
-		{
-			// We failed getting the URL. Maybe the API key is not set, or the release is not on the site?
-			return false;
-		}
-		else
-		{
-			$marray = json_decode($json, true);
-			$preg_title = str_replace('-', ' ', $title);
-			// Check if the name matches.
-			if(preg_match("/".$marray["0"]["title"]."/i", $preg_title))
-			{
-				print_r($tarray);
-			}
-			// Try the second name as a last ditched effort.
-			else if(preg_match("/".$marray["1"]["title"]."/i", $preg_title))
-			{
-				print_r($tarray);
-			}
-		}
 	}
 	
 	//
@@ -94,52 +37,6 @@ Class Trakttv
 			return $TVarray;
 		}
 			
-	}
-	
-	//
-	// Fetches summary from trakt.tv for the TV show using a TVDB ID.
-	//
-	public function traktTVDBsummary($tvdbid='')
-	{
-		$TVurl = 'http://api.trakt.tv/show/summary.json/'.$this->APIKEY.'/'.$tvdbid;
-		$TVjson = @file_get_contents($TVurl, 0, null, null);
-		
-		if($TVjson === false)
-		{
-			// We failed getting the URL. Maybe the API key is not set, or the release is not on the site?
-			return false;
-		}
-		else
-		{
-			$TVarray = json_decode($TVjson, true);
-			print_r($TVarray);
-		}
-	}
-	
-	//
-	// Fetches summarry from trakt.tv for the TV show using the title.
-	//
-	public function traktTVsummary($showtitle='', $type='')
-	{
-		$chars = array(' ', '_', '.');
-		$showtitle = str_replace($chars, '-', $showtitle);
-		$TVurl = 'http://api.trakt.tv/show/summary.json/'.$this->APIKEY.'/'.$showtitle;
-		$TVjson = @file_get_contents($TVurl, 0, null, null);
-		
-		if($TVjson === false)
-		{
-			// We failed getting the URL. Maybe the API key is not set, or the release is not on the site?
-			return false;
-		}
-		else
-		{
-			$TVarray = json_decode($TVjson, true);
-			
-			if ($type == "array")
-			{
-				return $TVarray;
-			}
-		}
 	}
 	
 	//
@@ -182,37 +79,4 @@ Class Trakttv
 		}
 	}
 }
-
-/*				Returned from TV season ep summary:
- 
- 
-			// Common Show stuff.
-			$Title =		$TVarray['show']['title'];
-			$Runtime =		$TVarray['show']['runtime'];
-			$Network =		$TVarray['show']['network'];
-			$IMDBid	=		$TVarray['show']['imdb_id'];
-			$TVRageid =		$TVarray['show']['tvrage_id'];
-			$Genre =		$TVarray['show']['genres']['0'];
-			$Background =	$TVarray['show']['images']['fanart'];
-			$Banner =		$TVarray['show']['images']['banner'];
-			
-			// Episode specific stuff.
-			$EpTitle =		$TVarray['episode']['title'];
-			$EpOverview =	$TVarray['episode']['overview'];
-			$EpTVDBid =		$TVarray['episode']['tvdb_id'];
-			$EpURL =		$TVarray['episode']['url'];
-			$EpDate =		$TVarray['episode']['first_aired'];
-			$EpScreen =		$TVarray['episode']['images']['screen'];
-			$EpRating =		$TVarray['episode']['ratings']['percentage'];
-			$EpNumber =		$TVarray['episode']['number'];
-			$EpSeason = 	$TVarray['episode']['season'];
-			$EpDate = 		gmdate("Y-m-d H:i:s", $EpDate);
-			
-			exit($Title.", Season ".$EpSeason." Episode ".$EpNumber.". ".$EpTitle.
-					".\n\nEpisode overview: ".$EpOverview.
-					"\n\nAir date: ".$EpDate." Network: ".$Network.
-					".\nRun time: ".$Runtime." minutes. Genre: ".$Genre.
-					"\nEpisode Rating: ".$EpRating.". IMDB: ".$IMDBid.
-					"\nTVRage: ".$TVRageid.", TVDB: ".$EpTVDBid.
-					"\nTrakt.tv URL: ".$EpURL."\n");*/
-					
+?>
