0ebff7375d7ac96a29a8c216dcf132e7c81fbc2f
kevin <kev@example.com>
2013-04-24 12:30:03 -0400

    Throw exception if amazon key/ass tag is not set.

--- www/lib/amazon.php
+++ www/lib/amazon.php
@@ -93,8 +93,12 @@
         {
             if ($response === False)
             {
-                throw new Exception("Could not connect to Amazon");
+                throw new Exception("Could not connect to Amazon.");
             }
+            else if ($response == "missingkey")
+            {
+				throw new Exception("Missing Amazon API key or associate tag.");
+			}
             else
             {
                 if (isset($response->Items->Item->ItemAttributes->Title))
@@ -231,71 +235,78 @@
 
 	function  aws_signed_request($region,$params,$public_key,$private_key,$associate_tag)
 	{
-
-		$method = "GET";
-		$host = "ecs.amazonaws.".$region; // must be in small case
-		$uri = "/onca/xml";
 		
-		
-		$params["Service"]          = "AWSECommerceService";
-		$params["AWSAccessKeyId"]   = $public_key;
-		$params["AssociateTag"]		= $associate_tag;
-		$params["Timestamp"]        = gmdate("Y-m-d\TH:i:s\Z");
-		$params["Version"]          = "2009-03-31";
-
-		/* The params need to be sorted by the key, as Amazon does this at
-		  their end and then generates the hash of the same. If the params
-		  are not in order then the generated hash will be different thus
-		  failing the authetication process.
-		*/
-		ksort($params);
-		
-		$canonicalized_query = array();
-
-		foreach ($params as $param=>$value)
+		if ($public_key !== "" && $private_key !== "" && $associate_tag !== "")
 		{
-			$param = str_replace("%7E", "~", rawurlencode($param));
-			$value = str_replace("%7E", "~", rawurlencode($value));
-			$canonicalized_query[] = $param."=".$value;
-		}
+			$method = "GET";
+			$host = "ecs.amazonaws.".$region; // must be in small case
+			$uri = "/onca/xml";
 		
-		$canonicalized_query = implode("&", $canonicalized_query);
+		
+			$params["Service"]          = "AWSECommerceService";
+			$params["AWSAccessKeyId"]   = $public_key;
+			$params["AssociateTag"]		= $associate_tag;
+			$params["Timestamp"]        = gmdate("Y-m-d\TH:i:s\Z");
+			$params["Version"]          = "2009-03-31";
 
-		$string_to_sign = $method."\n".$host."\n".$uri."\n".$canonicalized_query;
+			/* The params need to be sorted by the key, as Amazon does this at
+			their end and then generates the hash of the same. If the params
+			are not in order then the generated hash will be different thus
+			failing the authetication process.
+			*/
+			ksort($params);
 		
-		/* calculate the signature using HMAC with SHA256 and base64-encoding.
-		   The 'hash_hmac' function is only available from PHP 5 >= 5.1.2.
-		*/
-		$signature = base64_encode(hash_hmac("sha256", $string_to_sign, $private_key, True));
-		
-		/* encode the signature for the request */
-		$signature = str_replace("%7E", "~", rawurlencode($signature));
-		
-		/* create request */
-		$request = "http://".$host.$uri."?".$canonicalized_query."&Signature=".$signature;
-		/* I prefer using CURL */
-		$ch = curl_init();
-		curl_setopt($ch, CURLOPT_URL,$request);
-		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
-		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			$canonicalized_query = array();
 
-		$xml_response = curl_exec($ch);
+			foreach ($params as $param=>$value)
+			{
+				$param = str_replace("%7E", "~", rawurlencode($param));
+				$value = str_replace("%7E", "~", rawurlencode($value));
+				$canonicalized_query[] = $param."=".$value;
+			}
 		
-		/* If cURL doesn't work for you, then use the 'file_get_contents'
-		   function as given below.
-		*/
-		//$xml_response = file_get_contents($request);
+			$canonicalized_query = implode("&", $canonicalized_query);
+
+			$string_to_sign = $method."\n".$host."\n".$uri."\n".$canonicalized_query;
 		
-		if ($xml_response === False)
-		{
-			return False;
+			/* calculate the signature using HMAC with SHA256 and base64-encoding.
+			The 'hash_hmac' function is only available from PHP 5 >= 5.1.2.
+			*/
+			$signature = base64_encode(hash_hmac("sha256", $string_to_sign, $private_key, True));
+		
+			/* encode the signature for the request */
+			$signature = str_replace("%7E", "~", rawurlencode($signature));
+		
+			/* create request */
+			$request = "http://".$host.$uri."?".$canonicalized_query."&Signature=".$signature;
+			/* I prefer using CURL */
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL,$request);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+
+			$xml_response = curl_exec($ch);
+		
+			/* If cURL doesn't work for you, then use the 'file_get_contents'
+			function as given below.
+			*/
+			//$xml_response = file_get_contents($request);
+		
+			if ($xml_response === False)
+			{
+				return False;
+			}
+			else
+			{
+				/* parse XML */
+				$parsed_xml = @simplexml_load_string($xml_response);
+				return ($parsed_xml === False) ? False : $parsed_xml;
+			}
 		}
 		else
 		{
-			/* parse XML */
-			$parsed_xml = @simplexml_load_string($xml_response);
-			return ($parsed_xml === False) ? False : $parsed_xml;
+			return "missingkey";
 		}
 	}
 
