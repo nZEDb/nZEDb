6025dbafa5d0b527bfdadb959befe634c01f1e44
kevin <kevinlekiller@gmail.com>
2013-04-26 09:56:01 -0400

    Paginated file list.

--- www/pages/filelist.php
+++ www/pages/filelist.php
@@ -25,9 +25,20 @@ if (isset($_GET["id"]))
 	ob_end_clean();
 		
 	$ret = $nzb->nzbFileList($nzbfile);
-		
+	
+	$offset = (isset($_REQUEST["offset"]) && ctype_digit($_REQUEST['offset'])) ? $_REQUEST["offset"] : 0;
+	
+	$page->smarty->assign('pagertotalitems',sizeof($ret));
+	$page->smarty->assign('pageroffset',$offset);
+	$page->smarty->assign('pageritemsperpage',ITEMS_PER_PAGE);
+	$page->smarty->assign('pagerquerybase', WWW_TOP."/filelist/".$_GET["id"]."/&amp;offset=");
+	$page->smarty->assign('pagerquerysuffix', "#results");
+	
+	$pager = $page->smarty->fetch($page->getCommonTemplate("pager.tpl"));
+	$page->smarty->assign('pager', $pager);
+
 	$page->smarty->assign('rel', $rel);
-	$page->smarty->assign('files', $ret);
+	$page->smarty->assign('files', array_slice($ret, $offset, $offset+50));
 
 	$page->title = "File List";
 	$page->meta_title = "View Nzb file list";
