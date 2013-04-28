70af6d38d7933a4ab6e3fdb6a5297326fda8a113
jonnyboy <jonnyboy@jbmatrix.net>
2013-04-28 17:38:29 -0400

    added folnername to average

--- misc/testing/nzb-import-bulk.php
+++ misc/testing/nzb-import-bulk.php
@@ -244,7 +244,7 @@ else
 							echo $n."Imported #".$nzbCount." nzb's in ".relativeTime($time);
 							$seconds = TIME() - $time;
 							$nzbsperhour = number_format(round($nzbCount / $seconds * 3600),0);
-							echo $n."\033[38;5;".$color_blacklist."mAveraging ".$nzbsperhour." imports per hour\033[0m".$n;
+							echo $n."\033[38;5;".$color_blacklist."mAveraging ".$nzbsperhour." imports per hour from ".$path."\033[0m".$n;
 						}
 					}
 					else 
