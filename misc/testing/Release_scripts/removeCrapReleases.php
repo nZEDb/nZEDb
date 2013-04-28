8c91d51b3633d9d72c6a9c12eaab753fe3d9631b
kevin <kevinlekiller@gmail.com>
2013-04-28 14:10:15 -0400

    Update.

--- misc/testing/Release_scripts/removeCrapReleases.php
+++ misc/testing/Release_scripts/removeCrapReleases.php
@@ -11,13 +11,13 @@ require_once(FS_ROOT."/../../../www/lib/releases.php");
 
 if (isset($argv[1]) && $argv[1] == true)
 {
-	function deleteReleases($sql)
+	function deleteReleases($sql, $type)
 	{
 		$releases = new Releases();
 		$delcount = 0;
 		foreach ($sql as $rel)
 		{
-			echo "Deleting: ".$rel['searchname']."\n";
+			echo "Deleting, ".$type.": ".$rel['searchname']."\n";
 			$releases->delete($rel['ID']);
 			$delcount++;
 		}
@@ -27,27 +27,40 @@ if (isset($argv[1]) && $argv[1] == true)
 	// 15 or more letters or numbers, nothing else.
 	function deleteGibberish()
 	{
+		$type = "Gibberish";
 		$db = new Db;
 		$sql = $db->query("select ID, searchname from releases where searchname REGEXP '^[a-zA-Z0-9]{15,}$'");
-		$delcount = deleteReleases($sql);
+		$delcount = deleteReleases($sql, $type);
 		return $delcount;
 	}
 	
 	// 25 or more letters/numbers, probably hashed.
 	function deleteHashed()
 	{
+		$type = "Hashed";
 		$db = new Db;
 		$sql = $db->query("select ID, searchname from releases where searchname REGEXP '[a-zA-Z0-9]{25,}'");
-		$delcount = deleteReleases($sql);
+		$delcount = deleteReleases($sql, $type);
 		return $delcount;
 	}
 	
 	// 5 or less letters/numbers.
 	function deleteShort()
 	{
+		$type = "Short";
 		$db = new Db;
 		$sql = $db->query("select ID, searchname from releases where searchname REGEXP '^[a-zA-Z0-9]{0,5}$'");
-		$delcount = deleteReleases($sql);
+		$delcount = deleteReleases($sql, $type);
+		return $delcount;
+	}
+	
+	// Anything smaller than 30MB with an exe not in other misc or pc apps.
+	function deleteExe()
+	{
+		$type = "EXE";
+		$db = new Db;
+		$sql = $db->query('select r.ID, r.searchname from releases r left join releasefiles rf on rf.releaseID = r.ID where rf.name like "%.exe%" and r.size < 30000000 and r.categoryID not in (4010, 4020, 4030, 4040, 4050, 4060, 4070, 7010)');
+		$delcount = deleteReleases($sql, $type);
 		return $delcount;
 	}
 
@@ -56,8 +69,9 @@ if (isset($argv[1]) && $argv[1] == true)
 	$gibberishDeleted = deleteGibberish();
 	$hashedDeleted = deleteHashed();
 	$shortDeleted = deleteShort();
+	$exeDeleted = deleteExe();
 
-	$totalDeleted = $totalDeleted+$gibberishDeleted+$hashedDeleted+$shortDeleted;
+	$totalDeleted = $totalDeleted+$gibberishDeleted+$hashedDeleted+$shortDeleted+$exeDeleted;
 
 	if ($totalDeleted > 0)
 	{
@@ -67,7 +81,9 @@ if (isset($argv[1]) && $argv[1] == true)
 		if($hashedDeleted > 0)
 			echo "Hashed       : ".$hashedDeleted."\n";
 		if($shortDeleted > 0)
-			echo "Short        : ".$shortDeleted."\n";
+			echo "Short        : ".$shortDeleted."\n\n";
+		if($exeDeleted > 0)
+			echo "<30MB with .exe not in apps or misc: ".$exeDeleted."\n";
 	}
 	else
 		exit("Nothing was found to delete.\n");
