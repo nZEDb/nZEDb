1d51d7b70462cd58f44f49e0a251952183a228d6
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-07 13:45:01 -0400

    few bugfixes in tmux scripts

--- misc/update_scripts/nix_scripts/tmux/start.php
+++ misc/update_scripts/nix_scripts/tmux/start.php
@@ -55,32 +55,32 @@ function start_apps()
 	$mytop = $tmux->get()->MYTOP;
 	$console_bash = $tmux->get()->CONSOLE;
 
-	if (( $htop == "TRUE" ) && (!command_exist($htop)))
+	if (( $htop == "TRUE" ) && (command_exist("htop")))
 	{
 		shell_exec("tmux new-window -n htop 'printf \"\033]2;htop\033\" && htop'");
 	}
-	
-	if (( $nmon == "TRUE" ) && (!command_exist($nmon)))
+
+	if (( $nmon == "TRUE" ) && (command_exist("nmon")))
 	{
 		shell_exec("tmux new-window -n nmon 'printf \"\033]2;nmon\033\" && nmon -t'");
 	}
 
-	if (( $vnstat == "TRUE" ) && (!command_exist($vnstat)))
+	if (( $vnstat == "TRUE" ) && (command_exist("vnstat")))
 	{
 		shell_exec("tmux new-window -n vnstat 'printf \"\033]2;vnstat\033\" && watch -n10 \"vnstat ${vnstat_args}\"'");
 	}
 
-	if (( $tcptrack == "TRUE" ) && (!command_exist($tcptrack)))
+	if (( $tcptrack == "TRUE" ) && (command_exist("tcptrack")))
 	{
 		shell_exec("tmux new-window -n tcptrack 'printf \"\033]2;tcptrack\033\" && tcptrack ${tcptrack_args}'");
 	}
 
-	if (( $bwmng == "TRUE" ) && (!command_exist($bwmng)))
+	if (( $bwmng == "TRUE" ) && (command_exist("bwm-ng")))
 	{
 		shell_exec("tmux new-window -n bwm-ng 'printf \"\033]2;bwm-ng\033\" && bwm-ng'");
 	}
 
-	if (( $mytop == "TRUE" ) && (!command_exist($mytop)))	
+	if (( $mytop == "TRUE" ) && (command_exist("mytop")))
 	{
 		shell_exec("tmux new-window -n mytop 'printf \"\033]2;mytop\033\" && mytop -u'");
 	}
@@ -90,6 +90,30 @@ function start_apps()
 		shell_exec("tmux new-window -n bash 'printf \"\033]2;Bash\033\" && bash -i'");
 	}
 }
+
+function window_utilities()
+{
+        shell_exec("tmux new-window -n utils 'printf \"\033]2;fixReleaseNames\033\"'");
+        shell_exec("tmux splitw -v -p 50 'printf \"\033]2;postprocessing_additional\033\"'");
+        shell_exec("tmux splitw -h -p 50 'printf \"\033]2;updateTVandTheaters\033\"'");
+        shell_exec("tmux selectp -t 0 && tmux splitw -h -p 50 'printf \"\033]2;removeCrapReleases\033\"'");
+}
+
+function window_post()
+{
+        shell_exec("tmux new-window -n post 'printf \"\033]2;postprocessing_nfos\033\"'");
+        shell_exec("tmux splitw -v -p 50 'printf \"\033]2;postprocessing_movies_tv\033\"'");
+        shell_exec("tmux splitw -h -p 50 'printf \"\033]2;postprocessing_books_games\033\"'");
+        shell_exec("tmux selectp -t 0 && tmux splitw -h -p 50 'printf \"\033]2;postproccessing_music_anidb\033\"'");
+}
+
+
+function attach($DIR, $session)
+{
+        shell_exec("tmux respawnp -t $session:0.0 'php $DIR/misc/update_scripts/nix_scripts/tmux/monitor.php'");
+        shell_exec("tmux select-window -t$session:0 && tmux attach-session -d -t$session");
+}
+
 //create tmux
 if ( $seq == "TRUE" )
 {
@@ -97,15 +121,10 @@ if ( $seq == "TRUE" )
 	shell_exec("tmux selectp -t 0 && tmux splitw -h -p 67 'printf \"\033]2;update_releases\033\"'");
 	shell_exec("tmux selectp -t 0 && tmux splitw -v -p 50 'printf \"\033]2;nzb-import-bulk\033\"'");
 
-	shell_exec("tmux new-window -n post 'printf \"\033]2;fixReleaseNames\033\"'");
-	shell_exec("tmux splitw -v -p 50 'printf \"\033]2;postprocessing\033\"'");
-	shell_exec("tmux splitw -h -p 50 'printf \"\033]2;updateTVandTheaters\033\"'");
-	shell_exec("tmux selectp -t 0 && tmux splitw -h -p 50 'printf \"\033]2;removeCrapReleases\033\"'");
-
+	window_utilities();
+	//window_post();
 	start_apps();
-	
-	shell_exec("tmux respawnp -t $session:0.0 'php $DIR/misc/update_scripts/nix_scripts/tmux/monitor.php'");
-	shell_exec("tmux select-window -t$session:0 && tmux attach-session -d -t$session");
+	attach($DIR, $session);
 }
 else
 {
@@ -115,14 +134,9 @@ else
 	shell_exec("tmux selectp -t 2 && tmux splitw -v -p 67 'printf \"\033]2;backfill\033\"'");
 	shell_exec("tmux splitw -v -p 50 'printf \"\033]2;update_releases\033\"'");
 
-	shell_exec("tmux new-window -n post 'printf \"\033]2;fixReleaseNames\033\"'");
-	shell_exec("tmux splitw -v -p 50 'printf \"\033]2;postprocessing\033\"'");
-	shell_exec("tmux splitw -h -p 50 'printf \"\033]2;updateTVandTheaters\033\"'");
-	shell_exec("tmux selectp -t 0 && tmux splitw -h -p 50 'printf \"\033]2;removeCrapReleases\033\"'");
-
+        window_utilities();
+        //window_post();
 	start_apps();
-
-	shell_exec("tmux respawnp -t $session:0.0 'php $DIR/misc/update_scripts/nix_scripts/tmux/monitor.php'");
-	shell_exec("tmux select-window -t$session:0 && tmux attach-session -d -t$session");
+	attach($DIR, $session);
 }
 ?>
