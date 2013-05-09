68f1ebfd3d5adc2d1f35f65ca09a0921291e8fdb
kevin <kevinlekiller@gmail.com>
2013-05-09 16:21:40 -0400

    Not an insert, a patch.

--- db/patches/5_09_2013_3_site.sql
+++ db/patches/5_09_2013_3_site.sql
@@ -1,3 +1,3 @@
-INSERT IGNORE INTO `site` (`setting`, `value`) VALUES ('maxpartsprocessed','3');
+UPDATE `site` set `value` = '3' where `setting` = 'maxpartsprocessed';
 
 UPDATE `site` set `value` = '31' where `setting` = 'sqlpatch';
