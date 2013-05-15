7e9019e44cc82e56e0c793de4f599d8656a0d84c
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-15 11:32:14 -0400

    added script to finish the conversion from nn+

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."/lib/postprocess.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/tmux.php");
 
-$version="0.1r1507";
+$version="0.1r1555";
 
 $db = new DB();
 $DIR = WWW_DIR."/..";
