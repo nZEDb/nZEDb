0b07f5e14bcdff1f7f11eeac30e5b119acd6dd11
kevinlekiller <kevinlekiller@gmail.com>
2013-05-16 16:00:55 -0400

    Update.

--- www/lib/releases.php
+++ www/lib/releases.php
@@ -1920,7 +1920,7 @@ class Releases
 						$db->queryDirect(sprintf("UPDATE collections set dateadded = now() where ID = %s", $collectionID));
 					}
 					//Update the parts/binaries with the new info.
-					$binaryID = $db->queryDirect(sprintf("UPDATE binaries SET collectionID = %d where ID = %d", $collectionID, $row["bname"]));
+					$db->queryDirect(sprintf("UPDATE binaries SET collectionID = %d where ID = %d", $collectionID, $row["bname"]));
 					$db->queryDirect(sprintf("UPDATE parts SET binaryID = %d where binaryID = %d", $row["bID"], $row["bID"]));
 				}
 				//Remove the old collections.
