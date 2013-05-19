2f295129d5452ef0a0747313ce1367349d4197eb
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-19 12:59:07 -0400

    updated patch in start.php

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."/lib/postprocess.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/tmux.php");
 
-$version="0.1r1694";
+$version="0.1r1695";
 
 $db = new DB();
 $DIR = WWW_DIR."/..";
