49b5e1a0e2995512494bade4c402559845afa4c9
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-19 16:38:24 -0400

    fixed reset_postrpocessing.php not truncating tables when using all true

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."/lib/postprocess.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/tmux.php");
 
-$version="0.1r1695";
+$version="0.1r1700";
 
 $db = new DB();
 $DIR = WWW_DIR."/..";
