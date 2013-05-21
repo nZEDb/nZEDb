46d15d7683d39dd5b3876b8c927ac511a4e70627
kevin <kevinlekiller@gmail.com>
2013-05-20 20:15:15 -0400

    Hide them if they are 0.

--- www/lib/postprocess.php
+++ www/lib/postprocess.php
@@ -199,7 +199,7 @@ class PostProcess
 				where nzbstatus = 1 and (r.passwordstatus between %d and -1)
 				AND (r.haspreview = -1 and c.disablepreview = 0) order by r.postdate desc limit %d,%d", $i, floor(($this->addqty) * ($threads * 1.5)), $this->addqty);
 				$result = $db->query($query);
-				if ($this->echooutput)
+				if ($this->echooutput && count($result) > 0)
 				{
 					echo "Passwordstatus = ".$i."\n";
 					echo "Available to process = ".count($result)."\n";
