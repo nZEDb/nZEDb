a2e4a39f0b441e42671fbab2ab0896006594b4ab
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-22 18:00:35 -0400

    cleaned up the table

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."/lib/postprocess.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/tmux.php");
 
-$version="0.1r1918";
+$version="0.1r1919";
 
 $db = new DB();
 $DIR = WWW_DIR."/..";
