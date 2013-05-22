5a033278fbf58227305cc0c2ac9ac61f8c154604
kevin <kevinlekiller@gmail.com>
2013-05-21 21:53:27 -0400

    Update.

--- www/lib/namecleaning.php
+++ www/lib/namecleaning.php
@@ -20,7 +20,7 @@
 			//Parts/files
 			$cleansubject = preg_replace('/(\(|\[|\s)\d{1,4}(\/|(\s|_)of(\s|_)|\-)\d{1,4}(\)|\]|\s|$|:)|\(\d{1,3}\|\d{1,3}\)|\-\d{1,3}\-\d{1,3}\.|\s\d{1,3}\sof\s\d{1,3}\.|\s\d{1,3}\/\d{1,3}|\d{1,3}of\d{1,3}\.|^\d{1,3}\/\d{1,3}\s|\d{1,3} - of \d{1,3}|( - \d\d -| \(\d\d\) -| - \d\d\.|\d{4} \d\d -) .+\.mp3/i', ' ', $subject);
 			//Anything between the quotes. Too much variance within the quotes, so remove it completely.
-			$cleansubject = preg_replace('/\".+\"/i', '', $cleansubject);
+			$cleansubject = preg_replace('/\".+\"/i', ' ', $cleansubject);
 			//File extensions - If it was not quotes.
 			$cleansubject = preg_replace('/ [a-z0-9]+\.jpg|((\d{1,3})?\.part(\d{1,5})?|\d{1,5}|sample)?\.(7z|\d{3}(?=(\s|"))|avi|epub|idx|iso|jpg|m4a|mds|mkv|mobi|mp4|nfo|nzb|pdf|rar|rev|rtf|r\d\d|sfv|srs|srr|sub|txt|vol.+(par2)|par(\s?2|")|zip|z{2,3})"?|(\s|(\d{2,3})?\-)\d{2,3}\.mp3|\d{2,3}\.pdf|\.part\d{1,4}\./i', ' ', $cleansubject);
 			//File Sizes - Non unique ones.
