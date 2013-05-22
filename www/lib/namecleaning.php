b6651ce4e047d53429a8eaee5cf39e2090230b5d
kevin <kevinlekiller@gmail.com>
2013-05-22 13:57:32 -0400

    Improve for crap like this  	- [01/10] - "Martins Bildersammlung" yEnc - [1/1] - "159c0550341842e6ab50f6d73b924898.avi" yEnc

--- www/lib/namecleaning.php
+++ www/lib/namecleaning.php
@@ -38,10 +38,10 @@
 			else if ($type !== "split" && (strlen($cleansubject) <= 6 || preg_match('/^[a-z0-9 \-\$]{1,8}$/i', $cleansubject)))
 			{
 				$one = $two = "";
-				if (preg_match('/.+"(.+?)\.[a-z0-9].+?"/i', $subject, $matches))
+				if (preg_match('/.+?"(.+?)".+?".+?".+/', $subject, $matches))
 					$one = $matches[1];
-				else if (preg_match('/^"(.+?)\.[a-z0-9].+?"/i', $subject, $matches))
-					$one = $matches[1];
+				else if (preg_match('/(^|.+)"(.+?)\.[a-z0-9].+?"/i', $subject, $matches))
+					$one = $matches[2];
 				if(preg_match('/s\d{1,3}[.-_ ]?(e|d)\d{1,3}|EP[\.\-_ ]?\d{1,3}[\.\-_ ]|(19|20)\d\d/i', $subject, $matches2))
 					$two = $matches2[0];
 				return $cleansubject.$one.$two;
