439491f4bb1e6ab08591923ecb9c6b9b4e811055
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-26 09:00:11 -0400

    changed optimize to a function, added rm -r tmpunrar/* to screen scripts

--- misc/update_scripts/config.php
+++ misc/update_scripts/config.php
@@ -1,5 +1,5 @@
 <?php
 
-require(dirname(__FILE__)."/../../www/config.php");
+require_once(dirname(__FILE__)."/../../www/config.php");
 
 ?>
--- misc/update_scripts/optimise_db.php
+++ misc/update_scripts/optimise_db.php
@@ -1,14 +1,18 @@
 <?php
 
-require(dirname(__FILE__)."/config.php");
+require_once(dirname(__FILE__)."/config.php");
 require_once(WWW_DIR."/lib/framework/db.php");
 
-$db = new DB;
-echo "Optimizing MYSQL tables, this can take a while...\n";
-$tablecnt = $db->optimise();
-if ($tablecnt > 0)
-	exit ("Optimized ".$tablecnt." MYSQL tables succesfuly.\n");
-else
-	exit ("No MYSQL tables to optimize.\n");
+function optimize()
+{
+	$db = new DB;
+	echo "Optimizing MYSQL tables, this can take a while...\n";
+	$tablecnt = $db->optimise();
+	if ($tablecnt > 0)
+		exit ("Optimized ".$tablecnt." MYSQL tables succesfuly.\n");
+	else
+		exit ("No MYSQL tables to optimize.\n");
+}
+optimize();
 
 ?>
