88cc99776dd66b482c78de53f84da73135fc791e
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-27 18:55:29 -0400

    run dos2unix on all php and sh files

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -6,7 +6,7 @@ require_once(WWW_DIR."/lib/framework/db.php");
 require_once(WWW_DIR."/lib/tmux.php");
 require_once(WWW_DIR."/lib/site.php");
 
-$version="0.1r2075";
+$version="0.1r2076";
 
 $db = new DB();
 $DIR = WWW_DIR."/..";
