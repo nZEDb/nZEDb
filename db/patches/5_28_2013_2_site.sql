aab3dc7d5bc19426be7fcc403810a75771baf5ed
Brian Fincher <brian@fincherhome.com>
2013-05-28 08:56:26 -0500

    rename patch

--- db/patches/5_28_2013_2_site.sql
+++ db/patches/5_28_2013_2_site.sql
@@ -1,4 +1,4 @@
 INSERT IGNORE INTO `site` (`setting`, `value`) VALUE ('partretentionhours', 72);
 
-UPDATE `site` set `value` = '60' where `setting` = 'sqlpatch';
+UPDATE `site` set `value` = '61' where `setting` = 'sqlpatch';
 
