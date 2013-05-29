cf9d88e527b68098acaf742ad7ea497bd33ef565
kevin <kevinlekiller@gmail.com>
2013-05-29 12:48:44 -0400

    fix patch.

--- db/patches/5_29_2013_3_predb.sql
+++ db/patches/5_29_2013_3_predb.sql
@@ -2,4 +2,4 @@ ALTER TABLE `predb` ADD `releaseID` INT NULL AFTER `md5`;
 
 CREATE INDEX ix_predb_releaseID ON predb(`releaseID`);
 
-UPDATE `site` set `value` = '66' where `setting` = 'sqlpatch';
+UPDATE `site` set `value` = '67' where `setting` = 'sqlpatch';
