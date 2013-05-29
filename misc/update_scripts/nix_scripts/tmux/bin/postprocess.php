6df7a92e6a3fd82a405e2ec4b2b430dceab69a2b
jonnyboy <jonnyboy@jbmatrix.net>
2013-05-29 15:59:29 -0400

    updated tmux/bin/postprocess.php

--- misc/update_scripts/nix_scripts/tmux/bin/postprocess.php
+++ misc/update_scripts/nix_scripts/tmux/bin/postprocess.php
@@ -1,12 +1,11 @@
 <?php
 
-require_once(dirname(__FILE__)."/../config.php");
+require_once(dirname(__FILE__)."/../../../config.php");
 require_once(WWW_DIR."lib/postprocess.php");
 
 if (isset($argv[1]) && is_numeric($argv[1]))
 {
 	$postprocess = new PostProcess(true);
-	$postprocess->processSRRDB();
 	$postprocess->processPredbMe();
 	$postprocess->processAdditional($argv[1]);
 	$postprocess->processNfos($argv[1]);
