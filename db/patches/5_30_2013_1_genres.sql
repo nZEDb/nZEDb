48c347b5b750f5bcc3e3554730f8ed144290d3f6
Brian Fincher <brian@fincherhome.com>
2013-05-30 16:19:08 -0500

    rename patch

--- db/patches/5_30_2013_1_genres.sql
+++ db/patches/5_30_2013_1_genres.sql
@@ -1,5 +1,5 @@
 ALTER TABLE  `genre` ADD  `disablepreview` tinyint(1) NOT NULL default '0';
 
-UPDATE `site` set `value` = '61' where `setting` = 'sqlpatch';
+UPDATE `site` set `value` = '70' where `setting` = 'sqlpatch';
 
 
