6a1e606bfdb23b4018e0178f18a19b4c7ecdb32f
kevin <kevinlekiller@gmail.com>
2013-05-29 21:16:55 -0400

    Rechange.

--- misc/testing/DB_scripts/backfill_predb.php
+++ misc/testing/DB_scripts/backfill_predb.php
@@ -7,7 +7,7 @@ require_once(WWW_DIR."/lib/framework/db.php");
  */
 
 if (!isset($argv[1]) && !is_numeric($argv[1]))
-	exit("This script inserts pre info into the preDB mysql table from a dump made 5/29/2013.\nSupply an argument ex:(php backfill_predb.php 3), 3 will backfill 3000, you can backfill up to 1.419 million (1419 as an argument).\nIf you have already ran this script in the past, your status is saved, so you can go further.\n\nMake sure there are no data####.zip or data####.txt in the www folder before starting.\n");
+	exit("This script inserts pre info into the preDB mysql table from a dump made 5/29/2013.\nSupply an argument ex:(php backfill_predb.php 3), 3 will backfill 30000, you can backfill up to 1.42 million (142 as an argument).\nIf you have already ran this script in the past, your status is saved, so you can go further.\n\nMake sure there are no data###.zip or data###.txt in the www folder before starting.\n");
 
 $db = new DB;
 $predbv = $db->queryOneRow("SELECT value as v from site where setting = 'predbversion'");
@@ -15,13 +15,13 @@ if ($argv[1] < $predbv["v"])
 	exit("You have already reached file ".$predbv["v"]." please select a higher file.\n");
 else if ($argv[1] >= $predbv["v"])
 	$filenums = $argv[1];
-else if ($argv[1] == 0 || $argv[1] > 1419)
-	exit("Wrong argument. It must be a number between 1 and 1419.\n");
+else if ($argv[1] == 0 || $argv[1] > 142)
+	exit("Wrong argument. It must be a number between 1 and 142.\n");
 
 $done = 0;
 foreach (range($predbv["v"], $filenums) as $filenumber)
 {
-	$filenump = str_pad($filenumber, 4, '0', STR_PAD_LEFT);
+	$filenump = str_pad($filenumber, 3, '0', STR_PAD_LEFT);
 	$zipfile = WWW_DIR."data".$filenump.".gz";
 	if (!file_exists($zipfile))
 	{	$ziphandle = fopen("https://github.com/nZEDb/pre-info/raw/master/data".$filenump.".gz", 'r');
