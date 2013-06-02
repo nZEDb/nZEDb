f391f83b846e9fd116907abac3b44ca8019ebc3f
Brian Fincher <brian@fincherhome.com>
2013-06-02 13:12:17 -0500

    when considering min size for category, use parent min size if child is set to zero

--- www/lib/releases.php
+++ www/lib/releases.php
@@ -1582,8 +1582,10 @@ class Releases
 		echo $n."\033[1;33mStage 4.5 -> Delete releases smaller/larger than minimum size/file count from group/site setting.\033[0m".$n;
 		$stage4dot5 = TIME();
 
-		if ($resrel = $db->query("SELECT r.ID, r.guid from releases r left outer join category cat on cat.ID = r.categoryID where r.size < cat.minsize"))
-		{
+		$catresrel = $db->query("select c.ID as ID, CASE WHEN c.minsize = 0 THEN cp.minsize ELSE c.minsize END as minsize from category c left outer join category cp on cp.ID = c.parentID where c.parentID is not null");
+
+		foreach ($catresrel as $catrowrel) {
+			$resrel = $db->query("SELECT r.ID, r.guid from releases r where r.categoryID = $catrowrel['ID'] AND r.size < $catrowrel['minsize']");
 			foreach ($resrel as $rowrel)
 			{
 				$this->fastDelete($rowrel['ID'], $rowrel['guid'], $this->site);
