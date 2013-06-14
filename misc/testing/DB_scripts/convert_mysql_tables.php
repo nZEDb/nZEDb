22caee098e20ccf2b01136798f9aed5cf1ffcf76
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-14 04:42:51 -0400

    added convert to tokudb

--- misc/testing/DB_scripts/convert_mysql_tables.php
+++ misc/testing/DB_scripts/convert_mysql_tables.php
@@ -43,12 +43,22 @@ else if (isset($argv[1]) && $argv[1] == "cinnodb")
 			$db->query($sql);
 		}
 }
+else if (isset($argv[1]) && $argv[1] == "tokudb")
+{
+    $tables = $db->query($sql);
+    foreach($tables as $row)
+        {
+        $tbl = $row['Tables_in_'.DB_NAME];
+        printf("Converting $tbl\n");
+            $sql = "ALTER TABLE $tbl ENGINE=TokuDB";
+            $db->query($sql);
+        }
+}
 else
 {
 	exit("\nERROR: Wrong argument.\n\n"
 		."php convert_mysql_tables.php myisam	...: Converts all the tables to Myisam Dynamic.\n"
 		."php convert_mysql_tables.php dinnodb	...: Converts all the tables to InnoDB Dynamic.\n"
-		."php convert_mysql_tables.php cinnodb	...: Converts all the tables to InnoDB Compressed.\n\n");
+		."php convert_mysql_tables.php cinnodb	...: Converts all the tables to InnoDB Compressed.\n"
+		."php convert_mysql_tables.php tokudb  ...: Converts all the tables to Tokutek DB.\n\n");
 }
-
-?>
