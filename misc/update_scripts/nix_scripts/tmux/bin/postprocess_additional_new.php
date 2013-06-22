85aa2d2fbc6afa8cd6bbc9e0c1853925ce2a0df6
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-22 17:31:21 -0400

    move remove stale folders to python script

--- misc/update_scripts/nix_scripts/tmux/bin/postprocess_additional_new.php
+++ misc/update_scripts/nix_scripts/tmux/bin/postprocess_additional_new.php
@@ -1,16 +1,6 @@
 <?php
 require_once(dirname(__FILE__)."/../../../config.php");
 require_once(WWW_DIR."lib/postprocess.php");
-require_once(WWW_DIR."lib/site.php");
-
-//remove folders from tmpunrar
-$site = new Sites();
-$tmpunrar = $site->get()->tmpunrarpath;
-if ((count(glob("$tmpunrar/*",GLOB_ONLYDIR))) > 0)
-{
-	echo "Removing dead folders from ".$tmpunrar."\n";
-	exec("rm -r ".$tmpunrar."/*");
-}
 
 if (isset($argv[1]))
 {
