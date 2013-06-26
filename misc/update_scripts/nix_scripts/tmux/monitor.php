1a6436bc91fde6c8d62a8476cb6b6320c519a723
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-26 02:05:41 -0400

    added alternate nntp for postprocessing

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2505";
+$version="0.1r2513";
 
 $db = new DB();
 $DIR = MISC_DIR;
