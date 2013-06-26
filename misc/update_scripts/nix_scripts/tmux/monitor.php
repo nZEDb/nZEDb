f8ddc069e8a669882aeb1c533227163c8536ac7f
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-26 02:13:40 -0400

    another oops

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2514";
+$version="0.1r2515";
 
 $db = new DB();
 $DIR = MISC_DIR;
