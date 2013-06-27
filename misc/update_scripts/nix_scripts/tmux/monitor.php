04c94f14a3d33cdec28cc561ea66e4c53cb4ee21
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-27 13:12:23 -0400

    forgot Changelog

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2524";
+$version="0.1r2525";
 
 $db = new DB();
 $DIR = MISC_DIR;
