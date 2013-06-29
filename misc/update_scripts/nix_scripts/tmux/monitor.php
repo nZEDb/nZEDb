10d166cc23396d95bfa948641224e921f20d156c
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-29 07:35:21 -0400

    merging master to dev

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2548";
+$version="0.1r2565";
 
 $db = new DB();
 $DIR = MISC_DIR;
