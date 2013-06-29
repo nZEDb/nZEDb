002d68ea6b298d29bf7b50afaed0590e13b544ac
jonnyboy <jonnyboy@jbmatrix.net>
2013-06-29 18:47:27 -0400

    added safety argv

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2589";
+$version="0.1r2590";
 
 $db = new DB();
 $DIR = MISC_DIR;
