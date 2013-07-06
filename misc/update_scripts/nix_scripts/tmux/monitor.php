3f590e6fc2e51b96d7945b5209335adf67f33a73
jonnyboy <jonnyboy@jbmatrix.net>
2013-07-05 22:49:45 -0400

    typo

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2694";
+$version="0.1r2695";
 
 $db = new DB();
 $DIR = MISC_DIR;
