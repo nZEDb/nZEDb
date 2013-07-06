0be04d1f71ed55451191a90bcc28d6a0a2708e8b
jonnyboy <jonnyboy@jbmatrix.net>
2013-07-05 22:43:27 -0400

    fixed bug in postprocess

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2693";
+$version="0.1r2694";
 
 $db = new DB();
 $DIR = MISC_DIR;
