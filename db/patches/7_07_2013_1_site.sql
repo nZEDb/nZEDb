7d0f9bc4eb82e5a5d9496ff6de2777bbcdbee698
jonnyboy <jonnyboy@jbmatrix.net>
2013-07-07 02:38:01 -0400

    wrong order

--- db/patches/7_07_2013_1_site.sql
+++ db/patches/7_07_2013_1_site.sql
@@ -1,4 +1,4 @@
 ALTER TABLE `releases` ADD `relstatus` TINYINT(4) NOT NULL DEFAULT 0;
 
-UPDATE `site` set `value` = '93' where `setting` = 'sqlpatch';
+UPDATE `site` set `value` = '92' where `setting` = 'sqlpatch';
 
