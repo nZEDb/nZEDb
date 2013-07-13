11d51b7aeccd03e99167541d78695b7dac68a55a
Junkman690 <junkman690@hotmail.com>
2013-07-13 15:54:53 +1000

    Rss-info
    need to change info once feeds are fully operational

---
+++ www/pages/rss-info.php
@@ -0,0 +1,11 @@
+<?php
+
+$page->title = "RSS Info";
+$page->meta_title = "RSS Help Topics";
+$page->meta_keywords = "view,nzb,api,details,help,json,rss,atom";
+$page->meta_description = "View description of the site Nzb RSS.";
+
+$page->content = $page->smarty->fetch('rssdesc.tpl');
+$page->render();
+
+?>
