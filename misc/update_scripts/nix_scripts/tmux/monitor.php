a9e1772a4749d040de0a70969df3fd7b4a4101c5
jonnyboy <jonnyboy@jbmatrix.net>
2013-07-16 12:17:42 -0400

    added group check in binaries scan

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r2822";
+$version="0.1r2823";
 
 $db = new DB();
 $DIR = MISC_DIR;
