92272f4c9f83b8258d84bf7f13b8ab86160e04ef
kevinlekiller <kevinlekiller@gmail.com>
2013-08-01 15:13:05 -0400

    Update schema/patch/start.php

--- db/patches/7_31_2013_1_security.sql
+++ db/patches/7_31_2013_1_security.sql
@@ -1,10 +1,11 @@
-CREATE TABLE IF NOT EXISTS `logging` (
+DROP TABLE IF EXISTS `logging`;
+CREATE TABLE `logging` (
   `time` datetime DEFAULT NULL,
   `username` varchar(50) COLLATE utf8_unicode_ci DEFAULT NULL,
   `host` varchar(40) COLLATE utf8_unicode_ci DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
 
-INSERT IGNORE INTO `site` (setting, value, updatedate) values (loggingopt, 2, now());
-INSERT IGNORE INTO `site` (setting, value, updatedate) values (logfile, '/var/www/nZEDb/failed-login.log', now());
+INSERT IGNORE INTO `site` (`setting`, `value`) VALUES ('loggingopt', 2);
+INSERT IGNORE INTO `site` (`setting`, `value`) VALUES ('logfile', '/var/www/nZEDb/failed-login.log');
 
 UPDATE `site` set `value` = '100' where `setting` = 'sqlpatch';
