610583b4e201a22d7e51cabaee4140b45086a78d
kevinlekiller <kevinlekiller@gmail.com>
2013-08-13 14:41:19 -0400

    Fix by zoggy for python 2.6 errors in print.

--- misc/update_scripts/threaded_scripts/binaries_threaded.py
+++ misc/update_scripts/threaded_scripts/binaries_threaded.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import sys, os, time
 import threading
 try:
@@ -70,8 +71,7 @@ def main():
 	global time_of_last_run
 	time_of_last_run = time.time()
 
-	#Does not work in some python versions (len(datas))
-	#print("We will be using a max of %s threads, a queue of %s groups" % (run_threads[0], "{:,}".format(len(datas))))
+	print("We will be using a max of %s threads, a queue of %s groups" % (run_threads[0], "{:,}".format(len(datas))))
 	time.sleep(2)
 
 	def signal_handler(signal, frame):
--- misc/update_scripts/threaded_scripts/grabnzbs_threaded.py
+++ misc/update_scripts/threaded_scripts/grabnzbs_threaded.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from __future__ import print_function
 import sys, os, time
 import threading
 try:
@@ -75,8 +76,7 @@ def main():
 	global time_of_last_run
 	time_of_last_run = time.time()
 
-	#Does not work in some python versions (len(datas))
-	#print("We will be using a max of %s threads, a queue of %s nzbs" % (run_threads[0], "{:,}".format(len(datas))))
+	print("We will be using a max of %s threads, a queue of %s nzbs" % (run_threads[0], "{:,}".format(len(datas))))
 	time.sleep(2)
 
 	def signal_handler(signal, frame):
