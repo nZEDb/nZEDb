23bfe95fc47104c3646171c35566cbdbacaeae8b
jonnyboy <jonnyboy@jbmatrix.net>
2013-08-25 11:38:56 -0400

    changed remove creap releases password to all negative search items and not remove releases that are applications/misc

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3251";
+$version="0.1r3252";
 
 $db = new DB();
 $DIR = MISC_DIR;
