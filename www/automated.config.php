c8c0c75ec17c5dfd6153cb86f632c02bc89a7369
kevinlekiller <kevinlekiller@gmail.com>
2013-08-25 18:22:05 -0400

    Quick cleanup of files in www

--- www/automated.config.php
+++ www/automated.config.php
@@ -1,36 +1,23 @@
 <?php
-
 $www_top = str_replace("\\","/",dirname( $_SERVER['PHP_SELF'] ));
 if(strlen($www_top) == 1)
 	$www_top = "";
 
-//
-// used everywhere an href is output, includes the full path to the nZEDb install
-//
+// Used everywhere an href is output, includes the full path to the nZEDb install.
 define('WWW_TOP', $www_top);
 
-//
-// used to refer to the /www/lib class files
-//
+// Used to refer to the /www/lib class files.
 define('WWW_DIR', realpath(dirname(__FILE__)).'/');
 
-//
-// used to refer to the /misc class files
-//
+// Used to refer to the /misc class files.
 define('MISC_DIR', realpath(dirname(__FILE__)).'/../misc/');
 
-//
-// path to smarty files
-//
+// Path to smarty files.
 define('SMARTY_DIR', WWW_DIR.'lib/smarty/');
 
-//
-// path to themes directory
-//
+// Path to themes directory.
 define('THEMES_DIR', WWW_TOP.'themes');
 
-//
-// number of results per page
-//
+// Number of results per page.
 define("ITEMS_PER_PAGE", "50");
 define("ITEMS_PER_COVER_PAGE", "20");
