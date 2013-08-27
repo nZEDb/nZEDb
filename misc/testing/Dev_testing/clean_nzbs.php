23de9b2310480b99ffd27c9e73a0ea98b064aa28
jonnyboy <jonnyboy@jbmatrix.net>
2013-08-27 15:56:25 -0400

    fixed clean_nzbs.php to remove nzbs and releases

--- misc/testing/Dev_testing/clean_nzbs.php
+++ misc/testing/Dev_testing/clean_nzbs.php
@@ -3,52 +3,60 @@ define('FS_ROOT', realpath(dirname(__FILE__)));
 require_once(FS_ROOT."/../../../www/config.php");
 require_once(WWW_DIR."lib/site.php");
 require_once(WWW_DIR."lib/nzb.php");
+require_once(WWW_DIR."lib/releases.php");
+require_once(WWW_DIR."lib/consoletools.php");
 
 $s = new Sites();
 $site = $s->get();
 $db = new DB();
 $nzb = new NZB(true);
+$releases = new Releases();
+$consoletools = new ConsoleTools();
 
 //
-//  This script removes all nzbs not found in the db.
+//  This script removes all nzbs not found in the db and all releases with no nzb
 //
 
 if (isset($argv[1]) && $argv[1] === "true")
 {
+	$checked = 0;
+	$timestart = TIME();
+	echo "Verifying each nzb is present in the db, this could take a long time.\n";
 	$dirItr    = new RecursiveDirectoryIterator($site->nzbpath);
-	//$filterItr = new MyRecursiveFilterIterator($dirItr);
 	$itr       = new RecursiveIteratorIterator($dirItr, RecursiveIteratorIterator::LEAVES_ONLY);
-	foreach ($itr as $filePath) {
+	foreach ($itr as $filePath)
+	{
 		if (is_file($filePath))
 		{
 			$file = stristr($filePath->getFilename(), '.nzb.gz', true);
-
-			$res = $db->query(sprintf("SELECT * FROM `releases` where guid = %s", $db->escapeString($file)));
-
+			$res = $db->query(sprintf("SELECT id, guid FROM `releases` where guid = %s", $db->escapeString($file)));
 			if ($res === false)
 			{
-				echo "$filePath\n";
-
-				echo $filePath->getFilename() . PHP_EOL;
+				$releases->fastDelete($res['id'], $res['guid'], $site);
+				echo "Deleted NZB: ".$filePath."\n";
 			}
+			$time = $consoletools->convertTime(TIME() - $timestart);
+			$consoletools->overWrite("\nChecking NZBs: ".$checked++." exists in db,  Running time: ".$time);
 		}
 	}
-
-
-	$res = $db->queryDirect('SELECT ID, guid FROM `releases`');
+	$checked = 0;
+	$timestart = TIME();
+	echo "\nVerifying each release has an nzb, this could take a long time.\n";
+	$res = $db->queryDirect("SELECT id, guid FROM releases");
 	while ($row =  $db->fetchAssoc($res))
 	{
 		$nzbpath = $nzb->getNZBPath($row["guid"], $site->nzbpath, false, $site->nzbsplitlevel);
 		if (!file_exists($nzbpath))
 		{
-			echo "deleting ".$row['guid']."\n";
-			$db->query(sprintf("DELETE FROM `releases` WHERE `releases`.`ID` = %s", $row['ID']));
+			$releases->fastDelete($row['id'], $row['guid'], $site);
+			echo "Deleted ReleaseID ".$row['id']."\n";
 		}
-
+		$time = $consoletools->convertTime(TIME() - $timestart);
+		$consoletools->overWrite("Checking Releases: ".$checked++." have an nzb, Running time: ".$time);
 	}
 }
 else
 {
-	exit("This script removes all nzbs not found in the db.\nIf you are sure you want to run it, type php clean_nzbs.php true\n");
+	exit("This script removes all nzbs not found in the db and all releases with no nzb.\nIf you are sure you want to run it, type php clean_nzbs.php true\n");
 }
 ?>
