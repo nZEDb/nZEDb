9b1ba052c5aa4168e10404de36d1640c6a20e210
jonnyboy <jonnyboy@jbmatrix.net>
2013-09-02 11:45:48 -0400

    updated to get category, same method as fixReleaseNames, except for all categories, not just 7010

--- misc/update_scripts/decrypt_hashes.php
+++ misc/update_scripts/decrypt_hashes.php
@@ -9,6 +9,7 @@ require_once(WWW_DIR."lib/namecleaning.php");
 if (!isset($argv[1]))
 	exit ("Type php decrypt_hashes.php true to start.\n");
 
+echo "Matching predb MD5 to md5(releasename)\n";
 preName();
 
 function preName()
@@ -17,15 +18,11 @@ function preName()
 
 	if ($db->dbSystem() == "mysql")
 	{
-		$run = $db->prepare("UPDATE releases SET dehashstatus = -1 WHERE dehashstatus = 0 AND name REGEXP '[a-fA-F0-9]{32}'");
-		$run->execute();
-		$res = $db->query("SELECT id, name FROM releases WHERE dehashstatus BETWEEN -6 AND -1 AND name REGEXP '[a-fA-F0-9]{32}'");
+		$res = $db->query("SELECT id, name, searchname, groupid, categoryid FROM releases WHERE dehashstatus BETWEEN -6 AND 0 AND name REGEXP '[a-fA-F0-9]{32}'");
 	}
 	else if ($db->dbSystem() == "pgsql")
 	{
-		$run = $db->prepare("UPDATE releases SET dehashstatus = -1 WHERE dehashstatus = 0 AND regexp_matches(name, '[a-fA-F0-9]{32}')");
-		$run->execute();
-		$res = $db->query("SELECT id, name FROM releases WHERE dehashstatus BETWEEN -6 AND -1 AND regexp_matches(name, '[a-fA-F0-9]{32}')");
+		$res = $db->query("SELECT id, name, searchname, groupid, categoryid FROM releases WHERE dehashstatus BETWEEN -6 AND 0 AND regexp_matches(name, '[a-fA-F0-9]{32}')");
 	}
 
 	$counter = 0;
@@ -34,8 +31,10 @@ function preName()
 	if($total > 0)
 	{
 		$consoletools = new ConsoleTools();
+		$category = new Category();
 		$reset = 0;
 		$loops = 1;
+		$n = "\n";
 		foreach ($res as $row)
 		{
 			$success = false;
@@ -43,10 +42,25 @@ function preName()
 			{
 				if($res1 = $db->queryOneRow(sprintf("SELECT title FROM predb WHERE md5 = %s", $db->escapeString($match[1]))))
 				{
-					$result = $db->prepare(sprintf("UPDATE releases SET dehashstatus = 1, relnamestatus = 5, searchname = %s WHERE id = %d", $db->escapeString($res1['title']), $row['id']));
+					var_dump($resl);
+					$determinedcat = $category->determineCategory($res1['title'], $row["groupid"]);
+					$result = $db->prepare(sprintf("UPDATE releases SET dehashstatus = 1, relnamestatus = 5, searchname = %s, categoryid = %d WHERE id = %d", $db->escapeString($res1['title']), $determinedcat, $row['id']));
 					$result->execute();
 					if ($result->rowCount() > 0)
 					{
+						$groups = new Groups();
+						$groupname = $groups->getByNameByID($row["groupid"]);
+						$oldcatname = $category->getNameByID($row["categoryid"]);
+						$newcatname = $category->getNameByID($determinedcat);
+
+						echo	$n."New name:  ".$res1['title'].$n.
+							"Old name:  ".$row["searchname"].$n.
+							"New cat:   ".$newcatname.$n.
+							"Old cat:   ".$oldcatname.$n.
+							"Group:     ".$groupname.$n.
+							"ReleaseID: ". $row["id"].$n;
+
+
 						$show = $res1['title'];
 						$success = true;
 						$counter++;
@@ -58,7 +72,6 @@ function preName()
 				$fail = $db->prepare(sprintf("UPDATE releases SET dehashstatus = dehashstatus - 1 WHERE id = %d", $row['id']));
 				$fail->execute();
 			}
-			$consoletools->overWrite("Renaming hashed releases: ".$consoletools->percentString($loops++,$total)."=>".$show);
 		}
 	}
 	if ($total > 0)
