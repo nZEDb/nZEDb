768cd97eda78f3ac3aef2ff0d18df6e8c3507576
jonnyboy <jonnyboy@jbmatrix.net>
2013-09-14 11:32:09 -0400

    chenged patches to 10 instead of default 1000

--- db/pgsql_patches/09_14_2013_1_site.sql
+++ db/pgsql_patches/09_14_2013_1_site.sql
@@ -1,4 +1,4 @@
-INSERT IGNORE INTO site (setting, value) VALUE ('fixnamesperrun', '1000');
+INSERT IGNORE INTO site (setting, value) VALUE ('fixnamesperrun', '10');
 
 UPDATE site set value = '123' where setting = 'sqlpatch';
 
