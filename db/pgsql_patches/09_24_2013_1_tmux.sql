c0d6b0fa1894be7cda3f6f4ceed67bfceae991cf
jonnyboy <jonnyboy@jbmatrix.net>
2013-09-25 11:30:09 -0400

    added table for country codes, tvrage does not always return an abbreviation added yesterdays patch to schema, corrected patch for pgsql

--- db/pgsql_patches/09_24_2013_1_tmux.sql
+++ db/pgsql_patches/09_24_2013_1_tmux.sql
@@ -1,16 +1,24 @@
 INSERT IGNORE INTO tmux (setting, value) VALUE ('SHOWQUERY', 'FALSE');
 
-DROP TABLE IF EXISTS allgroups;
-CREATE TABLE allgroups (
-  id INT(11) NOT NULL AUTO_INCREMENT,
-  name VARCHAR(255) NOT NULL DEFAULT "",
-  first_record BIGINT UNSIGNED NOT NULL DEFAULT "0",
-  last_record BIGINT UNSIGNED NOT NULL DEFAULT "0",
-  updated DATETIME DEFAULT NULL,
-  PRIMARY KEY  (id)
-) ENGINE=MYISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci AUTO_INCREMENT=1 ;
+DROP SEQUENCE IF EXISTS "allgroups_id_seq" CASCADE;
+CREATE SEQUENCE "allgroups_id_seq" INCREMENT BY 1
+                                  NO MAXVALUE NO MINVALUE CACHE 1;
+SELECT pg_catalog.setval('allgroups_id_seq', 1, true);
 
+-- Table: allgroups
+DROP TABLE IF EXISTS "allgroups" CASCADE;
+CREATE TABLE "allgroups" (
+  "id" int(11) NOT NULL AUTO_INCREMENT,
+  "name" varchar(255) NOT NULL DEFAULT "",
+  "first_record" bigint UNSIGNED NOT NULL DEFAULT "0",
+  "last_record" bigint UNSIGNED NOT NULL DEFAULT "0",
+  "updated" timestamp without time zone NOT NULL
+)
+WITHOUT OIDS;
+
+DROP INDEX IF EXISTS "ix_allgroups_id" CASCADE;
 CREATE INDEX ix_allgroups_id ON allgroups(id);
+DROP INDEX IF EXISTS "ix_allgroups_name" CASCADE;
 CREATE INDEX ix_allgroups_name ON allgroups(name);
 
 UPDATE site SET value = '126' WHERE setting = 'sqlpatch';
--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3699";
+$version="0.1r3700";
 
 $db = new DB();
 $DIR = MISC_DIR;
