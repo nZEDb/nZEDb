97c6ef13a284898f977eab09894c1af34d7ede49
jonnyboy <jonnyboy@jbmatrix.net>
2013-09-28 21:02:39 -0400

    minor tweak

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3718";
+$version="0.1r3719";
 
 $db = new DB();
 $DIR = MISC_DIR;
