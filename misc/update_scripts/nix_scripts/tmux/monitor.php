e5522c424078c824872a665d0d6181f44375059a
jonnyboy <jonnyboy@jbmatrix.net>
2013-10-13 11:11:39 -0400

    fix DB_SOCKET

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3849";
+$version="0.1r3850";
 
 $db = new DB();
 $DIR = MISC_DIR;
