51d8023902efaff3bd3a786fba668ba463fcc974
jonnyboy <jonnyboy@jbmatrix.net>
2013-10-19 11:45:08 -0400

    removed var_dump in binaries.php

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3872";
+$version="0.1r3873";
 
 $db = new DB();
 $DIR = MISC_DIR;
