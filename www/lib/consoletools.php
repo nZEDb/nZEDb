d0d4c1eb2a80014cd04b88a8fe768bd2d679a8ca
Whitelighter <whitelighter@lboroents.co.uk>
2013-10-21 22:18:12 +0100

    Fix duplicate percentString2 function

--- www/lib/consoletools.php
+++ www/lib/consoletools.php
@@ -42,15 +42,6 @@ class ConsoleTools
 		return sprintf($formatString, $first, $last, $total, $percent1, $percent2);
 	}
 
-	function percentString2($first, $last, $total)
-	{
-			$percent1 = 100 * ($first-1) / $total;
-			$percent2 = 100 * $last / $total;
-			$formatString = "% ".strlen($total)."d-% ".strlen($total)."d/%d (% 2d%%-% 3d%%)";
-			//$formatString = "% ".(strlen($total)*2)."d-%d/%d (% 2d%%)";
-			return sprintf($formatString, $first, $last, $total, $percent1, $percent2);
-	}
-
 	//
 	// Convert seconds to minutes or hours.
 	// Accepts a number of time.
