4925c4094ad826842dad324ea743c792e34f0512
jonnyboy <jonnyboy@jbmatrix.net>
2013-10-29 13:43:21 -0400

    updated /update_groups.php

--- misc/update_scripts/nix_scripts/tmux/bin/update_groups.php
+++ misc/update_scripts/nix_scripts/tmux/bin/update_groups.php
@@ -9,21 +9,18 @@ $db->queryExec("TRUNCATE TABLE shortgroups");
 $nntp = new Nntp();
 $nntp->doConnect();
 
-echo "Getting first/last for all active groups\n";
+echo "Getting first/last for all your active groups\n";
 $data = $nntp->getGroups();
 $nntp->doQuit();
 
 if (PEAR::isError($data))
 	exit("Failed to getGroups() from nntp server.\n");
 
-if (!isset($data['group']))
-	exit("Failed to getGroups() from nntp server.\n");
-
 echo "Inserting new values into shortgroups table\n";
 
 foreach ($data as $newgroup)
 {
-	$res1 = $db->queryOneRow(sprintf('SELECT name FROM groups WHERE name = %s', $db->escapeString($newgroup['group'])));
+	$res1 = $db->queryOneRow(sprintf('SELECT name FROM groups WHERE name = %s AND active = 1', $db->escapeString($newgroup['group'])));
 	if (isset($res1['name']))
 	{
 		$db->queryInsert(sprintf("INSERT INTO shortgroups (name, first_record, last_record, updated) VALUES (%s, %s, %s, NOW())", $db->escapeString($newgroup["group"]), $db->escapeString($newgroup["first"]), $db->escapeString($newgroup["last"])));
