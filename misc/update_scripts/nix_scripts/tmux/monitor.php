af9b79d5ba5a65018855165b64865cb35af8b712
jonnyboy <jonnyboy@jbmatrix.net>
2013-10-30 18:25:26 -0400

    typo in a few threaded scripts

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r3962";
+$version="0.1r3963";
 
 $db = new DB();
 $DIR = MISC_DIR;
