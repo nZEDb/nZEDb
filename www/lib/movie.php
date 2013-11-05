43a8e6fcb645a448b94556e00e435005f06835cf
James Prestwood <j_prestwood@hotmail.com>
2013-11-05 02:42:13 +0000

    corrected query

--- www/lib/movie.php
+++ www/lib/movie.php
@@ -542,7 +542,7 @@ class Movie
 
 		if ($releaseToWork == '')
 		{
-			$res = $this->db->query(sprintf("SELECT r.searchname, r.id FROM releases r INNER JOIN category c ON r.categoryid = c.id WHERE r.imdbid IS NULL AND r.nzbstatus = 1 AND c.parentid = %d LIMIT %d", Category::CAT_PARENT_MOVIE, $this->movieqty));
+			$res = $this->db->query(sprintf("SELECT r.searchname AS name, r.id FROM releases r INNER JOIN category c ON r.categoryid = c.id WHERE r.imdbid IS NULL AND r.nzbstatus = 1 AND c.parentid = %d LIMIT %d", Category::CAT_PARENT_MOVIE, $this->movieqty));
 			$moviecount = count($res);
 		}
 		else
