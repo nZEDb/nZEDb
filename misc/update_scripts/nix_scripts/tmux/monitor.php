cf9c8d8f1f674b4e941c7b8005d091c68774e9de
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-07 14:24:46 -0500

    properly fixed GrabNZBs replacenzbs

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR."lib/framework/db.php");
 require_once(WWW_DIR."lib/tmux.php");
 require_once(WWW_DIR."lib/site.php");
 
-$version="0.1r4048";
+$version="0.1r4051";
 
 $db = new DB();
 $DIR = MISC_DIR;
