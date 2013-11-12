3a9095c3ca0f12b52cd56ad44679feabbacf4f19
niel <niel niel.archer@gmail.com>
2013-11-12 15:26:48 +0000

    Providing dynamic application base directory for shell scripts.

--- misc/update_scripts/nix_scripts/screen/threaded/helper.sh
+++ misc/update_scripts/nix_scripts/screen/threaded/helper.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-export NZEDB_PATH="/var/www/nZEDb/misc/update_scripts"
-export TEST_PATH="/var/www/nZEDb/misc/testing/Release_scripts"
+export NZEDB_ROOT="$(php ../../../../../nZEDbBase.php)"
+export NZEDB_PATH="${NZEDB_ROOT}/misc/update_scripts"
+export TEST_PATH="${NZEDB_ROOT}/misc/testing/Release_scripts"
 command -v php5 >/dev/null 2>&1 && export PHP=`command -v php5` || { export PHP=`command -v php`; }
 export NZEDB_SLEEP_TIME="60"
 
--- misc/update_scripts/nix_scripts/tmux/commit.sh
+++ misc/update_scripts/nix_scripts/tmux/commit.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
+export NZEDB_ROOT="$(php ../../../../../nZEDbBase.php)"
 nano Changelog
-cd /var/www/nZEDb
+cd ${NZEDB_ROOT}
 commit=`git log | grep "^commit" | wc -l`
 commit=`expr $commit + 1`
 
-sed -i -e "s/\$version=.*$/\$version=\"0.3r$commit\";/"  /var/www/nZEDb/misc/update_scripts/nix_scripts/tmux/monitor.php
+sed -i -e "s/\$version=.*$/\$version=\"0.3r$commit\";/"  ${NZEDB_ROOT}/misc/update_scripts/nix_scripts/tmux/monitor.php
 
 git commit -a
 
