d7a02f14a36f85bfde9026a5767d8a6945e2222f
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-12 15:40:12 -0500

    changed query in stage 6 to not create an array first

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once(WWW_DIR.'lib/framework/db.php');
 require_once(WWW_DIR.'lib/tmux.php');
 require_once(WWW_DIR.'lib/site.php');
 
-$version="0.3r4237";
+$version="0.3r4238";
 
 $db = new DB();
 $DIR = MISC_DIR;
