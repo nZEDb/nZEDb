959a3bf7bd1b498599bbe6e0d2abe31150250005
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-15 12:02:32 -0500

    more regexes and a bug fix

--- misc/testing/Dev_testing/test-all-regexes.php
+++ misc/testing/Dev_testing/test-all-regexes.php
@@ -27,7 +27,7 @@ if (isset($argv[2]) && is_numeric($argv[2]))
 if ($handle)
 {
 	echo $c->header($argv[1]);
-	if (isset($groupName))
+	if (isset($group))
 		echo $c->primary($group."\n");
 	while (($line = fgets($handle)) !== false)
 	{
@@ -100,5 +100,42 @@ if ($handle)
 			}
 		}
 	}
+}
+
+$file = nZEDb_MISC . '../apre.php';
+$handle = fopen($file, "r");
+
+$test_str = $argv[1];
+
+$e0 = '([-_](proof|sample|thumbs?))*(\.part\d*(\.rar)?|\.rar)?(\d{1,3}\.rev"|\.vol.+?"|\.[A-Za-z0-9]{2,4}"|")';
+$e1 = $e0.' yEnc$/';
+$groupName = 'apre';
+if ($handle)
+{
+    while (($line = fgets($handle)) !== false)
+    {
+        $line = preg_replace('/\$this->e0/', $e0, $line);
+        $line = preg_replace('/\$this->e1/', $e1, $line);
+        if (preg_match('/if \(preg_match\(\'(.+)\', \$this->subject\, \$match\)\)/', $line, $match) || preg_match('/if \(preg_match\(\'(.+)\', \$subject\, \$match\)\)/', $line, $match))
+        {
+            $regex = $match[1];
+            if (preg_match($regex, $test_str, $match1))
+            {
+                if ($groupName != '')
+                    echo $c->header("Group regex => ".$groupName."\n");
+                if ($match1)
+                    echo $c->primary($regex);
+                if (isset($match1[1]) && $match1[1] != '' && $match1[1] != '"')
+                    echo $c->primary("match[1]->".$match1[1]);
+                if (isset($match1[2]) && $match1[2] != '' && $match1[2] != '"')
+                    echo $c->primary("match[2]->".$match1[2]);
+                if (isset($match1[3]) && $match1[3] != '' && $match1[3] != '"')
+                    echo $c->primary("match[3]->".$match1[3]);
+                if (isset($match1['title']) && $match1['title'] != '' && $match1['title'] != '"')
+                    echo $c->primary("match['title']->".$match1['title']);
+                echo "\n\n\n";
+            }
+        }
+    }
 }
 
