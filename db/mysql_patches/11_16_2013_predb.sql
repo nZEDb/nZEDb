4ab41949a67b3448fadd2842f99ec407eca94101
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-16 12:38:20 -0500

    edited to deal with dupes

--- db/mysql_patches/11_16_2013_predb.sql
+++ db/mysql_patches/11_16_2013_predb.sql
@@ -1,4 +1,5 @@
 DROP INDEX ix_predb_md5 ON predb;
 ALTER IGNORE TABLE predb ADD UNIQUE INDEX ix_predb_md5 ON predb(md5);
+UPDATE releases SET preid = NULL where preid IS NOT NULL;
 
 UPDATE site SET value = '146' WHERE setting = 'sqlpatch';
