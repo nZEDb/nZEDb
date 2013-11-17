30b9c50456c9e2794f021d7311efd69a568a43c6
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-16 20:07:38 -0500

    changed site-edit so info lines up

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -5,7 +5,7 @@ require_once nZEDb_LIB . 'framework/db.php';
 require_once nZEDb_LIB . 'tmux.php';
 require_once nZEDb_LIB . 'site.php';
 
-$version="0.3r4312";
+$version="0.3r4313";
 
 $db = new DB();
 $DIR = nZEDb_MISC;
