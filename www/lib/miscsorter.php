9314392cece4711ccc74efb25a0399439fce48e0
jonnyboy <jonnyboy@jbmatrix.net>
2013-11-23 23:56:02 -0500

    bug fix

--- www/lib/miscsorter.php
+++ www/lib/miscsorter.php
@@ -28,8 +28,8 @@ class MiscSorter
 		$this->nc = new nameCleaning();
 
 
-		//$res = $this->db->query("SET NAMES 'utf8'");
-		//$res = $this->db->query("SET CHARACTER SET 'utf8'");
+		//$res = $this->db->queryExec("SET NAMES 'utf8'");
+		//$res = $this->db->queryExec("SET CHARACTER SET 'utf8'");
 
 		mb_internal_encoding("UTF-8");
 		mb_regex_encoding("UTF-8");
@@ -227,7 +227,7 @@ class MiscSorter
 		$this->doecho($query);
 		if (!$debug)
 		{
-			if ($this->db->query($query) !== false)
+			if ($this->db->queryExec($query) !== false)
 				return true;
 		}
 		else
