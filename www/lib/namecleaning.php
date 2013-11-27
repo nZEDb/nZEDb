4e4a043a1abe95c150b82ec0f033172342541255
Eric Gandt <egandt@fileserver.dirtypair.org>
2013-11-26 19:01:47 -0500

    Addition to namefixer for audiobooks, basically the same as MP3 currently, but these groups were previously missed.

--- www/lib/namecleaning.php
+++ www/lib/namecleaning.php
@@ -169,6 +169,14 @@ class nameCleaning
 				return $this->sounds_lossless();
 			case 'alt.binaries.sounds.mp3':
 				return $this->sounds_mp3();
+			case 'alt.binaries.mp3.audiobooks':
+				return $this->sounds_audiobooks();
+			case 'alt.binaries.sound.audiobooks':
+				return $this->sounds_audiobooks();
+			case 'alt.binaries.sounds.audiobooks.repost':
+				return $this->sounds_audiobooks();
+			case 'alt.binaries.sounds.mp3.audiobooks':
+				return $this->sounds_audiobooks();			
 			case 'alt.binaries.teevee':
 				return $this->teevee();
 			case 'alt.binaries.town':
@@ -1173,7 +1181,27 @@ class nameCleaning
 		else
 			return $this->generic();
 	}
-
+	// a.b.sounds.mp3.audiobooks
+	public function sounds_audiobooks()
+	{
+		// currently these are teh same as mp3, but in the future these should be modified to be unique to audiobooks
+		
+		//(dream-of-usenet.info) - [04/15] - "Enya-And_Winter_Came...-2008.part2.rar" yEnc
+		if (preg_match('/^\(dream-of-usenet\.info\) - \[\d+(\/\d+\] - ".+?)'.$this->e1, $this->subject, $match))
+			return $match[1];
+		//http://dream-of-usenet.org empfehlen newsconnection.eu - [02/32] - "Adam_Ant-Manners_and_Physique-(MCAD-6315)-CD-FLAC-1989-2Eleven.par2" yEnc
+		else if (preg_match('/^http:\/\/dream-of-usenet\.org .+? - \[\d+(\/\d+\] - ".+?)'.$this->e1, $this->subject, $match))
+			return $match[1];
+		//>>> CREATIVE COMMONS NZB <<< "dexter romweber duo-lookout" - File 1 of 9: "creative_commons_nzb_dexter_romweber_duo-lookout.rar" yEnc
+		else if (preg_match('/^(>>> CREATIVE COMMONS NZB <<< ".+?" - File )\d+ of \d+: ".+?" yEnc$/', $this->subject, $match))
+			return $match[1];
+		//<<<usenet-space-cowboys.info>>>  <<<Powered by https://secretusenet.com>< "Justin_Bieber-Believe_Acoustic-2013-pLAN9_usenet-space-cowbys.info.rar" >< 4/6 (78.65 MB) >< 60.84 MB > yEnc
+		else if (preg_match('/^(.+?usenet-space.+?Powered by.+? ".+?)'.$this->e0.'.+? \d+\/\d+ \(\d+[.,]\d+ [kKmMgG][bB]\) .+? \d+[.,]\d+ [kKmMgG][bB] .+?yEnc$/', $this->subject, $match))
+			return $match[1];
+		else
+		  return $this->generic();
+	}
+	
 	// a.b.sounds.mp3
 	public function sounds_mp3()
 	{
