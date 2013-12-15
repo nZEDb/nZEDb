d246c94696e8865094ac4cf16d39b3b8cb4073c9
jonnyboy <jonnyboy@jbmatrix.net>
2013-12-14 22:32:55 -0500

    added OMDb API lookup as first lookup for imdb id

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -6,7 +6,7 @@ require_once nZEDb_LIB . 'tmux.php';
 require_once nZEDb_LIB . 'site.php';
 require_once nZEDb_LIB . 'ColorCLI.php';
 
-$version="0.3r4611";
+$version="0.3r4612";
 
 $db = new DB();
 $DIR = nZEDb_MISC;
