af6846799c689df54946800b3daf5d26dee65874
jonnyboy <jonnyboy@jbmatrix.net>
2013-12-16 20:55:56 -0500

    added file

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -6,7 +6,7 @@ require_once nZEDb_LIB . 'tmux.php';
 require_once nZEDb_LIB . 'site.php';
 require_once nZEDb_LIB . 'ColorCLI.php';
 
-$version="0.3r4620";
+$version="0.3r4621";
 
 $db = new DB();
 $DIR = nZEDb_MISC;
