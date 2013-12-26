c49a2ba47d9ea7bc8265bab53ffbf5070f69e072
jonnyboy <jonnyboy@jbmatrix.net>
2013-12-26 08:46:55 -0500

    fixed IMDB lookup, changed sleep timers for tmdb/imdb update scripts, added updateTvRage.php

--- misc/testing/PostProc_scripts/getMovieCovers.php
+++ misc/testing/PostProc_scripts/getMovieCovers.php
@@ -1,30 +1,27 @@
 <?php
-//This script will update all records in the movieinfo table
+//This script will update all records in the movieinfo table where there is no cover
 
 require_once dirname(__FILE__) . '/../../../www/config.php';
-require_once nZEDb_WWW . 'config.php';
 require_once nZEDb_LIB . 'framework/db.php';
 require_once nZEDb_LIB . 'movie.php';
 
 $movie = new Movie(true);
 $db = new Db();
 
-$movies = $db->queryDirect("SELECT imdbid FROM movieinfo WHERE tmdbid IS NULL ORDER BY id DESC");
+$movies = $db->queryDirect("SELECT imdbid FROM movieinfo WHERE cover = 0 ORDER BY year DESC, id DESC");
 if ($movies->rowCount() > 0)
-    echo "Updating movie info for ".$movies->rowCount()." movies.\n";
+    echo "Updating ".$movies->rowCount()." movie covers.\n";
+
 foreach ($movies as $mov)
 {
     $starttime = microtime(true);
     $mov = $movie->updateMovieInfo($mov['imdbid']);
-    if (!$mov)
-        echo ".";
-    // tmdb limits are 30 per 10 sec, not certain for imdb
+    echo ".";
     $diff = floor((microtime(true) - $starttime) * 1000000);
-    if (333333 - $diff > 0)
+    if (1000000 - $diff > 0)
     {
         echo "sleeping\n";
-        usleep(333333 - $diff);
+        usleep(1000000 - $diff);
     }
 }
 echo "\n";
-
