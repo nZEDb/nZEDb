55991580d97fef93414b945462f7067342109bef
niel <niel.archer@gmail.com>
2013-12-27 07:31:40 +0000

    Adding formatted output and Sanity test ;-)

--- misc/testing/DB_scripts/setUserPasswordHashesToEmail.php
+++ misc/testing/DB_scripts/setUserPasswordHashesToEmail.php
@@ -19,17 +19,52 @@
 
 require_once dirname(__FILE__) . '/../../../www/config.php';
 require_once nZEDb_LIB . 'framework/db.php';
+require_once nZEDb_LIB . 'ColorCLI.php';
+
+$output = new ColorCLI();
+
+$warning = <<<WARNING
+This script will (re)set the password hashes for older hashes (pre Db patch
+158, others are ignored), with the user's email as the new password.
+  It is intended for use ONLY if you have a *lot* of users, as this is not
+secure (a user's email addresses may be known to other users). If you only have
+a few users then run setUsersPasswordHash.php for each of them instead.
+WARNING;
+
+echo $output->warning($warning);
+if ($argc != 2) {
+	exit($output->error("\nWrong number of parameters\nUsage: php {$argv[0]} <IUnderStandTheRisks>"));
+} else if ($argv[1] !== 1 && $argv[1] != '<IUnderStandTheRisks>' && $argv[1] != 'IUnderStandTheRisks' && $argv[1] != 'true') {
+	exit($output->error("\nInvalid parameter(s)\nUsage: php {$argv[0]} <IUnderStandTheRisks>"));
+}
+
 
 $db = new DB();
-$query = "SELECT `id`, `username`, `email` FROM users WHERE `email` IS NOT NULL AND `email` != '' AND `password` NOT LIKE '$%'";
+$query = "SELECT `id`, `username`, `email`, `password` FROM users";
 
 $users = $db->query($query);
 $update = $db->Prepare('UPDATE users SET password = :password WHERE id = :id');
 
 foreach($users as $user)
 {
-	$hash = crypt($user['email']);
-	$update->execute([':password' => $hash, ':id' => $user['id']]);
+	if (needUpdate($user)) {
+		$hash = crypt($user['email']);
+		$update->execute([':password' => $hash, ':id' => $user['id']]);
+		echo $output->primary('Updating hash for user:') . $user['username'];
+	}
 }
 
+function needUpdate($user)
+{
+	global $output;
+	$status = true;
+	if (empty($user['email'])) {
+		$status = false;
+		echo $output->error('Cannot update password hash - Email is not set for user: ' . $user['username']);
+	} else if (preg_match('#^\$.+$#', $user['password'])) {
+		$status = false;
+		echo $user['username'] . $output->primary(' is already using new style hash ;-)');
+	}
+	return $status;
+}
 ?>
