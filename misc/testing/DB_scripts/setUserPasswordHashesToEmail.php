f341606149e9a70babd3889106f51e930d803460
niel <niel.archer@gmail.com>
2013-12-27 07:34:01 +0000

    Fixing copyright ;-)

--- misc/testing/DB_scripts/setUserPasswordHashesToEmail.php
+++ misc/testing/DB_scripts/setUserPasswordHashesToEmail.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * Copyright (C) 2013 niel
+ * Copyright (C) 2013 nZEDb
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
