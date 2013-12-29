64cbba39e77b7b8fa40cccb10689e8aec5d6cd61
jonnyboy <jonnyboy@jbmatrix.net>
2013-12-29 07:42:13 -0500

    removed useless patch

--- db/mysql_patches/2013_12_29-site.sql
+++
@@ -1,3 +0,0 @@
-SELECT CONCAT('rename table ', column_name, ' TO ' , lower(column_name) , ';') FROM information_schema.columns WHERE table_schema = 'nzedb';
-
-UPDATE site SET value = '163' WHERE setting = 'sqlpatch'; 
