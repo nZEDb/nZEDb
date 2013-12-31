4ac016d8bfb9d4fee7202af3f8318152adabb8fa
jonnyboy <jonnyboy@jbmatrix.net>
2013-12-30 23:13:12 -0500

    resized cover

--- misc/update_scripts/nix_scripts/tmux/monitor.php
+++ misc/update_scripts/nix_scripts/tmux/monitor.php
@@ -7,7 +7,7 @@ require_once nZEDb_LIB . 'tmux.php';
 require_once nZEDb_LIB . 'site.php';
 require_once nZEDb_LIB . 'ColorCLI.php';
 
-$version = "0.3r4760";
+$version = "0.3r4761";
 
 $db = new DB();
 $DIR = nZEDb_MISC;
