4658906343200e95814490a20c47b41b58b38a46
jonnyboy <jonnyboy@jbmatrix.net>
2014-01-07 16:29:29 -0500

    added regex to match predb style to predb names before using regexes to clean the name - renametopre.php improved performance and output - reset_postprocessing.php, added color to patchDB.php added update_predb_md5.php to reset predb md5's after title change

--- misc/testing/DB_scripts/patchDB.php
+++ misc/testing/DB_scripts/patchDB.php
@@ -17,88 +17,65 @@ function SplitSQL($file, $delimiter = ';')
 {
 	set_time_limit(0);
 
-	if (is_file($file) === true)
-	{
+	if (is_file($file) === true) {
 		$file = fopen($file, 'r');
 
-		if (is_resource($file) === true)
-		{
+		if (is_resource($file) === true) {
 			$query = array();
 			$db = new DB();
 			$dbsys = $db->dbSystem();
 			$c = new ColorCLI();
 
-			while (feof($file) === false)
-			{
+			while (feof($file) === false) {
 				$query[] = fgets($file);
-				if (preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1)
-				{
+				if (preg_match('~' . preg_quote($delimiter, '~') . '\s*$~iS', end($query)) === 1) {
 					$query = trim(implode('', $query));
 
-					if ($dbsys == "pgsql")
+					if ($dbsys == "pgsql") {
 						$query = str_replace(array("`", chr(96)), '', $query);
+					}
 					try {
 						$qry = $db->prepare($query);
 						$qry->execute();
-						echo 'SUCCESS: '.$query."\n";
+						echo 'SUCCESS: ' . $query . "\n";
 					} catch (PDOException $e) {
-						if ($e->errorInfo[1] == 1091)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1060)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1054)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1061)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1062)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1071)
-							echo $c->error($e->errorInfo[2]." - Assuming MyIsam, Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1072)
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else if ($e->errorInfo[1] == 1146)
-							 echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						// PGSQL Error Codse
-						else if ($e->errorInfo[0] == 23505 || $e->errorInfo[0] == 42701 || $e->errorInfo[0] == 42703 || $e->errorInfo[0] == '42P07' || $e->errorInfo[0] == '42P16')
-							echo $c->error($e->errorInfo[2]." - Not Fatal.\n");
-						else
-						{
-							echo $e."\n";
-							exit($c->error($query." Failed\n"));
+						if ($e->errorInfo[1] == 1091 || $e->errorInfo[1] == 1060 || $e->errorInfo[1] == 1054 || $e->errorInfo[1] == 1061 || $e->errorInfo[1] == 1062 || $e->errorInfo[1] == 1071 || $e->errorInfo[1] == 1072 || $e->errorInfo[1] == 1146 || $e->errorInfo[0] == 23505 || $e->errorInfo[0] == 42701 || $e->errorInfo[0] == 42703 || $e->errorInfo[0] == '42P07' || $e->errorInfo[0] == '42P16') {
+							echo $c->error($query . " Skipped - Not Fatal.\n");
+						} else {
+							exit($c->error($query . " Failed\n"));
 						}
 					}
 
-					while (ob_get_level() > 0)
-					{
+					while (ob_get_level() > 0) {
 						ob_end_flush();
 					}
 					flush();
 				}
 
-				if (is_string($query) === true)
+				if (is_string($query) === true) {
 					$query = array();
+				}
 			}
 			return fclose($file);
-		}
-		else
+		} else {
 			return false;
-	}
-	else
+		}
+	} else {
 		return false;
+	}
 }
 
 function BackupDatabase()
 {
 	$db = new DB();
 	$c = new ColorCLI();
-	$returnvar = NULL;
-	$output = NULL;
 	$DIR = nZEDb_MISC;
 
-	if (command_exist("php5"))
+	if (command_exist("php5")) {
 		$PHP = "php5";
-	else
+	} else {
 		$PHP = "php";
+	}
 
 	//Backup based on database system
 	if($db->dbSystem() == "mysql")
@@ -113,10 +90,11 @@ function BackupDatabase()
 
 $os = (strtoupper(substr(PHP_OS, 0, 3)) == 'WIN') ? "windows" : "unix";
 
-if(isset($argv[1]) && $argv[1] == "safe")
+if (isset($argv[1]) && $argv[1] == "safe") {
 	$safeupgrade = true;
-else
+} else {
 	$safeupgrade = false;
+}
 
 if (isset($os) && $os == "unix")
 {
@@ -129,24 +107,23 @@ if (isset($os) && $os == "unix")
 	$backedup = false;
 	$c = new ColorCLI();
 
-	if ($db->dbSystem() == "mysql")
+	if ($db->dbSystem() == "mysql") {
 		$path = nZEDb_ROOT . 'db/mysql_patches/';
-	else if ($db->dbSystem() == "pgsql")
+	} else if ($db->dbSystem() == "pgsql") {
 		$path = nZEDb_ROOT . 'db/pgsql_patches/';
+	}
 
 	// Open the patch folder.
-	if ($handle = @opendir($path))
-	{
-		while (false !== ($patch = readdir($handle)))
-		{
+	if ($handle = @opendir($path)) {
+		while (false !== ($patch = readdir($handle))) {
 			$patches[] = $patch;
 		}
 		closedir($handle);
+	} else {
+		exit($c->error("\nHave you changed the path to the patches folder, or do you have the right permissions?\n"));
 	}
-	else
-		exit($c->error("Have you changed the path to the patches folder, or do you have the right permissions?\n"));
 
-/*	if ($db->dbSystem() == "mysql")
+	/*	if ($db->dbSystem() == "mysql")
 		$patchpath = preg_replace('/\/misc\/testing\/DB_scripts/i', '/db/mysql_patches/', nZEDb_ROOT);
 	else if ($db->dbSystem() == "pgsql")
 		$patchpath = preg_replace('/\/misc\/testing\/DB_scripts/i', '/db/pgsql_patches/', nZEDb_ROOT);
@@ -175,8 +152,7 @@ if (isset($os) && $os == "unix")
 		}
 	}
 }
-else if (isset($os) && $os == "windows")
-{
+else if (isset($os) && $os == "windows") {
 	$s = new Sites();
 	$site = $s->get();
 	$currentversion = $site->sqlpatch;
@@ -184,33 +160,27 @@ else if (isset($os) && $os == "windows")
 	$patches = array();
 
 	// Open the patch folder.
-	if (!isset($argv[1]))
-		exit($c->error("You must supply the directory to the patches.\n"));
-	if ($handle = @opendir($argv[1]))
-	{
-		while (false !== ($patch = readdir($handle)))
-		{
+	if (!isset($argv[1])) {
+		exit($c->error("\nYou must supply the directory to the patches.\n"));
+	}
+	if ($handle = @opendir($argv[1])) {
+		while (false !== ($patch = readdir($handle))) {
 			$patches[] = $patch;
 		}
 		closedir($handle);
+	} else {
+		exit($c->error("\nHave you changed the path to the patches folder, or do you have the right permissions?\n"));
 	}
-	else
-		exit($c->error("Have you changed the path to the patches folder, or do you have the right permissions?\n"));
 
 	sort($patches);
-	foreach($patches as $patch)
-	{
-		if (preg_match('/\.sql$/i', $patch))
-		{
-			$filepath = $argv[1].$patch;
+	foreach ($patches as $patch) {
+		if (preg_match('/\.sql$/i', $patch)) {
+			$filepath = $argv[1] . $patch;
 			$file = fopen($filepath, "r");
 			$patch = fread($file, filesize($filepath));
-			if (preg_match('/UPDATE `?site`? SET `?value`? = \'?(\d{1,})\'? WHERE `?setting`? = \'sqlpatch\'/i', $patch, $patchnumber))
-			{
-				if ($patchnumber['1'] > $currentversion)
-				{
-					if($safeupgrade == true && $backedup == false)
-					{
+			if (preg_match('/UPDATE `?site`? SET `?value`? = \'?(\d{1,})\'? WHERE `?setting`? = \'sqlpatch\'/i', $patch, $patchnumber)) {
+				if ($patchnumber['1'] > $currentversion) {
+					if ($safeupgrade == true && $backedup == false) {
 						BackupDatabase();
 						$backedup = true;
 					}
@@ -220,13 +190,13 @@ else if (isset($os) && $os == "windows")
 			}
 		}
 	}
+} else {
+	exit($c->error("\nUnable to determine OS.\n"));
 }
-else
-	exit($c->error("Unable to determine OS\n"));
 
-if ($patched > 0)
-	exit($patched." patch(es) applied. Now you need to delete the files inside of the www/lib/smarty/templates_c folder.\n");
-if ($patched == 0)
-	exit("Nothing to patch, you are already on patch version ".$currentversion.".\n");
-
-?>
+if ($patched > 0) {
+	exit($c->header($patched . " patch(es) applied.\nNow you need to delete the files inside of the www/lib/smarty/templates_c folder."));
+}
+if ($patched == 0) {
+	exit($c->info("Nothing to patch, you are already on patch version " . $currentversion));
+}
