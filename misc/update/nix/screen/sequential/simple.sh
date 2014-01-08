e5f4eef7aa70040446cabc79b1bb20afad5f95ee
niel <niel.archer@gmail.com>
2014-01-08 01:43:53 +0000

    Correcting path to fixReleaseNames.php which must have been broken for a while.

--- misc/update/nix/screen/sequential/simple.sh
+++ misc/update/nix/screen/sequential/simple.sh
@@ -8,7 +8,7 @@ else
 fi
 
 export NZEDB_PATH="${NZEDB_ROOT}/misc/update"
-export TEST_PATH="${NZEDB_ROOT}/misc/testing/PostProc"
+export TEST_PATH="${NZEDB_ROOT}/misc/testing"
 export NZEDB_SLEEP_TIME="60" # in seconds
 LASTOPTIMIZE=`date +%s`
 LASTOPTIMIZE1=`date +%s`
@@ -32,6 +32,7 @@ $PHP ${NZEDB_PATH}/nntpproxy.php
 cd ${NZEDB_PATH}
 $PHP ${NZEDB_PATH}/update_binaries.php
 
+set -e
 $PHP ${NZEDB_PATH}/update_releases.php 1 true
 
 cd ${TEST_PATH}
@@ -40,10 +41,10 @@ if [ "$DIFF" -gt 900 ] || [ "$DIFF" -lt 1 ]
 then
 	LASTOPTIMIZE=`date +%s`
 	echo "Cleaning DB..."
-	$PHP ${TEST_PATH}/fixReleaseNames.php 1 true all yes
-	$PHP ${TEST_PATH}/fixReleaseNames.php 3 true other yes
-	$PHP ${TEST_PATH}/fixReleaseNames.php 5 true other yes
-	$PHP ${TEST_PATH}/removeCrapReleases.php true 2
+	$PHP ${TEST_PATH}/Release/fixReleaseNames.php 1 true all yes
+	$PHP ${TEST_PATH}/Release/fixReleaseNames.php 3 true other yes
+	$PHP ${TEST_PATH}/Release/fixReleaseNames.php 5 true other yes
+	$PHP ${TEST_PATH}/Release/removeCrapReleases.php true 2
 fi
 
 cd ${NZEDB_PATH}
