9d75c61bb624d9014035298e53e109ea8f6a6ffa
niel <niel.archer@gmail.com>
2014-01-11 23:02:36 +0000

    Removing the default netbeans license ;-)

--- runonce/relocateOldDirectoryContents.php
+++ runonce/relocateOldDirectoryContents.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 require_once '../www/config.php';
 
 $output = new ColorCLI();
