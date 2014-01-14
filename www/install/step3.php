0fbd787b9762031de1bd778a2b09114c0385b828
jonnyboy <jonnyboy@jbmatrix.net>
2014-01-14 00:48:23 -0500

    fixed install scripts

--- www/install/step3.php
+++ www/install/step3.php
@@ -1,50 +1,50 @@
 <?php
-require_once '../lib/installpage.php';
-require_once '../lib/install.php';
 
-$page = new Installpage();
+require_once '../lib/InstallPage.php';
+require_once '../lib/Install.php';
+
+$page = new InstallPage();
 $page->title = "News Server Setup";
 
 $cfg = new Install();
 
-if (!$cfg->isInitialized())
-{
+if (!$cfg->isInitialized()) {
 	header("Location: index.php");
 	die();
 }
 
 $cfg = $cfg->getSession();
 
-if  ($page->isPostBack())
-{
+if ($page->isPostBack()) {
 	$cfg->doCheck = true;
 
 	$cfg->NNTP_SERVER = trim($_POST['server']);
 	$cfg->NNTP_USERNAME = trim($_POST['user']);
 	$cfg->NNTP_PASSWORD = trim($_POST['pass']);
 	$cfg->NNTP_PORT = (trim($_POST['port']) == '') ? 119 : trim($_POST['port']);
-	$cfg->NNTP_SSLENABLED = (isset($_POST['ssl'])?(trim($_POST['ssl']) == '1' ? true : false):false);
+	$cfg->NNTP_SSLENABLED = (isset($_POST['ssl']) ? (trim($_POST['ssl']) == '1' ? true : false) : false);
 
 	include $cfg->nZEDb_WWW . '/lib/Net_NNTP/NNTP/Client.php';
 	$test = new Net_NNTP_Client();
 
 	$enc = false;
-	if ($cfg->NNTP_SSLENABLED)
+	if ($cfg->NNTP_SSLENABLED) {
 		$enc = "ssl";
-
-	$cfg->nntpCheck = $test->connect($cfg->NNTP_SERVER, $enc, $cfg->NNTP_PORT);
-	if(PEAR::isError($cfg->nntpCheck))
-		$cfg->error = true;
-	// Commented out until the issue is resolved
-	elseif ($cfg->NNTP_USERNAME != "")
-	{
-		$cfg->nntpCheck = $test->authenticate($cfg->NNTP_USERNAME, $cfg->NNTP_PASSWORD);
-		if(PEAR::isError($cfg->nntpCheck))
-			$cfg->error = true;
 	}
 
-	if (!$cfg->error)
-	{
+	$cfg->nntpCheck = $test->connect($cfg->NNTP_SERVER, $enc, $cfg->NNTP_PORT);
+	if (PEAR::isError($cfg->nntpCheck)) {
+		$cfg->error = true;
+	}
+	// Commented out until the issue is resolved
+	elseif ($cfg->NNTP_USERNAME != "") {
+		$cfg->nntpCheck = $test->authenticate($cfg->NNTP_USERNAME, $cfg->NNTP_PASSWORD);
+		if (PEAR::isError($cfg->nntpCheck)) {
+			$cfg->error = true;
+		}
+	}
+
+	if (!$cfg->error) {
 		$cfg->setSession();
 		header("Location: ?success");
 		die();
@@ -56,5 +56,3 @@ $page->smarty->assign('page', $page);
 
 $page->content = $page->smarty->fetch('step3.tpl');
 $page->render();
-
-?>
