e22a1dd4fffe4283b460c0cbd292bf7f57bf8fbf
jonnyboy <jonnyboy@jbmatrix.net>
2014-01-19 12:35:23 -0500

    possible fix to safe scripts putting excessive parts into part repair

--- misc/update/nntpproxy.php
+++ misc/update/nntpproxy.php
@@ -1,16 +1,13 @@
 <?php
 
 require_once dirname(__FILE__) . '/config.php';
-//require_once nZEDb_LIB . 'framework/db.php';
-//require_once nZEDb_LIB . 'tmux.php';
-//require_once nZEDb_LIB . 'site.php';
 
 passthru("clear");
 
 $db = new DB();
 $t = new Tmux();
 $tmux = $t->get();
-$powerline = $tmux->powerline;
+$powerline = (isset($tmux->powerline)) ? $tmux->powerline : 0;
 $s = new Sites();
 $site = $s->get();
 
@@ -23,15 +20,14 @@ function python_module_exist($module)
 }
 
 $nntpproxy = $site->nntpproxy;
-if ($nntpproxy == 0)
+if ($nntpproxy == 0) {
 	exit();
-else
-{
+} else {
 	$modules = array("nntp", "socketpool");
-	foreach ($modules as &$value)
-	{
-		if (!python_module_exist($value))
-			exit($c->error("NNTP Proxy requires ".$value." python module but it's not installed. Aborting."));
+	foreach ($modules as &$value) {
+		if (!python_module_exist($value)) {
+			exit($c->error("NNTP Proxy requires " . $value . " python module but it's not installed. Aborting."));
+		}
 	}
 }
 
@@ -40,37 +36,32 @@ function window_proxy($tmux_session, $powerline)
 	$s = new Sites();
 	$site = $s->get();
 	$DIR = nZEDb_MISC;
-	if ($powerline == "TRUE")
-		$tmuxconfig = $DIR."update/nix/tmux/powerline/tmux.conf";
-	else
-		$tmuxconfig = $DIR."update/nix/tmux/tmux.conf";
+	if ($powerline === '1') {
+		$tmuxconfig = $DIR . "update/nix/tmux/powerline/tmux.conf";
+	} else {
+		$tmuxconfig = $DIR . "update/nix/tmux/tmux.conf";
+	}
 
 	$nntpproxy = $site->nntpproxy;
-	if ($nntpproxy == '1')
-	{
+	if ($nntpproxy === '1') {
 		$DIR = nZEDb_MISC;
-		$nntpproxypy = $DIR."update/python/nntpproxy.py";
-		if(file_exists($DIR."update/python/lib/nntpproxy.conf"))
-		{
-			$nntpproxyconf = $DIR."update/python/lib/nntpproxy.conf";
-			shell_exec("cd ${DIR}/update/nix/tmux; tmux -f $tmuxconfig attach-session -t $tmux_session || tmux new-session -d -s $tmux_session -n NNTPProxy 'printf \"\033]2;\"NNTPProxy\"\033\" && python $nntpproxypy $nntpproxyconf'");
+		$nntpproxypy = $DIR . "update/python/nntpproxy.py";
+		if (file_exists($DIR . "update/python/lib/nntpproxy.conf")) {
+			$nntpproxyconf = $DIR . "update/python/lib/nntpproxy.conf";
+			shell_exec("cd ${DIR}/update/nix/tmux; tmux -f $tmuxconfig attach-session -t $tmux_session || tmux -f $tmuxconfig new-session -d -s $tmux_session -n NNTPProxy 'printf \"\033]2;\"NNTPProxy\"\033\" && python $nntpproxypy $nntpproxyconf'");
 		}
 	}
 
 	$alternate_nntp = $site->alternate_nntp;
 	$grabnzbs = $site->grabnzbs;
-	if ($nntpproxy == '1' && ($alternate_nntp == '1' || $grabnzbs == '2'))
-	{
+	if ($nntpproxy == '1' && ($alternate_nntp == '1' || $grabnzbs == '2')) {
 		$DIR = nZEDb_MISC;
-		$nntpproxypy = $DIR."update/python/nntpproxy.py";
-		if (file_exists($DIR."update/python/lib/nntpproxy_a.conf"))
-		{
-			$nntpproxyconf = $DIR."update/python/lib/nntpproxy_a.conf";
+		$nntpproxypy = $DIR . "update/python/nntpproxy.py";
+		if (file_exists($DIR . "update/python/lib/nntpproxy_a.conf")) {
+			$nntpproxyconf = $DIR . "update/python/lib/nntpproxy_a.conf";
 			shell_exec("tmux selectp -t 0; tmux splitw -t $tmux_session:0 -h -p 50 'printf \"\033]2;NNTPProxy\033\" && python $nntpproxypy $nntpproxyconf'");
 		}
 	}
-
 }
 
 window_proxy($tmux_session, $powerline);
-?>
