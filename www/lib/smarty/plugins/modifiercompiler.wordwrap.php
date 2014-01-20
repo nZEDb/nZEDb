302c31502012c860fc096030d2f1e213e8bbb175
Josh <sinfuljosh@me.com>
2014-01-20 00:09:02 -0600

    Update Smarty to 3.1.16 Trunk 18.01.2014

--- www/lib/smarty/plugins/modifiercompiler.wordwrap.php
+++ www/lib/smarty/plugins/modifiercompiler.wordwrap.php
@@ -20,27 +20,26 @@
  */
 function smarty_modifiercompiler_wordwrap($params, $compiler)
 {
-	if (!isset($params[1])) {
-		$params[1] = 80;
-	}
-	if (!isset($params[2])) {
-		$params[2] = '"\n"';
-	}
-	if (!isset($params[3])) {
-		$params[3] = 'false';
-	}
-	$function = 'wordwrap';
-	if (Smarty::$_MBSTRING) {
-		if ($compiler->tag_nocache | $compiler->nocache) {
-			$compiler->template->required_plugins['nocache']['wordwrap']['modifier']['file'] = SMARTY_PLUGINS_DIR .'shared.mb_wordwrap.php';
-			$compiler->template->required_plugins['nocache']['wordwrap']['modifier']['function'] = 'smarty_mb_wordwrap';
-		} else {
-			$compiler->template->required_plugins['compiled']['wordwrap']['modifier']['file'] = SMARTY_PLUGINS_DIR .'shared.mb_wordwrap.php';
-			$compiler->template->required_plugins['compiled']['wordwrap']['modifier']['function'] = 'smarty_mb_wordwrap';
-		}
-		$function = 'smarty_mb_wordwrap';
-	}
-	return $function . '(' . $params[0] . ',' . $params[1] . ',' . $params[2] . ',' . $params[3] . ')';
-}
+    if (!isset($params[1])) {
+        $params[1] = 80;
+    }
+    if (!isset($params[2])) {
+        $params[2] = '"\n"';
+    }
+    if (!isset($params[3])) {
+        $params[3] = 'false';
+    }
+    $function = 'wordwrap';
+    if (Smarty::$_MBSTRING) {
+    if ($compiler->template->caching && ($compiler->tag_nocache | $compiler->nocache)) {
+            $compiler->template->required_plugins['nocache']['wordwrap']['modifier']['file'] = SMARTY_PLUGINS_DIR .'shared.mb_wordwrap.php';
+            $compiler->template->required_plugins['nocache']['wordwrap']['modifier']['function'] = 'smarty_mb_wordwrap';
+        } else {
+            $compiler->template->required_plugins['compiled']['wordwrap']['modifier']['file'] = SMARTY_PLUGINS_DIR .'shared.mb_wordwrap.php';
+            $compiler->template->required_plugins['compiled']['wordwrap']['modifier']['function'] = 'smarty_mb_wordwrap';
+        }
+        $function = 'smarty_mb_wordwrap';
+    }
 
-?>
+    return $function . '(' . $params[0] . ',' . $params[1] . ',' . $params[2] . ',' . $params[3] . ')';
+}
