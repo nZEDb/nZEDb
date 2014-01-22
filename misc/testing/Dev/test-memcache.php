79009146e573e56c122fd8b1fe4519444cccdf46
jonnyboy <jonnyboy@jbmatrix.net>
2014-01-22 11:27:50 -0500

    fixed bug in test-memcache.php

--- misc/testing/Dev/test-memcache.php
+++ misc/testing/Dev/test-memcache.php
@@ -1,7 +1,7 @@
 <?php
 // Test the memcache connection.
 require_once dirname(__FILE__) . '/../../../www/config.php';
-//require_once nZEDb_LIB . 'framework/db.php';
+require_once nZEDb_LIB . 'DB.php';
 
 if (!extension_loaded('memcache'))
 {
@@ -10,7 +10,8 @@ if (!extension_loaded('memcache'))
 }
 
 $memcache = new Mcached();
-if ($memcache !== false)
-	exit(print_r($memcache->Server_Stats())."\nIf you have a long list of items above this then your memcached server is probably working fine.\n");
-else
+if ($memcache !== false) {
+	exit(print_r($memcache->Server_Stats()) . "\nIf you have a long list of items above this then your memcached server is probably working fine.\n");
+} else {
 	exit("Make sure your host/port are set right in config.php for memcache.\n");
+}
