46c439ab2aeb063388526a6b6ab93694ac56440d
jonnyboy <jonnyboy@jbmatrix.net>
2014-01-29 13:26:20 -0500

    uncommented

--- misc/testing/Dev/update_predb_md5.php
+++ misc/testing/Dev/update_predb_md5.php
@@ -11,7 +11,7 @@ if (!isset($argv[1]) || $argv[1] != 'true') {
 	exit($c->error("\nThis script will recalculate and update the MD5 column for each pre.\n\n"
 					. "php $argv[0] true      ...: To reset every predb MD5.\n"));
 }
-/*
+
 
 // Drop the unique index
 $has_index = $db->queryDirect("SHOW INDEXES IN predb WHERE Key_name = 'ix_predb_md5'");
@@ -38,7 +38,6 @@ echo "\n" . $c->info("Creating index ix_predb_md5.");
 $db->queryDirect("ALTER IGNORE TABLE predb ADD CONSTRAINT ix_predb_md5 UNIQUE (md5)");
 echo $c->info("Updating Predb matches in releases.");
 
-*/
 $releases = $db->queryDirect("SELECT id, searchname FROM releases WHERE preid IS NOT NULL");
 $newtotal = $releases->rowCount();
 $matched = $counter = 0;
