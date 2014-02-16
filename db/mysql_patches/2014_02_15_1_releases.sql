fdc980b0bdfcd923d91f75afdfb8a32828ef41c9
jonnyboy <jonnyboy@jbmatrix.net>
2014-02-15 19:19:10 -0500

    updated schemas

--- db/mysql_patches/2014_02_15_1_releases.sql
+++ db/mysql_patches/2014_02_15_1_releases.sql
@@ -12,4 +12,10 @@ UPDATE releases SET isrequestid = 1 WHERE (bitwise & 1024) = 1024;
 DROP INDEX ix_releases_status ON releases;
 CREATE INDEX ix_releases_status ON releases (nzbstatus, iscategorized, isrenamed, nfostatus, ishashed, isrequestid, passwordstatus, dehashstatus, reqidstatus, musicinfoid, consoleinfoid, bookinfoid, haspreview, categoryid, imdbid, rageid);
 
+DROP TRIGGER IF EXISTS check_insert;
+DROP TRIGGER IF EXISTS check_update;
+
+CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;
+CREATE TRIGGER check_update BEFORE UPDATE ON releases FOR EACH ROW BEGIN IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1;ELSEIF NEW.name REGEXP '^\\[[[:digit:]]+\\]' THEN SET NEW.isrequestid = 1; END IF; END;
+
 UPDATE site SET value = '177' WHERE setting = 'sqlpatch';
