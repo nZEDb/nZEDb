64a9fbaef218c90fa9bdfc22b311cd1b86bffc05
niel <niel.archer@gmail.com>
2014-02-17 02:29:08 +0000

    Fixing a couple of missed slashes in file paths.

--- www/install/step1.php
+++ www/install/step1.php
@@ -129,7 +129,7 @@ if (!$cfg->pearCheck) {
 }
 
 $cfg->schemaCheck = false;
-if (is_readable($cfg->DB_DIR . '/schema.mysql') && is_readable($cfg->DB_DIR . 'schema.pgsql')) {
+if (is_readable($cfg->DB_DIR . 'schema.mysql') && is_readable($cfg->DB_DIR . 'schema.pgsql')) {
 	$cfg->schemaCheck = true;
 }
 if ($cfg->schemaCheck === false) {
@@ -151,7 +151,7 @@ if (preg_match('/apache/i', $_SERVER['SERVER_SOFTWARE'])) {
 }
 
 // Load previous config.php.
-if (file_exists(nZEDb_WWW . '/config.php') && is_readable(nZEDb_WWW . 'config.php')) {
+if (file_exists(nZEDb_WWW . 'config.php') && is_readable(nZEDb_WWW . 'config.php')) {
 	$tmpCfg = file_get_contents(nZEDb_WWW . 'config.php');
 	$cfg->setConfig($tmpCfg);
 }
