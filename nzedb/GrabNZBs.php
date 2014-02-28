a59a6c0800b57c95ae86c443af8adb41d536e38a
jonnyboy <jonnyboy@jbmatrix.net>
2014-02-28 11:51:55 -0500

    changed to catch all variations

--- nzedb/GrabNZBs.php
+++ nzedb/GrabNZBs.php
@@ -113,7 +113,7 @@ class GrabNZBs
 				$postdate[] = $date;
 				$partless = preg_replace('/(\(\d+\/\d+\))*$/', 'yEnc', $firstname['0']);
 				$partless = preg_replace('/yEnc.*?$/', 'yEnc', $partless);
-				$partless = str_ireplace('#a.b.teevee', 'a.b.teevee', $partless);
+				$partless = preg_replace('/\[#?a\.b\.teevee@?EFNet\]/', '[#a.b.teevee@EFNet]', $partless);
 				$subject = utf8_encode(trim($partless));
 
 				// Make a fake message object to use to check the blacklist.
