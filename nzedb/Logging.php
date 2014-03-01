50bcaa48b1d6e7ec56546a481cb81494c7d5e903
kevinlekiller <kevinlekiller@gmail.com>
2014-03-01 15:18:09 -0500

    Add methods for logging debug info to file.

--- nzedb/Logging.php
+++ nzedb/Logging.php
@@ -5,6 +5,46 @@
 class Logging
 {
 	/**
+	 * Turn off debug logging here.
+	 *
+	 * @note You must turn on the site debug setting, otherwise logging will not occur regardless of this setting.
+	 *
+	 * @const bool $debugLogging
+	 */
+	private $debugLogging = true;
+
+	/**
+	 * Log all debug info?
+	 * If set to false, change $debugLogLevel
+	 *
+	 * @const bool
+	 */
+	const debugLogAll = true;
+
+	/**
+	 * What to log, if $debugLogAll is set to false.
+	 *
+	 * 1 Info     (Events like connecting to usenet).
+	 * 2 Notice   (Minor things like failed queries?).
+	 * 3 Warning  (Wrong usage of api's, libraries, etc?)
+	 * 4 Error    (Errors that can cause issues?)
+	 * 5 Fatal    (This caused the program to close?).
+	 *
+	 * @const int
+	 */
+	const debugLogLevel = 4;
+
+	/**
+	 * @const int Max log size in KB. Default, 512.
+	 */
+	const logFileSize = 512;
+
+	/**
+	 * @var string If windows "\r\n" if unix "\n".
+	 */
+	private $newLine;
+
+	/**
 	 * @var object DB Class instance.
 	 */
 	private $db;
@@ -16,7 +56,15 @@ class Logging
 	 */
 	public function __construct()
 	{
+		$site = new Sites();
+		$this->site = $site->get();
 		$this->db = new DB();
+
+		if ($this->debugLogging === true) {
+			$this->debugLogging = ($this->site->debuginfo == '0') ? false : true;
+		}
+
+		$this->newLine = ((strtolower(substr(php_uname('s'), 0, 3)) === 'win') ? "\r\n" : "\n");
 	}
 
 	/**
@@ -39,8 +87,7 @@ class Logging
 	 */
 	public function LogBadPasswd($username='', $host='')
 	{
-		$site = new Sites();
-		$s = $site->get();
+		$s = $this->site ;
 		// If logggingopt is = 0, then we do nothing, 0 = logging off.
 		if ($s->loggingopt == '1') {
 			$this->db->queryInsert(sprintf('INSERT INTO logging (time, username, host) VALUES (NOW(), %s, %s)',
@@ -80,20 +127,126 @@ class Logging
 		return $this->db->query('SELECT MAX(time) AS time, host, COUNT(host) AS count FROM logging GROUP BY host ORDER BY count DESC LIMIT 10');
 	}
 
-	/**
-	 * @param string $message  The message to log.
+	/*
 	 * @param string $class    The class this is coming from.
 	 * @param string $method   The method this is coming from.
-	 * @param int    $severity How severe of a warning is this?
+	 * @param string $message  The message to log.
+	 * @param int    $severity How severe is this message?
+	 *               1 Fatal   - The program had to stop.
+	 *               2 Error   - Something went very wrong but we recovered.
+	 *               3 Warning - Not an error, but something we can probably fix.
+	 *               4 Notice  - Like warning but not as bad?
+	 *               5 Info    - General info, like we logged in to usenet for example.
+	 *               Anything else is unknown.
 	 *
 	 * @return void
 	 */
-	public function logDebug($message, $class, $method, $severity)
+	public function logDebug ($class, $method, $message, $severity)
 	{
-		// Path to store the log file.
-		$path = nZEDb_RES . DS . 'debug' . DS;
+		// Check if debugging is on.
+		if (!$this->debugLogging){
+			return;
+		}
+
+		// Check if we should log this type of message if the setting is on in the top of this script.
+		if (!self::debugLogAll && self::debugLogLevel !== $severity) {
+			return;
+		}
+
+		// Create a string base on the severity of the this message.
+		switch ($severity) {
+			case 1:
+				$severity = '] [FATAL]    [';
+				break;
+			case 2:
+				$severity = '] [ERROR]    [';
+				break;
+			case 3:
+				$severity = '] [WARNING]  [';
+				break;
+			case 4:
+				$severity = '] [NOTICE]   [';
+				break;
+			case 5:
+				$severity = '] [INFO]     [';
+				break;
+			default:
+				$severity = '] [UNKNOWN]  [';
+		}
+
+		// Path to folder where log files are stored..
+		$path = nZEDb_RES . DS . 'logs' . DS;
+
+		// Check if the folder exists.
+		if (!is_dir($path)) {
+			if (!mkdir($path)) {
+// Error creating log folder.
+				return;
+			}
+		}
+
+		// Name of the log file.
+		$fileName = 'debug.log';
+
+		// Full path to the log file.
+		$fileLocation = $path.$fileName;
 
 		// Current time. RFC2822 style ; Thu, 21 Dec 2000 16:01:07 +0200
-		$time = Date(r);
+		$time = '[' . Date('r');
+
+		// Initiate a new log file if we don't have one.
+		if (!file_exists($fileLocation)) {
+			if (!$this->initiateLog($fileLocation, $time)) {
+// Error creating log file.
+				return;
+			}
+		}
+
+		// Check if we need to rotate the log if it exceeds max size..
+		$logSize = filesize($fileLocation);
+		if ($logSize === false) {
+// Error getting log size.
+			return;
+		} else if ($logSize >= (self::logFileSize * 1024)) {
+			if (!rename($fileLocation, $path . 'debug.old.' . time())) {
+// Error renaming log.
+				return;
+			}
+
+			// Create a new log.
+			if (!$this->initiateLog($fileLocation, $time)) {
+// Error creating log file.
+				return;
+			}
+		}
+
+		// Strip new lines/ line returns.
+		$message = str_replace(array('\r', '\n'), ' ', $message);
+
+		// Create message : [Sat, 1 Mar 2014 16:01:07 +0500] [ERROR] [NNTP.doConnect() Could not connect to news.tweaknews.com (ssl) Password is wrong.]
+		$data = $time . $severity . $class . '.' . $method . '() ' . $message . ']' . $this->newLine;
+		// Append the message to the log.
+		if (!file_put_contents($fileLocation, $data, FILE_APPEND)) {
+// Error appending message to log file.
+			return;
+		}
+	}
+
+	/**
+	 * Initiate a log file.
+	 *
+	 * @param string $path The full path to the log file.
+	 * @param string $time The time in RFC2822 style.
+	 *
+	 * @return bool
+	 */
+	protected function initiateLog($path, $time)
+	{
+		if (!file_exists($path)) {
+			if (file_put_contents($path, $time . '] [INITIATE] [Initiating new log file.]' . $this->newLine)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
