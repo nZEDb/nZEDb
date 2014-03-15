7720f6f8792de9a8b30547873167704d348d8a5c
kevinlekiller <kevinlekiller@gmail.com>
2014-03-15 14:27:50 -0400

    Print the errors nicely on the web page (poster wall).

--- www/pages/newposterwall.php
+++ www/pages/newposterwall.php
@@ -6,6 +6,7 @@ if (!$users->isLoggedIn()) {
 $releases = new Releases();
 $contents = new Contents();
 $category = new Category();
+$error = false;
 
 // Array with all the possible poster wall types.
 $startTypes = array(/*'Books', */'Console', 'Movies', 'Audio'/*, 'Recent'*/);
@@ -21,64 +22,67 @@ if (count($categories > 0)) {
 		}
 	}
 } else {
-	exit("You have no categories enabled!");
+	$error = "No categories are enabled!";
 }
 
 if (count($types) === 0) {
-	exit("You have no categories enabled for the new poster wall.<br>Possible choices are: " . implode(', ', $startTypes) . '.');
+	$error = 'No categories enabled for the new poster wall. Possible choices are: ' . implode(', ', $startTypes) . '.';
 }
 
-// Check if the user did not pass the required t parameter, set it to the first type.
-if (!isset($_REQUEST['t'])) {
-	$_REQUEST['t'] = $types[0];
+if (!$error) {
+
+	// Check if the user did not pass the required t parameter, set it to the first type.
+	if (!isset($_REQUEST['t'])) {
+		$_REQUEST['t'] = $types[0];
+	}
+
+	// Check if the user passed an invalid t parameter.
+	if (!in_array($_REQUEST['t'], $types)) {
+		$_REQUEST['t'] = $types[0];
+	}
+
+	$page->smarty->assign('types', $types);
+	$page->smarty->assign('type', $_REQUEST['t']);
+
+	switch ($_REQUEST['t']) {
+		case 'Movies':
+			$getnewestmovies = $releases->getNewestMovies();
+			$page->smarty->assign('newest', $getnewestmovies);
+
+			$user = $users->getById($users->currentUserId());
+			$page->smarty->assign('cpapi', $user['cp_api']);
+			$page->smarty->assign('cpurl', $user['cp_url']);
+			$page->smarty->assign('goto', 'movies');
+			break;
+
+		case 'Console':
+			$getnewestconsole = $releases->getNewestConsole();
+			$page->smarty->assign('newest', $getnewestconsole);
+			$page->smarty->assign('goto', 'console');
+			break;
+
+		case 'Audio':
+			$getnewestmp3 = $releases->getnewestMP3s();
+			$page->smarty->assign('newest', $getnewestmp3);
+			$page->smarty->assign('goto', 'music');
+			break;
+
+		case 'Books':
+			$getnewestbooks = $releases->getNewestBooks();
+			$page->smarty->assign('newest', $getnewestbooks);
+			$page->smarty->assign('goto', 'books');
+			break;
+
+		case 'Recent':
+			$recent = $releases->getRecentlyAdded();
+			$page->smarty->assign('newest', $recent);
+			$page->smarty->assign('goto', 'browse');
+			break;
+
+		default:
+			$error = "ERROR: Invalid ?t parameter (" . $_REQUEST['t'] . ").";
+	}
 }
-
-// Check if the user passed an invalid t parameter.
-if (!in_array($_REQUEST['t'], $types)) {
-	$_REQUEST['t'] = $types[0];
-}
-
-$page->smarty->assign('types', $types);
-$page->smarty->assign('type', $_REQUEST['t']);
-
-switch ($_REQUEST['t']) {
-	case 'Movies':
-		$getnewestmovies = $releases->getNewestMovies();
-		$page->smarty->assign('newest', $getnewestmovies);
-
-		$user = $users->getById($users->currentUserId());
-		$page->smarty->assign('cpapi', $user['cp_api']);
-		$page->smarty->assign('cpurl', $user['cp_url']);
-		$page->smarty->assign('goto', 'movies');
-		break;
-
-	case 'Console':
-		$getnewestconsole = $releases->getNewestConsole();
-		$page->smarty->assign('newest', $getnewestconsole);
-		$page->smarty->assign('goto', 'console');
-		break;
-
-	case 'Audio':
-		$getnewestmp3 = $releases->getnewestMP3s();
-		$page->smarty->assign('newest', $getnewestmp3);
-		$page->smarty->assign('goto', 'music');
-		break;
-
-	case 'Books':
-		$getnewestbooks = $releases->getNewestBooks();
-		$page->smarty->assign('newest', $getnewestbooks);
-		$page->smarty->assign('goto', 'books');
-		break;
-
-	case 'Recent':
-		$recent = $releases->getRecentlyAdded();
-		$page->smarty->assign('newest', $recent);
-		$page->smarty->assign('goto', 'browse');
-		break;
-
-	default:
-		exit("ERROR: Invalid ?t parameter (" . $_REQUEST['t'] . ").");
-}
-
+$page->smarty->assign('error', $error);
 $page->content = $page->smarty->fetch('newposterwall.tpl');
 $page->render();
