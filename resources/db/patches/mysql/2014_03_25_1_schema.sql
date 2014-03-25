c26c1747d5971de4c8930fc72d781aef75692007
ruhllatio <ruhllatio@gmail.com>
2014-03-25 15:02:21 -0400

    Fix FT search patch to remove delimiters

--- resources/db/patches/mysql/2014_03_25_1_schema.sql
+++ resources/db/patches/mysql/2014_03_25_1_schema.sql
@@ -11,10 +11,12 @@ INSERT INTO releasesearch (releaseid, name, searchname) (SELECT id, name, search
 
 CREATE FULLTEXT INDEX ix_releasesearch_name_searchname_ft ON releasesearch (name, searchname);
 
-DELIMITER $$
-CREATE TRIGGER insert_search AFTER INSERT ON releases FOR EACH ROW BEGIN INSERT INTO releasesearch (releaseid, name, searchname) VALUES (NEW.id, NEW.name, NEW.searchname); END; $$
-CREATE TRIGGER update_search AFTER UPDATE ON releases FOR EACH ROW BEGIN IF NEW.name != OLD.name THEN UPDATE releasesearch SET name = NEW.name WHERE releaseid = OLD.id; END IF; IF NEW.searchname != OLD.searchname THEN UPDATE releasesearch SET searchname = NEW.searchname WHERE releaseid = OLD.id; END IF; END; $$
-CREATE TRIGGER delete_search AFTER DELETE ON releases FOR EACH ROW BEGIN DELETE FROM releasesearch WHERE releaseid = OLD.id; END; $$
-DELIMITER ;
+DROP TRIGGER IF EXISTS insert_search;
+DROP TRIGGER IF EXISTS update_search;
+DROP TRIGGER IF EXISTS delete_search;
+
+CREATE TRIGGER insert_search AFTER INSERT ON releases FOR EACH ROW BEGIN INSERT INTO releasesearch (releaseid, name, searchname) VALUES (NEW.id, NEW.name, NEW.searchname); END;
+CREATE TRIGGER update_search AFTER UPDATE ON releases FOR EACH ROW BEGIN IF NEW.name != OLD.name THEN UPDATE releasesearch SET name = NEW.name WHERE releaseid = OLD.id; END IF; IF NEW.searchname != OLD.searchname THEN UPDATE releasesearch SET searchname = NEW.searchname WHERE releaseid = OLD.id; END IF; END;
+CREATE TRIGGER delete_search AFTER DELETE ON releases FOR EACH ROW BEGIN DELETE FROM releasesearch WHERE releaseid = OLD.id; END;
 
 UPDATE site SET value = '187' WHERE setting = 'sqlpatch';
