f6c5416b4131b3b706f25ae538960c7cbdbb0b18
niel <niel.archer@gmail.com>
2014-04-03 19:08:51 +0100

    Reverting a patch pending investigating the problems.

--- resources/db/patches/mysql/2014_04_03_3_schema.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE countries DROP COLUMN id;
-ALTER TABLE countries DROP INDEX ix_countries_name;
-ALTER TABLE countries CHANGE COLUMN code code CHAR(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' FIRST;
-ALTER TABLE countries ADD PRIMARY KEY (name);
-
-UPDATE site SET value = '196' WHERE setting = 'sqlpatch';
