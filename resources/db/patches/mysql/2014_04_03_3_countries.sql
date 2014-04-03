ec3b8cbf05416c4b51a1cbaac4da6cdf8f7aae19
niel <niel.archer@gmail.com>
2014-04-03 19:27:33 +0100

    Fixing duplicate Korea entries in countries.

---
+++ resources/db/patches/mysql/2014_04_03_3_countries.sql
@@ -0,0 +1,4 @@
+UPDATE countries SET name = 'Korea, North' WHERE code = 'KP';
+UPDATE countries SET name = 'Korea, South' WHERE code = 'KR';
+
+UPDATE site SET value = '196' WHERE setting = 'sqlpatch';
