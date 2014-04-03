2421730574d132e3ce8bbd7090da2089b97f1c06
niel <niel.archer@gmail.com>
2014-04-03 20:19:33 +0100

    Fixing patch versions. Removing unneeded country table rename.

--- resources/db/patches/mysql/2014_03_26_1_schema.sql
+++ resources/db/patches/mysql/2014_03_26_1_schema.sql
@@ -1,5 +1,3 @@
-RENAME TABLE country TO countries;
-
 DROP FUNCTION IF EXISTS `countryCode`;
 CREATE FUNCTION `countryCode`(country VARCHAR(255) CHARACTER SET utf8 COLLATE utf8_unicode_ci) RETURNS VARCHAR(5) CHARSET utf8 COLLATE utf8_unicode_ci READS SQL DATA DETERMINISTIC COMMENT 'Convert country name into code, returns NULL if the country does not exist in the table.' BEGIN DECLARE c VARCHAR(5) CHARACTER SET utf8; IF country = '' THEN SET c := NULL; ELSEIF country IS NULL THEN SET c := NULL; ELSE SET country := LOWER(country); 	SELECT `code` INTO c FROM country WHERE LOWER(`name`) = country; END IF; RETURN c; END;
 
