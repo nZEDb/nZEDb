c177b576b7b3f80c53b7e23e21040dedbd747b25
Bart39 <bart39@sky.com>
2014-04-03 22:37:27 +0100

    tmux IRCScraper fixes

--- misc/update/nix/tmux/monitor.php
+++ misc/update/nix/tmux/monitor.php
@@ -288,50 +288,6 @@ function command_exist($cmd)
 	return (empty($returnVal) ? false : true);
 }
 
-function run_ircscraper($_php, $pane, $scrape_cz, $scrape_efnet)
-{
-	if ($scrape_cz == 1 && $scrape_efnet == 1) {
-		//Check to see if the pane is dead, if so respawn it.
-		if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
-			$DIR = nZEDb_MISC;
-			$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
-			shell_exec(
-				"tmux respawnp -t${tmux_session}:${pane}.0 ' \
-						$_php $ircscraper cz false false true'"
-			);
-		}
-		if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^1 | grep -c dead") == 1) {
-			$DIR = nZEDb_MISC;
-			$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
-			shell_exec(
-				"tmux respawnp -t${tmux_session}:${pane}.1 ' \
-						$_php $ircscraper efnet false false true'"
-			);
-		}
-	} else if ($scrape_cz == 1) {
-		if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
-			$DIR = nZEDb_MISC;
-			$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
-			shell_exec(
-				"tmux respawnp -t${tmux_session}:${pane}.0 ' \
-						$_php $ircscraper cz false false true'"
-			);
-		}
-	} else if ($scrape_efnet == 1) {
-		if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
-			$DIR = nZEDb_MISC;
-			$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
-			shell_exec(
-				"tmux respawnp -t${tmux_session}:${pane}.0 ' \
-						$_php $ircscraper efne false false true'"
-			);
-		}
-	} else {
-		$color = get_color($colors_start, $colors_end, $colors_exc);
-		shell_exec("tmux respawnp -t${tmux_session}:${pane}.0 'echo \"\033[38;5;\"$color\"m\nIRCScraper has been disabled/terminated by IRCScraping\"'");
-	}
-}
-
 //create timers
 $time = TIME();
 $time1 = TIME();
@@ -1012,6 +968,50 @@ while ($i > 0) {
 		}
 	}
 
+	function run_ircscraper($_php, $pane, $scrape_cz, $scrape_efnet)
+	{
+		if ($scrape_cz == 1 && $scrape_efnet == 1) {
+			//Check to see if the pane is dead, if so respawn it.
+			if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
+				$DIR = nZEDb_MISC;
+				$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
+				shell_exec(
+					"tmux respawnp -t${tmux_session}:${pane}.0 ' \
+						$_php $ircscraper cz false false true'"
+				);
+			}
+			if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^1 | grep -c dead") == 1) {
+				$DIR = nZEDb_MISC;
+				$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
+				shell_exec(
+					"tmux respawnp -t${tmux_session}:${pane}.1 ' \
+						$_php $ircscraper efnet false false true'"
+				);
+			}
+		} else if ($scrape_cz == 1) {
+			if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
+				$DIR = nZEDb_MISC;
+				$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
+				shell_exec(
+					"tmux respawnp -t${tmux_session}:${pane}.0 ' \
+						$_php $ircscraper cz false false true'"
+				);
+			}
+		} else if ($scrape_efnet == 1) {
+			if (shell_exec("tmux list-panes -t${tmux_session}:${pane} | grep ^0 | grep -c dead") == 1) {
+				$DIR = nZEDb_MISC;
+				$ircscraper = $DIR . "testing/IRCScraper/scrape.php";
+				shell_exec(
+					"tmux respawnp -t${tmux_session}:${pane}.0 ' \
+						$_php $ircscraper efne false false true'"
+				);
+			}
+		} else {
+			$color = get_color($colors_start, $colors_end, $colors_exc);
+			shell_exec("tmux respawnp -t${tmux_session}:${pane}.0 'echo \"\033[38;5;\"$color\"m\nIRCScraper has been disabled/terminated by IRCScraping\"'");
+		}
+	}
+
 	if ($compressed === '1') {
 		$mask2 = $c->headerOver("%-20s") . " " . $c->tmuxOrange("%-33.33s");
 	} else {
@@ -1498,7 +1498,8 @@ while ($i > 0) {
 			}
 
 			//run IRCScraper
-			run_ircscraper($_php, 3, $scrape_cz, $scrape_efnet);
+			$pane = ($colors == 1) ? 4 : 3;
+			run_ircscraper($_php, $pane, $scrape_cz, $scrape_efnet);
 		} else if ($seq == 2) {
 			// Show all available colors
 			if ($colors == 1) {
@@ -1553,7 +1554,8 @@ while ($i > 0) {
 					${DIR}update/nix/screen/sequential/user_threaded.sh true $log; date +\"%D %T\"' 2>&1 1> /dev/null");
 
 			//run IRCScraper
-			run_ircscraper($_php, 2, $scrape_cz, $scrape_efnet);
+			$pane = ($colors == 1) ? 3 : 2;
+			run_ircscraper($_php, $pane, $scrape_cz, $scrape_efnet);
 		} else {
 			//run update_binaries
 			$color = get_color($colors_start, $colors_end, $colors_exc);
@@ -1622,7 +1624,8 @@ while ($i > 0) {
 			}
 
 			//run IRCScraper
-			run_ircscraper($_php, 3, $scrape_cz, $scrape_efnet);
+			$pane = ($colors == 1) ? 4 : 3;
+			run_ircscraper($_php, $pane, $scrape_cz, $scrape_efnet);
 		}
 	} else if ($seq == 0) {
 		for ($g = 1; $g <= 4; $g++) {
