960b2ea3b98c05de07862a9c25b406e3308ef893
Bart39 <bart39@sky.com>
2014-04-07 16:03:46 +0100

    Fix: overriding efnet password

--- nzedb/controllers/IRCScraper.php
+++ nzedb/controllers/IRCScraper.php
@@ -172,9 +172,9 @@ class IRCScraper
 				if (defined('SCRAPE_IRC_EFNET_IGNORED_CHANNELS') && SCRAPE_IRC_EFNET_IGNORED_CHANNELS != '') {
 					$ignored = explode(',', SCRAPE_IRC_EFNET_IGNORED_CHANNELS);
 					$newList = array();
-					foreach($channelList as $channel => $password) {
+					foreach($channelList as $channel => $chanpass) {
 						if (!in_array($channel, $ignored)) {
-							$newList[$channel] = $password;
+							$newList[$channel] = $chanpass;
 						}
 					}
 					if (empty($newList)) {
