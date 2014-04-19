f8bfc515efd53674ac450174c6c32c1aa2d9509b
niel <niel.archer@gmail.com>
2014-04-19 19:37:27 +0100

    Adding sql patch forgotten from last commit.

---
+++ resources/db/patches/mysql/2014-04-19_212_settings.sql
@@ -0,0 +1,5 @@
+ALTER TABLE settings CHANGE value value VARCHAR(1000) NOT NULL DEFAULT '';
+ALTER TABLE settings CHANGE hint hint TEXT NOT NULL;
+INSERT IGNORE INTO settings (section, subsection, name, value, hint, setting) VALUES
+("site", "main", "tandc", "<p>All information within this database is indexed by an automated process, without any human intervention. It is obtained from global Usenet newsgroups over which this site has no control. We cannot prevent that you might find obscene or objectionable material by using this service. If you do find obscene, incorrect or objectionable results, let us know by using the contact form.</p>", "Text displayed in the terms and conditions page.", "tandc"),
+("", "", "maxpartrepair", 15000, "The maximum amount of articles to attempt to repair at a time. If you notice that you are getting a lot of parts into the partrepair table, it is possible that you USP is not keeping up with the requests. Try to reduce the threads to safe scripts, stop using safe scripts or stop using nntpproxy until improves. Ar least until the cause can be determined.", "maxpartrepair");
