9dcafeefd27832ab05af50aea6608087af0b3593
niel <niel.archer@gmail.com>
2014-04-20 12:58:45 +0100

    Bring patch 210 in line with schema.

--- resources/db/patches/mysql/2014-04-17_210_settings.sql
+++ resources/db/patches/mysql/2014-04-17_210_settings.sql
@@ -1,14 +1,14 @@
+DROP TABLE IF EXISTS settings;
 CREATE TABLE settings (
   section VARCHAR(25)  NOT NULL DEFAULT '',
   subsection VARCHAR(25)  NOT NULL DEFAULT '',
   name    VARCHAR(25)  NOT NULL DEFAULT '',
-  value   VARCHAR(255) NOT NULL DEFAULT '',
-  hint    VARCHAR(19000) NOT NULL DEFAULT '',
+  value   VARCHAR(1000) NOT NULL DEFAULT '',
+  hint   TEXT NOT NULL,
   setting VARCHAR(64) NOT NULL DEFAULT '',
-  PRIMARY KEY (section, subsection, name)
+  PRIMARY KEY (section, subsection, name),
+  UNIQUE INDEX ui_settings_setting (setting)
 ) ENGINE=MYISAM DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-
-CREATE UNIQUE INDEX ui_settings_setting ON settings (setting);
 
 LOAD DATA {:local:}INFILE '{:data:}10-settings.tsv' IGNORE INTO TABLE settings FIELDS TERMINATED BY '\t' OPTIONALLY  ENCLOSED BY '"' ESCAPED BY '\\' LINES TERMINATED BY '\n' IGNORE 1 LINES (section, subsection, name, value, hint, setting);
 
