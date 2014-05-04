c4b9fb1946c43d7971754aca80028094cf4f7fc0
Bart39 <bart39@sky.com>
2014-05-04 22:37:22 +0100

    typo in last commit

--- nzedb/controllers/ReleaseCleaning.php
+++ nzedb/controllers/ReleaseCleaning.php
@@ -49,7 +49,7 @@ class ReleaseCleaning
 			}
 		}
 		// Get pre style name from requestid
-		if (preg_match('/^(\[ ?(\d{4.6}) ?\]/', $this->subject, $match) || preg_match('/^REQ\s*(\d{4,6})/i',
+		if (preg_match('/^(\[ ?(\d{4,6}) ?\]/', $this->subject, $match) || preg_match('/^REQ\s*(\d{4,6})/i',
 				$this->subject, $match) || preg_match('/^(\d{4,6})-\d\[/', $this->subject, $match)) {
 			$title = $this->db->queryOneRow(
 				sprintf(
