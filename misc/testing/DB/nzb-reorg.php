7ce8cd01b8ea36405177706ee67529d6faaf4134
niel <niel.archer@gmail.com>
2014-05-05 04:12:23 +0100

    Simplifying nzb-reorg to make it faster and avoid the MySQL has gone away error.

--- misc/testing/DB/nzb-reorg.php
+++ misc/testing/DB/nzb-reorg.php
@@ -18,58 +18,50 @@ $filestoprocess = Array();
 $iFilesProcessed = $iFilesCounted = 0;
 $time = TIME();
 
-foreach($objects as $filestoprocess => $nzbFile)
-{
-	if($nzbFile->getExtension() != "gz")
+echo "\nReorganizing files to Level $newLevel from: $sourcePath This could take a while...\n";
+//$consoleTools = new ConsoleTools();
+foreach ($objects as $filestoprocess => $nzbFile) {
+	if ($nzbFile->getExtension() != "gz") {
 		continue;
-	$consoleTools->overWrite("Getting filecount: ".$iFilesCounted++." nzbs  ");
-}
-$time1 = TIME();
+	}
 
-echo "\nReorganizing files to Level ".$newLevel." from: ".$sourcePath." This could take a while...\n";
-$consoleTools = new ConsoleTools();
-foreach($objects as $filestoprocess => $nzbFile)
-{
-	if($nzbFile->getExtension() != "gz")
-		continue;
-
-	$newFileName = $nzb->getNZBPath(str_replace(".nzb.gz", "", $nzbFile->getBasename()), $newLevel, true);
-	if ($newFileName != $nzbFile)
-	{
+	$newFileName = $nzb->getNZBPath(str_replace(".nzb.gz", "", $nzbFile->getBasename()),
+									$newLevel,
+									true);
+	if ($newFileName != $nzbFile) {
 		rename($nzbFile, $newFileName);
 		chmod($newFileName, 0777);
 	}
 	$iFilesProcessed++;
-	$est = (int)((TIME() -$time1)/$iFilesProcessed*($iFilesCounted-$iFilesProcessed)/60);
-	$perc = (int)($iFilesProcessed/$iFilesCounted*100);
-	$consoleTools->overWrite("Reorganized ".$iFilesProcessed."/".$iFilesCounted."(".$perc."%) - est. ".$est." minutes  ");
+	if ($iFilesProcessed % 100 == 0) {
+		$consoleTools->overWrite("Reorganized $iFilesProcessed");
+	}
 }
 
-$db = new DB();
+$db->ping(true);
 $db->queryExec(sprintf("UPDATE settings SET value = %s WHERE setting = 'nzbsplitlevel'", $argv[1]));
-exit("Processed ".$iFilesProcessed." nzbs in ".relativeTime($time)."\n");
+$consoleTools->overWrite("Processed $iFilesProcessed nzbs in " . relativeTime($time) . "\n");
 
-function relativeTime($_time)
+function relativeTime ($_time)
 {
-	$d[0] = array(1,"sec");
-	$d[1] = array(60,"min");
-	$d[2] = array(3600,"hr");
-	$d[3] = array(86400,"day");
-	$d[4] = array(31104000,"yr");
+	$d[0] = array(1, "sec");
+	$d[1] = array(60, "min");
+	$d[2] = array(3600, "hr");
+	$d[3] = array(86400, "day");
+	$d[4] = array(31104000, "yr");
 
 	$w = array();
 
-	$return = "";
-	$now = TIME();
-	$diff = ($now-$_time);
+	$return      = "";
+	$now         = TIME();
+	$diff        = ($now - $_time);
 	$secondsLeft = $diff;
 
-	for($i=4;$i>-1;$i--)
-	{
-		$w[$i] = intval($secondsLeft/$d[$i][0]);
-		$secondsLeft -= ($w[$i]*$d[$i][0]);
-		if($w[$i]!=0)
-			$return.= $w[$i]. " " . $d[$i][1] . (($w[$i]>1)?'s':'') ." ";
+	for ($i = 4; $i > -1; $i--) {
+		$w[$i] = intval($secondsLeft / $d[$i][0]);
+		$secondsLeft -= ($w[$i] * $d[$i][0]);
+		if ($w[$i] != 0)
+			$return .= $w[$i] . " " . $d[$i][1] . (($w[$i] > 1) ? 's' : '') . " ";
 	}
 	return $return;
 }
