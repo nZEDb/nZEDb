81178571b2e930e90e111004337d48ce6b7cf164
ruhllatio <ruhllatio@gmail.com>
2014-05-06 20:45:09 -0400

    Add filename to predb / ishashed to releasefiles

---
+++ resources/db/patches/mysql/0221~predb.sql
@@ -0,0 +1,10 @@
+ALTER TABLE predb ADD COLUMN filename varchar(255) NOT NULL DEFAULT '';
+ALTER TABLE releasefiles ADD COLUMN ishashed tinyint(1) NOT NULL DEFAULT 0 AFTER size;
+
+DROP TRIGGER IF EXISTS check_rfinsert;
+DROP TRIGGER IF EXISTS check_rfupdate;
+
+CREATE TRIGGER check_rfinsert BEFORE INSERT ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;
+CREATE TRIGGER check_rfupdate BEFORE UPDATE ON releasefiles FOR EACH ROW BEGIN IF NEW.name REGEXP '[a-fA-F0-9]{32}' THEN SET NEW.ishashed = 1; END IF; END;
+
+UPDATE releasefiles SET ishashed = 1 WHERE name REGEXP '[a-fA-F0-9]{32}';
