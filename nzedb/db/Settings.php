22145cd2375fbfab18a07a884b61ddf5fce30fe5
niel <niel.archer@gmail.com>
2014-05-08 17:48:29 +0100

    Flipping logic so site/settings table check defaults to site. Should help with failed patch 210s.

--- nzedb/db/Settings.php
+++ nzedb/db/Settings.php
@@ -27,7 +27,7 @@ use nzedb\utility\Utility;
 
 class Settings extends DB
 {
-	private $table = 'settings';
+	private $table = 'site';
 
 	public function __construct (array $options = array())
 	{
@@ -35,7 +35,7 @@ class Settings extends DB
 		$table  = $this->table;
 		$result = self::queryExec("describe $table");
 		if ($result === false || empty($result)) {
-			$this->table = 'site';
+			$this->table = 'settings';
 		}
 		return self::$pdo;
 	}
