8ec23104a23e7009008b477ce2bfced4ab540a7f
Darko <dkrisan@gmail.com>
2014-07-02 16:04:55 +0200

    Missed one edit

--- www/pages/profileedit.php
+++ www/pages/profileedit.php
@@ -66,6 +66,7 @@ switch ($action) {
 					$data["invites"],
 					(isset($_POST['movieview']) ? "1" : "0"),
 					(isset($_POST['musicview']) ? "1" : "0"),
+					(isset($_POST['gameview']) ? "1" : "0"),
 					(isset($_POST['consoleview']) ? "1" : "0"),
 					(isset($_POST['bookview']) ? "1" : "0"),
 					$_POST['cp_url'],
