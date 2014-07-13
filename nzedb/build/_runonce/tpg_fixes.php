f7bd323bef54292f75e1449263bdcd7e2dcb5ce4
kevinlekiller <kevinlekiller@gmail.com>
2014-07-13 08:32:46 -0400

    Mod tpg_fixes.

--- nzedb/build/_runonce/tpg_fixes.php
+++ nzedb/build/_runonce/tpg_fixes.php
@@ -33,15 +33,28 @@ $groups = $pdo->queryDirect('SELECT id FROM groups WHERE active = 1 OR backfill 
 if ($groups === false) {
 	echo "No active groups. Fix not needed.\n";
 } else {
-	$sql  = "ALTER TABLE %s CHANGE COLUMN groupid group_id INT (10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'FK to groups'";
-	$sql2 = "ALTER TABLE binaries_%s ADD COLUMN currentparts INT UNSIGNED NOT NULL DEFAULT '0' AFTER totalparts";
-	$sql3 = "UPDATE binaries_%s b SET currentparts = (SELECT COUNT(*) FROM parts_%s p WHERE p.binaryid = b.id)";
+	$query1 = "ALTER TABLE %s CHANGE COLUMN groupid group_id INT (10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'FK to groups'";
+	$query2 = "ALTER TABLE binaries_%s ADD COLUMN currentparts INT UNSIGNED NOT NULL DEFAULT '0' AFTER totalparts";
+	$query3 = "UPDATE binaries_%s b SET currentparts = (SELECT COUNT(*) FROM parts_%s p WHERE p.binaryid = b.id)";
+
+	$query4[] = "ALTER TABLE parts_%s DROP INDEX ix_parts_messageid";
+	$query4[] = "ALTER TABLE parts_%s DROP INDEX ix_parts_number";
+	$query4[] = "ALTER TABLE parts_%s ADD COLUMN collection_id INT(11) UNSIGNED NOT NULL DEFAULT '0'";
+	$query4[] = "ALTER TABLE parts_%s ADD INDEX ix_parts_collection_id(collection_id)";
+	$query4[] = "DROP TRIGGER IF EXISTS delete_collections_%s";
+
+	$query5 = "CREATE TRIGGER delete_collections_%s AFTER DELETE ON collections_%s FOR EACH ROW BEGIN DELETE FROM binaries_%s WHERE collectionid = OLD.id; DELETE FROM parts_%s WHERE collection_id = OLD.id; END";
+
 	foreach ($groups as $group) {
 		echo 'Fixing group ' . $group['id'] . PHP_EOL;
-		$pdo->queryExec(sprintf($sql2, $group['id']), true);
-		$pdo->queryExec(sprintf($sql3, $group['id'], $group['id']), true);
-		$pdo->queryExec(sprintf($sql, 'collections_' . $group['id']), true);
-		$pdo->queryExec(sprintf($sql, 'partrepair_' . $group['id']), true);
+		$pdo->queryExec(sprintf($query1, 'collections_' . $group['id']), true);
+		$pdo->queryExec(sprintf($query1, 'partrepair_' . $group['id']), true);
+		$pdo->queryExec(sprintf($query2, $group['id']), true);
+		$pdo->queryExec(sprintf($query3, $group['id'], $group['id']), true);
+		foreach ($query4 as $singleQuery) {
+			$pdo->queryExec(sprintf($singleQuery, $group['id']), true);
+		}
+		$pdo->queryExec(sprintf($query5, $group['id'], $group['id'], $group['id'], $group['id']), true);
 		echo 'Finished fixing group ' . $group['id'] . PHP_EOL;
 	}
 	echo 'All done!' . PHP_EOL;
