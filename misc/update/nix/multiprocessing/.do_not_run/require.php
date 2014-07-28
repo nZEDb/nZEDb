44d5a549bf8a0474432dad7193f9b6911ffbaba8
niel <niel.archer@gmail.com>
2014-07-28 10:42:49 +0100

    Fixing code style and paths.

--- misc/update/nix/multiprocessing/.do_not_run/require.php
+++ misc/update/nix/multiprocessing/.do_not_run/require.php
@@ -1,6 +1,7 @@
 <?php
-$path = dirname(__FILE__) . '/../';
-require($path . '../../config.php');
-if (is_file($path . 'settings.php')) {
-	require_once($path . 'settings.php');
+require_once dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'config.php';
+
+if (!is_file(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'settings.php')) {
+	exit("Cannot run multiprocessing, no settings file found!");
 }
+require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . 'settings.php';
