bad241c2a332b6bfd2410246c9025232e43fabb2
DariusIII <dkrisan@gmail.com>
2014-07-31 22:51:03 +0200

    Fixed some problems i created.

--- www/pages/profileedit.php
+++ www/pages/profileedit.php
@@ -67,7 +67,6 @@ switch ($action) {
 					(isset($_POST['movieview']) ? "1" : "0"),
 					(isset($_POST['xxxview']) ? "1" : "0"),
 					(isset($_POST['musicview']) ? "1" : "0"),
-					(isset($_POST['gameview']) ? "1" : "0"),
 					(isset($_POST['consoleview']) ? "1" : "0"),
 					(isset($_POST['gameview']) ? "1" : "0"),
 					(isset($_POST['bookview']) ? "1" : "0"),
