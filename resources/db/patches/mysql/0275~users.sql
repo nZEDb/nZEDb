ea6c4b9b4390715bf02454069ea1e3608eb6d731
niel <niel.archer@gmail.com>
2014-08-12 01:35:41 +0100

    Converting use of 'userid' to 'user_id'.

---
+++ resources/db/patches/mysql/0275~users.sql
@@ -0,0 +1,5 @@
+ALTER TABLE forumpost CHANGE COLUMN userid user_id INT(11) UNSIGNED NOT NULL COMMENT 'FK to users';
+ALTER TABLE userinvite CHANGE COLUMN userid user_id INT(11) UNSIGNED NOT NULL COMMENT 'FK to users';
+ALTER TABLE usermovies CHANGE COLUMN userid user_id INT(11) UNSIGNED NOT NULL COMMENT 'FK to users';
+ALTER TABLE userrequests CHANGE COLUMN userid user_id INT(11) UNSIGNED NOT NULL COMMENT 'FK to users';
+ALTER TABLE userseries CHANGE COLUMN userid user_id INT(11) UNSIGNED NOT NULL COMMENT 'FK to users';
