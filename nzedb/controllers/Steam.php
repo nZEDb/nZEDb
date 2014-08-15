d236ce7d44147b646c943d01e26da6f60ac8f789
tasky <sexcnerd@gmail.com>
2014-08-15 23:23:46 +0200

    phpDoc Block changed

--- nzedb/controllers/Steam.php
+++ nzedb/controllers/Steam.php
@@ -298,8 +298,8 @@ Class Steam
 									preg_match('/\/app\/(?<id>\d+)\//', $result->href, $matches);
 									$this->_steamGameID = $matches['id'];
 									$this->_directURL = self::GAMEURL . $this->_steamGameID . '/';
-									$this->_getURL($result->href);
-									$this->ageCheck();
+									@$this->_getURL($result->href);
+									@$this->ageCheck();
 									break;
 								} else {
 									return false;
@@ -373,17 +373,17 @@ Class Steam
 					"ageYear" => "1966"
 				);
 				// Do twice so steam can set a birthtime/lastagecheckage cookie value
-				$this->_getURL(self::AGECHECKURL . $this->_steamGameID . '/', true);
-				$this->_getURL(self::AGECHECKURL . $this->_steamGameID . '/', true);
+				@$this->_getURL(self::AGECHECKURL . $this->_steamGameID . '/', true);
+				@$this->_getURL(self::AGECHECKURL . $this->_steamGameID . '/', true);
 			}
 		}
-		$this->_getURL(self::GAMEURL . $this->_steamGameID . '/');
+		@$this->_getURL(self::GAMEURL . $this->_steamGameID . '/');
 	}
 
 	/**
 	 * Gets Raw Html
 	 *
-	 * @param null $fetchURL
+	 * @param string $fetchURL
 	 * @param bool $usePost
 	 *
 	 * @return bool
