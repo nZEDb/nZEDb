21b1b3116c3f1054e36f0f1447307139b3cf39c7
niel <niel.archer@gmail.com>
2014-08-17 21:34:38 +0100

    Removing the DROP platform patch, as it would potentially lose user's data without any real gain.

--- resources/db/patches/mysql/+1~gamesinfo.sql
+++ resources/db/patches/mysql/+1~gamesinfo.sql
@@ -1,4 +1,3 @@
-ALTER TABLE gamesinfo DROP platform;
 ALTER TABLE gamesinfo ADD COLUMN backdrop TINYINT(1) DEFAULT 0 AFTER cover;
 ALTER TABLE gamesinfo ADD COLUMN trailer VARCHAR(1000) DEFAULT '' AFTER backdrop;
 ALTER TABLE gamesinfo ADD COLUMN classused VARCHAR(10) DEFAULT 'steam' AFTER trailer;
