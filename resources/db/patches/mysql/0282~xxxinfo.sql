8b264a6387c441940b8eb00d0040212a80c15bab
kevinlekiller <kevinlekiller@gmail.com>
2014-08-22 17:15:39 -0400

    Fix issue with sql patch file left over.

--- resources/db/patches/mysql/0282~xxxinfo.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE xxxinfo DROP INDEX ix_xxxinfo_title;
-
-ALTER IGNORE TABLE xxxinfo ADD UNIQUE INDEX ix_xxxinfo_title (title);
-
-UPDATE releases SET xxxinfo_id = 0 WHERE categoryid BETWEEN 6000 AND 6999 AND xxxinfo_id > 0 AND xxxinfo_id NOT IN (SELECT id FROM xxxinfo);
