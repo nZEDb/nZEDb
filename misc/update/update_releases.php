44a3e3d2c968499de65ac9059c0589361fbf9c18
ruhllatio <ruhllatio@gmail.com>
2014-08-26 15:48:02 -0400

    Attempt fix for consoleinfoid not setting for release and fix namespace issue in update_releases

--- misc/update/update_releases.php
+++ misc/update/update_releases.php
@@ -1,6 +1,8 @@
 <?php
 require_once dirname(__FILE__) . '/config.php';
 
+use \nzedb\processing\post\ProcessReleases;
+
 $pdo = new \nzedb\db\Settings();
 
 if (isset($argv[2]) && $argv[2] === 'true') {
