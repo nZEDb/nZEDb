8c276dd3bd7c3a2b546bd1c9f48523fe93baf92d
tasky <sexcnerd@gmail.com>
2014-08-31 20:06:34 +0200

    Match "Private" Series Regex added

--- nzedb/controllers/XXX.php
+++ nzedb/controllers/XXX.php
@@ -600,7 +600,8 @@ class XXX
 				$name = preg_replace('/\(.*?\)|[-._]/i', ' ', $name);
 				// Finally remove multiple spaces and trim leading spaces.
 				$name = trim(preg_replace('/\s{2,}/', ' ', $name));
-
+				// Remove Private Movies {d} from name better matching
+				$name = trim(preg_replace('/^Private\s(Specials|Blockbusters|Sports|Gold|Lesbian|Movies)\s\d+/i', '', $name));
 
 				// Check if the name is long enough and not just numbers and not file (d) of (d) and does not contain Episodes and any dated 00.00.00 which are site rips..
 				if (strlen($name) > 5 && !preg_match('/^\d+$/', $name) && !preg_match('/( File \d+ of \d+|\d+.\d+.\d+)/',$name) && !preg_match('/(E\d+)/',$name) && !preg_match('/\d\d\.\d\d.\d\d/', $name)) {
