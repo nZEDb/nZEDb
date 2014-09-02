2c0222af5333e77c98108d1615cd3403f0ef3943
tasky <sexcnerd@gmail.com>
2014-09-02 22:11:13 +0200

    Change xxxinfo actors field

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -441,7 +441,7 @@ CREATE TABLE         xxxinfo (
   plot        BLOB                           NULL DEFAULT NULL,
   genre       VARCHAR(64)                    NOT NULL,
   director    VARCHAR(64)                    DEFAULT NULL,
-  actors      VARCHAR(2000)                  NOT NULL,
+  actors      VARCHAR(2500)                  NOT NULL,
   extras      TEXT                           DEFAULT NULL,
   productinfo TEXT                           DEFAULT NULL,
   trailers    TEXT                           DEFAULT NULL,
@@ -451,8 +451,8 @@ CREATE TABLE         xxxinfo (
   backdrop    TINYINT(1) UNSIGNED            NOT NULL DEFAULT '0',
   createddate DATETIME                       NOT NULL,
   updateddate DATETIME                       NOT NULL,
-  PRIMARY KEY                   (id),
-  UNIQUE INDEX ix_xxxinfo_title (title)
+  PRIMARY KEY                      (id),
+  INDEX        ix_xxxinfo_title  (title)
 )
   ENGINE          = MYISAM
   DEFAULT CHARSET = utf8
