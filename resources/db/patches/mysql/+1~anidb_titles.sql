09fb052dca64faa0c6f30c6f39532c1e6ba0ba48
niel <niel.archer@gmail.com>
2014-09-03 19:34:39 +0100

    rename the anime patch to anime_titles.

--- resources/db/patches/mysql/+1~anidb_titles.sql
+++ resources/db/patches/mysql/+1~anidb_titles.sql
@@ -1,10 +1,10 @@
 DROP TABLE IF EXISTS animetitles;
 DROP TABLE IF EXISTS anidb;
 
-CREATE TABLE anidb (
-  id INT(10) UNSIGNED NOT NULL COMMENT 'ID from anidb site, also the ID other tables use as FK',
+CREATE TABLE anidb_titles (
+  anidbid INT(10) UNSIGNED NOT NULL COMMENT 'Main ID from AniDB for title',
   type VARCHAR(25) COLLATE utf8_unicode_ci NOT NULL COMMENT 'type of title.',
   lang VARCHAR(25) COLLATE utf8_unicode_ci NOT NULL,
   title VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL,
-  PRIMARY KEY (anidb_id, type, lang, title)
+  PRIMARY KEY (anidbid, type, lang, title)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
