615b68b1ab90c5387ee4f5593e3a6a6136acf42a
niel <niel.archer@gmail.com>
2014-09-03 19:48:55 +0100

    Replacing isset check on $argv with more appropriate check of $argc

--- misc/testing/PostProc/populate_anidb.php
+++ misc/testing/PostProc/populate_anidb.php
@@ -1,14 +1,12 @@
 <?php
 require_once dirname(__FILE__) . '/../../../www/config.php';
 
-use nzedb\db\Settings;
+use \nzedb\db\Settings;
 
 $pdo = new Settings();
 
-if (isset($argv[1]) && $argv[1] == 'true') {
-
+if ($argc > 1 && $argv[1] == true) {
 	(new \nzedb\db\populate\AniDB(['Settings' => $pdo, 'Echo' => true]))->populateTable('full');
-
 } else {
 	$pdo->log->doEcho(PHP_EOL . $pdo->log->error(
 				"To execute this script you must provide a boolean argument." . PHP_EOL .
