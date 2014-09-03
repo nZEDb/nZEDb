a5ccdb55616484f0122417bb0c17c82b1916b2cd
niel <niel.archer@gmail.com>
2014-09-03 04:58:14 +0100

    Correct patches usage of anidb.anidb_id to anidb.id

--- resources/db/patches/mysql/+3~anidb_episodes.sql
+++ resources/db/patches/mysql/+3~anidb_episodes.sql
@@ -1,7 +1,7 @@
 DROP TABLE IF EXISTS anidb_episodes;
 
 CREATE TABLE anidb_episodes (
-  anidb_id INT(10) UNSIGNED NOT NULL COMMENT 'anidb id for main title',
+  anidb_id INT(10) UNSIGNED NOT NULL COMMENT 'FK for main title',
   episode_id INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'anidb id for this episode',
   episode_no SMALLINT(5) UNSIGNED NOT NULL COMMENT 'Numeric version of episode (leave 0 for combined episodes).',
   episode_title VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the episode (en, x-jat)',
