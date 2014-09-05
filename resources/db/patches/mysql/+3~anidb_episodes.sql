d45e55ee62b079bb3aff900b5706ec7fefcc111c
niel <niel.archer@gmail.com>
2014-09-05 04:05:14 +0100

    Fixing invalid default value and index column.

--- resources/db/patches/mysql/+3~anidb_episodes.sql
+++ resources/db/patches/mysql/+3~anidb_episodes.sql
@@ -5,6 +5,6 @@ CREATE TABLE anidb_episodes (
   episodeid INT(10) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'anidb id for this episode',
   episode_no SMALLINT(5) UNSIGNED NOT NULL COMMENT 'Numeric version of episode (leave 0 for combined episodes).',
   episode_title VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the episode (en, x-jat)',
-  airdate date NOT NULL DEFAULT '',
-  PRIMARY KEY (anidbid, episode_id)
+  airdate date NOT NULL,
+  PRIMARY KEY (anidbid, episodeid)
 ) DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
