c7934575ea9266cb2fe95e296928bb10d9bca3d4
niel <niel.archer@gmail.com>
2014-10-05 17:52:13 +0100

    Changing name of menu table to menu_items.

--- nzedb/controllers/Menu.php
+++ nzedb/controllers/Menu.php
@@ -28,7 +28,7 @@ class Menu
 			$guest .= sprintf(" AND role != %d ", \Users::ROLE_ADMIN);
 		}
 
-		$data = $this->pdo->query(sprintf("SELECT * FROM menu WHERE role <= %d %s ORDER BY ordinal", $role, $guest));
+		$data = $this->pdo->query(sprintf("SELECT * FROM menu_items WHERE role <= %d %s ORDER BY ordinal", $role, $guest));
 
 		$ret = array();
 		foreach ($data as $d) {
@@ -44,26 +44,26 @@ class Menu
 
 	public function getAll()
 	{
-		return $this->pdo->query("SELECT * FROM menu ORDER BY role, ordinal");
+		return $this->pdo->query("SELECT * FROM menu_items ORDER BY role, ordinal");
 	}
 
 	public function getById($id)
 	{
-		return $this->pdo->queryOneRow(sprintf("SELECT * FROM menu WHERE id = %d", $id));
+		return $this->pdo->queryOneRow(sprintf("SELECT * FROM menu_items WHERE id = %d", $id));
 	}
 
 	public function delete($id)
 	{
-		return $this->pdo->queryExec(sprintf("DELETE FROM menu WHERE id = %d", $id));
+		return $this->pdo->queryExec(sprintf("DELETE FROM menu_items WHERE id = %d", $id));
 	}
 
 	public function add($menu)
 	{
-		return $this->pdo->queryInsert(sprintf("INSERT INTO menu (href, title, tooltip, role, ordinal, menueval, newwindow ) VALUES (%s, %s,  %s, %d, %d, %s, %d)", $this->pdo->escapeString($menu["href"]), $this->pdo->escapeString($menu["title"]), $this->pdo->escapeString($menu["tooltip"]), $menu["role"], $menu["ordinal"], $this->pdo->escapeString($menu["menueval"]), $menu["newwindow"]));
+		return $this->pdo->queryInsert(sprintf("INSERT INTO menu_items (href, title, tooltip, role, ordinal, menueval, newwindow ) VALUES (%s, %s,  %s, %d, %d, %s, %d)", $this->pdo->escapeString($menu["href"]), $this->pdo->escapeString($menu["title"]), $this->pdo->escapeString($menu["tooltip"]), $menu["role"], $menu["ordinal"], $this->pdo->escapeString($menu["menueval"]), $menu["newwindow"]));
 	}
 
 	public function update($menu)
 	{
-		return $this->pdo->queryExec(sprintf("UPDATE menu SET href = %s, title = %s, tooltip = %s, role = %d, ordinal = %d, menueval = %s, newwindow = %d WHERE id = %d", $this->pdo->escapeString($menu["href"]), $this->pdo->escapeString($menu["title"]), $this->pdo->escapeString($menu["tooltip"]), $menu["role"], $menu["ordinal"], $this->pdo->escapeString($menu["menueval"]), $menu["newwindow"], $menu["id"]));
+		return $this->pdo->queryExec(sprintf("UPDATE menu_items SET href = %s, title = %s, tooltip = %s, role = %d, ordinal = %d, menueval = %s, newwindow = %d WHERE id = %d", $this->pdo->escapeString($menu["href"]), $this->pdo->escapeString($menu["title"]), $this->pdo->escapeString($menu["tooltip"]), $menu["role"], $menu["ordinal"], $this->pdo->escapeString($menu["menueval"]), $menu["newwindow"], $menu["id"]));
 	}
 }
