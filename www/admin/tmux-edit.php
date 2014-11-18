92171d365af323ef3b2a6b3cf80e9830bdcbc06e
niel <niel.archer@gmail.com>
2014-11-18 23:41:19 +0000

    Changes to RemoveCrap WMV method. Removing from ALL as it's too wide and many will be using All as their default.
    Conflicts:
    	misc/testing/Release/removeCrapReleases.php
    	nzedb/build/nZEDb.xml
    	nzedb/controllers/ReleaseRemover.php
    	www/admin/tmux-edit.php

--- www/admin/tmux-edit.php
+++ www/admin/tmux-edit.php
@@ -25,33 +25,33 @@ switch($action) {
 		break;
 }
 
-$page->smarty->assign('yesno_ids', array(1, 0));
-$page->smarty->assign('yesno_names', array('yes', 'no'));
+$page->smarty->assign('yesno_ids', [1, 0]);
+$page->smarty->assign('yesno_names', ['yes', 'no']);
 
-$page->smarty->assign('backfill_ids', array(0,4,2,1));
-$page->smarty->assign('backfill_names', array('Disabled', 'Safe', 'Group', 'All'));
-$page->smarty->assign('backfill_group_ids', array(1,2,3,4,5,6));
-$page->smarty->assign('backfill_group', array('Newest', 'Oldest', 'Alphabetical', 'Alphabetical - Reverse', 'Most Posts', 'Fewest Posts'));
-$page->smarty->assign('backfill_days', array('Days per Group', 'Safe Backfill day'));
-$page->smarty->assign('backfill_days_ids', array(1,2));
-$page->smarty->assign('dehash_ids', array(0,1,2,3));
-$page->smarty->assign('dehash_names', array('Disabled', 'Decrypt Hashes', 'Predb', 'All'));
-$page->smarty->assign('import_ids', array(0,1,2));
-$page->smarty->assign('import_names', array('Disabled', 'Import - Do Not Use Filenames', 'Import - Use Filenames'));
-$page->smarty->assign('releases_ids', array(0,1,2));
-$page->smarty->assign('releases_names', array('Disabled', 'Update Releases', 'Update Releases Threaded'));
-$page->smarty->assign('post_ids', array(0,1,2,3));
-$page->smarty->assign('post_names', array('Disabled', 'PostProcess Additional', 'PostProcess NFOs', 'All'));
-$page->smarty->assign('fix_crap_radio_ids', array('Disabled', 'All', 'Custom'));
-$page->smarty->assign('fix_crap_radio_names', array('Disabled', 'All', 'Custom'));
-$page->smarty->assign('fix_crap_check_ids', array('blacklist', 'blfiles', 'executable', 'gibberish', 'hashed', 'installbin', 'passworded', 'passwordurl', 'sample', 'scr', 'short', 'size', 'huge', 'codec', 'wmv'));
-$page->smarty->assign('fix_crap_check_names', array('blacklist', 'blfiles', 'executable', 'gibberish', 'hashed', 'installbin', 'passworded', 'passwordurl', 'sample', 'scr', 'short', 'size', 'huge', 'codec', 'wmv'));
-$page->smarty->assign('sequential_ids', array(0,1,2));
-$page->smarty->assign('sequential_names', array('Disabled', 'Basic Sequential', 'Complete Sequential'));
-$page->smarty->assign('binaries_ids', array(0,1,2));
-$page->smarty->assign('binaries_names', array('Disabled', 'Simple Threaded Update', 'Complete Threaded Update'));
-$page->smarty->assign('post_non_ids', array(0,1,2));
-$page->smarty->assign('post_non_names', array('Disabled', 'All Available Releases', 'Properly Renamed Releases'));
+$page->smarty->assign('backfill_ids', [0,4,2,1]);
+$page->smarty->assign('backfill_names', ['Disabled', 'Safe', 'Group', 'All']);
+$page->smarty->assign('backfill_group_ids', [1,2,3,4,5,6]);
+$page->smarty->assign('backfill_group', ['Newest', 'Oldest', 'Alphabetical', 'Alphabetical - Reverse', 'Most Posts', 'Fewest Posts']);
+$page->smarty->assign('backfill_days', ['Days per Group', 'Safe Backfill day']);
+$page->smarty->assign('backfill_days_ids', [1,2]);
+$page->smarty->assign('dehash_ids', [0,1,2,3]);
+$page->smarty->assign('dehash_names', ['Disabled', 'Decrypt Hashes', 'Predb', 'All']);
+$page->smarty->assign('import_ids', [0,1,2]);
+$page->smarty->assign('import_names', ['Disabled', 'Import - Do Not Use Filenames', 'Import - Use Filenames']);
+$page->smarty->assign('releases_ids', [0,1,2]);
+$page->smarty->assign('releases_names', ['Disabled', 'Update Releases', 'Update Releases Threaded']);
+$page->smarty->assign('post_ids', [0,1,2,3]);
+$page->smarty->assign('post_names', ['Disabled', 'PostProcess Additional', 'PostProcess NFOs', 'All']);
+$page->smarty->assign('fix_crap_radio_names', ['Disabled', 'All (except wmv_all)', 'Custom']);
+$page->smarty->assign('fix_crap_check_names', ['blacklist', 'blfiles', 'codec', 'executable', 'gibberish', 'hashed',
+											   'huge', 'installbin', 'passworded', 'passwordurl',
+											   'sample', 'scr', 'short', 'size', 'wmv_all']);
+$page->smarty->assign('sequential_ids', [0,1,2]);
+$page->smarty->assign('sequential_names', ['Disabled', 'Basic Sequential', 'Complete Sequential']);
+$page->smarty->assign('binaries_ids', [0,1,2]);
+$page->smarty->assign('binaries_names', ['Disabled', 'Simple Threaded Update', 'Complete Threaded Update']);
+$page->smarty->assign('post_non_ids', [0,1,2]);
+$page->smarty->assign('post_non_names', ['Disabled', 'All Available Releases', 'Properly Renamed Releases']);
 
 $page->content = $page->smarty->fetch('tmux-edit.tpl');
 $page->render();
