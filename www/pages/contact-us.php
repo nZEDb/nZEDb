c9e388243205b745cd5a1f70410abc987a0fee2b
KingCat <kingcat@nzb.cat>
2014-11-22 00:05:21 -0500

    Setting the contact-us page to use the global email method.

--- www/pages/contact-us.php
+++ www/pages/contact-us.php
@@ -1,11 +1,13 @@
 <?php
+
+use \nzedb\utility\Utility;
+
 if (isset($_POST["useremail"])) {
 	// Send the contact info and report back to user.
 	$email = $_POST["useremail"];
 	$mailto = $page->settings->getSetting('email');
 
 	$mailsubj = "Contact Form Submitted";
-	$mailhead = "From: $email\n";
 	$mailbody = "Values submitted from contact form:\n";
 
 	while (list ($key, $val) = each($_POST)) {
@@ -13,7 +15,7 @@ if (isset($_POST["useremail"])) {
 	}
 
 	if (!preg_match("/\n/i", $_POST["useremail"])) {
-		@mail($mailto, $mailsubj, $mailbody, $mailhead);
+		Utility::sendEmail($mailto, $mailsubj, $mailbody, $email);
 	}
 
 	$page->smarty->assign("msg", "<h2 style='text-align:center;'>Thank you for getting in touch with " . $page->settings->getSetting('title') . ".</h2>");
