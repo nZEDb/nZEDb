00bac48bcd62b891df314ac15bf5dab6b6c7da32
niel <niel.archer@gmail.com>
2015-01-26 12:44:24 +0000

    Finish converting to use Predb class.

--- cli/data/predb_import_daily_batch.php
+++ cli/data/predb_import_daily_batch.php
@@ -3,17 +3,11 @@
 /* TODO better tune the queries for performance, including using prepared statements and
    pre-fetching group_id and other data for faster inclusion in the main query.
 */
+require_once dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'www' . DIRECTORY_SEPARATOR . 'config.php';
 
-use nzedb\db\PreDb;
-use nzedb\utility\Utility;
-
-$config = dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'www' . DIRECTORY_SEPARATOR .
-		  'config.php';
-if (!is_file($config)) {
-	exit('Place this script in the cli/data folder of nZEDb.' . PHP_EOL);
-}
-require_once $config;
-unset($config);
+use \nzedb\db\PreDb;
+use \nzedb\db\Settings;
+use \nzedb\utility\Utility;
 
 if (!Utility::isWin()) {
 	$fullPath = DS;
@@ -34,8 +28,6 @@ if (!is_writable(nZEDb_RES)) {
 	exit('The (' . nZEDb_RES . ') folder must be writable.' . PHP_EOL);
 }
 
-$progress = rw_progress(settings_array());
-
 if (!isset($argv[1]) || !is_numeric($argv[1]) && $argv[1] != 'progress' || !isset($argv[2]) ||
 	!in_array($argv[2], ['local', 'remote']) || !isset($argv[3]) ||
 	!in_array($argv[3], ['true', 'false'])
@@ -51,35 +43,41 @@ if (!isset($argv[1]) || !is_numeric($argv[1]) && $argv[1] != 'progress' || !isse
 		 'Argument 3: Show output of dump_predb.php or not, true | false' . PHP_EOL
 	);
 }
-$fileName         = '_predb_dump.csv.gz';
-$innerUrl         = 'fb2pffwwriruyco';
-$baseUrl          = 'https://www.dropbox.com/sh/' . $innerUrl;
-$folderUrl['url'] = $baseUrl . '/AACy9Egno_v2kcziVHuvWbbxa';
 
-$result = nzedb\utility\Utility::getUrl($folderUrl);
+$fileName = '_predb_dump.csv.gz';
+$innerUrl = 'fb2pffwwriruyco';
+$baseUrl  = 'https://www.dropbox.com/sh/' . $innerUrl;
+
+$result = nzedb\utility\Utility::getUrl(['url' => $baseUrl . '/AACy9Egno_v2kcziVHuvWbbxa']);
 
 if (!$result) {
 	exit('Error connecting to dropbox.com, try again later?' . PHP_EOL);
 }
 
-$result = preg_match_all('/<a href="https:\/\/www.dropbox.com\/sh\/' . $innerUrl . '\/(\S+\/\d+' .
-						 $fileName . '\?dl=0)"/',
-						 $result,
-						 $all_matches);
+Utility::clearScreen();
+
+$result = preg_match_all(
+	'/<a href="https:\/\/www.dropbox.com\/sh\/' . $innerUrl . '\/(\S+\/\d+' . $fileName . '\?dl=0)"/',
+	$result,
+	$links);
+
 if ($result) {
-	exec('clear');
-	$all_matches = array_unique($all_matches[1]);
-	$total       = count($all_matches);
-	$pdo         = new \nzedb\db\Settings();
+	$links		= array_unique($links[1]);
+	$total      = count($links);
+	$pdo        = new Settings();
+	$predb      = new PreDb();
+
+	$progress = $predb->progress(settings_array());
 
 	if ($argv[1] != 'progress') {
 		$progress['last'] = !is_numeric($argv[1]) ? time() : $argv[1];
 	}
 
-	$pdo->queryExec('TRUNCATE TABLE tmp_pre');
+	$predb->executeTruncate();
 
-	foreach ($all_matches as $matches) {
-		if (preg_match('#^(.+)/(\d+)_#', $matches, $match)) {
+	foreach ($links as $link)
+	{
+		if (preg_match('#^(.+)/(\d+)_#', $link, $match)) {
 			$timematch = -1 + $progress['last'];
 
 			// Skip patches the user does not want.
@@ -91,18 +89,16 @@ if ($result) {
 			}
 
 			// Download the dump.
-			$file['url'] = $baseUrl . '/' . $match[1] . '/' . $match[2] . $fileName . '?dl=1';
-			$dump        = nzedb\utility\Utility::getUrl($file);
+			$dump = Utility::getUrl(['url' => "$baseUrl/{$match[1]}/{$match[2]}$fileName?dl=1"]);
 
 			if (!$dump) {
-				echo 'Error downloading dump ' . $match[2] . ' you can try manually importing it.' .
-					 PHP_EOL;
+				echo "Error downloading dump {$match[2]} you can try manually importing it." .PHP_EOL;
 				continue;
 			}
 
 			// Make sure we didn't get an HTML page.
 			if (strlen($dump) < 5000 && strpos($dump, '<!DOCTYPE html>') !== false) {
-				echo 'The dump file ' . $match[2] . ' might be missing from dropbox.' . PHP_EOL;
+				echo "The dump file {$match[2]} might be missing from dropbox." . PHP_EOL;
 				continue;
 			}
 
@@ -110,7 +106,7 @@ if ($result) {
 			$dump = gzdecode($dump);
 
 			if (!$dump) {
-				echo 'Error decompressing dump ' . $match[2] . '.' . PHP_EOL;
+				echo "Error decompressing dump {$match[2]}." . PHP_EOL;
 				continue;
 			}
 
@@ -118,7 +114,7 @@ if ($result) {
 			$dumpFile = nZEDb_RES . $match[2] . '_predb_dump.csv';
 			$fetched  = file_put_contents($dumpFile, $dump);
 			if (!$fetched) {
-				echo 'Error storing dump file ' . $match[2] . ' in (' . nZEDb_RES . ').' . PHP_EOL;
+				echo "Error storing dump file {$match[2]} in (" . nZEDb_RES . ').' . PHP_EOL;
 				continue;
 			}
 
@@ -126,20 +122,19 @@ if ($result) {
 			chmod($dumpFile, 0777);
 			$local   = strtolower($argv[2]) == 'local' ? true : false;
 			$verbose = $argv[3] == true ? true : false;
+
 			importDump($dumpFile, $local, $verbose);
 
 			// Delete the dump.
 			unlink($dumpFile);
 
-			$progress = rw_progress(settings_array($match[2] + 1, $progress), false);
-			echo 'Successfully imported PreDB dump ' . $match[2] . ' ' . (--$total) .
+			$progress = $predb->progress(settings_array($match[2] + 1, $progress), false);
+			echo "Successfully imported PreDB dump {$match[2]} " . (--$total) .
 				 ' dumps remaining to import.' . PHP_EOL;
 		}
 	}
-
-	// Drop tmp_pre table
-	$pdo->queryExec('DROP TABLE IF EXISTS tmp_pre');
 }
+
 
 function settings_array($last = null, $settings = null)
 {
@@ -154,84 +149,36 @@ function settings_array($last = null, $settings = null)
 	return $settings;
 }
 
-function rw_progress($settings, $read = true)
-{
-	if (!$read || !is_file(__DIR__ . DS . 'predb_progress.txt')) {
-		file_put_contents(__DIR__ . DS . 'predb_progress.txt', base64_encode(serialize($settings)));
-	} else {
-		$settings = unserialize(base64_decode(file_get_contents(__DIR__ . DS .
-																'predb_progress.txt')));
-	}
-
-	return $settings;
-}
 
 // This function duplicates how dump_predb works but does not drop the hashes/triggers as recreating
 // potentially millions for the small addition that dailies add, isn't worth it.
-function importDump($path, $local, $verbose = true, $table = 'predb')
+function importDump($path, $local, $verbose = true)
 {
-	global $pdo;
+	global $pdo, $predb;
 
 	// Create temp table to allow updating
 	if ($verbose) {
-		echo $pdo->log->info("Creating temporary table");
+		echo $pdo->log->info("Clearing import table");
 	}
 
 	// Truncate to clear any old data
-	$pdo->queryDirect("TRUNCATE TABLE tmp_pre");
+	$predb->executeTruncate();
 
 	// Import file into tmp_pre
-	$sqlLoad = sprintf(
-		"LOAD DATA %s INFILE '%s' IGNORE INTO TABLE tmp_pre FIELDS TERMINATED BY '\\t\\t' ENCLOSED BY \"'\" LINES TERMINATED BY '\\r\\n' (title, nfo, size, files, filename, nuked, nukereason, category, predate, source, requestid, groupname);",
-		($local === false ? 'LOCAL' : ''),
-		$path
-	);
-	if (nZEDb_DEBUG) {
-		echo $pdo->log->header($sqlLoad);
-	}
-	$pdo->queryDirect($sqlLoad);
+	$predb->executeLoadData($path, $local);
 
 	// Remove any titles where length <=8
 	if ($verbose) {
 		echo $pdo->log->info("Deleting any records where title <=8 from Temporary Table");
 	}
-	$pdo->queryDirect("DELETE FROM tmp_pre WHERE LENGTH(title) <= 8");
+	$predb->executeDeleteShort();
 
 	// Add any groups that do not currently exist
-	$sqlAddGroups = <<<SQL_ADD_GROUPS
-INSERT IGNORE INTO groups (`name`, description)
-	SELECT groupname, 'Added by predb import script'
-	FROM tmp_pre AS t LEFT JOIN groups AS g ON t.`groupname` = g.`name`
-	WHERE t.`groupname` IS NOT NULL AND g.`name` IS NULL
-	GROUP BY groupname;
-SQL_ADD_GROUPS;
-
-	$pdo->queryDirect($sqlAddGroups);
+	$predb->executeAddGroups();
 
 	// Fill the group_id
-	$pdo->queryDirect("UPDATE tmp_pre AS t SET group_id = (SELECT id FROM groups WHERE name = t.groupname) WHERE groupname IS NOT NULL");
+	$predb->executeUpdateGroupID();
 
-	// Insert and update table
-	$sqlInsert = <<<SQL_INSERT
-INSERT INTO $table (title, nfo, size, files, filename, nuked, nukereason, category, predate, SOURCE, requestid, group_id)
-  SELECT t.title, t.nfo, t.size, t.files, t.filename, t.nuked, t.nukereason, t.category, t.predate, t.source, t.requestid, t.group_id
-    FROM tmp_pre AS t
-  ON DUPLICATE KEY UPDATE predb.nfo = IF(predb.nfo IS NULL, t.nfo, predb.nfo),
-	  predb.size = IF(predb.size IS NULL, t.size, predb.size),
-	  predb.files = IF(predb.files IS NULL, t.files, predb.files),
-	  predb.filename = IF(predb.filename = '', t.filename, predb.filename),
-	  predb.nuked = IF(t.nuked > 0, t.nuked, predb.nuked),
-	  predb.nukereason = IF(t.nuked > 0, t.nukereason, predb.nukereason),
-	  predb.category = IF(predb.category IS NULL, t.category, predb.category),
-	  predb.requestid = IF(predb.requestid = 0, t.requestid, predb.requestid),
-	  predb.group_id = IF(predb.group_id = 0, t.group_id, predb.group_id);
-SQL_INSERT;
-
-	echo $pdo->log->info("Inserting records from temporary table into $table");
-	if (nZEDb_DEBUG) {
-		echo $pdo->log->primary($sqlInsert);
-	}
-	if ($pdo->queryDirect($sqlInsert) === false) {
-		echo "FAILED\n";
-	}
+	echo $pdo->log->info("Inserting records from temporary table into predb table");
+	$predb->executeInsert();
 }
