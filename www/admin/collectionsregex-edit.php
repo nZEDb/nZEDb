42d406da187925c3181842dd4bd04e80338b20bd
kevin <kevinlekiller@gmail.com>
2015-02-21 18:06:02 -0500

    Add in the page for adding/editing collections regex.

---
+++ www/admin/collectionsregex-edit.php
@@ -0,0 +1,58 @@
+<?php
+require_once './config.php';
+
+$page = new AdminPage();
+$cc = new CollectionsCleaning(['Settings' => $page->settings]);
+
+// Set the current action.
+$action = isset($_REQUEST['action']) ? $_REQUEST['action'] : 'view';
+
+switch($action) {
+	case 'submit':
+		if ($_POST["group_regex"] == "") {
+			$page->smarty->assign('error', "Group regex must not be empty!");
+			break;
+		}
+
+		if ($_POST["regex"] == "") {
+			$page->smarty->assign('error', "Regex cannot be empty");
+			break;
+		}
+
+		if ($_POST['description'] == '') {
+			$_POST['description'] = '';
+		}
+
+		if (!is_numeric($_POST['ordinal']) || $_POST['ordinal'] < 0) {
+			$page->smarty->assign('error', "Ordinal must be a number, 0 or higher.");
+			break;
+		}
+
+		if ($_POST["id"] == "") {
+			$cc->addRegex($_POST);
+		} else {
+			$cc->updateRegex($_POST);
+		}
+
+		header("Location:".WWW_TOP."/collectionsregex-list.php");
+		break;
+
+	case 'view':
+	default:
+		if (isset($_GET["id"])) {
+			$page->title = "Collections Regex Edit";
+			$id = $_GET["id"];
+			$r = $cc->getRegexByID($id);
+		} else {
+			$page->title = "Collections Regex Add";
+			$r = ['status' => 1];
+		}
+		$page->smarty->assign('regex', $r);
+		break;
+}
+
+$page->smarty->assign('status_ids', array(Category::STATUS_ACTIVE,Category::STATUS_INACTIVE));
+$page->smarty->assign('status_names', array( 'Yes', 'No'));
+
+$page->content = $page->smarty->fetch('collectionsregex-edit.tpl');
+$page->render();
