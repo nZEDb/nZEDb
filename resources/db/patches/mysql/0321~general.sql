40c72e46b1cef231d5ca41a931f2c9abc666e9c2
niel <niel.archer@gmail.com>
2015-03-19 03:44:07 +0000

    Fixing sql patch 0321~general.sql

--- resources/db/patches/mysql/0321~general.sql
+++ resources/db/patches/mysql/0321~general.sql
@@ -1,22 +1,10 @@
-DELIMITER $$
-DROP PROCEDURE IF EXISTS alter_tables $$
--- Create the stored procedure to alter tables
-CREATE PROCEDURE alter_tables()
-  BEGIN
--- change to myisam if version < 5.6
-    select version() regexp '^[0-5]\.[0-5]' into @version;
-    IF @version = 1 THEN
-      ALTER TABLE bookinfo engine=myisam;
-      ALTER TABLE consoleinfo engine=myisam;
-      ALTER TABLE musicinfo engine=myisam;
-    END IF;
-
-  END $$
--- Execute the stored procedures
-CALL alter_tables() $$
--- Don't forget to drop the stored procedure when you're done!
-DROP PROCEDURE IF EXISTS alter_tables $$
-DELIMITER ;
+DROP PROCEDURE IF EXISTS alter_tables;
+/* Create the stored procedure to alter tables -- change to myisam if version < 5.6 */
+CREATE PROCEDURE alter_tables() BEGIN SELECT version() regexp '^[0-5]\.[0-5]' into @version; IF @version = 1 THEN ALTER TABLE bookinfo engine=myisam; ALTER TABLE consoleinfo engine=myisam; ALTER TABLE musicinfo engine=myisam; END IF; END;
+/* Execute the stored procedures */
+CALL alter_tables();
+/* Don't forget to drop the stored procedure when you're done! */
+DROP PROCEDURE IF EXISTS alter_tables;
 
 ALTER TABLE musicinfo ADD FULLTEXT INDEX ix_musicinfo_artist_title_ft (artist, title);
 ALTER TABLE bookinfo ADD FULLTEXT INDEX ix_bookinfo_author_title_ft (author, title);
