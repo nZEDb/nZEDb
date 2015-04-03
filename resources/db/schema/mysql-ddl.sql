06cfe4f27761a53f6c4d264718588c10a9f74c9b
DariusIII <dkrisan@gmail.com>
2015-04-03 10:32:54 +0200

    Fix the ddl.

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -302,21 +302,18 @@ CREATE TABLE countries (
   COLLATE = utf8_unicode_ci;
   
   
-DROP TABLE IF EXISTS failed_downloads;
-CREATE TABLE failed_downloads (
+DROP TABLE IF EXISTS dnzb_failures;
+CREATE TABLE dnzb_failures (
   id          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  guid        VARCHAR(50)      NOT NULL,
   userid      INT(11) UNSIGNED NOT NULL,
-  status      TINYINT NOT NULL DEFAULT 0,
+  guid        VARCHAR(50)      NOT NULL,
   PRIMARY KEY (id)
 )
   ENGINE =MYISAM
   DEFAULT CHARSET =utf8
   COLLATE =utf8_unicode_ci
   AUTO_INCREMENT =1;
-  CREATE INDEX ix_failed_downloads_guid ON failed_downloads (guid);
-  CREATE INDEX ix_failed_downloads_userid ON failed_downloads (userid);
-  CREATE UNIQUE INDEX ux_index_failed ON failed_downloads (guid, userid);
+  CREATE UNIQUE INDEX ux_dnzb_failures ON dnzb_failures (userid, guid);
 
 
 DROP TABLE IF EXISTS forum_posts;
