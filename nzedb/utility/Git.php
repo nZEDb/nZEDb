0b28d7d513984b3322a588ac233b595b75194dcc
niel <niel.archer@gmail.com>
2015-04-17 00:20:09 +0100

    Adding new method (isCommited) to Utility:Git() to determine if a git object (e.g. "dev:Changelog") exists in the repo already.

--- nzedb/utility/Git.php
+++ nzedb/utility/Git.php
@@ -74,6 +74,23 @@ class Git extends \GitRepo
 		return $this->branch;
 	}
 
+	public function isCommited($gitObject)
+	{
+		$cmd = "cat-file -e $gitObject";
+
+		try {
+			$result = $this->run($cmd);
+		} catch (\Exception $e) {
+			$message = explode("\n", $e->getMessage());
+			if ($message[0] === "fatal: Not a valid object name $gitObject") {
+				$result = false;
+			} else {
+				throw new \Exception($message);
+			}
+		}
+		return ($result === '');
+	}
+
 	public function log($options = null)
 	{
 		return $this->run("log $options");
