656ad8d4df0a76f51418d0db82874d192494325c
niel <niel.archer@gmail.com>
2015-04-17 00:22:15 +0100

    Use new Utility\Git::isCommited() method to check if sql patch file needs updating.

--- nzedb/db/DbUpdate.php
+++ nzedb/db/DbUpdate.php
@@ -179,7 +179,9 @@ class DbUpdate
 							   $matches['table'] . '.sql';
 					rename($matches[0], $newName);
 					$this->git->add($newName);
-					$this->git->run("add -uv {$matches[0]}"); // add updates to old filename, this should remove the temp file from the index.
+					if ($this->git->isCommited($matches[0])) {
+						$this->git->run("add -uv {$matches[0]}"); // add updates to old filename, this should remove the temp file from the index.
+					}
 				}
 			}
 		}
