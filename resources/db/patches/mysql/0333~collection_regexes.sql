7d87b3b53411f7c0ddc5c622ba95cf22a548147b
niel <niel.archer@gmail.com>
2015-05-09 16:14:56 +0100

    Updating Changelog.

---
+++ resources/db/patches/mysql/0333~collection_regexes.sql
@@ -0,0 +1,9 @@
+INSERT INTO collection_regexes (id, group_regex, regex, status, description, ordinal)
+VALUES (
+	585,
+	'^alt\\.binaries\\.fz$',
+	'#^(?P<match0>Uploader\\.Presents-.+?)\\s+\\[\\d{1,3}(?P<match1>/\\d{1,3}\\]\\s*").+?"\\s+yEnc$#i',
+	1,
+	'Uploader.Presents-Black.Sails.S02E02.Die.schwarze.Flagge.GERMAN.DUBBED.BLURAYRiP.x264-SOF [00/23]"sof-black-sails-s02e02.nzb" yEnc',
+	10
+);
