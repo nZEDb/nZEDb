6a5905ef884c4952419b990b8a0b12e84e09d734
niel <niel.archer@gmail.com>
2015-05-11 17:50:04 +0100

    Trying another possible fix for removing temporary sql patch files.

--- nzedb/db/DbUpdate.php
+++ nzedb/db/DbUpdate.php
@@ -180,7 +180,7 @@ class DbUpdate
 					rename($matches[0], $newName);
 					$this->git->add($newName);
 					if ($this->git->isCommited($this->git->getBranch() . ':' . $matches[0])) {
-						$this->git->run("add -uv {$matches[0]}"); // add updates to old filename, this should remove the temp file from the index.
+						$this->git->rm("{$matches[0]}"); // remove old filename from the index.
 					}
 				}
 			}
