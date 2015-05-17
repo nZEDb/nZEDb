c122239e913cfe219376e21a7908dd20baf17d31
niel <niel.archer@gmail.com>
2015-05-17 01:13:26 +0100

    Fixing namespace issues in newly merged script.

--- misc/testing/PostProcess/check_previews.php
+++ misc/testing/PostProcess/check_previews.php
@@ -4,23 +4,29 @@
 // --------------------------------------------------------------
 require_once dirname(__FILE__) . '/../../../www/config.php';
 
+use nzedb\ConsoleTools;
+use nzedb\NZB;
+use nzedb\ReleaseImage;
+use nzedb\Releases;
 use nzedb\db\Settings;
+use nzedb\utility\Utility;
 
 $pdo = new Settings();
 
 $row = $pdo->queryOneRow("SELECT value FROM settings WHERE setting = 'coverspath'");
 if ($row !== false) {
-        \nzedb\utility\Utility::setCoversConstant($row['value']);
+	Utility::setCoversConstant($row['value']);
 } else {
-        die("Unable to determine covers path!\n");
+	die("Unable to determine covers path!\n");
 }
+
 $path2preview = nZEDb_COVERS . 'preview' . DS;
 
 if (isset($argv[1]) && ($argv[1] === "true" || $argv[1] === "check")) {
-	$releases = new \Releases(['Settings' => $pdo]);
-	$nzb = new \NZB($pdo);
-	$releaseImage = new \ReleaseImage($pdo);
-	$consoletools = new \ConsoleTools(['ColorCLI' => $pdo->log]);
+	$releases = new Releases(['Settings' => $pdo]);
+	$nzb = new NZB($pdo);
+	$releaseImage = new ReleaseImage($pdo);
+	$consoletools = new ConsoleTools(['ColorCLI' => $pdo->log]);
 	$couldbe = $argv[1] === "true" ? $couldbe = "were " : "could be ";
 	$limit = $counterfixed = 0;
 	if (isset($argv[2]) && is_numeric($argv[2])) {
@@ -35,14 +41,14 @@ if (isset($argv[1]) && ($argv[1] === "true" || $argv[1] === "check")) {
 				$counterfixed++;
 				echo $pdo->log->warning("Missing preview " . $nzbpath);
 				if ($argv[1] === "true") {
-					$pdo->queryExec(sprintf("UPDATE releases SET consoleinfoid = NULL, gamesinfo_id = 0, imdbid = NULL, musicinfoid = NULL,
-									bookinfoid = NULL, rageid = -1, xxxinfo_id = 0, passwordstatus = -1, haspreview = -1,
-									jpgstatus = 0, videostatus = 0, audiostatus = 0, nfostatus = -1	
-								WHERE id = %s", $row['id']));
+					$pdo->queryExec(
+						sprintf("UPDATE releases SET consoleinfoid = NULL, gamesinfo_id = 0, imdbid = NULL, musicinfoid = NULL,	bookinfoid = NULL, rageid = -1, xxxinfo_id = 0, passwordstatus = -1, haspreview = -1, jpgstatus = 0, videostatus = 0, audiostatus = 0, nfostatus = -1 WHERE id = %s", $row['id']));
 				}
 			}
 
-		if (($limit > 0) && ($counterfixed >= $limit)) {break;}  // QUAD!
+			if (($limit > 0) && ($counterfixed >= $limit)) {
+				break;
+			} // QUAD!
 		}
 	}
 	echo $pdo->log->header("Total releases missing previews that " . $couldbe . "reset for reprocessing= " . number_format($counterfixed));
