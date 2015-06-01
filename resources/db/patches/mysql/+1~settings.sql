a6cc6a26d4a1f5e5ec3ffadfcf3172c1f2cb706e
niel <niel.archer@gmail.com>
2015-06-01 07:34:30 +0100

    Deleting temporary SQL patch file.

--- resources/db/patches/mysql/+1~settings.sql
+++
@@ -1,1 +0,0 @@
-INSERT INTO settings (section, subsection, name, value, hint, setting) VALUES ('', '', 'privateprofiles', 1, 'Hide profiles from other users (admin/mod can still access).', 'privateprofiles');
