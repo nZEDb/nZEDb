a6be31bdf3181381c935927e8589c17130fbfbbd
niel <niel.archer@gmail.com>
2015-06-10 16:11:57 +0100

    Fixing typo in constant name.

--- www/automated.config.php
+++ www/automated.config.php
@@ -13,7 +13,7 @@ define('nZEDb_ROOT', realpath(dirname(dirname(__FILE__))) . DS);
 define('nZEDb_LIB', nZEDb_ROOT . 'nzedb' . DS);
 define('nZEDb_CORE', nZEDb_LIB);
 
-define('nZEDb_CONFIGS', nZEDB_CORE . 'config' . DS);
+define('nZEDb_CONFIGS', nZEDb_CORE . 'config' . DS);
 
 // Used to refer to the third party library files.
 define('nZEDb_LIBS', nZEDb_ROOT . 'libs' . DS);
