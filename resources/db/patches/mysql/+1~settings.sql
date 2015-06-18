fae7a7b106662f8829cf89b3c239a0e47977caa0
niel <niel.archer@gmail.com>
2015-06-19 00:35:00 +0100

    Expanding setting key to use multilevel.

--- resources/db/patches/mysql/+1~settings.sql
+++ resources/db/patches/mysql/+1~settings.sql
@@ -1,8 +1,8 @@
 INSERT IGNORE INTO settings (section, subsection, name, value, hint, setting)
 VALUES (
-'',
-'',
-'fetchlastcompressedfiles',
+'archive',
+'fetch',
+'end',
 0,
 'Try to download the last rar or zip file? (This is good if most of the files are at the end.) Note: The first rar/zip is still downloaded.',
 'fetchlastcompressedfiles'
