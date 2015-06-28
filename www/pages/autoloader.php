6db083bad23d07afd93ce5abcb1e1ed8d82a473c
niel <niel.archer@gmail.com>
2015-06-28 04:31:07 +0100

    Stripping down old smarty autoload file for use with only the smarty page files.

--- www/pages/autoloader.php
+++ www/pages/autoloader.php
@@ -3,26 +3,11 @@
 spl_autoload_register(
 	function($className)
 	{
-		if ($className == 'Smarty') {
-			$className = 'Smarty.class';
-		}
-
-		$paths = array(
-			SMARTY_DIR,
-			nZEDb_WWW . 'pages' . DIRECTORY_SEPARATOR,
-			SMARTY_DIR . 'plugins' . DIRECTORY_SEPARATOR,
-			SMARTY_DIR . 'sysplugins' . DIRECTORY_SEPARATOR
-		);
-
-		foreach ($paths as $path)
-		{
-			$spec = str_replace('\\', DIRECTORY_SEPARATOR, $path . $className . '.php');
-			if (file_exists($spec)) {
-				require_once $spec;
-				break;
-			} else if (nZEDb_LOGAUTOLOADER) {
-				var_dump($spec);
-			}
+		$spec = str_replace('\\', DIRECTORY_SEPARATOR, nZEDb_WWW . 'pages' . DIRECTORY_SEPARATOR . $className . '.php');
+		if (file_exists($spec)) {
+			require_once $spec;
+		} else if (nZEDb_LOGAUTOLOADER) {
+			var_dump($spec);
 		}
 	}
 );
