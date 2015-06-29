0a53bd82ca55c3ba6b6ef72f3f62c7114789e4eb
niel <niel.archer@gmail.com>
2015-06-29 03:32:12 +0100

    Oops, reverting overwriting of ...www/pages/autoloader.php with admin version.

--- www/pages/autoloader.php
+++ www/pages/autoloader.php
@@ -4,8 +4,7 @@ spl_autoload_register(
 	function ($className) {
 		$spec = str_replace('\\',
 							DIRECTORY_SEPARATOR,
-							nZEDb_WWW . 'pages' . DIRECTORY_SEPARATOR . 'admin' . $className .
-							'.php');
+							nZEDb_WWW . 'pages' . $className . '.php');
 
 		if (file_exists($spec)) {
 			require_once $spec;
