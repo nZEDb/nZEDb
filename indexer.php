9a4eb1aabb8e707e788752f72256fa51e211464a
niel <niel.archer@gmail.com>
2015-07-06 16:49:15 +0100

    Adding the new nzedb\config\Config class (and renaming the previous instances of Initiate).

--- indexer.php
+++ indexer.php
@@ -19,12 +19,24 @@
  * @copyright 2015 nZEDb
  */
 require_once 'SPLClassLoader.php';
+require_once 'constants.php';
 
-use nzedb\config\Initiate;
+use nzedb\config\Config;
 
 $classLoader = new SplClassLoader('nzedb', [__DIR__ . DIRECTORY_SEPARATOR . 'nzedb']);
 $classLoader->register();
 
-$config = new Initiate('indexer');
+$config = new Config('indexer');
+
+define('HAS_WHICH', nzedb\utility\Utility::hasWhich() ? true : false);
+
+// Check if they updated config.php for the openssl changes. Only check 1 to save speed.
+if (!defined('nZEDb_SSL_VERIFY_PEER')) {
+	define('nZEDb_SSL_CAFILE', '');
+	define('nZEDb_SSL_CAPATH', '');
+	define('nZEDb_SSL_VERIFY_PEER', '0');
+	define('nZEDb_SSL_VERIFY_HOST', '0');
+	define('nZEDb_SSL_ALLOW_SELF_SIGNED', '1');
+}
 
 ?>
