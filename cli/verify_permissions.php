d312cd744e6dc15205115e6885a9b86a7a5a9e00
niel <niel.archer@gmail.com>
2015-07-06 14:39:52 +0100

    Updating 'require_once's to use appropriate (indexer|smarty) loading file.

--- cli/verify_permissions.php
+++ cli/verify_permissions.php
@@ -17,7 +17,7 @@ $www_path = dirname(__DIR__) . DIRECTORY_SEPARATOR . 'www' . DIRECTORY_SEPARATOR
 if (is_file($www_path . 'config.php')) {
 	require($www_path . 'config.php');
 } else {
-	require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'constants.php');
+	require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'indexer.php');
 }
 
 define('R', 1);
