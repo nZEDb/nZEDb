e6c953c39a875c72c61019c8f741410057519cc6
niel <niel.archer@gmail.com>
2015-07-06 17:08:43 +0100

    Fixing path to SPLClassLoader for smarty stuff.

--- www/smarty.php
+++ www/smarty.php
@@ -18,12 +18,11 @@
  * @author niel
  * @copyright 2015 nZEDb
  */
-require_once 'SPLClassLoader.php';
+require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'SPLClassLoader.php');
+require_once 'autoloader.php';
 
 use nzedb\config\Config;
 
-$classLoader = new SplClassLoader('nzedb', [__DIR__ . DIRECTORY_SEPARATOR . 'nzedb']);
-$classLoader->register();
 
 $config = new Config('smarty');
 
