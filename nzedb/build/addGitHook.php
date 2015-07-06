b5a5cb251f29631715176eddbca301d137d2f23a
niel <niel.archer@gmail.com>
2015-07-06 14:39:52 +0100

    Updating 'require_once's to use appropriate (indexer|smarty) loading file.

--- nzedb/build/addGitHook.php
+++ nzedb/build/addGitHook.php
@@ -18,7 +18,7 @@
  * @author niel
  * @copyright 2014 nZEDb
  */
-require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'constants.php');
+require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'indexer.php');
 
 use nzedb\ColorCLI;
 
