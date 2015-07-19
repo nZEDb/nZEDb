9ad85cc6d8d7d9e9bf43428073dc7b5c281256aa
niel <niel.archer@gmail.com>
2015-07-19 04:22:28 +0100

    Moving cutStringUsingLast() from Misc to Text classes.

--- nzedb/utility/Text.php
+++ nzedb/utility/Text.php
@@ -45,4 +45,40 @@ class Text
 		);
 	}
 
+	/**
+	 * Removes the preceeding or proceeding portion of a string
+	 * relative to the last occurrence of the specified character.
+	 * The character selected may be retained or discarded.
+	 *
+	 * @param string $character      the character to search for.
+	 * @param string $string         the string to search through.
+	 * @param string $side           determines whether text to the left or the right of the character is returned.
+	 *                               Options are: left, or right.
+	 * @param bool   $keepCharacter  determines whether or not to keep the character.
+	 *                               Options are: true, or false.
+	 *
+	 * @return string
+	 */
+	public static function cutStringUsingLast($character, $string, $side, $keepCharacter = true)
+	{
+		$offset      = ($keepCharacter ? 1 : 0);
+		$wholeLength = strlen($string);
+		$rightLength = (strlen(strrchr($string, $character)) - 1);
+		$leftLength  = ($wholeLength - $rightLength - 1);
+		switch ($side) {
+			case 'left':
+				$piece = substr($string, 0, ($leftLength + $offset));
+				break;
+			case 'right':
+				$start = (0 - ($rightLength + $offset));
+				$piece = substr($string, $start);
+				break;
+			default:
+				$piece = false;
+				break;
+		}
+
+		return ($piece);
+	}
+
 }
