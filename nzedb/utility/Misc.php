a2cd207ad6f13381f57c03384b6cf4f7d342578e
niel <niel.archer@gmail.com>
2015-07-19 04:23:58 +0100

    Moving stripNonPrintingChars() from Misc to Text classes.

--- nzedb/utility/Misc.php
+++ nzedb/utility/Misc.php
@@ -408,29 +408,6 @@ class Misc
 		}
 	}
 
-	/**
-	 * Strips non-printing characters from a string.
-	 *
-	 * Operates directly on the text string, but also returns the result for situations requiring a
-	 * return value (use in ternary, etc.)/
-	 *
-	 * @param $text        String variable to strip.
-	 *
-	 * @return string    The stripped variable.
-	 */
-	public static function stripNonPrintingChars(&$text)
-	{
-		$lowChars = [
-			"\x00", "\x01", "\x02", "\x03", "\x04", "\x05", "\x06", "\x07",
-			"\x08", "\x09", "\x0A", "\x0B", "\x0C", "\x0D", "\x0E", "\x0F",
-			"\x10", "\x11", "\x12", "\x13", "\x14", "\x15", "\x16", "\x17",
-			"\x18", "\x19", "\x1A", "\x1B", "\x1C", "\x1D", "\x1E", "\x1F",
-		];
-		$text = str_replace($lowChars, '', $text);
-
-		return $text;
-	}
-
 	public static function trailingSlash($path)
 	{
 		if (substr($path, strlen($path) - 1) != '/') {
