b47132d4188e0c73e06c334d1b83c98b8565d04f
Paul G <git@golownia.com>
2015-08-05 21:45:28 -0400

    converted binaries.binaryhash to BINARY(16) support for TPG

--- resources/db/patches/mysql/+1~binaries.sql
+++ resources/db/patches/mysql/+1~binaries.sql
@@ -1,10 +1,10 @@
-ALTER TABLE binaries MODIFY binaryhash CHAR(32) CHARACTER SET latin1 NOT NULL DEFAULT '0';
 DROP PROCEDURE IF EXISTS tpg_change;
 
 DELIMITER $$
 
-CREATE PROCEDURE tpg_change(IN databaseName VARCHAR(255)) BEGIN DECLARE done INT DEFAULT false; DECLARE _table CHAR(255); DECLARE _stmt VARCHAR(1000); DECLARE cur1 CURSOR FOR SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = databaseName AND TABLE_NAME LIKE "binaries\_%"; DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE; OPEN cur1; myloop: loop FETCH cur1 INTO _table; IF done THEN LEAVE myloop; END IF; SET @sql := CONCAT("ALTER TABLE ", _table," MODIFY binaryhash CHAR(32) CHARACTER SET latin1 NOT NULL DEFAULT '0'"); PREPARE _stmt FROM @sql; EXECUTE _stmt; DROP PREPARE _stmt; END loop; CLOSE cur1; END $$
+CREATE PROCEDURE tpg_change(IN databaseName varchar(255)) BEGIN DECLARE done INT DEFAULT false; DECLARE _table CHAR(255); DECLARE _stmt VARCHAR(1000); DECLARE cur1 CURSOR FOR SELECT TABLE_NAME FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_SCHEMA = databaseName AND (TABLE_NAME LIKE "binaries\_%" OR TABLE_NAME="binaries"); DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE; OPEN cur1; myloop: loop FETCH cur1 INTO _table; IF done THEN LEAVE myloop; END IF; SET @sql1 := CONCAT("DROP TABLE IF EXISTS ", _table, "_tmp"); SET @sql2 := CONCAT("CREATE TABLE ", _table, "_tmp LIKE ", _table); SET @sql3 := CONCAT("ALTER TABLE ", _table, "_tmp MODIFY binaryhash BINARY(16) NOT NULL DEFAULT '0'"); SET @sql4 := CONCAT("INSERT INTO ", _table, "_tmp SELECT id, name, collection_id, filenumber, totalparts, currentparts, UNHEX(binaryhash), partcheck, partsize FROM ",_table); SET @sql5 := CONCAT("RENAME TABLE ", _table," TO ", _table, "_old, ", _table, "_tmp TO ", _table); SET @sql6 := CONCAT("DROP TABLE IF EXISTS ", _table, "_old"); PREPARE _stmt FROM @sql1; EXECUTE _stmt; DROP PREPARE _stmt; PREPARE _stmt FROM @sql2; EXECUTE _stmt; DROP PREPARE _stmt; PREPARE _stmt FROM @sql3; EXECUTE _stmt; DROP PREPARE _stmt; PREPARE _stmt FROM @sql4; EXECUTE _stmt; DROP PREPARE _stmt; PREPARE _stmt FROM @sql5; EXECUTE _stmt; DROP PREPARE _stmt; PREPARE _stmt FROM @sql6; EXECUTE _stmt; DROP PREPARE _stmt; END loop; CLOSE cur1; END $$
 
 DELIMITER ;
+
 CALL tpg_change( DATABASE() );
 DROP PROCEDURE tpg_change;
