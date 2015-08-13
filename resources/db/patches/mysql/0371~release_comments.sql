873dc0f523b64c1035d155d759ea3cdfc81746f2
Bart39 <bart39@sky.com>
2015-08-13 16:22:59 +0100

    fixing patches 370/371

--- resources/db/patches/mysql/0371~release_comments.sql
+++ resources/db/patches/mysql/0371~release_comments.sql
@@ -6,7 +6,8 @@
 
 CREATE TABLE release_comments_tmp LIKE release_comments;
 ALTER TABLE release_comments_tmp MODIFY nzb_guid BINARY(16) NOT NULL DEFAULT '0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0';
-INSERT INTO release_comments_tmp (SELECT id, releaseid, text, username, user_id, createddate, host, shared, shareid, siteid, UNHEX(nzb_guid) FROM release_comments);
+INSERT INTO release_comments_tmp (id, releaseid, text, username, user_id, createddate, host, shared, shareid, siteid, nzb_guid)
+SELECT id, releaseid, text, username, user_id, createddate, host, shared, shareid, siteid, UNHEX(nzb_guid) FROM release_comments;
 
 DROP TABLE release_comments;
 ALTER TABLE release_comments_tmp RENAME release_comments;
