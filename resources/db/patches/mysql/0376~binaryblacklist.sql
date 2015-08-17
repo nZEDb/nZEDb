325d070eaa337eb529118411a8d3bbef206f5387
ruhllatio <ruhllatio@gmail.com>
2015-08-17 15:40:40 -0400

    Fix: Blacklist regex not matching properly for yEnc

---
+++ resources/db/patches/mysql/0376~binaryblacklist.sql
@@ -0,0 +1,2 @@
+REPLACE INTO binaryblacklist (id, groupname, regex, msgcol, optype, status, description)
+VALUES (13, '^alt\\.binaries\\.(kenpsx|frogs)$', '^\\s*([a-f0-9]{16})\\s\\[\\d+\\/\\d+\\]\\s-\\s"\\1"\\s+yEnc$', 1, 1, 0, 'Block 16 character hash floods in kenpsx, frogs.');
