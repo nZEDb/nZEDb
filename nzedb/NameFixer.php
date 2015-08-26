f1f7fc72e8dab91daff60377c260aaaf0e059102
ruhllatio <ruhllatio@gmail.com>
2015-08-26 10:29:07 -0400

    Revert "Chg: PreDB FT and ReleaseRemover Blacklist (not blfiles) now applicable to filenames with FTS"
    This reverts commit b74744815fedd65ffae523267e607ccfa04001d0.
    
    No longer needed with one index method

--- nzedb/NameFixer.php
+++ nzedb/NameFixer.php
@@ -688,7 +688,7 @@ class NameFixer
 				$titlematch = SphinxSearch::escapeString($preTitle);
 				$join = sprintf(
 						'INNER JOIN releases_se rse ON rse.id = r.id
-						WHERE rse.query = "@@relaxed @(name,searchname) %s @filename %1\$s;index=releases_rt,release_files_rt;mode=extended"',
+						WHERE rse.query = "@(name,searchname) %s;mode=extended"',
 						$titlematch
 				);
 				break;
