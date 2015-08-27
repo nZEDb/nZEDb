af074243ef5186e9e57316291288b7692203d9d2
niel <niel.archer@gmail.com>
2015-08-27 08:43:40 +0100

    Added default filename generation to PreDb Export script, for when a filename is omitted.

--- cli/data/predb_export.php
+++ cli/data/predb_export.php
@@ -23,6 +23,7 @@ require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'indexer
 
 use nzedb\db\PreDb;
 use nzedb\utility\Misc;
+use nzedb\utility\Text;
 
 if (!Misc::isWin()) {
 	$canExeRead = Misc::canExecuteRead(nZEDb_RES);
@@ -35,18 +36,22 @@ if (!Misc::isWin()) {
 if (!isset($argv[1])) {
 	$message = <<<HELP
 This script can export a predb dump file. You may use the full path, or a relative path.
-For exporting, the path must be writeable by mysql, any existing file [predb_dump.csv] will be overwritten.
+For exporting, the path must be writeable by mysql, any existing file will be overwritten.
+Filename is optional, if not included it defaults to predb_export_YYYYMMDDhhmmss.tsv where
+YYYYMMDDhhmmss is the current UTC date-time.
 
-php {$argv[0]} /path/to/write/to                     ...: To export.
+php {$argv[0]} /path/to/write/to/[<file-name>]                    ...: To export.
 
 HELP;
 	exit($message);
 } else {
-	$path = !preg_match('#^/#', $argv[1]) ? getcwd() . '/' . $argv[1] : $argv[1];
+	$path = !preg_match('#^/#', $argv[1]) ? realpath($argv[1]) : $argv[1];
 }
 
 if (file_exists($path) && is_file($path)) {
 	unlink($path);
+} else if (is_dir($path)) {
+	$path = Text::trailingSlash($path) . 'predb_export_' . strftime('%Y%m%d%H%M%S') . '.tsv';
 }
 
 Misc::clearScreen();
@@ -66,7 +71,7 @@ $result = $predb->executeExport([
 	]);
 
 if ($result == false) {
-	echo "ERROR: Failed to export file!\n";
+	echo "ERROR: Failed to export file!\nMake sure the MySQL user has permission to write to the location.\n";
 }
 
 ?>
