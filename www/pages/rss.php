a5a64029a078046e391fdc0859f49673a21936d2
Darko <dkrisan@gmail.com>
2015-09-10 15:25:34 +0200

    Update the PR per kevin123 suggestion.

--- www/pages/rss.php
+++ www/pages/rss.php
@@ -97,6 +97,8 @@ if (!isset($_GET["t"]) && !isset($_GET["rage"]) && !isset($_GET["anidb"])) {
 	}
 
 	$page->smarty->assign('releases', $relData);
+	$response = trim($page->smarty->fetch('rss.tpl'));
 	header("Content-type: text/xml");
-	echo trim($page->smarty->fetch('rss.tpl'));
+	header('Content-Length: ' . strlen($response) );
+	echo $response;
 }
