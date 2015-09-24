6c0c96ec84736befbebf90d4a64333e3f7821268
kevinlekiller <kevinlekiller@users.noreply.github.com>
2015-09-24 17:02:04 -0400

    Refactor www/pages/failed.php
    `Page` class is already instanciated here https://github.com/nZEDb/nZEDb/blob/dev/www/index.php#L6
    
    `$rel` was not used
    
    `$users` was not used.
    
    `'.nzb'` is removed so it's not needed: https://github.com/nZEDb/nZEDb/blob/dev/www/pages/getnzb.php#L50

--- www/pages/failed.php
+++ www/pages/failed.php
@@ -1,27 +1,12 @@
 <?php
 
 use nzedb\Releases;
-use nzedb\Users;
 
 if (isset($_GET['guid']) && isset($_GET['searchname']) && isset($_GET['userid']) && is_numeric($_GET['userid']) && isset($_GET['rsstoken'])) {
 
-	$page = new Page();
-	$releases = new Releases(['Settings' => $page->settings]);
-	$users = new Users();
-
-	$userid = $_GET['userid'];
-	$rsstoken = $_GET['rsstoken'];
-	$rel = $releases->getByGuid($_GET['guid']);
-
-	if (!$rel) {
-		$page->show404();
-	}
-
-	$alt = $releases->getAlternate($_GET['guid'], $_GET['searchname'], $userid);
+	$alt = (new Releases(['Settings' => $page->settings]))->getAlternate($_GET['guid'], $_GET['searchname'], $_GET['userid']);
 	if (!$alt) {
 		$page->show404();
 	}
-
-	$url = $page->serverurl . 'getnzb/' . $alt['guid'] . '.nzb' . '&i=' . $userid . '&r=' . $rsstoken;
-	header('Location: ' . $url . '');
+	header('Location: ' . $page->serverurl . 'getnzb/' . $alt['guid'] . '&i=' . $_GET['userid'] . '&r=' . $_GET['rsstoken']);
 }
