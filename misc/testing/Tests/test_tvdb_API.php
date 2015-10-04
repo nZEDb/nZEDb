4590e1113124327cc83029dab43a4a620c691e99
ruhllatio <ruhllatio@gmail.com>
2015-10-04 16:30:01 -0400

    Expand TVDB test file to allow show name and season/episode arguments

--- misc/testing/Tests/test_tvdb_API.php
+++ misc/testing/Tests/test_tvdb_API.php
@@ -5,16 +5,27 @@ require_once realpath(dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR .
 use nzedb\db\Settings;
 use nzedb\TVDB;
 
-// Test if your TvDB API key and configuration are working.
-// If it works you should get a var dumped array of the Walking Dead's first episode
-
+$c = new nzedb\ColorCLI();
 $tvdb = new TVDB();
 
-$serverTime = $tvdb->client->getServerTime();
+if (!empty($argv[1]) && is_numeric($argv[2]) && is_numeric($argv[3])) {
 
-// Search for a show
-$data = $tvdb->client->getSeries('Walking Dead');
+	// Test if your TvDB API key and configuration are working
+	// If it works you should get a var dumped array of the show/season/episode entered
 
-// Use the first show found and get the S01E01 episode
-$episode = $tvdb->client->getEpisode($data[0]->id, 1, 1, 'en');
-print_r($episode);
+	$serverTime = $tvdb->client->getServerTime();
+
+	// Search for a show
+	$data = $tvdb->client->getSeries((string)$argv[1]);
+
+	// Use the first show found and get the requested season/episode from $argv
+
+	echo PHP_EOL . $c->info("Server Time: " . $serverTime) .  PHP_EOL;
+
+	$episode = $tvdb->client->getEpisode($data[0]->id, (int)$argv[2], (int)$argv[3], 'en');
+	
+	print_r($episode);
+
+} else {
+	exit($c->error("Invalid arguments.  This script requires a text string (show name) followed by a season and episode number."));
+}
