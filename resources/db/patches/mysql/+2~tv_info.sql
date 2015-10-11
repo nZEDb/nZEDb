6e48eacfbc41f2234fa0b611bcfcde73238a8adb
ruhllatio <ruhllatio@gmail.com>
2015-10-11 10:12:52 -0400

    Change summary field in tv_episodes to text as VARCHAR(10000) isn't enough

--- resources/db/patches/mysql/+2~tv_info.sql
+++ resources/db/patches/mysql/+2~tv_info.sql
@@ -4,7 +4,7 @@
 DROP TABLE IF EXISTS tv_info;
 CREATE TABLE tv_info (
   videos_id MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'FK to video.id',
-  summary   VARCHAR(1000) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description/summary of the show.',
+  summary   TEXT COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description/summary of the show.',
   publisher VARCHAR(255)  COLLATE utf8_unicode_ci NOT NULL COMMENT 'The channel/network of production/release (ABC, BBC, Showtime, etc.).',
   image     TINYINT(1)    UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'Does the video have a cover image?',
   PRIMARY KEY titles (videos_id)
