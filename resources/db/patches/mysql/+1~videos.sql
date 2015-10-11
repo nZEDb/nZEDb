0f60705ec2f2ffd5192be27d8560a8ce7f45eedc
ruhllatio <ruhllatio@gmail.com>
2015-10-11 10:07:49 -0400

    Add MySQL patches and fix a few things.  Add image column to tv_info to denote cover status

---
+++ resources/db/patches/mysql/+1~videos.sql
@@ -0,0 +1,24 @@
+# This patch creates the new videos table
+# It will not fill it with data at this time
+
+DROP TABLE IF EXISTS videos;
+CREATE TABLE videos (
+  id           MEDIUMINT(11) UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT 'Show''s ID to be used in other tables as reference.',
+  type         TINYINT(1) UNSIGNED     NOT NULL DEFAULT '0' COMMENT '0 = TV, 1 = Film, 2 = Anime',
+  title        VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the video.',
+  countries_id CHAR(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Two character country code (FK to countries table).',
+  started      DATE                    NOT NULL COMMENT 'Date of production''s first airing.',
+  imdb         MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for IMDB site (without the ''tt'' prefix).',
+  trakt        MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TraktTV site.',
+  tvdb         MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TVDB site',
+  tvmaze       MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TVMaze site.',
+  tvrage       MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TVRage site.',
+  source       TINYINT(1)    UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'Which site did we use for info?',
+  PRIMARY KEY (id),
+  UNIQUE KEY title (title, type, started, countries_id),
+  INDEX       ix_videos_tvrage (tvrage)
+)
+  ENGINE = MyISAM
+  AUTO_INCREMENT = 1
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
---
+++ resources/db/patches/mysql/+2~tv_info.sql
@@ -0,0 +1,14 @@
+# This patch creates the new tv_info table
+# It will not fill it with data at this time
+
+DROP TABLE IF EXISTS tv_info;
+CREATE TABLE tv_info (
+  videos_id MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'FK to video.id',
+  summary   VARCHAR(1000) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description/summary of the show.',
+  publisher VARCHAR(255)  COLLATE utf8_unicode_ci NOT NULL COMMENT 'The channel/network of production/release (ABC, BBC, Showtime, etc.).',
+  image     TINYINT(1)    UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'Does the video have a cover image?',
+  PRIMARY KEY titles (videos_id)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci;
