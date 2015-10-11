c1c2f282ff8a1f61bf73412d37bd13022526a17f
niel <niel.archer@gmail.com>
2015-10-11 22:37:08 +0100

    Updating the comment in ddl SQL for videos table.

--- resources/db/patches/mysql/+1~videos.sql
+++ resources/db/patches/mysql/+1~videos.sql
@@ -7,7 +7,7 @@ CREATE TABLE videos (
   type         TINYINT(1) UNSIGNED     NOT NULL DEFAULT '0' COMMENT '0 = TV, 1 = Film, 2 = Anime',
   title        VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the video.',
   countries_id CHAR(2) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Two character country code (FK to countries table).',
-  started      DATE                    NOT NULL COMMENT 'Date of production''s first airing.',
+  started      DATE                    NOT NULL COMMENT 'Date (UTC) of production''s first airing.',
   imdb         MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for IMDB site (without the ''tt'' prefix).',
   trakt        MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TraktTV site.',
   tvdb         MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for TVDB site',
