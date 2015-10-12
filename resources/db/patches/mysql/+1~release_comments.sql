065d4319098baa61a47efee3f6b3f7896cf89780
DariusIII <dkrisan@gmail.com>
2015-10-12 22:37:55 +0200

    Remove text_hash column, trigger and index from release_comments.

---
+++ resources/db/patches/mysql/+1~release_comments.sql
@@ -0,0 +1,10 @@
+# This patch will remove column and index added by patch 383
+
+#Drop the text_hash column
+ALTER TABLE release_comments DROP text_hash;
+
+#Drop the trigger
+DROP TRIGGER IF EXISTS insert_MD5:
+
+#Drop the index
+ALTER TABLE release_comments DROP INDEX ix_release_comments_hash_releaseid;
