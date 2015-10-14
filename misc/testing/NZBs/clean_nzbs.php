326b10863a5c512b86c2e5cae49f2b1cf8f1394b
kevinlekiller <kevinlekiller@users.noreply.github.com>
2015-10-14 15:50:19 -0400

    Change argument to "move" instead and use "rename"
    function instead of "copy"/"unlink".

--- misc/testing/NZBs/clean_nzbs.php
+++ misc/testing/NZBs/clean_nzbs.php
@@ -8,10 +8,10 @@ use nzedb\Releases;
 use nzedb\db\Settings;
 use nzedb\utility\Misc;
 
-if (!isset($argv[1]) || !in_array($argv[1], ["true", "delete"])) {
-	exit($pdo->log->error("\nThis script can remove all nzbs not found in the db and all releases with no nzbs found. It can also delete invalid nzbs.\n\n"
-		. "php $argv[0] true     ...: For a dry run, to see how many would be deleted.\n"
-		. "php $argv[0] delete   ...: To delete all affected.\n"));
+if (!isset($argv[1]) || !in_array($argv[1], ["true", "move"])) {
+	exit($pdo->log->error("\nThis script can remove all nzbs not found in the db and all releases with no nzbs found. It can also move invalid nzbs.\n\n"
+		. "php $argv[0] true     ...: For a dry run, to see how many would be moved.\n"
+		. "php $argv[0] move     ...: Move NZBs that are possibly bad or have no release. They are moved into this folder: " . (nZEDb_ROOT . "movednzbs/") . "\n"));
 }
 
 $pdo = new Settings();
@@ -20,11 +20,11 @@ $nzb = new NZB($pdo);
 $releaseImage = new ReleaseImage($pdo);
 
 $timestart = date("r");
-$checked = $deleted = 0;
+$checked = $moved = 0;
 $couldbe = ($argv[1] === "true") ? "could be " : "";
 
 echo $pdo->log->header('Getting List of nzbs to check against db.');
-echo $pdo->log->header("Checked / {$couldbe}deleted\n");
+echo $pdo->log->header("Checked / {$couldbe}moved\n");
 
 $dirItr = new \RecursiveDirectoryIterator($pdo->getSetting('nzbpath'));
 $itr = new \RecursiveIteratorIterator($dirItr, \RecursiveIteratorIterator::LEAVES_ONLY);
@@ -35,30 +35,30 @@ foreach ($itr as $filePath) {
 		if ($nzbfile && @simplexml_load_string($nzbfile)) {
 			$res = $pdo->queryOneRow(sprintf("SELECT id, guid FROM releases WHERE guid = %s", $pdo->escapeString(stristr($filePath->getFilename(), '.nzb.gz', true))));
 			if ($res === false) {
-				$deleted++;
-				if ($argv[1] === "delete") {
-					@copy($filePath, nZEDb_ROOT . "pooped/" . $guid[1] . ".nzb.gz");
+				$moved++;
+				if ($argv[1] === "move") {
+					@rename($filePath, nZEDb_ROOT . "movednzbs/" . $guid[1] . ".nzb.gz");
 					$releases->deleteSingle(['g' => $guid[1], 'i' => false], $nzb, $releaseImage);
 				}
 			} else {
 				$pdo->queryExec(sprintf("UPDATE releases SET nzbstatus = 1 WHERE id = %s", $res['id']));
 			}
 		} else {
-			$deleted++;
-			if ($argv[1] === "delete") {
-				@copy($filePath, nZEDb_ROOT . "pooped/" . $guid[1] . ".nzb.gz");
+			$moved++;
+			if ($argv[1] === "move") {
+				@rename($filePath, nZEDb_ROOT . "movednzbs/" . $guid[1] . ".nzb.gz");
 			}
 		}
 		++$checked;
-		echo "$checked / $deleted\r";
+		echo "$checked / $moved\r";
 	}
 }
 
-echo $pdo->log->header("\n" . number_format($checked) . ' nzbs checked, ' . number_format($deleted) . ' nzbs ' . $couldbe . 'deleted.');
+echo $pdo->log->header("\n" . number_format($checked) . ' nzbs checked, ' . number_format($moved) . ' nzbs ' . $couldbe . 'moved.');
 echo $pdo->log->header("Getting List of releases to check against nzbs.");
-echo $pdo->log->header("Checked / {$couldbe}deleted\n");
+echo $pdo->log->header("Checked / {$couldbe}moved\n");
 
-$checked = $deleted = 0;
+$checked = $moved = 0;
 
 $res = $pdo->queryDirect('SELECT id, guid FROM releases');
 if ($res instanceof \Traversable) {
@@ -67,15 +67,15 @@ if ($res instanceof \Traversable) {
 		if (is_file($nzbpath)) {
 			$pdo->queryExec(sprintf("UPDATE releases SET nzbstatus = 1 WHERE id = %d", $row['id']));
 		} else {
-			$deleted++;
-			if ($argv[1] === "delete") {
-				@copy($nzbpath, nZEDb_ROOT . "pooped/" . $row["guid"] . ".nzb.gz");
+			$moved++;
+			if ($argv[1] === "move") {
+				@rename($nzbpath, nZEDb_ROOT . "pooped/" . $row["guid"] . ".nzb.gz");
 				$releases->deleteSingle(['g' => $row['guid'], 'i' => $row['id']], $nzb, $releaseImage);
 			}
 		}
 		++$checked;
-		echo "$checked / $deleted\r";
+		echo "$checked / $moved\r";
 	}
 }
-echo $pdo->log->header("\n" . number_format($checked) . " releases checked, " . number_format($deleted) . " releases " . $couldbe . "deleted.");
+echo $pdo->log->header("\n" . number_format($checked) . " releases checked, " . number_format($moved) . " releases " . $couldbe . "moved.");
 echo $pdo->log->header("Script started at [$timestart], finished at [" . date("r") . "]");
