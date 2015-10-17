65c281027edb8beffe2f0b4931c4dd20be763f24
niel <niel.archer@gmail.com>
2015-10-17 02:12:24 +0100

    Updated SQL patches. Made 'started' and 'firstaired' DATETIME.

--- resources/db/patches/mysql/+3~tv_episodes.sql
+++ resources/db/patches/mysql/+3~tv_episodes.sql
@@ -4,12 +4,12 @@
 DROP TABLE IF EXISTS tv_episodes;
 CREATE TABLE tv_episodes (
   id          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
-  videos_id    MEDIUMINT(11) UNSIGNED  NOT NULL COMMENT 'FK to videos.id of the parent series.',
+  videos_id   MEDIUMINT(11) UNSIGNED  NOT NULL COMMENT 'FK to videos.id of the parent series.',
   series      SMALLINT(5) UNSIGNED    NOT NULL DEFAULT '0' COMMENT 'Number of series/season.',
   episode     SMALLINT(5) UNSIGNED    NOT NULL DEFAULT '0' COMMENT 'Number of episode within series',
   se_complete VARCHAR(10) COLLATE utf8_unicode_ci NOT NULL COMMENT 'String version of Series/Episode as taken from release subject (i.e. S02E21+22).',
   title       VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the episode.',
-  firstaired  DATE NOT NULL COMMENT 'Date of original airing/release.',
+  firstaired  DATETIME NOT NULL COMMENT 'Date of original airing/release.',
   summary     VARCHAR(1000) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description/summary of the episode.',
   PRIMARY KEY                           (id),
   UNIQUE INDEX ix_tv_episodes_videos_id (videos_id, series, episode)
