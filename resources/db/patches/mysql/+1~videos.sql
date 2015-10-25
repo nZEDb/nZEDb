2173938b84692dbb8384bb46e65971ce408c87d9
niel <niel.archer@gmail.com>
2015-10-25 01:54:43 +0100

    Update schema to allow for indexing on utf8mb4 fields.

--- resources/db/patches/mysql/+1~videos.sql
+++ resources/db/patches/mysql/+1~videos.sql
@@ -5,7 +5,7 @@ DROP TABLE IF EXISTS videos;
 CREATE TABLE videos (
   id           MEDIUMINT(11)  UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT 'Show''s ID to be used in other tables as reference.',
   type         TINYINT(1)     UNSIGNED     NOT NULL DEFAULT '0' COMMENT '0 = TV, 1 = Film, 2 = Anime',
-  title        VARCHAR(255)   CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the video.',
+  title        VARCHAR(245)   CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the video.',
   countries_id CHAR(2)        COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT 'Two character country code (FK to countries table).',
   started      DATETIME                NOT NULL COMMENT 'Date (UTC) of production''s first airing.',
   imdb         MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'ID number for IMDB site (without the ''tt'' prefix).',
