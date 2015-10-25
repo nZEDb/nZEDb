0ae98b6e0ccfd6e711698f33d23001d542dbd090
ruhllatio <ruhllatio@gmail.com>
2015-10-25 10:41:00 -0400

    Change VARCHAR values in new tables to support utf8mb4 index across engines

--- resources/db/patches/mysql/+3~tv_episodes.sql
+++ resources/db/patches/mysql/+3~tv_episodes.sql
@@ -8,7 +8,7 @@ CREATE TABLE tv_episodes (
   series      SMALLINT(5)   UNSIGNED    NOT NULL DEFAULT '0' COMMENT 'Number of series/season.',
   episode     SMALLINT(5)   UNSIGNED    NOT NULL DEFAULT '0' COMMENT 'Number of episode within series',
   se_complete VARCHAR(10)   COLLATE utf8_unicode_ci NOT NULL COMMENT 'String version of Series/Episode as taken from release subject (i.e. S02E21+22).',
-  title       VARCHAR(255)  CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Title of the episode.',
+  title       VARCHAR(180)  CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Title of the episode.',
   firstaired  DATETIME      NOT NULL COMMENT 'Date of original airing/release.',
   summary     TEXT          CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Description/summary of the episode.',
   PRIMARY KEY                           (id),
