69d608a8e2be139ccf2845a0e388676c6a1caf0b
niel <niel.archer@gmail.com>
2015-10-25 01:40:57 +0100

    Updated title/summary fields to use utf8mb4 character set.

--- resources/db/patches/mysql/+2~tv_info.sql
+++ resources/db/patches/mysql/+2~tv_info.sql
@@ -4,8 +4,8 @@
 DROP TABLE IF EXISTS tv_info;
 CREATE TABLE tv_info (
   videos_id MEDIUMINT(11) UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'FK to video.id',
-  summary   TEXT COLLATE utf8_unicode_ci NOT NULL COMMENT 'Description/summary of the show.',
-  publisher VARCHAR(255)  COLLATE utf8_unicode_ci NOT NULL COMMENT 'The channel/network of production/release (ABC, BBC, Showtime, etc.).',
+  summary   TEXT          CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Description/summary of the show.',
+  publisher VARCHAR(255)  CHARSET = utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'The channel/network of production/release (ABC, BBC, Showtime, etc.).',
   image     TINYINT(1)    UNSIGNED  NOT NULL DEFAULT '0' COMMENT 'Does the video have a cover image?',
   PRIMARY KEY          (videos_id),
   KEY ix_tv_info_image (image)
