cf95043bc94b3991bf92226f0d2032cadfb82600
ruhllatio <ruhllatio@gmail.com>
2015-10-31 08:58:29 -0400

    Add videos_akas table to capture aliases for local lookups

---
+++ resources/db/patches/mysql/+1~videos_akas.sql
@@ -0,0 +1,13 @@
+# This patch creates the videos_akas table to store AKAs retrieved
+# from scrape sources.  This will keep a record of them for local
+# lookup searches to speed up processing and prevent API hammering
+
+CREATE TABLE videos_akas (
+  videos_id   MEDIUMINT(11) UNSIGNED  NOT NULL COMMENT 'FK to videos.id of the parent title.',
+  title VARCHAR(180) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'AKA of the video.',
+  PRIMARY KEY (videos_id, title)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci
+  AUTO_INCREMENT  = 100000;
--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -1193,12 +1193,19 @@ CREATE TABLE videos (
   KEY            ix_videos_type_source (type, source)
 )
   ENGINE = MyISAM
-  AUTO_INCREMENT = 1
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci
   AUTO_INCREMENT = 10000000;
 
-
+CREATE TABLE videos_akas (
+  videos_id   MEDIUMINT(11) UNSIGNED  NOT NULL COMMENT 'FK to videos.id of the parent title.',
+  title VARCHAR(180) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'AKA of the video.',
+  PRIMARY KEY (videos_id, title)
+)
+  ENGINE = MyISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci
+  AUTO_INCREMENT  = 10000000;
 
 DROP TABLE IF EXISTS xxxinfo;
 CREATE TABLE         xxxinfo (
