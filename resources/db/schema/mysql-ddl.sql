4d58e80b7525848b567c96dc735a7ce0b721ceca
niel <niel.archer@gmail.com>
2015-11-02 22:55:17 +0000

    Switch countries.country column to use utf8mb4. Who knows what characters them foreigners will use.

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -297,7 +297,7 @@ DROP TABLE IF EXISTS countries;
 CREATE TABLE countries (
   id      CHAR(2) COLLATE utf8_unicode_ci NOT NULL COMMENT '2 character code.',
   iso3    CHAR(3) COLLATE utf8_unicode_ci NOT NULL COMMENT '3 character code.',
-  country VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Name of the country.',
+  country VARCHAR(255) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the country.',
   PRIMARY KEY (id),
   UNIQUE KEY code3 (iso3),
   UNIQUE KEY country (country)
