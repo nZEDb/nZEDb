6ee71f3cf5a1ef3801c4db71fff9e48f3eeed866
Bart39 <bart39@sky.com>
2015-11-03 09:52:46 +0000

    Fix: Change patch 391 to drop and recreate user_series table due to unknown issue on InnoDB #1923.
    # Conflicts:
    #	Changelog

--- resources/db/patches/mysql/0391~user_series.sql
+++ resources/db/patches/mysql/0391~user_series.sql
@@ -3,11 +3,20 @@
 # At this time, we're not sure if we can preserve the users shows
 # data as the tvrage data is largely useless due to the title insert bug
 
-# Truncate for now
-TRUNCATE TABLE user_series;
+# Drop the old user_series table
+DROP TABLE IF EXISTS user_series;
 
-# Change rageid column to new videos_id
-ALTER TABLE user_series
-  DROP INDEX ix_userseries_userid,
-  CHANGE COLUMN rageid videos_id INT(16) NOT NULL COMMENT 'FK to videos.id',
-  ADD INDEX ix_userseries_videos_id (user_id, videos_id);
+# Create user_series table with new schema
+CREATE TABLE user_series (
+  id          INT(16) UNSIGNED NOT NULL AUTO_INCREMENT,
+  user_id     INT(16)          NOT NULL,
+  videos_id   INT(16)          NOT NULL COMMENT 'FK to videos.id',
+  categoryid  VARCHAR(64)      NULL DEFAULT NULL,
+  createddate DATETIME         NOT NULL,
+  PRIMARY KEY (id),
+  INDEX ix_userseries_videos_id (user_id, videos_id)
+)
+  ENGINE = MYISAM
+  DEFAULT CHARSET = utf8
+  COLLATE = utf8_unicode_ci
+  AUTO_INCREMENT = 1;
