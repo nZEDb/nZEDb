0fc3aa9d01d89e78a81c54b9278606e9924ce911
DariusIII <dkrisan@gmail.com>
2015-11-04 15:01:21 +0100

    Add missing lines into checkRequired from tmdb

--- nzedb/processing/tv/TV.php
+++ nzedb/processing/tv/TV.php
@@ -730,6 +730,12 @@ abstract class TV extends Videos
 			case 'tvmazeE':
 				$required = ['name', 'season', 'number', 'airdate', 'summary'];
 				break;
+			case 'tmdbS':
+				$required = ['id', 'original_name', 'overview', 'first_air_date', 'origin_country'];
+				break;
+			case 'tmdbE':
+				$required = ['name', 'season_number', 'episode_number', 'air_date', 'overview'];
+				break;
 		}
 
 		if (is_array($required)) {
