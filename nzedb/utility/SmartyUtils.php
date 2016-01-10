3d9a0d532b9c4c632cbbce8320588ac3856c8c35
niel <niel.archer@gmail.com>
2016-01-10 20:21:36 +0000

    Refactor release_flag function to use switch instead of repeated IFs.

--- nzedb/utility/SmartyUtils.php
+++ nzedb/utility/SmartyUtils.php
@@ -30,93 +30,118 @@ function release_flag($text, $page)
 {
 	$code = $language = "";
 
-	if (preg_match('/\bCzech\b/i', $text)) {
-		$code = "cz";
-		$language = "Czech";
-	} else if (preg_match('/Chinese|Mandarin|\bc[hn]\b/i', $text)) {
-		$code = "cn";
-		$language = "Chinese";
-	} else if (preg_match('/German(bed)?|\bger\b/i', $text)) {
-		$code = "de";
-		$language = "German";
-	} else if (preg_match('/Danish/i', $text)) {
-		$code = "dk";
-		$language = "Danish";
-	} else if (preg_match('/English|\beng?\b/i', $text)) {
-		$code = "en";
-		$language = "English";
-	} else if (preg_match('/Spanish/i', $text)) {
-		$code = "es";
-		$language = "Spanish";
-	} else if (preg_match('/Finnish/i', $text)) {
-		$code = "fi";
-		$language = "Finnish";
-	} else if (preg_match('/French|Vostfr|Multi/i', $text)) {
-		$code = "fr";
-		$language = "French";
-	} else if (preg_match('/\bGreek\b/i', $text)) {
-		$code = "gr";
-		$language = "Greek";
-	} else if (preg_match('/Hungarian|\bhun\b/i', $text)) {
-		$code = "hu";
-		$language = "Hungarian";
-	} else if (preg_match('/Hebrew|Yiddish/i', $text)) {
-		$code = "il";
-		$language = "Hebrew";
-	} else if (preg_match('/\bHindi\b/i', $text)) {
-		$code = "in";
-		$language = "Hindi";
-	} else if (preg_match('/Italian|\bita\b/i', $text)) {
-		$code = "it";
-		$language = "Italian";
-	} else if (preg_match('/Japanese|\bjp\b/i', $text)) {
-		$code = "jp";
-		$language = "Japanese";
-	} else if (preg_match('/Korean|\bkr\b/i', $text)) {
-		$code = "kr";
-		$language = "Korean";
-	} else if (preg_match('/Flemish|\b(Dutch|nl)\b|NlSub/i', $text)) {
-		$code = "nl";
-		$language = "Dutch";
-	} else if (preg_match('/Norwegian/i', $text)) {
-		$code = "no";
-		$language = "Norwegian";
-	} else if (preg_match('/Tagalog|Filipino/i', $text)) {
-		$code = "ph";
-		$language = "Tagalog|Filipino";
-	} else if (preg_match('/Arabic/i', $text)) {
-		$code = "pk";
-		$language = "Arabic";
-	} else if (preg_match('/Polish/i', $text)) {
-		$code = "pl";
-		$language = "Polish";
-	} else if (preg_match('/Portugese/i', $text)) {
-		$code = "pt";
-		$language = "Portugese";
-	} else if (preg_match('/Romanian/i', $text)) {
-		$code = "ro";
-		$language = "Romanian";
-	} else if (preg_match('/Russian/i', $text)) {
-		$code = "ru";
-		$language = "Russian";
-	} else if (preg_match('/Swe(dish|sub)/i', $text)) {
-		$code = "se";
-		$language = "Swedish";
-	} else if (preg_match('/\bThai\b/i', $text)) {
-		$code = "th";
-		$language = "Thai";
-	} else if (preg_match('/Turkish/i', $text)) {
-		$code = "tr";
-		$language = "Turkish";
-	} else if (preg_match('/Cantonese/i', $text)) {
-		$code = "tw";
-		$language = "Cantonese";
-	} else if (preg_match('/Vietnamese/i', $text)) {
-		$code = "vn";
-		$language = "Vietnamese";
+	switch (true) {
+		case preg_match('/Arabic/i', $text):
+			$code = "pk";
+			$language = "Arabic";
+			break;
+		case preg_match('/Cantonese/i', $text):
+			$code = "tw";
+			$language = "Cantonese";
+			break;
+		case preg_match('/Chinese|Mandarin|\bc[hn]\b/i', $text):
+			$code = "cn";
+			$language = "Chinese";
+			break;
+		case preg_match('/\bCzech\b/i', $text):
+			$code = "cz";
+			$language = "Czech";
+			break;
+		case preg_match('/Danish/i', $text):
+			$code = "dk";
+			$language = "Danish";
+			break;
+		case preg_match('/Finnish/i', $text):
+			$code = "fi";
+			$language = "Finnish";
+			break;
+		case preg_match('/Flemish|\b(Dutch|nl)\b|NlSub/i', $text):
+			$code = "nl";
+			$language = "Dutch";
+			break;
+		case preg_match('/French|Vostfr|Multi/i', $text):
+			$code = "fr";
+			$language = "French";
+			break;
+		case preg_match('/German(bed)?|\bger\b/i', $text):
+			$code = "de";
+			$language = "German";
+			break;
+		case preg_match('/\bGreek\b/i', $text):
+			$code = "gr";
+			$language = "Greek";
+			break;
+		case preg_match('/Hebrew|Yiddish/i', $text):
+			$code = "il";
+			$language = "Hebrew";
+			break;
+		case preg_match('/\bHindi\b/i', $text):
+			$code = "in";
+			$language = "Hindi";
+			break;
+		case preg_match('/Hungarian|\bhun\b/i', $text):
+			$code = "hu";
+			$language = "Hungarian";
+			break;
+		case preg_match('/Italian|\bita\b/i', $text):
+			$code = "it";
+			$language = "Italian";
+			break;
+		case preg_match('/Japanese|\bjp\b/i', $text):
+			$code = "jp";
+			$language = "Japanese";
+			break;
+		case preg_match('/Korean|\bkr\b/i', $text):
+			$code = "kr";
+			$language = "Korean";
+			break;
+		case preg_match('/Norwegian/i', $text):
+			$code = "no";
+			$language = "Norwegian";
+			break;
+		case preg_match('/Polish/i', $text):
+			$code = "pl";
+			$language = "Polish";
+			break;
+		case preg_match('/Portugese/i', $text):
+			$code = "pt";
+			$language = "Portugese";
+			break;
+		case preg_match('/Romanian/i', $text):
+			$code = "ro";
+			$language = "Romanian";
+			break;
+		case preg_match('/Spanish/i', $text):
+			$code = "es";
+			$language = "Spanish";
+			break;
+		case preg_match('/Swe(dish|sub)/i', $text):
+			$code = "se";
+			$language = "Swedish";
+			break;
+		case preg_match('/Tagalog|Filipino/i', $text):
+			$code = "ph";
+			$language = "Tagalog|Filipino";
+			break;
+		case preg_match('/\bThai\b/i', $text):
+			$code = "th";
+			$language = "Thai";
+			break;
+		case preg_match('/Turkish/i', $text):
+			$code = "tr";
+			$language = "Turkish";
+			break;
+		case preg_match('/Russian/i', $text):
+			$code = "ru";
+			$language = "Russian";
+			break;
+		case preg_match('/Vietnamese/i', $text):
+			$code = "vn";
+			$language = "Vietnamese";
+			break;
 	}
 
-	if ($code !== "" && $page == "browse") {
+	if ($code !== '' && $page == "browse") {
 		$www = WWW_TOP;
 		if (!in_array(substr($www, -1), ['\\', '/'])) {
 			$www .= DS;
