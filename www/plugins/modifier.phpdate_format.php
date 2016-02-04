764a4c11bebb0d142755e5844fc990fc05409058
niel <niel.archer@gmail.com>
2016-02-04 17:05:14 +0000

    Re-adding missing smarty plugins.

--- www/plugins/modifier.phpdate_format.php
+++ www/plugins/modifier.phpdate_format.php
@@ -33,10 +33,10 @@ if (!is_dir($plugins_dir)) {
 	exit('Fatal: Unable to find smarty plugins directory.' . PHP_EOL);
 }
 // End fix by nZEDb.
-require_once($plugins_dir . 'shared.make_timestamp.php');
-
+require_once ($plugins_dir . 'shared.make_timestamp.php');
 /**
  * Smarty phpdate_format modifier plugin
+ *
  * Type:     modifier<br>
  * Name:     date_format<br>
  * Purpose:  format datestamps via date()<br>
@@ -44,64 +44,39 @@ require_once($plugins_dir . 'shared.make_timestamp.php');
  *         - string: input date string
  *         - format: strftime format for output
  *         - default_date: default date if $string is empty
- *
- * @link    http://smarty.php.net/manual/en/language.modifier.date.format.php
+ * @link http://smarty.php.net/manual/en/language.modifier.date.format.php
  *          date_format (Smarty online manual)
- *
  * @param string
  * @param string
  * @param string
- *
  * @return string|void
- * @uses    smarty_make_timestamp()
+ * @uses smarty_make_timestamp()
  */
-function smarty_modifier_phpdate_format($string, $format = "Y/m/d H:i:s", $default_date = null)
+function smarty_modifier_phpdate_format($string, $format="Y/m/d H:i:s", $default_date=null)
 {
-	/*  if (substr(PHP_OS,0,3) == 'WIN') {
-			 $_win_from = array ('%e',  '%T',       '%D');
-			 $_win_to   = array ('%#d', '%H:%M:%S', '%m/%d/%y');
-			 $format = str_replace($_win_from, $_win_to, $format);
-	  }*/
-	if (substr($format, 0, 5) == 'DATE_') {
-		switch ($format) {
-			case 'DATE_ATOM':
-				$nformat = DATE_ATOM;
-				break;
-			case 'DATE_COOKIE':
-				$nformat = DATE_COOKIE;
-				break;
-			case 'DATE_ISO8601':
-				$nformat = DATE_ISO8601;
-				break;
-			case 'DATE_RFC822':
-				$nformat = "D, d M y H:i:s O";
-				break; //The php constant is not quite right - as the time-zone comes out with invalid values like "UTC"...
-			case 'DATE_RFC850':
-				$nformat = DATE_RFC850;
-				break;
-			case 'DATE_RFC1036':
-				$nformat = DATE_RFC1036;
-				break;
-			case 'DATE_RFC1123':
-				$nformat = DATE_RFC1123;
-				break;
-			case 'DATE_RFC2822':
-				$nformat = DATE_RFC2822;
-				break;
-			case 'DATE_RFC3339':
-				$nformat = DATE_RFC3339;
-				break;
-			case 'DATE_RSS':
-				$nformat = "D, d M Y H:i:s O";
-				break; //as rfc822 ...
-			case 'DATE_W3C':
-				$nformat = DATE_W3C;
-				break;
+  /*  if (substr(PHP_OS,0,3) == 'WIN') {
+		   $_win_from = array ('%e',  '%T',       '%D');
+		   $_win_to   = array ('%#d', '%H:%M:%S', '%m/%d/%y');
+		   $format = str_replace($_win_from, $_win_to, $format);
+	}*/
+	if (substr($format,0,5)=='DATE_'){
+		switch ($format){
+			case 'DATE_ATOM': $nformat=DATE_ATOM; break;
+			case 'DATE_COOKIE': $nformat=DATE_COOKIE; break;
+			case 'DATE_ISO8601': $nformat=DATE_ISO8601; break;
+			case 'DATE_RFC822': $nformat="D, d M y H:i:s O"; break; //The php constant is not quite right - as the time-zone comes out with invalid values like "UTC"...
+			case 'DATE_RFC850': $nformat=DATE_RFC850; break;
+			case 'DATE_RFC1036': $nformat=DATE_RFC1036; break;
+			case 'DATE_RFC1123': $nformat=DATE_RFC1123; break;
+			case 'DATE_RFC2822': $nformat=DATE_RFC2822; break;
+			case 'DATE_RFC3339': $nformat=DATE_RFC3339; break;
+			case 'DATE_RSS': $nformat="D, d M Y H:i:s O"; break; //as rfc822 ...
+			case 'DATE_W3C': $nformat=DATE_W3C; break;
 		}
 	} else {
-		$nformat = $format;
+		$nformat=$format;
 	}
-	if ($string != '') {
+	if($string != '') {
 		return date($nformat, smarty_make_timestamp($string));
 	} elseif (isset($default_date) && $default_date != '') {
 		return date($nformat, smarty_make_timestamp($default_date));
@@ -109,6 +84,5 @@ function smarty_modifier_phpdate_format($string, $format = "Y/m/d H:i:s", $defau
 		return;
 	}
 }
-
 /* vim: set expandtab: */
 ?>
