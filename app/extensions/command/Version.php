35e2ee3abb36dec5d2d00e077cfa5e0c8d5949bf
niel <niel.archer@gmail.com>
2016-04-07 14:48:09 +0100

    Add method to command Version to load XML file, to allow getting the SQL patch from it.

--- app/extensions/command/Version.php
+++ app/extensions/command/Version.php
@@ -27,6 +27,7 @@ use \nzedb\utility\Git;
  * Returns the current version (or branch) of the indexer.
  *
  * Actions:
+ *  * all	Show all of following info.
  *  * git	Show git tag for current version.
  *  * sql	Show SQL patch level
  *
@@ -43,6 +44,11 @@ class Version extends \app\extensions\console\Command
 	 * @var array of stable branches.
 	 */
 	//protected $stable = ['0.x'];
+
+	/**
+	 * @var object simpleXMLElement
+	 */
+	protected $xml = null;
 
 	/**
 	 * Constructor.
@@ -68,19 +74,74 @@ class Version extends \app\extensions\console\Command
 		return false;
 	}
 
-	protected function git()
+	protected function all()
+	{
+		$this->git();
+		$this->sql();
+	}
+
+	/**
+	 * Fetch git tag for latest version.
+	 *
+	 * @param null $path	Optional path to the versions XML file.
+	 */
+	protected function git($versions = null)
 	{
 		$git = new Git();
-		//$branch = $git->getBranch();
 		$latest = $git->tagLatest();
 
-		//$this->header('nZEDb');
 		$this->out("nZEDb version: $latest");
 	}
 
-	protected function sql()
+	/**
+	 * Fetch SQL latest patch version.
+	 *
+	 * @param null $path Optional path to the versions XML file.
+	 */
+	protected function sql($versions = null)
 	{
-		;
+		$this->_loadVersionsFile($versions);
+		$latest = $this->getSQLPatchFromFile();
+		$this->out("SQL version: $latest");
+	}
+
+	protected function getSQLPatchFromFile()
+	{
+		return ($this->xml === null) ? null : $this->_vers->sql->file->__toString();
+	}
+
+	protected function _loadVersionsFile($versions = null)
+	{
+		if ($this->xml === null) {
+			if ($versions == '') {
+				$versions = nZEDb_VERSIONS;
+			}
+
+			$temp = libxml_use_internal_errors(true);
+			$this->xml = simplexml_load_file($versions);
+			libxml_use_internal_errors($temp);
+
+			if ($this->xml === false) {
+				if (Misc::isCLI()) {
+					$this->error("Your versions XML file ($versions) is broken, try updating from git.");
+				}
+				throw new \Exception("Failed to open versions XML file '$versions'");
+			}
+
+			if ($this->xml->count() > 0) {
+				$vers = $this->xml->xpath('/nzedb/versions');
+
+				if ($vers[0]->count() == 0) {
+					$this->error("Your versions XML file ({nZEDb_VERSIONS}) does not contain version info, try updating from git.");
+					throw new \Exception("Failed to find versions node in XML file '$versions'");
+				} else {
+					$this->primary("Your versions XML file ({nZEDb_VERSIONS}) looks okay, continuing.");
+					$this->_vers = &$this->xml->versions;
+				}
+			} else {
+				throw new \RuntimeException("No elements in file!\n");
+			}
+		}
 	}
 
 	/**
