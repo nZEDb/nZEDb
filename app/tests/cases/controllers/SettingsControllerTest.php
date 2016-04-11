8caeb61bee13dd00e47daad1f587586bc3c34683
niel <niel.archer@gmail.com>
2016-04-11 16:00:17 +0100

    Adding Controller, Model, and tests for Settings table.

---
+++ app/tests/cases/controllers/SettingsControllerTest.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see LICENSE.txt in the base directory.  If
+ * not, see:
+ *
+ * @link      <http://www.gnu.org/licenses/>.
+ * @author    niel
+ * @copyright 2016 nZEDb
+ */
+
+namespace app\tests\cases\controllers;
+
+use app\controllers\SettingsController;
+
+class SettingsControllerTest extends \lithium\test\Unit {
+
+	public function setUp() {}
+
+	public function tearDown() {}
+
+	public function testIndex() {}
+	public function testView() {}
+	public function testAdd() {}
+	public function testEdit() {}
+	public function testDelete() {}
+}
+
+?>
