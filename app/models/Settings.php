868f96c02f1195bf17998b3a77c2421427eecd74
niel <niel.archer@gmail.com>
2016-04-11 18:17:57 +0100

    Update Settings model to specify the key (primary index) for the table.

--- app/models/Settings.php
+++ app/models/Settings.php
@@ -22,6 +22,10 @@ namespace app\models;
 class Settings extends \lithium\data\Model {
 
 	public $validates = [];
+
+	protected $_meta = [
+		'key' => ['section', 'subsection', 'name']
+	];
 }
 
 ?>
