a7046cc38e0c829d81ad88664719c2dfb9626ea6
niel <niel.archer@gmail.com>
2016-04-23 02:27:09 +0100

    Dropping 'manual' git pull as composer should handle that.

--- app/extensions/command/Update.php
+++ app/extensions/command/Update.php
@@ -83,7 +83,6 @@ class Update extends \app\extensions\console\Command
 			$db = new DbUpdate(['backup' => false]);
 			$db->processPatches(['safe' => false]);
 		} else {
-			var_dump($currentDb, $currentXML);
 			$this->out("Up to date.");
 		}
 
@@ -145,6 +144,7 @@ class Update extends \app\extensions\console\Command
 		if (in_array($this->gitBranch, $this->git->getBranchesStable())) {
 			$command .= ' --no-dev';
 		}
+		$this->primary('Running composer install process...');
 		passthru($command);
 	}
 
