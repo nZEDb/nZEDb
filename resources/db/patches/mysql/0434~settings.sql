f3c1050a7d9c4aa83422ec94b1fb1c8ac0f77025
niel <niel.archer@gmail.com>
2016-05-07 18:26:50 +0100

    Fix bad merge of SQL patch.

--- resources/db/patches/mysql/0434~settings.sql
+++ resources/db/patches/mysql/0434~settings.sql
@@ -1,6 +1,2 @@
 # Fix recaptcha enabled setting typo.
 UPDATE settings SET setting = 'recaptchaenabled' WHERE setting = 'recaptchenabled';
-# Move recaptcha site/secret keys to proper setting hierarchy.
-UPDATE settings SET subsection = 'recaptcha', name = 'secretkey' WHERE setting =
-'recaptchasecretkey';
-UPDATE settings SET subsection = 'recaptcha', name = 'sitekey' WHERE setting = 'recaptchasitekey';
