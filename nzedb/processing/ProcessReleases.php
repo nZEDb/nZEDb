42dcffd70fe218a2f1a2e8b244f83125630e1343
DariusIII <dkrisan@gmail.com>
2016-05-11 09:09:28 +0200

    Fix indenting in processStuckCollections function.

--- nzedb/processing/ProcessReleases.php
+++ nzedb/processing/ProcessReleases.php
@@ -1641,13 +1641,13 @@ class ProcessReleases
 		$lastRun = $this->pdo->getSetting('last_run_time');
 		$obj = $this->pdo->queryExec(
 			sprintf("
-                DELETE c, b, p FROM %s c
-                LEFT JOIN %s b ON (c.id=b.collection_id)
-                LEFT JOIN %s p ON (b.id=p.binaryid)
-                WHERE
-                    c.added <
-                    DATE_SUB({$this->pdo->escapeString($lastRun)}, INTERVAL %d HOUR)
-                %s",
+				DELETE c, b, p FROM %s c
+				LEFT JOIN %s b ON (c.id=b.collection_id)
+				LEFT JOIN %s p ON (b.id=p.binaryid)
+				WHERE
+					c.added <
+					DATE_SUB({$this->pdo->escapeString($lastRun)}, INTERVAL %d HOUR)
+					%s",
 				$group['cname'],
 				$group['bname'],
 				$group['pname'],
