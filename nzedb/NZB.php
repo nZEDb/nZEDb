409b33573baa8261e4c058f08bab7f775b8f294a
Bart39 <bart39@sky.com>
2016-05-17 00:03:41 +0100

    re-add split query that got taken out by accident

--- nzedb/NZB.php
+++ nzedb/NZB.php
@@ -255,12 +255,18 @@ class NZB
 			return false;
 		}
 
-		// Mark release as having NZB and delete CBP.
+		// Mark release as having NZB.
 		$this->pdo->queryExec(
 				sprintf('
-				UPDATE releases SET nzbstatus = %d %s WHERE id = %d;
-				DELETE c, b, p FROM %s c JOIN %s b ON(c.id=b.collection_id) STRAIGHT_JOIN %s p ON(b.id=p.binaryid) WHERE c.releaseid = %d',
-						NZB::NZB_ADDED, ($nzb_guid === '' ? '' : ', nzb_guid = UNHEX( ' . $this->pdo->escapestring(md5($nzb_guid)) . ' )'), $relID,
+						UPDATE releases SET nzbstatus = %d %s WHERE id = %d',
+						NZB::NZB_ADDED, ($nzb_guid === '' ? '' : ', nzb_guid = UNHEX( ' . $this->pdo->escapeString(md5($nzb_guid)) . ' )'),
+						$relID
+				)
+		);
+		// Delete CBP for release that has its NZB created.
+		$this->pdo->queryExec(
+				sprintf('
+						DELETE c, b, p FROM %s c JOIN %s b ON(c.id=b.collection_id) STRAIGHT_JOIN %s p ON(b.id=p.binaryid) WHERE c.releaseid = %d',
 						$this->_tableNames['cName'], $this->_tableNames['bName'], $this->_tableNames['pName'], $relID
 				)
 		);
