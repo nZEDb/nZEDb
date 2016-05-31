57429c1703d92d82d0459f91b55e4e48c6a58ae0
ruhllatio <ruhllatio@gmail.com>
2016-05-31 17:04:02 -0400

    Remove one-time PHP populate script in favor of SP

--- resources/db/patches/mysql/+1~release_unique.sql
+++ resources/db/patches/mysql/+1~release_unique.sql
@@ -7,3 +7,37 @@
   ENGINE = MyISAM
   DEFAULT CHARSET = utf8
   COLLATE = utf8_unicode_ci;
+
+  DROP PROCEDURE IF EXISTS populate_uniqueids;
+
+  DELIMITER $$
+  CREATE PROCEDURE populate_uniqueids()
+  LANGUAGE SQL DETERMINISTIC CONTAINS SQL READS SQL DATA COMMENT 'Populates release_unique IDs table'
+    BEGIN
+      DROP TABLE IF EXISTS temp_uniqueids;
+      CREATE TEMPORARY TABLE temp_uniqueids (
+        releases_id INT(11) UNSIGNED NOT NULL,
+        uniqueid VARCHAR(255) CHARSET utf8 COLLATE utf8_unicode_ci,
+        PRIMARY KEY (uniqueid)
+      );
+
+      INSERT IGNORE INTO temp_uniqueids (releases_id, uniqueid)
+        SELECT
+          releases_id,
+          REPLACE(SUBSTRING_INDEX(ExtractValue(mediainfo, '//Unique_ID'), 'x', -1), ')', '')
+        FROM releaseextrafull
+        WHERE mediainfo IS NOT NULL
+        AND LENGTH(mediainfo) > 0;
+
+      DELETE FROM temp_uniqueids WHERE uniqueid NOT REGEXP '^[0-9][A-F]{32}$';
+
+      INSERT INTO release_unique (releases_id, uniqueid)
+        SELECT releases_id, UNHEX(uniqueid) FROM temp_uniqueids;
+
+      DROP TABLE temp_uniqueids;
+
+    END $$
+  DELIMITER ;
+
+  CALL populate_uniqueids();
+  DROP PROCEDURE IF EXISTS populate_uniqueids;
