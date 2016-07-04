70c66bcfac3d2d312169070bb4ee96c4231e968e
niel <niel.archer@gmail.com>
2016-07-04 23:03:35 +0100

    Update DocBlock.

--- nzedb/utility/Versions.php
+++ nzedb/utility/Versions.php
@@ -136,7 +136,8 @@ class Versions
 
 	/**
 	 * Checks the git's latest version tag against the XML's stored value. Version should be
-	 * Major.Minor.Revision (**commit number is NOT revision**)
+	 * Major.Minor.Revision[.fix] (**commit number is NOT revision**)
+	 *
 	 * @param boolean $update Whether the XML should be updated by the check.
 	 * @return boolean The new git's latest version tag, or false.
 	 */
