d0ac72a1f1dfa9a75f0923563a676fe2f2e188e5
niel <niel.archer@gmail.com>
2016-07-04 23:19:50 +0100

    Refactor Sites for app\extensions\util\Versions' methods

--- nzedb/Sites.php
+++ nzedb/Sites.php
@@ -27,7 +27,7 @@ class Sites
 	protected $_db;
 
 	/**
-	 * @var \nzedb\utility\Versions|bool
+	 * @var \app\extensions\util\Versions|bool
 	 */
 	protected $_versions = false;
 
@@ -45,7 +45,7 @@ class Sites
 
 		if (defined('nZEDb_VERSIONS')) {
 			try {
-				$this->_versions = new Versions(['path' => nZEDb_VERSIONS]);
+				$this->_versions = new Versions();
 			} catch (\Exception $e) {
 				$this->_versions = false;
 			}
@@ -54,7 +54,7 @@ class Sites
 
 	public function version()
 	{
-		return ($this->_versions === false ? '0.0.0' : $this->_versions->getTagVersion());
+		return ($this->_versions === false ? '0.0.0' : $this->_versions->getGitTagFromRepo());
 	}
 
 	public function update($form)
