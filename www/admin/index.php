ce0ee3f6abd2eb17ccc55899d738d9bb109f290e
niel <niel.archer@gmail.com>
2016-07-19 12:48:37 +0100

    Add lithium libraries bootstrap for Admin pages (Issue #2215).

--- www/admin/index.php
+++ www/admin/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'smarty.php');
+require_once nZEDb_ROOT . 'app' . DS . 'config' . DS . 'bootstrap' . DS . 'libraries.php';
 
 $page = new AdminPage();
 
