5f8431dab7699b60a548cba12fa5ff2b7f94dd97
niel <niel.archer@gmail.com>
2016-07-27 04:32:22 +0100

    Create new yenc.php file and add it to bootstrap process.

---
+++ app/config/bootstrap/yenc.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see LICENSE.txt in the base directory.  If
+ * not, see:
+ *
+ * @link      <http://www.gnu.org/licenses/>.
+ * @author    niel
+ * @copyright 2016 nZEDb
+ */
+
+// TODO add code to determine which extensions are available and configure default as appropriate.
+
+app\extensions\util\Yenc::config(
+	[
+		'default' => ['adapter' => 'Php'],
+	]
+);
+
+?>
