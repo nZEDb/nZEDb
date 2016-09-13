3ae49321da0f740734134f783e14a9fc3f5e71eb
niel <niel.archer@gmail.com>
2016-09-13 20:11:51 +0100

    Fix categorizeForeign settings lookup to use full setting info.

--- nzedb/Categorize.php
+++ nzedb/Categorize.php
@@ -54,7 +54,7 @@ class Categorize extends Category
 	public function __construct(array $options = [])
 	{
 		parent::__construct($options);
-		$this->categorizeForeign = (Settings::value('categorizeforeign') == "0") ? false : true;
+		$this->categorizeForeign = (Settings::value('indexer.categorise.categorizeforeign') == "0") ? false : true;
 		$this->catWebDL = (Settings::value('catwebdl') == "0") ? false : true;
 		$this->regexes = new Regexes(['Settings' => $this->pdo, 'Table_Name' => 'category_regexes']);
 	}
