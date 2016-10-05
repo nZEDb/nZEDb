8600bec5e5089f30e9907c5aa91587519b5a702a
DariusIII <dkrisan@gmail.com>
2016-10-05 10:29:21 +0200

    Add caching to getPagerCount function to reduce load on indexers with many API calls

--- nzedb/Releases.php
+++ nzedb/Releases.php
@@ -1233,7 +1233,7 @@ class Releases
 				'SELECT COUNT(z.id) AS count FROM (%s LIMIT %s) z',
 				preg_replace('/SELECT.+?FROM\s+releases/is', 'SELECT r.id FROM releases', $query),
 				nZEDb_MAX_PAGER_RESULTS
-			)
+			), true, nZEDb_CACHE_EXPIRY_SHORT
 		);
 		return (isset($count[0]['count']) ? $count[0]['count'] : 0);
 	}
