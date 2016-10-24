7e1a8ed45714d30381bcf1cbaa9babf936d53ca7
niel <niel.archer@gmail.com>
2016-10-24 19:37:05 +0100

    Update two patches to use REPLACE INTO instead of INSERT IGNOT INTO

--- resources/db/patches/mysql/0454~settings.sql
+++ resources/db/patches/mysql/0454~settings.sql
@@ -1,2 +1,3 @@
 # Make sure 'indexer', 'ppa', 'innerfileblacklist' exists in Settings table.
-INSERT IGNORE INTO settings (section, subsection, name, value, hint, setting) VALUES ('indexer', 'ppa', 'innerfileblacklist', '/setup\.exe|password\.url/i', 'You can add a regex here to set releases to potentially passworded when a file name inside a rar/zip matches this regex.', 'innerfileblacklist');
+REPLACE INTO settings (section, subsection, name, value, hint, setting) VALUES ('indexer', 'ppa',
+'innerfileblacklist', '/setup\.exe|password\.url/i', 'You can add a regex here to set releases to potentially passworded when a file name inside a rar/zip matches this regex.', 'innerfileblacklist');
