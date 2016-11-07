7812495874fd0a5d511eb20303179063568aee8c
DariusIII <dkrisan@gmail.com>
2016-11-07 12:31:18 +0100

    Fix movie coverview

--- nzedb/Movie.php
+++ nzedb/Movie.php
@@ -309,7 +309,7 @@ class Movie
 					ORDER BY %s %s %s",
 					$this->showPasswords,
 					$this->getBrowseBy(),
-					(!empty($catsrch) ? 'AND ' . $catsrch : ''),
+					(!empty($catsrch) ? $catsrch : ''),
 					($maxAge > 0
 							? 'AND r.postdate > NOW() - INTERVAL ' . $maxAge . 'DAY '
 							: ''
@@ -363,7 +363,7 @@ class Movie
 			ORDER BY %s %s",
 				(is_array($movieIDs) ? implode(',', $movieIDs) : -1),
 				(is_array($releaseIDs) ? implode(',', $releaseIDs) : -1),
-				(!empty($catsrch) ? 'AND ' . $catsrch : ''),
+				(!empty($catsrch) ? $catsrch : ''),
 				$order[0],
 				$order[1]
 		);
