77921ce8bc95d325fbda518c1b29f0fce98e0ebf
niel <niel.archer@gmail.com>
2016-11-07 11:06:51 +0000

    Fix patch 455 myself.

--- resources/db/patches/mysql/0455~settings.sql
+++ resources/db/patches/mysql/0455~settings.sql
@@ -1,3 +1,3 @@
 # Change reqid server used
 
-REPLACE INTO settings (section, subsection, name, value, hint, setting) VALUES ('', '', 'request_url', 'http://reqid.newznab-tmux.pw/index.php', 'Optional URL to lookup Request IDs.', 'request_url');
+UPDATE settings SET value = 'http://reqid.newznab-tmux.pw/index.php' WHERE section = '' AND subsection = '' AND name = 'request_url';
