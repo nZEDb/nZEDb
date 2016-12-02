29914a995a8323abfd4fbdd3e868d88efa721f6b
niel <niel.archer@gmail.com>
2016-12-02 02:19:46 +0000

    Remove groups_name field, fix formatting

--- resources/db/patches/mysql/+1~releases_groups.sql
+++ resources/db/patches/mysql/+1~releases_groups.sql
@@ -1,10 +1,7 @@
 # Add new table for handling mulitple group per release support.
 CREATE TABLE releases_groups (
-  releases_id INT(11) UNSIGNED NOT NULL DEFAULT '0'
-  COMMENT 'FK to releases.id',
-  groups_id   INT(11) UNSIGNED NOT NULL DEFAULT '0'
-  COMMENT 'FK to groups.id',
-  groups_name  VARCHAR(255)    NOT NULL DEFAULT '',
+  releases_id INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'FK to releases.id',
+  groups_id   INT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'FK to groups.id',
   PRIMARY KEY (releases_id, groups_id)
 )
   ENGINE = MYISAM
