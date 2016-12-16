6550117047c448fc323b40009305c198fef0a5a7
niel <niel.archer@gmail.com>
2016-12-16 16:12:44 +0000

    Update SQL Schema to include the Stored Procedures from the releases_groups changes.

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -1266,6 +1266,7 @@ CREATE TABLE         xxxinfo (
 
 
 DELIMITER $$
+
 CREATE TRIGGER check_insert BEFORE INSERT ON releases FOR EACH ROW
   BEGIN
     IF NEW.searchname REGEXP '[a-fA-F0-9]{32}' OR NEW.name REGEXP '[a-fA-F0-9]{32}'
@@ -1348,5 +1349,81 @@ CREATE TRIGGER insert_MD5 BEFORE INSERT ON release_comments FOR EACH ROW
   SET
     NEW.text_hash = MD5(NEW.text);
     $$
+
+#
+# Stored Procedures
+#
+CREATE PROCEDURE loop_cbpm(IN method CHAR(10))
+  COMMENT 'Performs tasks on All CBPM tables one by one -- REPAIR/ANALYZE/OPTIMIZE or DROP/TRUNCATE'
+
+    main: BEGIN
+    DECLARE done INT DEFAULT 0;
+    DECLARE tname VARCHAR(255) DEFAULT '';
+    DECLARE cur1 CURSOR FOR
+      SELECT TABLE_NAME
+      FROM information_schema.TABLES
+      WHERE
+        TABLE_SCHEMA = (SELECT DATABASE())
+        AND
+        (
+          TABLE_NAME LIKE 'collections%'
+          OR TABLE_NAME LIKE 'parts%'
+          OR TABLE_NAME LIKE 'binaries%'
+          OR TABLE_NAME LIKE 'missed%'
+        )
+      ORDER BY TABLE_NAME ASC;
+    DECLARE CONTINUE HANDLER FOR NOT FOUND SET done = TRUE;
+
+    IF method NOT IN ('repair', 'analyze', 'optimize', 'drop', 'truncate')
+    THEN LEAVE main; END IF;
+
+    OPEN cur1;
+    alter_loop: LOOP FETCH cur1
+    INTO tname;
+      IF done
+      THEN LEAVE alter_loop; END IF;
+      SET @SQL := CONCAT(method, " TABLE ", tname);
+      PREPARE _stmt FROM @SQL;
+      EXECUTE _stmt;
+      DEALLOCATE PREPARE _stmt;
+    END LOOP;
+    CLOSE cur1;
+  END;
+$$
+
+
+CREATE PROCEDURE delete_release(IN is_numeric BOOLEAN, IN identifier VARCHAR(40))
+  COMMENT 'Cascade deletes release from child tables when parent row is deleted'
+  COMMENT 'If is_numeric is true, identifier should be the releases_id, if false the guid'
+
+    main: BEGIN
+
+    DECLARE where_constr VARCHAR(255) DEFAULT '';
+
+    IF is_numeric IS TRUE
+    THEN SET where_constr = CONCAT('r.id = ', identifier);
+
+    ELSEIF is_numeric IS FALSE
+      THEN SET where_constr = CONCAT("r.guid = '", identifier, "'");
+
+    ELSE LEAVE main;
+    END IF;
+
+    DELETE r, rn, rc, uc, rf, ra, rs, rv, re, df, rg
+    FROM releases r
+      LEFT OUTER JOIN release_nfos rn ON rn.releases_id = r.id
+      LEFT OUTER JOIN release_comments rc ON rc.releases_id = r.id
+      LEFT OUTER JOIN users_releases uc ON uc.releases_id = r.id
+      LEFT OUTER JOIN release_files rf ON rf.releases_id = r.id
+      LEFT OUTER JOIN audio_data ra ON ra.releases_id = r.id
+      LEFT OUTER JOIN release_subtitles rs ON rs.releases_id = r.id
+      LEFT OUTER JOIN video_data rv ON rv.releases_id = r.id
+      LEFT OUTER JOIN releaseextrafull re ON re.releases_id = r.id
+      LEFT OUTER JOIN dnzb_failures df ON df.release_id = r.id
+      LEFT OUTER JOIN releases_groups rg ON rg.releases_id = r.id
+    WHERE where_constr;
+
+  END;
+$$
 
 DELIMITER ;
