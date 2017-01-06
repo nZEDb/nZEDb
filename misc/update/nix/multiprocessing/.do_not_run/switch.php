db3635863bf3cb5a9cfacaa610dcc1fba6428a65
niel <niel.archer@gmail.com>
2017-01-06 06:05:39 +0000

    Remove no longer used $pdo parameter from processReleases call.

--- misc/update/nix/multiprocessing/.do_not_run/switch.php
+++ misc/update/nix/multiprocessing/.do_not_run/switch.php
@@ -232,7 +232,7 @@ switch ($options[1]) {
 			collectionCheck($pdo, $options[2]);
 
 			// Create releases.
-			processReleases($pdo, new ProcessReleases(['Settings' => $pdo]), new ReleasesMultiGroup(), $options[2]);
+			processReleases(new ProcessReleases(['Settings' => $pdo]), new ReleasesMultiGroup(), $options[2]);
 
 			// Post process the releases.
 			(new ProcessAdditional(['Echo' => true, 'NNTP' => $nntp, 'Settings' => $pdo]))->start($options[2]);
