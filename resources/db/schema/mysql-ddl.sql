59a4fee00d46b710f1416ba2b7ae8145f3668029
DariusIII <dkrisan@gmail.com>
2017-01-16 10:26:07 +0100

    Update mysql-ddl with new tables

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -530,6 +530,48 @@ CREATE TABLE musicinfo (
   COLLATE = utf8_unicode_ci
   AUTO_INCREMENT = 1;
 
+DROP TABLE IF EXISTS multigroup_collections;
+CREATE TABLE         multigroup_collections (
+  id             INT(11) UNSIGNED    NOT NULL AUTO_INCREMENT,
+  subject        VARCHAR(255)        NOT NULL DEFAULT '',
+  fromname       VARCHAR(255)        NOT NULL DEFAULT '',
+  date           DATETIME            DEFAULT NULL,
+  xref           VARCHAR(510)        NOT NULL DEFAULT '',
+  totalfiles     INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  group_id       INT(11) UNSIGNED    NOT NULL DEFAULT '0',
+  collectionhash VARCHAR(255)        NOT NULL DEFAULT '0',
+  dateadded      DATETIME            DEFAULT NULL,
+  added          TIMESTAMP           NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  filecheck      TINYINT(3) UNSIGNED NOT NULL DEFAULT '0',
+  filesize       BIGINT UNSIGNED     NOT NULL DEFAULT '0',
+  releaseid      INT                 NULL,
+  noise          CHAR(32)            NOT NULL DEFAULT '',
+  PRIMARY KEY                               (id),
+  INDEX        fromname                     (fromname),
+  INDEX        date                         (date),
+  INDEX        group_id                     (group_id),
+  INDEX        ix_collection_filecheck      (filecheck),
+  INDEX        ix_collection_dateadded      (dateadded),
+  INDEX        ix_collection_releaseid      (releaseid),
+  UNIQUE INDEX ix_collection_collectionhash (collectionhash)
+)
+  ENGINE          = MYISAM
+  DEFAULT CHARSET = utf8
+  COLLATE         = utf8_unicode_ci
+  AUTO_INCREMENT  = 1;
+
+DROP TABLE IF EXISTS multigroup_posters;
+CREATE TABLE multigroup_posters (
+  id             INT(11) UNSIGNED    NOT NULL AUTO_INCREMENT,
+  poster       VARCHAR(255)        NOT NULL DEFAULT '',
+  PRIMARY KEY (id) ,
+  UNIQUE KEY (poster)
+)
+  ENGINE          = MYISAM
+  DEFAULT CHARSET = utf8
+  COLLATE         = utf8_unicode_ci
+  AUTO_INCREMENT = 1;
+
 
 DROP TABLE IF EXISTS page_contents;
 CREATE TABLE page_contents (
@@ -1198,11 +1240,11 @@ CREATE TABLE video_data (
 
 DROP TABLE IF EXISTS videos;
 CREATE TABLE videos (
-  id           MEDIUMINT(11) UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT 'Show\'s ID to be used in other tables as reference.',
+  id           MEDIUMINT(11) UNSIGNED  NOT NULL AUTO_INCREMENT COMMENT 'Show ID to be used in other tables as reference.',
   type         TINYINT(1) UNSIGNED     NOT NULL DEFAULT '0' COMMENT '0 = TV, 1 = Film, 2 = Anime',
   title        VARCHAR(180) CHARSET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL COMMENT 'Name of the video.',
   countries_id CHAR(2) COLLATE utf8_unicode_ci NOT NULL DEFAULT '' COMMENT 'Two character country code (FK to countries table).',
-  started      DATETIME                NOT NULL COMMENT 'Date (UTC) of production''s first airing.',
+  started      DATETIME                NOT NULL COMMENT 'Date (UTC) of production first airing',
   anidb        MEDIUMINT(11) UNSIGNED NOT NULL DEFAULT '0'  COMMENT 'ID number for anidb site',
   imdb         MEDIUMINT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'ID number for IMDB site (without the ''tt'' prefix).',
   tmdb         MEDIUMINT(11) UNSIGNED NOT NULL DEFAULT '0' COMMENT 'ID number for TMDB site.',
@@ -1263,6 +1305,14 @@ CREATE TABLE         xxxinfo (
   COLLATE         = utf8_unicode_ci
   AUTO_INCREMENT  = 1;
 
+DROP TABLE IF EXISTS multigroup_binaries;
+CREATE TABLE multigroup_binaries LIKE binaries;
+
+DROP TABLE IF EXISTS multigroup_parts;
+CREATE TABLE multigroup_parts LIKE parts;
+
+DROP TABLE IF EXISTS multigroup_missed_parts;
+CREATE TABLE multigroup_missed_parts LIKE missed_parts;
 
 
 DELIMITER $$
