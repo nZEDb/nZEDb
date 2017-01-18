d6a5957461373cdd59355316711f1982a85d17db
niel <niel.archer@gmail.com>
2017-01-18 19:55:43 +0000

    Fix missing comma.

--- resources/db/patches/mysql/+1~settings.sql
+++ resources/db/patches/mysql/+1~settings.sql
@@ -3,6 +3,6 @@
 # redirected to the login page immediately. Default behaviour is to show the page.
 INSERT IGNORE INTO settings (section, subsection, name, value, hint, setting) VALUES ('site',
 'main', 'incognito', 0,
-'Determines whether a vistor that has not logged, in sees the default page or is redirected to the login page immediately. Default behaviour is to show the page.'
+'Determines whether a vistor that has not logged, in sees the default page or is redirected to the login page immediately. Default behaviour is to show the page.',
 'site.main.incognito'
 );
