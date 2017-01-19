150f6a0648baf2c9f419fd43ade7d0e52b1718c5
niel <niel.archer@gmail.com>
2017-01-19 16:48:43 +0000

    Fix class name, now it is not aliased.

--- app/models/Groups.php
+++ app/models/Groups.php
@@ -38,7 +38,7 @@ class Groups extends \lithium\data\Model
 
 	public static function findByID($groupID)
 	{
-		return GroupInfo::find('first', ['conditions' => ['id' => $groupID]]);
+		return Group::find('first', ['conditions' => ['id' => $groupID]]);
 	}
 
 	/**
