8729903ad20c306bf6fb2d9bfa6e9474aa4d5560
niel <niel.archer@gmail.com>
2017-01-20 20:09:49 +0000

    Simplify page counts.

--- www/admin/group-list.php
+++ www/admin/group-list.php
@@ -10,13 +10,15 @@ $groups = new Groups(['Settings' => $page->settings]);
 $groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ? $_REQUEST['groupname'] : '');
 $pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
 
+$count = $groups->getCount($groupName, -1);
+
 $page->smarty->assign(
 	[
 		'grouplist'			=> GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName),
 		'groupname'			=> $groupName,
 		'pagecurrent'		=> $pageno,
-		'pagemaximum'		=> (int)($groups->getCount($groupName, -1) / ITEMS_PER_PAGE) + 1,
-		'pagertotalitems'	=> $groups->getCount($groupName, -1),
+		'pagemaximum'		=> (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagertotalitems'	=> $count,
 		'pagerquerybase'	=>
 			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'page='
 	]
