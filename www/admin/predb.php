23dabecf9ed1646f254b323cc1092b0199b6bc14
niel <niel.archer@gmail.com>
2017-01-20 20:11:07 +0000

    Quick changes to test pager.tpl with PreDb list (something larger than Groups' five pages).

--- www/admin/predb.php
+++ www/admin/predb.php
@@ -9,37 +9,6 @@ $page->meta_title = "View PreDb info";
 $page->meta_keywords = "view,predb,info,description,details";
 $page->meta_description = "View PreDb info";
 
-$predb = new PreDb();
-
-$offset = (isset($_REQUEST["offset"]) && ctype_digit($_REQUEST['offset'])) ? $_REQUEST["offset"] : 0;
-
-if (isset($_REQUEST['presearch'])) {
-	$lastSearch = $_REQUEST['presearch'];
-	$parr = $predb->getAll($offset, ITEMS_PER_PAGE, $_REQUEST['presearch']);
-} else {
-	$lastSearch = '';
-	$parr = $predb->getAll($offset, ITEMS_PER_PAGE);
-}
-
-// TODO modelise.
-$count = $parr['count'];
-$page->smarty->assign('lastSearch', $lastSearch);
-
-$page->smarty->assign('results', $parr['arr']);
-
-
-$pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
-$page->smarty->assign(
-	[
-		'pagecurrent'      => (int)$pageno,
-		'pagemaximum'      => (int)($count / ITEMS_PER_PAGE) + 1,
-		'pager'            => $page->smarty->fetch("paginate.tpl"),
-		'pagerquerybase'   => WWW_TOP . "/predb.php?page=",
-		'pagerquerysuffix' => '#results',
-		'pagertotalitems'  => $count,
-	]
-);
-
 $count = Predb::findRangeCount($_REQUEST['presearch']);
 $lastsearch = isset($_REQUEST['presearch']) ? $_REQUEST['presearch'] : '';
 
