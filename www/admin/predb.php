881b4c5a077ef04006d7dc2abc97c0a6d0b12c7b
niel <niel.archer@gmail.com>
2017-01-20 20:11:07 +0000

    Quick changes to test pager.tpl with PreDb list (something larger than Groups' five pages).

--- www/admin/predb.php
+++ www/admin/predb.php
@@ -4,6 +4,38 @@ require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'smarty.php');
 use app\models\Predb;
 
 $page = new AdminPage();
+
+$predb = new PreDb();
+
+$offset = (isset($_REQUEST["offset"]) && ctype_digit($_REQUEST['offset'])) ? $_REQUEST["offset"] : 0;
+
+if (isset($_REQUEST['presearch'])) {
+	$lastSearch = $_REQUEST['presearch'];
+	$parr = $predb->getAll($offset, ITEMS_PER_PAGE, $_REQUEST['presearch']);
+} else {
+	$lastSearch = '';
+	$parr = $predb->getAll($offset, ITEMS_PER_PAGE);
+}
+
+$pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
+$page->smarty->assign(
+	[
+		'pagecurrent'     => $pageno,
+		'pagemaximum'     => (int)($parr['count'] / ITEMS_PER_PAGE) + 1,
+	]
+);
+
+$page->smarty->assign('pagertotalitems', $parr['count']);
+$page->smarty->assign('pageroffset', $offset);
+$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
+$page->smarty->assign('pagerquerybase', WWW_TOP . "/predb.php?page=");
+$page->smarty->assign('pagerquerysuffix', "#results");
+$page->smarty->assign('lastSearch', $lastSearch);
+
+$page->smarty->assign('pager', $page->smarty->fetch("pager.tpl"));
+$page->smarty->assign('results', $parr['arr']);
+
+
 $page->title = "Browse PreDb";
 $page->meta_title = "View PreDb info";
 $page->meta_keywords = "view,predb,info,description,details";
