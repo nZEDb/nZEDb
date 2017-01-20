ae198fa816de48827ffdff04b01e68ed378e80c8
niel <niel.archer@gmail.com>
2017-01-20 18:32:26 +0000

    Cleanup no longer used pager stuff.

--- www/admin/group-list.php
+++ www/admin/group-list.php
@@ -8,21 +8,17 @@ $page   = new AdminPage();
 $groups = new Groups(['Settings' => $page->settings]);
 
 $groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ? $_REQUEST['groupname'] : '');
-$offset = (isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0);
 $pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
 
 $page->smarty->assign(
 	[
+		'grouplist'			=> GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName),
+		'groupname'			=> $groupName,
 		'pagecurrent'		=> $pageno,
 		'pagemaximum'		=> (int)($groups->getCount($groupName, -1) / ITEMS_PER_PAGE) + 1,
-		'groupname' 		=> $groupName,
 		'pagertotalitems'	=> $groups->getCount($groupName, -1),
-		'pageroffset'		=> $offset,
-		'pageritemsperpage'	=> ITEMS_PER_PAGE,
 		'pagerquerybase'	=>
-			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'page=',
-		'pagerquerysuffix'	=> '',
-		'grouplist'			=> GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName)
+			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'page='
 	]
 );
 $page->smarty->assign('pager', $page->smarty->fetch('pager.tpl'));
