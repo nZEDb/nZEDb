72bb3872511663f6bcd91430a80d8dfd5332ce20
niel <niel.archer@gmail.com>
2017-01-20 18:28:53 +0000

    Modify group-list-php to use shared pager.tpl

--- www/admin/group-list.php
+++ www/admin/group-list.php
@@ -1,24 +1,28 @@
 <?php
 require_once './config.php';
 
+use app\models\Groups as GroupInfo;
 use nzedb\Groups;
 
 $page   = new AdminPage();
 $groups = new Groups(['Settings' => $page->settings]);
 
 $groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ? $_REQUEST['groupname'] : '');
-$offset    = (isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0);
+$offset = (isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0);
+$pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
 
 $page->smarty->assign(
 	[
-		'groupname' => $groupName,
-		'pagertotalitems' => $groups->getCount($groupName, -1),
-		'pageroffset' => $offset,
-		'pageritemsperpage' => ITEMS_PER_PAGE,
-		'pagerquerybase' =>
-			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'offset=',
-		'pagerquerysuffix' => '',
-		'grouplist' => $groups->getRange($offset, ITEMS_PER_PAGE, $groupName, -1)
+		'pagecurrent'		=> $pageno,
+		'pagemaximum'		=> (int)($groups->getCount($groupName, -1) / ITEMS_PER_PAGE) + 1,
+		'groupname' 		=> $groupName,
+		'pagertotalitems'	=> $groups->getCount($groupName, -1),
+		'pageroffset'		=> $offset,
+		'pageritemsperpage'	=> ITEMS_PER_PAGE,
+		'pagerquerybase'	=>
+			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'page=',
+		'pagerquerysuffix'	=> '',
+		'grouplist'			=> GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName)
 	]
 );
 $page->smarty->assign('pager', $page->smarty->fetch('pager.tpl'));
