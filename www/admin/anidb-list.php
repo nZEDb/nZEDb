acae33753133775092651c29be069db9ec358657
niel <niel.archer@gmail.com>
2017-01-28 20:13:54 +0000

    Update anidb-list.php to use new pager methodology.

--- www/admin/anidb-list.php
+++ www/admin/anidb-list.php
@@ -1,6 +1,7 @@
 <?php
 require_once './config.php';
 
+use app\models\AnidbTitles;
 use nzedb\AniDB;
 
 $page  = new AdminPage();
@@ -8,27 +9,29 @@ $AniDB = new AniDB(['Settings' => $page->settings]);
 
 $page->title = 'AniDB Titles';
 
-$aname = '';
+$name = '';
+
 if (isset($_REQUEST['animetitle']) && !empty($_REQUEST['animetitle'])) {
-	$aname = $_REQUEST['animetitle'];
+	$name = $_REQUEST['animetitle'];
 }
 
-$animecount = $AniDB->getAnimeCount($aname);
-
-$offset  = isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0;
-$asearch = ($aname != '') ? 'animetitle=' . $aname . '&amp;' : '';
-
-$page->smarty->assign('pagertotalitems', $animecount);
-$page->smarty->assign('pageroffset', $offset);
-$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
-$page->smarty->assign('pagerquerybase', WWW_TOP . '/anidb-list.php?' . $asearch . '&offset=');
+$options = $name == '' ? [] : ['conditions' => ['title' => ['LIKE' => "%$name%"]]];
+$count = AnidbTitles::find('count', $options);
+$pageno = isset($_REQUEST['page']) ? $_REQUEST['page'] : 1;
 $pager = $page->smarty->fetch('pager.tpl');
-$page->smarty->assign('pager', $pager);
+$search = ($name != '') ? 'animetitle=' . $name . '&amp;' : '';
+$page->smarty->assign(
+	[
+		'anidblist'			=> (AnidbTitles::findRange($pageno, ITEMS_PER_PAGE, $name)),
+		'animetitle'		=> $name,
+		'pagecurrent'		=> (int)$pageno,
+		'pagemaximum'		=> (int)($count / ITEMS_PER_PAGE) + 1,
+		'pager'				=> $pager,
+		'pagertotalitems'	=> $count,
+		'pagerquerybase'	=> WWW_TOP . '/anidb-list.php?' . $search . '&page='
+	]
+);
 
-$page->smarty->assign('animetitle', $aname);
-
-$anidblist = $AniDB->getAnimeRange($offset, ITEMS_PER_PAGE, $aname);
-$page->smarty->assign('anidblist', $anidblist);
 
 $page->content = $page->smarty->fetch('anidb-list.tpl');
 $page->render();
