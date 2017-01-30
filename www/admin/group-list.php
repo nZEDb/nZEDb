118dea95f598cb5388d5d5e99d090c737e69cb1f
niel <niel.archer@gmail.com>
2017-01-30 23:26:21 +0000

    Clean up pager stuff for group-list page.

--- www/admin/group-list.php
+++ www/admin/group-list.php
@@ -7,23 +7,21 @@ use nzedb\Groups;
 $page   = new AdminPage();
 $groups = new Groups(['Settings' => $page->settings]);
 
+$count = $groups->getCount($groupName, -1);
 $groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ? $_REQUEST['groupname'] : '');
 $pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
-
-$count = $groups->getCount($groupName, -1);
-
+$search = $groupName != '' ? "groupname=$groupName&amp;" : '';
 $page->smarty->assign(
 	[
-		'grouplist'			=> (GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName))->to('array'),
+		'grouplist'			=> GroupInfo::findRange($pageno, ITEMS_PER_PAGE, $groupName)->to('array'),
 		'groupname'			=> $groupName,
 		'pagecurrent'		=> (int)$pageno,
 		'pagemaximum'		=> (int)($count / ITEMS_PER_PAGE) + 1,
+		'pager'				=> $page->smarty->fetch('pager.tpl'),
 		'pagertotalitems'	=> $count,
-		'pagerquerybase'	=>
-			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'page='
+		'pagerquerybase'	=> WWW_TOP . "/group-list.php?" . $search . 'page='
 	]
 );
-$page->smarty->assign('pager', $page->smarty->fetch('pager.tpl'));
 
 $page->title = 'Group List';
 $page->content = $page->smarty->fetch('group-list.tpl');
