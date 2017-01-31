76602ab56725cdaf69886d79ba3fa26dc6b21c7f
niel <niel.archer@gmail.com>
2017-01-31 00:23:52 +0000

    Convert category_regexes-list to use Model and new pager.

--- www/admin/category_regexes-list.php
+++ www/admin/category_regexes-list.php
@@ -1,25 +1,41 @@
 <?php
 require_once './config.php';
 
+use app\models\CategoryRegexes;
 use nzedb\Regexes;
 
 $page = new AdminPage();
-$regexes = new Regexes(['Settings' => $page->settings, 'Table_Name' => 'category_regexes']);
-
 $page->title = "Category Regex List";
 
 $group = ((isset($_REQUEST['group']) && !empty($_REQUEST['group'])) ? $_REQUEST['group'] : '');
-$offset = (isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0);
-$regex = $regexes->getRegex($group, ITEMS_PER_PAGE, $offset);
-$count = $regexes->getCount($group);
+$pageno = (isset($_REQUEST["page"]) ? $_REQUEST["page"] : 1);
+
+$options = [];
+if ($group != '') {
+	$options += ['conditions' => ['group_regex' => ['LIKE' => "%$name%"]]];
+}
+
+$count = CategoryRegexes::find('count', $options);
+
+if ($count > ITEMS_PER_PAGE) {
+	$options += [
+		'limit' => ITEMS_PER_PAGE,
+		'order' => 'id',
+		'page'  => $pageno,
+	];
+} else {
+	$options += ['order' => 'id'];
+}
+$regex = CategoryRegexes::find('all', $options)->to('array');
 
 $page->smarty->assign(
 	[
 		'group'				=> $group,
+		'pagecurrent'		=> (int)$pageno,
 		'pagemaximum'		=> (int)($count / ITEMS_PER_PAGE) + 1,
-		'pager'				=> $page->smarty->fetch("pager.tpl"),
-		'pagerquerybase'	=> (WWW_TOP . "/category_regexes-list.php?" . $group . "offset="),
-		'pagertotalitems'	=> (int)$count,
+		'pager'				=> $page->smarty->fetch('pager.tpl'),
+		'pagerquerybase'	=> (WWW_TOP . "/category_regexes-list.php?" . $group . "page="),
+		'pagertotalitems'	=> $count,
 		'regex'				=> $regex,
 	]
 );
