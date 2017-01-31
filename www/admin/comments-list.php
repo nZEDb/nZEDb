75cb09a5cc1399efd6fab184becbce2675e3749a
niel <niel.archer@gmail.com>
2017-01-31 04:33:29 +0000

    Convert comments-list page to use Models and new pager.

--- www/admin/comments-list.php
+++ www/admin/comments-list.php
@@ -1,24 +1,25 @@
 <?php
 require_once './config.php';
 
-use nzedb\ReleaseComments;
+use app\models\ReleaseComments;
 
-$page     = new AdminPage();
-$releases = new ReleaseComments($page->settings);
-
+$page = new AdminPage();
 $page->title = "Comments List";
 
-$commentcount = $releases->getCommentCount();
-$offset       = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$page->smarty->assign('pagertotalitems', $commentcount);
-$page->smarty->assign('pageroffset', $offset);
-$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
-$page->smarty->assign('pagerquerybase', WWW_TOP . "/comments-list.php?offset=");
+$count = ReleaseComments::find('count', []);
+$pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
+
+$page->smarty->assign(
+	[
+		'commentslist'    => ReleaseComments::findRange($pageno, ITEMS_PER_PAGE)->to('array'),
+		'pagecurrent'     => (int)$pageno,
+		'pagemaximum'     => (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagertotalitems' => $count,
+		'pagerquerybase'  => WWW_TOP . "/comments-list.php?page="
+	]
+);
 $pager = $page->smarty->fetch("pager.tpl");
 $page->smarty->assign('pager', $pager);
-
-$commentslist = $releases->getCommentsRange($offset, ITEMS_PER_PAGE);
-$page->smarty->assign('commentslist', $commentslist);
 
 $page->content = $page->smarty->fetch('comments-list.tpl');
 $page->render();
