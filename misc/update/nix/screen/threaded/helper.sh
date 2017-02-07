422086ad5e541f38474d068506947560968f5fea
kaibosh <kaibosh@users.noreply.github.com>
2017-02-07 11:22:38 +0100

    Standardise THREADED_PATH env variable across sequential scripts.

--- misc/update/nix/screen/threaded/helper.sh
+++ misc/update/nix/screen/threaded/helper.sh
@@ -8,7 +8,7 @@ else
 fi
 
 export NZEDB_PATH="${NZEDB_ROOT}/misc/update"
-export THREAD_PATH="${NZEDB_ROOT}/misc/update/nix/multiprocessing"
+export THREADED_PATH="${NZEDB_ROOT}/misc/update/nix/multiprocessing"
 export TEST_PATH="${NZEDB_ROOT}/misc/testing"
 command -v php5 >/dev/null 2>&1 && export PHP=`command -v php5` || { export PHP=`command -v php`; }
 export NZEDB_SLEEP_TIME="60"
@@ -17,7 +17,7 @@ while :
 do
 
 	cd ${NZEDB_PATH}
-	$PHP $THREAD_PATH/releases.php 
+	$PHP $THREADED_PATHED/releases.php 
 	cd ${TEST_PATH}
 	$PHP ${TEST_PATH}/Release/removeCrapReleases.php true 1
 
