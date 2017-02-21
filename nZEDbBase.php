d6e6ad7d33d433140656f2cff68147273fb72eb2
niel <niel.archer@gmail.com>
2017-02-21 00:15:38 +0000

    Update nZEDbBase.php file to be able to return any of the 'constant' values.

--- nZEDbBase.php
+++ nZEDbBase.php
@@ -1,1 +1,15 @@
-<?php exit(dirname(__FILE__)); ?>
+<?php
+include_once 'nzedb/constants.php';
+
+if ($argc > 1) {
+	$constant = $argv[1];
+	include_once 'nzedb/constants.php';
+	if (defined($constant)) {
+		exit(constant($constant));
+	}
+
+}
+
+exit(dirname(__FILE__));
+
+?>
