452ec95f52499ac923b8446a51e82844df722f3b
niel <niel.archer@gmail.com>
2017-02-25 03:06:04 +0000

    Check memory_limit for a real value, not 1024 bytes.

--- nzedb/utility/Misc.php
+++ nzedb/utility/Misc.php
@@ -537,6 +537,23 @@ class Misc
 		return trim(preg_replace('/[^\w\s.-]*/i', '', $filename));
 	}
 
+	public static function returnBytes($val)
+	{
+		$val = trim($val);
+		$last = strtolower($val[strlen($val) - 1]);
+		switch ($last) {
+			// The 'G' modifier is available since PHP 5.1.0
+			case 'g':
+				$val *= 1024;
+			case 'm':
+				$val *= 1024;
+			case 'k':
+				$val *= 1024;
+		}
+
+		return $val;
+	}
+
 	/**
 	 * Get human readable size string from bytes.
 	 *
