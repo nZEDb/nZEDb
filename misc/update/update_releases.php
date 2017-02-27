398e7284630b222fcb08f0db4b53f2f97d8dc5a9
DariusIII <dkrisan@gmail.com>
2017-02-27 12:17:45 +0100

    Change error message in single-threaded update_releases.

--- misc/update/update_releases.php
+++ misc/update/update_releases.php
@@ -1,10 +1,11 @@
 <?php
 require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
-use app\models\Settings;
+use nzedb\db\DB;
+
+$pdo = new DB();
 
 
-if (Settings::value('..tablepergroup') === 1) {
-	exit($pdo->log->error("Non-threaded update_releases.php is not supported anymore.You must use .../misc/update/nix/multiprocessing/releases.php"));
-}
+exit($pdo->log->error('Non-threaded update_releases.php is not supported anymore.You must use /misc/update/nix/multiprocessing/releases.php'));
+
 
