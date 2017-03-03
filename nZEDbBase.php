3e24c7abefe47fe8804f0c3313b5af23dd0c730c
niel <niel.archer@gmail.com>
2017-03-03 16:23:39 +0000

    Change include_once to require_once in nZEDbBase.php

--- nZEDbBase.php
+++ nZEDbBase.php
@@ -1,12 +1,12 @@
 <?php
 
 if ($argc > 1) {
-	include_once 'nzedb/constants.php';
 	$constant = $argv[1];
+	require_once 'nzedb/constants.php';
+
 	if (defined($constant)) {
 		exit(constant($constant));
 	}
-
 }
 
 exit(dirname(__FILE__));
