b84b45629fd40ab4a6ba2f28ccbfbf3c9dac32fb
niel <niel.archer@gmail.com>
2017-03-09 09:09:12 +0000

    Add better error reporting for creating admin user in setup process.

--- www/install/step6.php
+++ www/install/step6.php
@@ -19,6 +19,8 @@ $cfg = $cfg->getSession();
 if ($page->isPostBack()) {
 	$cfg->doCheck = true;
 
+	$cfg->error = false;
+	$cfg->errMessage = '';
 	$cfg->ADMIN_USER = trim($_POST['user']);
 	$cfg->ADMIN_FNAME = trim($_POST['fname']);
 	$cfg->ADMIN_LNAME = trim($_POST['lname']);
@@ -27,6 +29,7 @@ if ($page->isPostBack()) {
 
 	if ($cfg->ADMIN_USER == '' || $cfg->ADMIN_PASS == '' || $cfg->ADMIN_EMAIL == '') {
 		$cfg->error = true;
+		$cfg->errMessage = "One or more of User, Password, and Email are empty!";
 	} else {
 		switch (DB_SYSTEM) {
 			case 'mysql':
@@ -63,23 +66,28 @@ if ($page->isPostBack()) {
 		$user = new Users();
 		if (!$user->isValidUsername($cfg->ADMIN_USER)) {
 			$cfg->error = true;
+			$cfg->errMessage = "invalid user name '{$cfg->ADMIN_USER}'";
 			$cfg->ADMIN_USER = '';
 		} else {
 			$usrCheck = $user->getByUsername($cfg->ADMIN_USER);
 			if ($usrCheck) {
 				$cfg->error = true;
+				$cfg->errMessage = "'{$cfg->ADMIN_USER}' matches an existing user name!";
 				$cfg->ADMIN_USER = '';
 			}
 		}
 		if (!$user->isValidEmail($cfg->ADMIN_EMAIL)) {
 			$cfg->error = true;
+			$cfg->errMessage = "invalid email '{$cfg->ADMIN_EMAIL}'";
 			$cfg->ADMIN_EMAIL = '';
 		}
 
 		if (!$cfg->error) {
 			$cfg->adminCheck = $user->add($cfg->ADMIN_USER, $cfg->ADMIN_FNAME, $cfg->ADMIN_LNAME, $cfg->ADMIN_PASS, $cfg->ADMIN_EMAIL, 2, '');
 			if (!is_numeric($cfg->adminCheck)) {
+				//var_dump($user->pdo->errorInfo());
 				$cfg->error = true;
+				$cfg->errMessage = "Failed to add user '{$cfg->ADMIN_USER}', reason unknown!";
 			} else {
 				$user->login($cfg->adminCheck, "", 1);
 			}
@@ -92,7 +100,6 @@ if ($page->isPostBack()) {
 		die();
 	}
 }
-
 $page->smarty->assign('cfg', $cfg);
 $page->smarty->assign('page', $page);
 
