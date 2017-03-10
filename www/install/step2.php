da805ce602f8e55ae161dbfe4ed3bb84d6d3b573
niel <niel.archer@gmail.com>
2017-03-10 22:40:48 +0000

    Add new RuntimeException codes to catch block. Trigger a user error, that will be logged to the error log.

--- www/install/step2.php
+++ www/install/step2.php
@@ -112,8 +112,11 @@ if ($page->isPostBack()) {
 				case 1:
 				case 2:
 				case 3:
+				case 4:
+				case 5:
 					$cfg->error    = true;
 					$cfg->emessage = $e->getMessage();
+					trigger_error($e->getMessage(), E_USER_WARNING);
 					break;
 				default:
 					var_dump($e);
