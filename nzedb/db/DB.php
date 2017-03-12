9b6cfbba3e91b3840be89f0bce363e7febc57fe1
niel <niel.archer@gmail.com>
2017-03-12 13:49:47 +0000

    ... and for DB::reconnect();

--- nzedb/db/DB.php
+++ nzedb/db/DB.php
@@ -1427,7 +1427,18 @@ class DB extends \PDO
 	 */
 	protected function reconnect()
 	{
-		$this->connect();
+		$this->connect([
+			'checkVersion' => false,
+			'createDb'     => false,
+			'dbhost'       => $this->host,
+			'dbname'       => $this->name, // '' means it is a Sphinx connection
+			'dbpass'       => $this->password,
+			'dbport'       => $this->port,
+			'dbsock'       => $this->socket,
+			'dbtype'       => $this->dbSystem,
+			'dbuser'       => $this->user,
+			'persist'      => $this->persist,
+		]);
 
 		return $this->ping();
 	}
