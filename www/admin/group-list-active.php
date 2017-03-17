d106a2184bddcf855b38001d2e3cc6ea1ac8c989
niel <niel.archer@gmail.com>
2017-03-17 19:50:54 +0000

    Finish converting group-list*.php pages to models use.

--- www/admin/group-list-active.php
+++ www/admin/group-list-active.php
@@ -1,42 +1,28 @@
 <?php
 require_once './config.php';
 
-use nzedb\Groups;
+use app\models\Groups;
 
-$page   = new AdminPage();
+$page = new AdminPage();
 $page->title = "Group List";
 
-$groups = new Groups(['Settings' => $page->settings]);
+$groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ?
+	$_REQUEST['groupname'] : '');
+$search = $groupName != '' ? "groupname=$groupName&amp;" : '';
 
-$gname = "";
-if (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) {
-	$gname = $_REQUEST['groupname'];
-}
-
-// TODO modelise.
-$count = $groups->getCount($gname, 1);
-
-$offset    = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$groupname = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) ?
-	$_REQUEST['groupname'] : '';
-
-$page->smarty->assign('groupname', $groupname);
-$page->smarty->assign('pageroffset', $offset);
-
-$groupsearch = ($gname != "") ? 'groupname=' . $gname . '&amp;' : '';
-
-$grouplist = $groups->getRange($offset, ITEMS_PER_PAGE, $gname, 1);
-$page->smarty->assign('grouplist', $grouplist);
+$count = Groups::findRangeCount($groupName, 1);
 
 $pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
 $page->smarty->assign(
 	[
-		'pagecurrent'      => (int)$pageno,
-		'pagemaximum'      => (int)($count / ITEMS_PER_PAGE) + 1,
-		'pager'            => $page->smarty->fetch("paginate.tpl"),
-		'pagerquerybase'   => WWW_TOP . "/group-list-active.php?" . $groupsearch . "offset=",
-		'pagerquerysuffix' => '',
-		'pagertotalitems'  => $count,
+		'grouplist'			=> Groups::findRange($pageno, ITEMS_PER_PAGE, $groupName, 1)->to('array'),
+		'groupname'			=> $groupName,
+		'pagecurrent'		=> (int)$pageno,
+		'pagemaximum'		=> (int)($count / ITEMS_PER_PAGE) + 1,
+		'pager'				=> $page->smarty->fetch("paginate.tpl"),
+		'pagerquerybase'	=> WWW_TOP . "/group-list-active.php?" . $search . "offset=",
+		'pagerquerysuffix'	=> '',
+		'pagertotalitems'	=> $count,
 	]
 );
 
