e616b84078791809276a324767b1534e3e472e26
niel <niel.archer@gmail.com>
2017-03-19 19:30:42 +0000

    Adding Gamesinfo and Genres model files.

---
+++ app/models/Gamesinfo.php
@@ -0,0 +1,52 @@
+<?php
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License
+ * along with this program (see LICENSE.txt in the base directory.  If
+ * not, see:
+ *
+ * @link      <http://www.gnu.org/licenses/>.
+ * @author    niel
+ * @copyright 2017 nZEDb
+ */
+namespace app\models;
+
+
+/**
+ * Games - class for games table.
+ *
+ * @package app\models
+ */
+class Gamesinfo extends \lithium\data\Model
+{
+//	public $belongsTo = ['Genres'];
+
+	public $hasMany;
+
+	public $hasOne;
+
+	public $validates = [];
+
+	public static function findRange($page = 1, $limit = ITEMS_PER_PAGE)
+	{
+		$options = [
+			'limit' => $limit,
+			'order' => ['name' => 'ASC'],
+			'page'  => (int)$page,
+			'with'	=> ['Genres'],
+		];
+
+		$result = Gamesinfo::find('all', $options);
+
+		return $result;
+	}
+}
+
+?>
