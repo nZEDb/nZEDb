f486a078e264a6e0fee72564232973fe95ede552
niel <niel.archer@gmail.com>
2017-03-25 18:08:23 +0000

    Update import predb daily batch script to handle changed layout on github. Also adds the date of each file to successful download message.

--- cli/data/predb_import_daily_batch.php
+++ cli/data/predb_import_daily_batch.php
@@ -68,102 +68,120 @@ $url         = 'https://api.github.com/repos/nZEDb/nZEDbPre_Dumps/contents/dumps
 $filePattern = '(?P<filename>(?P<stamp>\d+)_predb_dump\.csv\.gz)';
 
 if (nZEDb_DEBUG) {
-	echo "Fetching predb_dump list from GitHub\n";
+	echo "Fetching predb_dump directory list from GitHub\n";
 }
 
-$result = Misc::getUrl(
-		[
-		   'url'            => $url,
-		   'requestheaders' => [
-				   'Content-Type: application/json',
-				   'User-Agent: nZEDb'
-		   ]
-	   ]);
+$result = getDirListing($url);
 
-if ($result === false) {
-	exit('Error connecting to GitHub, try again later?' . PHP_EOL);
-}
+$dirs = json_decode($result, true);
 
-if (nZEDb_DEBUG) {
-	echo "Extracting filenames from list.\n";
-}
-
-$data = json_decode($result, true);
-if (is_null($data)) {
+if (is_null($dirs) ||
+	(isset($dirs['message']) && substr($dirs['message'], 0, 27) == 'API rate limit exceeded for')) {
 	exit("Error: $result");
 }
 
-$total = count($data) - 1;
+
+if (nZEDb_DEBUG) {
+	echo "Fetching predb_dump lists from GitHub\n";
+}
+
+foreach ($dirs as $dir) {
+	if ($dir['name'] == "0README.txt") {
+		continue;
+	}
+
+	$result = getDirListing($url . $dir['name'] . '/');
+
+	if (nZEDb_DEBUG) {
+		echo "Extracting filenames from list.\n";
+	}
+
+	$temp = json_decode($result, true);
+	if (is_null($temp)) {
+		exit("Error: $result");
+	}
+
+	$data[$dir['name']] = $temp;
+}
+
+$total = 0;
+foreach ($data as $dir => $files) {
+	$total += count($files);
+}
+$total -= 1;
+
 $predb = new PreDb();
 
 $progress = $predb->progress(settings_array());
 
-foreach ($data as $file) {
-	if (preg_match("#^https://raw\.githubusercontent\.com/nZEDb/nZEDbPre_Dumps/master/dumps/$filePattern$#",
-				   $file['download_url'])) {
-		if (preg_match("#^$filePattern$#", $file['name'], $match)) {
-			$timematch = $progress['last'];
+foreach ($data as $dir => $files) {
+	foreach ($files as $file) {
+		//var_dump($file);
+		if (preg_match("#^https://raw\.githubusercontent\.com/nZEDb/nZEDbPre_Dumps/master/dumps/$dir/$filePattern$#",
+			$file['download_url'])) {
+			if (preg_match("#^$filePattern$#", $file['name'], $match)) {
+				$timematch = $progress['last'];
 
-			// Skip patches the user does not want.
-			if ($match[1] < $timematch) {
-				echo 'Skipping dump ' . $match[2] .
-					 ', as your minimum unix time argument is ' .
-					 $timematch . PHP_EOL;
-				--$total;
-				continue;
-			}
-
-			// Download the dump.
-			$dump = Misc::getUrl(['url' => $file['download_url']]);
-			echo "Downloading: {$file['download_url']}\n";
-
-			if (!$dump) {
-				echo "Error downloading dump {$match[2]} you can try manually importing it." .
-					 PHP_EOL;
-				continue;
-			} else {
-				if (nZEDb_DEBUG) {
-					echo "Dump {$match[2]} downloaded\n";
+				// Skip patches the user does not want.
+				if ($match[1] < $timematch) {
+					echo 'Skipping dump ' . $match[2] .
+						', as your minimum unix time argument is ' .
+						$timematch . PHP_EOL;
+					--$total;
+					continue;
 				}
-			}
 
-			// Make sure we didn't get an HTML page.
-			if (strpos($dump, '<!DOCTYPE html>') !== false) {
-				echo "The dump file {$match[2]} might be missing from GitHub." . PHP_EOL;
-				continue;
-			}
+				// Download the dump.
+				$dump = Misc::getUrl(['url' => $file['download_url']]);
+				echo "Downloading: {$file['download_url']}\n";
 
-			// Decompress.
-			$dump = gzdecode($dump);
+				if (!$dump) {
+					echo "Error downloading dump {$match[2]} you can try manually importing it." .
+						PHP_EOL;
+					continue;
+				} else {
+					if (nZEDb_DEBUG) {
+						echo "Dump {$match[2]} downloaded\n";
+					}
+				}
 
-			if (!$dump) {
-				echo "Error decompressing dump {$match[2]}." . PHP_EOL;
-				continue;
-			}
+				// Make sure we didn't get an HTML page.
+				if (strpos($dump, '<!DOCTYPE html>') !== false) {
+					echo "The dump file {$match[2]} might be missing from GitHub." . PHP_EOL;
+					continue;
+				}
 
-			// Store the dump.
-			$dumpFile = nZEDb_RES . $match[2] . '_predb_dump.csv';
-			$fetched  = file_put_contents($dumpFile, $dump);
-			if (!$fetched) {
-				echo "Error storing dump file {$match[2]} in (" . nZEDb_RES . ').' .
-					 PHP_EOL;
-				continue;
-			}
+				// Decompress.
+				$dump = gzdecode($dump);
 
-			// Make sure it's readable by all.
-			chmod($dumpFile, 0777);
-			$local   = strtolower($argv[2]) == 'local' ? true : false;
-			$verbose = $argv[3] == true ? true : false;
+				if (!$dump) {
+					echo "Error decompressing dump {$match[2]}." . PHP_EOL;
+					continue;
+				}
 
-			if ($verbose) {
-				echo $predb->log->info("Clearing import table");
-			}
+				// Store the dump.
+				$dumpFile = nZEDb_RES . $match[2] . '_predb_dump.csv';
+				$fetched = file_put_contents($dumpFile, $dump);
+				if (!$fetched) {
+					echo "Error storing dump file {$match[2]} in (" . nZEDb_RES . ').' .
+						PHP_EOL;
+					continue;
+				}
 
-			// Truncate to clear any old data
-			$predb->executeTruncate();
+				// Make sure it's readable by all.
+				chmod($dumpFile, 0777);
+				$local = strtolower($argv[2]) == 'local' ? true : false;
+				$verbose = $argv[3] == true ? true : false;
 
-			// Import file into predb_imports
-			$predb->executeLoadData(
+				if ($verbose) {
+					echo $predb->log->info("Clearing import table");
+				}
+
+				// Truncate to clear any old data
+				$predb->executeTruncate();
+
+				// Import file into predb_imports
+				$predb->executeLoadData(
 					[
 						'fields' => '\\t\\t',
 						'lines'  => '\\r\\n',
@@ -171,35 +189,43 @@ foreach ($data as $file) {
 						'path'   => $dumpFile,
 					]);
 
-			// Remove any titles where length <=8
-			if ($verbose === true) {
-				echo $predb->log->info("Deleting any records where title <=8 from Temporary Table");
+				// Remove any titles where length <=8
+				if ($verbose === true) {
+					echo $predb->log->info("Deleting any records where title <=8 from Temporary Table");
+				}
+				$predb->executeDeleteShort();
+
+				// Add any groups that do not currently exist
+				$predb->executeAddGroups();
+
+				// Fill the groups_id
+				$predb->executeUpdateGroupID();
+
+				echo $predb->log->info("Inserting records from temporary table into predb table");
+				$predb->executeInsert();
+
+				// Delete the dump.
+				unlink($dumpFile);
+
+				$progress = $predb->progress(settings_array($match[2] + 1, $progress),
+					['read' => false]);
+				echo sprintf("Successfully imported PreDB dump %d (%s), %d dumps remaining\n",
+					$match[2],
+					 date('Y-m-d', $match[2]),
+					--$total
+					);
+			} else {
+				echo "Ignoring: {$file['download_url']}\n";
 			}
-			$predb->executeDeleteShort();
-
-			// Add any groups that do not currently exist
-			$predb->executeAddGroups();
-
-			// Fill the groups_id
-			$predb->executeUpdateGroupID();
-
-			echo $predb->log->info("Inserting records from temporary table into predb table");
-			$predb->executeInsert();
-
-			// Delete the dump.
-			unlink($dumpFile);
-
-			$progress = $predb->progress(settings_array($match[2] + 1, $progress),
-										 ['read' => false]);
-			echo "Successfully imported PreDB dump {$match[2]}, " . (--$total) .
-				 ' dumps remaining.' . PHP_EOL;
 		} else {
-			echo "Ignoring: {$file['download_url']}\n";
+			if (nZEDb_DEBUG) {
+				echo "^https://raw.githubusercontent.com/nZEDb/nZEDbPre_Dumps/master/dumps/$dir/$filePattern$\n {$file['download_url']}\n";
+			}
 		}
-	} else if (nZEDb_DEBUG) {
-			echo "^https://raw.githubusercontent.com/nZEDb/nZEDbPre_Dumps/master/dumps/$filePattern$\n {$file['download_url']}\n";
 	}
 }
+
+////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function settings_array($last = null, $settings = null)
 {
@@ -212,6 +238,24 @@ function settings_array($last = null, $settings = null)
 	}
 
 	return $settings;
+}
+
+function getDirListing($url)
+{
+	$result = Misc::getUrl(
+		[
+			'url'            => $url,
+			'requestheaders' => [
+				'Content-Type: application/json',
+				'User-Agent: nZEDb'
+			]
+		]);
+
+	if ($result === false) {
+		exit('Error connecting to GitHub, try again later?' . PHP_EOL);
+	}
+
+	return $result;
 }
 
 ?>
