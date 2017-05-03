b6ac4519040bdbacd61b521affa30f8cecbd75b0
Amir Zarrinkafsh <nightah@me.com>
2017-05-03 16:20:03 +1000

    Updated new group regexes to match more explicitly

--- resources/db/patches/mysql/+1~release_naming_regexes.sql
+++ resources/db/patches/mysql/+1~release_naming_regexes.sql
@@ -14,21 +14,21 @@ UPDATE release_naming_regexes SET ordinal = 40 WHERE id = 32;
 INSERT INTO release_naming_regexes (id, group_regex, regex, status, description, ordinal)
 VALUES (
   33,
-  '^alt\\.binaries\\.(anime|multimedia.anime)\\.?(highspeed|repost)?',
+  '^alt\\.binaries\\.(multimedia\\.|)anime\\.?(highspeed|repost)?$',
   '/^[[(]\\d+\\/\\d+[])] - " ?(?P<match0>.+?) ?[. ](7z|avi|md5|mkv|mp4|nzb|par|vol)t?\\d+.+yEnc$/',
   1,
   '//[01/17] - "[neko-raws] Niji-iro Days 02 [BD][1080p][FLAC][768CC18E]v2.par2" - 590,59 MB yEnc',
   5
 ), (
   34,
-  '^alt\\.binaries\\.(anime|multimedia.anime)\\.?(highspeed|repost)?',
+  '^alt\\.binaries\\.(multimedia\\.|)anime\\.?(highspeed|repost)?$',
   '/^.+\\" ?[ .-]?(?P<match0>.+?) ?[ .](7z|avi|md5|mkv|mp4|nzb|par|vol)t?\\d?+.+yEnc$/',
   1,
   '//[SpaceFish] Galilei Donna - Batch [BD][720p][MP4][AAC] [1/7] - "[SpaceFish] Galilei Donna - 07 [BD][720p][AAC] mp4" yEnc',
   10
 ), (
   35,
-  '^alt\\.binaries\\.(anime|multimedia.anime)\\.?(highspeed|repost)?',
+  '^alt\\.binaries\\.(multimedia\\.|)anime\\.?(highspeed|repost)?$',
   '/^.+\\" ?[ .-]?(?P<match0>.+?) ?[ .](7z|avi|md5|mkv|mp4|nfo|nzb|par|vol)t?\\d?+.+[[(]\\d+\\/\\d+[])]$/',
   1,
   '//My Hero Academia Textless Opening Song \'THE DAY\' (BD AVC 1080p FLAC) [6D660059] - "My Hero Academia Textless Opening Song \'THE DAY\' (BD AVC 1080p FLAC) [6D660059] nfo" yEnc (01/35)',
