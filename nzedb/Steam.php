9037ee14b2cc6cd923a1be6d8bd84b5d1f703c1c
b3rs3rk <b3rs3rk@protonmail.com>
2017-05-19 11:23:13 -0400

    Fix: Remove function void return declarations as they are throwing UnknownType errors

--- nzedb/Steam.php
+++ nzedb/Steam.php
@@ -9,27 +9,27 @@ use nzedb\db\DB;
 class Steam
 {
 	const STEAM_MATCH_PERCENTAGE = 90;
-	
+
 	/**
 	 * @var string The parsed game name from searchname
 	 */
 	public $searchTerm;
-	
+
 	/**
 	 * @var int The ID of the Steam Game matched
 	 */
 	protected $steamGameID;
-	
+
 	/**
 	 * @var DB
 	 */
 	protected $pdo;
-	
+
 	/**
 	 * @var
 	 */
 	protected $lastUpdate;
-	
+
 	/**
 	 * Steam constructor.
 	 *
@@ -39,9 +39,9 @@ class Steam
 	{
 		$defaults = ['DB' => null];
 		$options += $defaults;
-		
+
 		$this->pdo = ($options['DB'] instanceof DB ? $options['DB'] : new DB());
-		
+
 		$this->steamFront = new Main(
 			[
 				'country_code' => 'us',
@@ -49,7 +49,7 @@ class Steam
 			]
 		);
 	}
-	
+
 	/**
 	 * Gets all Information for the game.
 	 *
@@ -60,7 +60,7 @@ class Steam
 	public function getAll($appID)
 	{
 		$res = $this->steamFront->getAppDetails($appID);
-		
+
 		if ($res !== false) {
 			$result = [
 				'title'       => $res->name,
@@ -74,17 +74,17 @@ class Steam
 				'releasedate' => $res->releasedate['date'],
 				'genres'      => implode(',', array_column($res->genres, 'description'))
 			];
-			
+
 			return $result;
 		}
-		
+
 		if ($res === false) {
 			ColorCLI::doEcho(ColorCLI::notice('Steam did not return game data'));
 		}
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * Searches Steam Apps table for best title match -- prefers 100% match but returns highest over 90%
 	 *
@@ -96,22 +96,22 @@ class Steam
 	public function search($searchTerm)
 	{
 		$bestMatch = false;
-		
+
 		if (empty($searchTerm)) {
 			ColorCLI::doEcho(ColorCLI::notice('Search term cannot be empty'));
-			
+
 			return $bestMatch;
 		}
-		
+
 		$this->populateSteamAppsTable();
-		
+
 		$results = $this->pdo->queryDirect("
 			SELECT name, appid
 			FROM steam_apps
 			WHERE MATCH(name) AGAINST({$this->pdo->escapeString($searchTerm)})
 			LIMIT 20"
 		);
-		
+
 		if ($results instanceof \Traversable) {
 			$bestMatchPct = 0;
 			foreach ($results as $result) {
@@ -120,7 +120,7 @@ class Steam
 					$bestMatch = $result['appid'];
 					break;
 				}
-				
+
 				similar_text(strtolower($result['name']), strtolower($searchTerm), $percent);
 				// If similar_text reports an exact match set best match and break out
 				if ($percent === 100) {
@@ -136,15 +136,15 @@ class Steam
 		if ($bestMatch === false) {
 			ColorCLI::doEcho(ColorCLI::notice('Steam search returned no valid results'));
 		}
-		
+
 		return $bestMatch;
 	}
-	
+
 	/**
 	 * Downloads full Steam Store dump and imports data into local table
 	 * @throws \Exception
 	 */
-	public function populateSteamAppsTable(): void
+	public function populateSteamAppsTable()
 	{
 		$lastUpdate = Settings::value('APIs.Steam.last_update');
 		$this->lastUpdate = $lastUpdate > 0 ? $lastUpdate : 0;
@@ -168,7 +168,7 @@ class Steam
 								'limit'      => 1,
 							]
 						);
-						
+
 						if ($dupeCheck === null) {
 							$this->pdo->queryExec(sprintf('INSERT IGNORE INTO steam_apps (name, appid) VALUES (%s, %d)', $this->pdo->escapeString($app['name']), $app['appid']));
 							$inserted++;
@@ -186,11 +186,11 @@ class Steam
 			echo PHP_EOL . 'Added ' . $inserted . ' new steam apps, ' . $dupe . ' duplicates skipped' . PHP_EOL;
 		}
 	}
-	
+
 	/**
 	 * Sets the database time for last full AniDB update
 	 */
-	private function setLastUpdated(): void
+	private function setLastUpdated()
 	{
 		Settings::update(
 			['value' => time()],
