c9d0a329cc7c37cca2a6e7a799af257155c84441
niel <niel.archer@gmail.com>
2017-05-24 00:27:37 +0100

    Remove duplicated SQL.

--- resources/db/patches/mysql/0479~predb.sql
+++ resources/db/patches/mysql/0479~predb.sql
@@ -9,12 +9,3 @@ UPDATE predb SET created = predate;
 
 #Dropping the predate column which is now accessed as 'created'
 ALTER TABLE predb DROP COLUMN predate;
-
-#Altering Predb_Imports table
-ALTER TABLE predb_imports
-  ADD COLUMN created TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
-COMMENT 'Unix time of when the pre was created, or first noted by the system'
-  AFTER category,
-  ADD COLUMN updated TIMESTAMP NOT NULL ON UPDATE CURRENT_TIMESTAMP
-COMMENT 'Unix time of when the entry was last updated'
-  AFTER created;
