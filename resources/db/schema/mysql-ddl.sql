40b9299450ff2a17e49c7bba9804ab65b199f48a
niel <niel.archer@gmail.com>
2017-06-15 05:07:26 +0100

    Fix more formatting

--- resources/db/schema/mysql-ddl.sql
+++ resources/db/schema/mysql-ddl.sql
@@ -16,15 +16,10 @@ CREATE TABLE allgroups (
 
 DROP TABLE IF EXISTS anidb_episodes;
 CREATE TABLE anidb_episodes (
-  anidbid       INT(10) UNSIGNED        NOT NULL
-  COMMENT 'ID of title from AniDB',
-  episodeid     INT(10) UNSIGNED        NOT NULL DEFAULT '0'
-  COMMENT 'anidb id for this episode',
-  episode_no    SMALLINT(5) UNSIGNED    NOT NULL
-  COMMENT 'Numeric version of episode (leave 0 for combined episodes).',
-  episode_title VARCHAR(255)
-                COLLATE utf8_unicode_ci NOT NULL
-  COMMENT 'Title of the episode (en, x-jat)',
+  anidbid       INT(10) UNSIGNED        NOT NULL COMMENT 'ID of title from AniDB',
+  episodeid     INT(10) UNSIGNED        NOT NULL DEFAULT '0' COMMENT 'anidb id for this episode',
+  episode_no    SMALLINT(5) UNSIGNED    NOT NULL COMMENT 'Numeric version of episode (leave 0 for combined episodes).',
+  episode_title VARCHAR(255) COLLATE utf8_unicode_ci NOT NULL COMMENT 'Title of the episode (en, x-jat)',
   airdate       DATE                    NOT NULL,
   PRIMARY KEY (anidbid, episodeid)
 )
