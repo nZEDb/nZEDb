240799ef1020d74cb8a6a5c39ba941ced2437540
niel <niel.archer@gmail.com>
2017-06-16 20:36:55 +0100

    Adding AnidbEpisodes and AnidbInfo models so AnidbTitles can have a relationship with them.

---
+++ app/models/AnidbEpisodes.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+class AnidbEpisodes extends \app\extensions\data\Model
+{
+	public $validates = [];
+}
+
+?>
---
+++ app/models/AnidbInfo.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace app\models;
+
+class AnidbInfo extends \app\extensions\data\Model
+{
+	public $validates = [];
+}
+
+?>
