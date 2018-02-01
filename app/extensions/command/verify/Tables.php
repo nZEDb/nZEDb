de335bbd29a7724c8a67e89e1a526b96147a8d15
Niel Archer <niel.archer@gmail.com>
2018-02-01 20:48:49 +0000

    Adding closures for tableSettings to handle validate and output.

--- app/extensions/command/verify/Tables.php
+++ app/extensions/command/verify/Tables.php
@@ -66,10 +66,31 @@ class Tables extends \app\extensions\console\Command
 
 	protected function tableSettings()
 	{
-		$dummy = $this->validate(new Settings(),
+		$output = static function($row, $header, $error) {
+			if ($error === false) {
+				$this->out("section, subsection, name", ['style' => 'primary']);
+				$error = true;
+			}
+			$this->out(" {$row['section']}, {$row['subsection']}, {$row['name']}: MISSING!",
+				['style' => 'info']);
+		};
+
+		$validate = static function($row) {
+			$result = Settings::value(
+				[
+					'section'    => $row['section'],
+					'subsection' => $row['subsection'],
+					'name'       => $row['name']
+				],
+				true);
+			return ($result !== null);
+		};
+
+		return $this->validate(new Settings(),
 			[
 				'file' => nZEDb_RES . 'db/schema/data/10-settings.tsv',
-				'test' => function() {},
+				'output' => $output,
+				'test' => $validate,
 			]
 		);
 	}
@@ -79,7 +100,13 @@ class Tables extends \app\extensions\console\Command
 	 *                                            It must be a child of \app\extensions\data\Model
 	 * @param array                      $options Settings for the validation. Some more optional than
 	 *                                            others. Entries include:
-	 *                                            `test` - closure to use for validation.
+	 *                                            `file`  - filename of data file to use
+	 *                                            `fix`   - whether to fix failures (false|closure)
+	 *                                            `output`- closure to format text output.
+	 *                                            `silent - disable output (boolean)
+	 *                                            `test`  - closure to use for validation.
+	 *
+	 * @return boolean	true if no errors found, false otherwise
 	 */
 	private function validate(Model $model, array $options = [])
 	{
@@ -91,10 +118,18 @@ class Tables extends \app\extensions\console\Command
 				'data',
 				'10-' . strtolower($table) . '.tsv'
 			], nZEDb_RES),
+			'fix'		=> false,
+			'output'	=> null,
 			'silent'	=> true,
 			'test'		=> null,
 		];
 		$options += $defaults;
+		$fix = $output = $test = '';
+		extract($options, EXTR_IF_EXISTS | EXTR_REFS); // create short-name variable refs
+
+		if (!is_callable($test)) {
+			throw new \InvalidArgumentException('The option "test" must be a closure to perform the test!');
+		}
 
 		if (!file_exists($options['file'])) {
 			throw new \InvalidArgumentException("Unable to find {$options['file']}!");
@@ -106,21 +141,45 @@ class Tables extends \app\extensions\console\Command
 			throw new \InvalidArgumentException("File {$options['file']} did not return a list of rows!");
 		}
 
-		$columns = array_shift($rows);	// Move the column names/order off of the array.
-		$result = false;
-		//$row = [];
-		if ($columns !== null) {
-			if ($options['silent'] === true) {
-				$this->out("Verifying $table table...", ['style' => 'primary']);
-			}
+		// Move the column names/order off of the array.
+		$header = array_shift($rows);
+		$columns = explode([','], $header);
+		array_walk($columns, function(&$value) { $value = trim($value); });
 
-			$noOutput = true;
-			$result = true;
-			foreach ($rows as $row) {
-				$message = '';
-				//$this->out('():', ['style' => 'header']);
+
+		if ($options['silent'] != true) {
+			$this->out("Verifying $table table...", ['style' => 'primary']);
+		}
+
+		$error = false;
+		$result = true;
+		foreach ($rows as $row) {
+			$data = array_combine($columns, explode("\t", $row));
+			$check = $test($data, $columns);
+			if ($check === false) {
+				if ($options['silent'] === false) {
+					if (is_callable($output)) {
+						$output($data, $header, $error);
+					} else {
+						$this->defaultOutput($row, $header, $error);
+					}
+				}
+
+				if (is_callable($fix)) {
+					$fix($data);
+				}
+
+				$error = true;
 			}
 		}
+
+		if ($error === false) {
+			$this->out('No problems found.', ['style' => 'primary']);
+		} else if ($options['fix'] === false) {
+			$this->out('Please fix the above problems.', ['style' => 'warning']);
+		}
+
+		return $result;
 	}
 
 	public static function hasAllEntry($console = null)
@@ -156,5 +215,15 @@ class Tables extends \app\extensions\console\Command
 		}
 
 		return $result;
+	}
+
+
+	protected function defaultOutput($row, $header, &$error)
+	{
+		if ($error === false) {
+			$this->out($header, ['style' => 'primary']);
+			$error = true;
+		}
+		$this->out($row, ['style' => 'info']);
 	}
 }
