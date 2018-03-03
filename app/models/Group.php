37231a55e1ed0555fdf48e9e4d7e0031cd67b8ed
Niel Archer <niel.archer@gmail.com>
2018-03-03 22:09:03 +0000

    Fix passing of parameters to Group::createMissing().

--- app/models/Group.php
+++ app/models/Group.php
@@ -49,9 +49,10 @@ class Group extends \lithium\data\Model
 		);
 
 		if ($group === null && $options['create'] === true) {
-			$group = self::createMissing($name,
+			$group = static::createMissing(
 				[
-					'description'	=> $options['description']
+					'description'	=> $options['description'],
+					'name'			=> $name,
 				]
 			);
 		}
@@ -72,7 +73,7 @@ class Group extends \lithium\data\Model
 			throw new \InvalidArgumentException("");
 		}
 
-		$group = self::create($data);
+		$group = static::create($data);
 
 		$group->save();
 
