621aac854874d899401b1785d7c5404f83136fdb
Niel Archer <niel.archer@gmail.com>
2018-03-04 19:35:10 +0000

    Make boolean check a strict comparison.

--- app/config/bootstrap/connections.php
+++ app/config/bootstrap/connections.php
@@ -79,7 +79,7 @@ $installed = nZEDb_CONFIGS . 'install.lock';
 
 if (! defined('DB_MOCK')) {
 	// Add new condition to use DB_MOCK mode here.
-	if (defined('MAINTENANCE_MODE_ENABLED') && MAINTENANCE_MODE_ENABLED == true) {
+	if (defined('MAINTENANCE_MODE_ENABLED') && MAINTENANCE_MODE_ENABLED === true) {
 		define('DB_MOCK', true);
 	} else {
 		define('DB_MOCK', false);
