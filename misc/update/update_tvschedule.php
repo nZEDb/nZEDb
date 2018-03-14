5a3708917d7b37f7c7c72e3fb21f3e7f3bcd9ee6
Niel Archer <niel.archer@gmail.com>
2018-03-14 12:19:00 +0000

    Remove unused file.

--- misc/update/update_tvschedule.php
+++
@@ -1,7 +0,0 @@
-<?php
-// Run this once per day.
-require_once realpath(dirname(dirname(__DIR__)) . DIRECTORY_SEPARATOR . 'bootstrap.php');
-
-use nzedb\TvRage;
-
-(new TvRage(['Echo' => true]))->updateSchedule();
