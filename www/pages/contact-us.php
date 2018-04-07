5cb75bf2fb4f4c9bf993fc569809e1cb382c83d3
Niel Archer <niel.archer@gmail.com>
2018-04-07 01:28:49 +0100

    Minor tweaks for www/pages/contact-us.php

--- www/pages/contact-us.php
+++ www/pages/contact-us.php
@@ -6,6 +6,7 @@ use nzedb\utility\Misc;
 
 $captcha = new Captcha($page);
 $msg = '';
+$title = Settings::value('site.main.title');
 if (isset($_POST['useremail']) && $captcha->getError() === false) {
 	// Send the contact info and report back to user.
 	$email = $_POST['useremail'];
@@ -14,23 +15,24 @@ if (isset($_POST['useremail']) && $captcha->getError() === false) {
 	$mailsubj = 'Contact Form Submitted';
 	$mailbody = 'Values submitted from contact form:<br/>';
 
-	//@TODO take this loop out, it's not safe.
-	while (list($key, $val) = \each($_POST)) {
-		if ($key != 'submit') {
-			$mailbody .= "$key : $val<br/>";
+	// @TODO take this loop out, it's not safe.
+	foreach ($_POST as $key => $value) {
+		if ($key !== 'submit') {
+			$mailbody .= "$key : $value<br/>\n";
 		}
 	}
 
 	if (! \preg_match("/\n/i", $_POST['useremail'])) {
 		Misc::sendEmail($mailto, $mailsubj, $mailbody, $email);
 	}
-	$msg = "<h2 style='text-align:center;'>Thank you for getting in touch with " . Settings::value('site.main.title') . '.</h2>';
+	$msg = "<h2 style='text-align:center;'>Thank you for getting in touch with " . $title . '.</h2>';
 }
+
 $page->smarty->assign('msg', $msg);
-$page->title = 'Contact ' . Settings::value('site.main.title');
-$page->meta_title = 'Contact ' . Settings::value('site.main.title');
+$page->title = 'Contact ' . $title;
+$page->meta_title = 'Contact ' . $title;
 $page->meta_keywords = 'contact us,contact,get in touch,email';
-$page->meta_description = 'Contact us at ' . Settings::value('site.main.title') . ' and submit your feedback';
+$page->meta_description = 'Contact us at ' . $title . ' and submit your feedback';
 
 $page->content = $page->smarty->fetch('contact.tpl');
 
