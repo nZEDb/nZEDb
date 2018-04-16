c3af2c5a3b0ff459fd8238bde6ca5c3f02f3d7b2
Niel Archer <niel.archer@gmail.com>
2018-04-16 12:10:39 +0100

    Update code style.

--- app/extensions/command/verify/Tables.php
+++ app/extensions/command/verify/Tables.php
@@ -4,6 +4,7 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
@@ -13,15 +14,15 @@
  * not, see:
  *
  * @link      <http://www.gnu.org/licenses/>.
+ *
  * @author    niel
  * @copyright 2018 nZEDb
  */
-
 namespace app\extensions\command\verify;
-
 
 use app\models\Group;
 use app\models\Settings;
+use lithium\data\Connections;
 
 
 class Tables extends \app\extensions\console\Command
@@ -42,9 +43,11 @@ class Tables extends \app\extensions\console\Command
 				case 'Settings':
 				case 'settings':
 					$this->tableSettings();
+
 					break;
 				case 'cpb':
 					$this->tableSetCPB();
+
 					break;
 //				case '':
 //					$this->();
@@ -60,6 +63,12 @@ class Tables extends \app\extensions\console\Command
 		parent::_init();
 	}
 
+	/**
+	 * Check the table set of collections, binaries, parts, and missed parts for enabled groups
+	 * without corresponding table(s).
+	 *
+	 * @return void
+	 */
 	protected function tableSetCPB()
 	{
 		$active = Group::find('all',
@@ -71,23 +80,22 @@ class Tables extends \app\extensions\console\Command
 					'id',
 					'name',
 				],
-				'order'		=> 'name ASC'
+				'order'		=> 'name ASC',
 			]
 		);
 
-		if (count($active->data()) < 1) {
-			$this->out("No active groups found to verify!");
+		if (\count($active->data()) < 1) {
+			$this->out('No active groups found to verify!');
 		} else {
 			// TODO grab list of binaries_*, collections_*, and parts_* tables
 			;
-			// TODO compare list of group IDs against created tables. Create any missing tables.
+			// TODO compare list of group IDs against existing tables. Create any missing tables.
 		}
 	}
 
 	protected function tableSettings()
 	{
-		$output = function($row, $header, &$firstLine, $result)
-		{
+		$output = function($row, $header, &$firstLine, $result) {
 			if ($firstLine === true) {
 				$this->out('section, subsection, name', ['style' => 'info']);
 				$firstLine = false;
@@ -99,16 +107,16 @@ class Tables extends \app\extensions\console\Command
 			}
 		};
 
-		$validate = static function($row)
-		{
+		$validate = static function($row) {
 			$result = Settings::value(
 				[
 					'section'    => $row['section'],
 					'subsection' => $row['subsection'],
-					'name'       => $row['name']
+					'name'       => $row['name'],
 				],
 				true);
-			return ($result !== null);
+
+			return $result !== null;
 		};
 
 		$dummy = $this->validate(
@@ -124,42 +132,50 @@ class Tables extends \app\extensions\console\Command
 		return $dummy;
 	}
 
+	protected function defaultOutput($row, $header, &$error)
+	{
+		if ($error === false) {
+			$this->out($header, ['style' => 'primary']);
+			$error = true;
+		}
+		$this->out($row, ['style' => 'info']);
+	}
+
 	/**
-	 * @param array $options	Settings for the validation. Some more optional than others. Entries
-	 *                          include:
-	 *                          `file`  - filename of data file to use
-	 *                          `fix`   - whether to fix failures (false|closure)
-	 *                          `output`- closure to format text output.
-	 *                          `silent - disable output (boolean)
-	 *                          `test`  - closure to use for validation.
+	 * @param array $options Settings for the validation. Some more optional than others. Entries
+	 *                       include:
+	 *                       `file`  - filename of data file to use
+	 *                       `fix`   - whether to fix failures (false|closure)
+	 *                       `output`- closure to format text output.
+	 *                       `silent - disable output (boolean)
+	 *                       `test`  - closure to use for validation.
 	 *
-	 * @return boolean	true if no errors found, false otherwise
+	 * @return bool true if no errors found, false otherwise
 	 */
 	private function validate(array $options = [])
 	{
 		$fix = $output = $test = '';
-		extract($options, EXTR_IF_EXISTS | EXTR_REFS); // create short-name variable refs
+		\extract($options, EXTR_IF_EXISTS | EXTR_REFS); // create short-name variable refs
 
-		if (!is_callable($test)) {
+		if (! \is_callable($test)) {
 			throw new \InvalidArgumentException('The option "test" must be a closure to perform the test!');
 		}
 
-		if (!file_exists($options['file'])) {
+		if (! \file_exists($options['file'])) {
 			throw new \InvalidArgumentException("Unable to find {$options['file']}!");
 		}
-		$rows = file($options['file']);
+		$rows = \file($options['file']);
 
-		if (!is_array($rows)) {
+		if (! \is_array($rows)) {
 			throw new \InvalidArgumentException("File {$options['file']} did not return a list of rows!");
 		}
 
 		// Move the column names/order off of the array.
-		$header = trim(array_shift($rows));
-		$columns = explode(',', $header);
-		array_walk($columns,
-			function(&$value)
-			{
-				$value = trim($value);
+		$header = \trim(\array_shift($rows));
+		$columns = \explode(',', $header);
+		\array_walk($columns,
+			function(&$value) {
+				$value = \trim($value);
 			}
 		);
 
@@ -171,10 +187,10 @@ class Tables extends \app\extensions\console\Command
 		$result = $firstLine = true;
 		$error = false;
 		foreach ($rows as $row) {
-			$data = array_combine($columns, explode("\t", $row));
+			$data = \array_combine($columns, \explode("\t", $row));
 			$check = $test($data, $columns);
 			if ($options['silent'] === false) {
-				if (is_callable($output)) {
+				if (\is_callable($output)) {
 					$output($data, $header, $firstLine, $check);
 				} else {
 					$this->defaultOutput($row, $header, $error);
@@ -182,7 +198,7 @@ class Tables extends \app\extensions\console\Command
 			}
 
 			if ($check === false) {
-				if (is_callable($fix)) {
+				if (\is_callable($fix)) {
 					$fix($data);
 				}
 
@@ -192,19 +208,10 @@ class Tables extends \app\extensions\console\Command
 
 		if ($error === false) {
 			$this->out('No problems found.', ['style' => 'primary']);
-		} else if ($options['fix'] === false) {
+		} elseif ($options['fix'] === false) {
 			$this->out('Please fix the above problems.', ['style' => 'warning']);
 		}
 
 		return $result;
-	}
-
-	protected function defaultOutput($row, $header, &$error)
-	{
-		if ($error === false) {
-			$this->out($header, ['style' => 'primary']);
-			$error = true;
-		}
-		$this->out($row, ['style' => 'info']);
 	}
 }
