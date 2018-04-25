b56b7f725aba8b6da951deba88e45e4f8ccb708a
Niel Archer <niel.archer@gmail.com>
2018-04-25 15:22:25 +0100

    Use null-coalescing operator where appropriate in predb.php

--- www/admin/predb.php
+++ www/admin/predb.php
@@ -4,21 +4,21 @@ require_once realpath(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'smarty.php');
 use app\models\Predb;
 
 $page = new AdminPage();
-$page->title = "Browse PreDb";
-$page->meta_title = "View PreDb info";
-$page->meta_keywords = "view,predb,info,description,details";
-$page->meta_description = "View PreDb info";
+$page->title = 'Browse PreDb';
+$page->meta_title = 'View PreDb info';
+$page->meta_keywords = 'view,predb,info,description,details';
+$page->meta_description = 'View PreDb info';
 
-$lastsearch = isset($_REQUEST['presearch']) ? $_REQUEST['presearch'] : '';
+$lastsearch = $_REQUEST['presearch'] ?? '';
 $count = Predb::findRangeCount($lastsearch);
 
-$pageno = (isset($_REQUEST['page']) ? $_REQUEST['page'] : 1);
+$pageno = $_REQUEST['page'] ?? 1;
 $page->smarty->assign(
 	[
 		'lastSearch'       => $lastsearch,
 		'pagecurrent'      => (int)$pageno,
 		'pagemaximum'      => (int)($count / ITEMS_PER_PAGE) + 1,
-		'pagerquerybase'   => WWW_TOP . "/predb.php?" . $lastsearch . 'page=',
+		'pagerquerybase'   => WWW_TOP . '/predb.php?' . $lastsearch . 'page=',
 		'pagerquerysuffix' => '',
 		'pagertotalitems'  => $count,
 		'results'          => Predb::findRange($pageno, ITEMS_PER_PAGE, $lastsearch)->to('array'),
@@ -27,7 +27,7 @@ $page->smarty->assign(
 );
 // Pager has to be set outside the main assignment, or it will not recieve the scope of those
 // variables.
-$page->smarty->assign('pager', $page->smarty->fetch("paginate.tpl"));
+$page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
 
 $page->content = $page->smarty->fetch('predb.tpl');
 $page->render();
