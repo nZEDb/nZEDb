4f3418b0ba2104cbe29db44b7589f2de000a8f5b
Niel Archer <niel.archer@gmail.com>
2018-06-20 03:57:55 +0100

    Fix namespacing issue for ErrorException.

--- app/config/bootstrap/libraries.php
+++ app/config/bootstrap/libraries.php
@@ -75,7 +75,7 @@ if (!include_once LITHIUM_LIBRARY_PATH . '/lithium/core/Libraries.php') {
 	$message  = "Lithium core could not be found.  Check the value of LITHIUM_LIBRARY_PATH in ";
 	$message .= __FILE__ . ".  It should point to the directory containing your ";
 	$message .= "/libraries directory.";
-	throw new ErrorException($message);
+	throw new \ErrorException($message);
 }
 
 use lithium\core\Libraries;
