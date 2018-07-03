nZEDb/nZEDb

commit 8b5235b8db50b59624fdd3f7f66980f59ac13cad

Fixed merge conflicts.

www/admin/group-list-active.php
@@ -1,44 +1,38 @@
 <?php
 require_once './config.php';
 
-use app\models\Groups as Group;
-use nzedb\Groups;
+use app\models\Groups;
 
-$page   = new AdminPage();
-$groups = new Groups(['Settings' => $page->settings]);
+$pageno = $_REQUEST['pageno'] ?? 1;
+$page->title = $page->meta_title = 'Group List';
 
-$gname = "";
-if (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) {
-	$gname = $_REQUEST['groupname'];
-}
+$pageno = $_REQUEST['pageno'] ?? 1;
 
-//$groupcount = $groups->getCount($gname, 1);
-$conditions = ['active' => 0];
-if (!empty($gname)) {
-	$conditions += ['name' => ['LIKE' => "%$gname%"]];
-}
-$groupcount = Group::count(['conditions' => $conditions]);
+$conditions = ['active' => 1];
+$conditions += empty($groupname) ? [] : ['name' => ['LIKE' => '%$groupname%']];
 
-$offset    = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$groupname = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) ?
-	$_REQUEST['groupname'] : '';
+$count = Groups::find('count', ['conditions' => $conditions]);
+$grouplist = Groups::getRange($pageno,ITEMS_PER_PAGE, $_REQUEST['groupname'], 1);
 
-$page->smarty->assign('groupname', $groupname);
-$page->smarty->assign('pagertotalitems', $groupcount);
-$page->smarty->assign('pageroffset', $offset);
-$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
+$groupsearch = empty($_REQUEST['groupname']) ? '' : '&amp;groupname=' . $_REQUEST['groupname'];
 
-$groupsearch = ($gname != "") ? 'groupname=' . $gname . '&amp;' : '';
-$page->smarty->assign('pagerquerybase',
-					  WWW_TOP . "/group-list-active.php?" . $groupsearch . "offset=");
-$pager = $page->smarty->fetch("pager.tpl");
-$page->smarty->assign('pager', $pager);
+$page->smarty->assign(
+	[
+		'groupname'        => $_REQUEST['groupname'],
+		'pagecurrent'      => (int)$pageno,
+		'pagerlast'         => (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagerquerybase'   => WWW_TOP . '/group-list-active?pageno=',
+		'pagerquerysuffix' => $groupsearch,
+		'pagertotalitems'  => $count,
+		'results'          => $grouplist,
+		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
+	]
+);
 
-$grouplist = $groups->getRange($offset, ITEMS_PER_PAGE, $gname, 1);
-
-$page->smarty->assign('grouplist', $grouplist);
-
-$page->title = "Group List";
+// Pager must be set outside the main assignment, so it can receive the scope of those variables.
+$page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
 
 $page->content = $page->smarty->fetch('group-list.tpl');
 $page->render();
+
+?>

www/admin/group-list-inactive.php
@@ -1,43 +1,38 @@
 <?php
 require_once './config.php';
 
-use nzedb\Groups;
+use app\models\Group as Groups;
 
-$page   = new AdminPage();
-$groups = new Groups(['Settings' => $page->settings]);
+$page = new AdminPage();
+$page->title = $page->meta_title = 'Group List';
 
-$gname = "";
-if (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) {
-	$gname = $_REQUEST['groupname'];
-}
+$pageno = $_REQUEST['pageno'] ?? 1;
 
-//$groupcount = $groups->getCount($gname, 0);
 $conditions = ['active' => 0];
-if (!empty($gname)) {
-	$conditions += ['name' => ['LIKE' => "%$gname%"]];
-}
-$groupcount = Group::count(['conditions' => $conditions]);
+$conditions += empty($groupname) ? [] : ['name' => ['LIKE' => '%$groupname%']];
 
-$offset    = isset($_REQUEST["offset"]) ? $_REQUEST["offset"] : 0;
-$groupname = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) ?
-	$_REQUEST['groupname'] : '';
+$count = Groups::find('count', ['conditions' => $conditions]);
+$grouplist = Groups::getRange($pageno,ITEMS_PER_PAGE, $_REQUEST['groupname'], 1);
 
-$page->smarty->assign('groupname', $groupname);
-$page->smarty->assign('pagertotalitems', $groupcount);
-$page->smarty->assign('pageroffset', $offset);
-$page->smarty->assign('pageritemsperpage', ITEMS_PER_PAGE);
+$groupsearch = empty($_REQUEST['groupname']) ? '' : '&amp;groupname=' . $_REQUEST['groupname'];
 
-$groupsearch = ($gname != "") ? 'groupname=' . $gname . '&amp;' : '';
-$page->smarty->assign('pagerquerybase',
-					  WWW_TOP . "/group-list-inactive.php?" . $groupsearch . "offset=");
-$pager = $page->smarty->fetch("pager.tpl");
-$page->smarty->assign('pager', $pager);
+$page->smarty->assign(
+	[
+		'groupname'        => $_REQUEST['groupname'],
+		'pagecurrent'      => (int)$pageno,
+		'pagerlast'         => (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagerquerybase'   => WWW_TOP . '/group-list-inactive?pageno=',
+		'pagerquerysuffix' => $groupsearch,
+		'pagertotalitems'  => $count,
+		'results'          => $grouplist,
+		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
+	]
+);
 
-$grouplist = $groups->getRange($offset, ITEMS_PER_PAGE, $gname, 0);
-
-$page->smarty->assign('grouplist', $grouplist);
-
-$page->title = "Group List";
+// Pager must be set outside the main assignment, so it can receive the scope of those variables.
+$page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
 
 $page->content = $page->smarty->fetch('group-list.tpl');
 $page->render();
+
+?>

www/admin/group-list.php
@@ -1,29 +1,38 @@
 <?php
 require_once './config.php';
 
-use nzedb\Groups;
+use app\models\Group as Groups;
 
-$page   = new AdminPage();
-$groups = new Groups(['Settings' => $page->settings]);
+$page = new AdminPage();
+$page->title = $page->meta_title = 'Group List';
 
-$groupName = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname']) ? $_REQUEST['groupname'] : '');
-$offset    = (isset($_REQUEST['offset']) ? $_REQUEST['offset'] : 0);
-$groupCount = Group::count(['conditions' => ['name' => ['LIKE' => "%$groupName%"]]]);
+$pageno = $_REQUEST['pageno'] ?? 1;
+
+
+$conditions = empty($groupname) ? [] : ['name' => ['LIKE' => '%$groupname%']];
+
+$count = Groups::find('count', ['conditions' => $conditions]);
+$grouplist = Groups::getRange($pageno, ITEMS_PER_PAGE, $_REQUEST['groupname']);
+
+$groupsearch = empty($_REQUEST['groupname']) ? '' : '&amp;groupname=' . $_REQUEST['groupname'];
 
 $page->smarty->assign(
 	[
-		'groupname' => $groupName,
-		'pagertotalitems' => $groupCount,
-		'pageroffset' => $offset,
-		'pageritemsperpage' => ITEMS_PER_PAGE,
-		'pagerquerybase' =>
-			WWW_TOP . "/group-list.php?" . (($groupName != '') ? "groupname=$groupName&amp;" : '') . 'offset=',
-		'pagerquerysuffix' => '',
-		'grouplist' => $groups->getRange($offset, ITEMS_PER_PAGE, $groupName, -1)
+		'groupname'        => $_REQUEST['groupname'],
+		'pagecurrent'      => (int)$pageno,
+		'pagerlast'        => (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagerquerybase'   => WWW_TOP . '/group-list.php?pageno=',
+		'pagerquerysuffix' => $groupsearch,
+		'pagertotalitems'  => $count,
+		'results'          => $grouplist,
+		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
 	]
 );
-$page->smarty->assign('pager', $page->smarty->fetch('pager.tpl'));
 
-$page->title = 'Group List';
+// Pager must be set outside the main assignment, so it can receive the scope of those variables.
+$page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
+
 $page->content = $page->smarty->fetch('group-list.tpl');
 $page->render();
+
+?>

www/admin/predb.php
@@ -13,6 +13,7 @@ $lastsearch = $_REQUEST['presearch'] ?? '';
 $count = Predb::findRangeCount($lastsearch);
 
 $pageno = $_REQUEST['page'] ?? 1;
+
 $page->smarty->assign(
 	[
 		'lastSearch'       => $lastsearch,
@@ -25,8 +26,8 @@ $page->smarty->assign(
 		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
 	]
 );
-// Pager has to be set outside the main assignment, or it will not recieve the scope of those
-// variables.
+
+// Pager has to be set outside of main assign, or it will not recieve the scope of those variables.
 $page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
 
 $page->content = $page->smarty->fetch('predb.tpl');

www/pages/browsegroup.php
@@ -1,19 +1,45 @@
 <?php
 
-use nzedb\Groups;
+use app\models\Group as Groups;
 
 if (!$page->users->isLoggedIn()) {
 	$page->show403();
 }
 
-$groups = new Groups(['Settings' => $page->settings]);
+$page->meta_title = 'Browse Groups';
+$page->meta_keywords = 'browse,groups,description,details';
+$page->meta_description = 'Browse groups';
 
-$grouplist = $groups->getRange();
-$page->smarty->assign('results', $grouplist);
+$pageno = $_REQUEST['pageno'] ?? 1;
 
-$page->meta_title = "Browse Groups";
-$page->meta_keywords = "browse,groups,description,details";
-$page->meta_description = "Browse groups";
+$grouplist = Groups::find('all',
+	[
+		'fields' => ['name', 'description', 'last_updated'],
+		'limit'  => ITEMS_PER_PAGE,
+		'order'  => ['name'],
+		'page'   => $pageno,
+	]
+)->data();
+
+$count = Groups::find('count', ['fields' => []]);
+
+$page->smarty->assign(
+	[
+		//'lastSearch'       => $lastsearch,
+		'pagecurrent'      => (int)$pageno,
+		'pagerlast'        => (int)($count / ITEMS_PER_PAGE) + 1,
+		'pagerquerybase'   => WWW_TOP . '/browsegroup?pageno=',
+		'pagerquerysuffix' => '',
+		'pagertotalitems'  => $count,
+		'results'          => $grouplist,
+		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
+	]
+);
+
+// Pager must be set outside the main assignment, so it can receive the scope of those variables.
+$page->smarty->assign('pager', $page->smarty->fetch('paginate.tpl'));
 
 $page->content = $page->smarty->fetch('browsegroup.tpl');
 $page->render();
+
+?>

www/themes/Default/templates/browsegroup.tpl
@@ -3,15 +3,16 @@
 {/if}
 <h1>Browse Groups</h1>
 {if $results|@count > 0}
-<table style="width:100%;" class="data highlight Sortable" id="browsetable">
+{$pager}
+	<table style="width:100%;" class="data highlight Sortable" id="browsetable">
 	<tr>
 		<th>name</th>
 		<th>description</th>
 		<th>updated</th>
 	</tr>
 
-	{foreach $results as $result}
-		{if $result.num_releases > 0}
+	{if $pagertotalitems > 0}
+		{foreach $results as $result}
 		<tr class="{cycle values=",alt"}">
 			<td>
 				<a title="Browse releases from {$result.name|replace:"alt.binaries":"a.b"}" href="{$smarty.const.WWW_TOP}/browse?g={$result.name}">{$result.name|replace:"alt.binaries":"a.b"}</a>
@@ -21,9 +22,9 @@
 			</td>
 			<td class="less">{$result.last_updated|timeago} ago</td>
 		</tr>
-		{/if}
-	{/foreach}
+		{/foreach}
+	{/if}
 
 </table>
-
+{$pager}
 {/if}

www/themes/shared/templates/admin/group-list.tpl
@@ -1,9 +1,7 @@
 <div id="group_list">
 	<h1>{$page->title}</h1>
-		<p>
-			Below is a list of all usenet groups available to be indexed. Click 'Activate' to start indexing a group. Backfill works independently of active.
-		</p>
-	{if $grouplist}
+		<p>Below is a list of all usenet groups available to be indexed. Click 'Activate' to start indexing a group. Backfill works independently of active.</p>
+	{if $results|@count > 0}
 		<div style="position:relative;margin-bottom:5px;">
 			<form name="groupsearch" action="" style="margin-bottom:5px;">
 				<label for="groupname">Group</label>
@@ -12,7 +10,6 @@
 				<input type="submit" value="Go" />
 			</form>
 			<div style="position:absolute;">
-				{$pager}
 			</div>
 			<div style="text-align:center;">
 				<a title="Reset all groups" href="javascript:ajax_all_reset()" class="all_reset" onclick="return confirm('Are you sure? This will reset all groups, deleting all collections/binaries/parts (does not delete releases).');" >Reset all</a> |
@@ -23,6 +20,7 @@
 			</div>
 		</div>
 		<div id="message">msg</div>
+		{$pager}
 		<table style="width:100%;" class="data highlight Sortable">
 			<tr>
 				<th>group</th>
@@ -36,31 +34,31 @@
 				<th>Backfill Days</th>
 				<th>options</th>
 			</tr>
-			{foreach from=$grouplist item=group}
-				<tr id="grouprow-{$group.id}" class="{cycle values=",alt"}">
+			{foreach $results as $result}
+				<tr id="grouprow-{$result.id}" class="{cycle values=",alt"}">
 					<td>
-						<a href="{$smarty.const.WWW_TOP}/group-edit.php?id={$group.id}">{$group.name|replace:"alt.binaries":"a.b"}</a>
-						<div class="hint">{$group.description}</div>
+						<a href="{$smarty.const.WWW_TOP}/group-edit.php?id={$result.id}">{$result.name|replace:"alt.binaries":"a.b"}</a>
+						<div class="hint">{$result.description}</div>
 					</td>
-					<td class="less">{$group.first_record_postdate}<br />{$group.first_record_postdate|timeago}</td>
-					<td class="less">{$group.last_record_postdate}<br />{$group.last_record_postdate|timeago}</td>
-					<td class="less">{$group.last_updated|timeago} ago</td>
-					<td class="less" id="group-{$group.id}">{if $group.active == "1"}<a href="javascript:ajax_group_status({$group.id}, 0)" class="group_active">Deactivate</a>{else}<a href="javascript:ajax_group_status({$group.id}, 1)" class="group_deactive">Activate</a>{/if}</td>
-					<td class="less" id="backfill-{$group.id}">{if $group.backfill == "1"}<a href="javascript:ajax_backfill_status({$group.id}, 0)" class="backfill_active">Deactivate</a>{else}<a href="javascript:ajax_backfill_status({$group.id}, 1)" class="backfill_deactive">Activate</a>{/if}</td>
-					<td class="less">{if $group.minfilestoformrelease==""}n/a{else}{$group.minfilestoformrelease}{/if}</td>
-					<td class="less">{if $group.minsizetoformrelease==""}n/a{else}{$group.minsizetoformrelease|fsize_format:"MB"}{/if}</td>
-					<td class="less">{$group.backfill_target}</td>
-					<td class="less" id="groupdel-{$group.id}">
-						<a title="Reset this group" href="javascript:ajax_group_reset({$group.id})" class="group_reset">Reset</a> |
-						<a title="Delete this group and all of its releases" href="javascript:ajax_group_delete({$group.id})" class="group_delete" onclick="return confirm('Are you sure? This will delete the group from this list.');" >Delete</a> |
-						<a title="Remove all releases from this group" href="javascript:ajax_group_purge({$group.id})" class="group_purge" onclick="return confirm('Are you sure? This will delete all releases, binaries/parts in the selected group');" >Purge</a>
+					<td class="less">{$result.first_record_postdate}<br />{$result.first_record_postdate|timeago}</td>
+					<td class="less">{$result.last_record_postdate}<br />{$result.last_record_postdate|timeago}</td>
+					<td class="less">{$result.last_updated|timeago} ago</td>
+					<td class="less" id="group-{$result.id}">{if $result.active == "1"}<a href="javascript:ajax_group_status({$result.id}, 0)" class="group_active">Deactivate</a>{else}<a href="javascript:ajax_group_status({$result.id}, 1)" class="group_deactive">Activate</a>{/if}</td>
+					<td class="less" id="backfill-{$result.id}">{if $result.backfill == "1"}<a href="javascript:ajax_backfill_status({$result.id}, 0)" class="backfill_active">Deactivate</a>{else}<a href="javascript:ajax_backfill_status({$result.id}, 1)" class="backfill_deactive">Activate</a>{/if}</td>
+					<td class="less">{if $result.minfilestoformrelease==""}n/a{else}{$result.minfilestoformrelease}{/if}</td>
+					<td class="less">{if $result.minsizetoformrelease==""}n/a{else}{$result.minsizetoformrelease|fsize_format:"MB"}{/if}</td>
+					<td class="less">{$result.backfill_target}</td>
+					<td class="less" id="groupdel-{$result.id}">
+						<a title="Reset this group" href="javascript:ajax_group_reset({$result.id})" class="group_reset">Reset</a> |
+						<a title="Delete this group and all of its releases" href="javascript:ajax_group_delete({$result.id})" class="group_delete" onclick="return confirm('Are you sure? This will delete the group from this list.');" >Delete</a> |
+						<a title="Remove all releases from this group" href="javascript:ajax_group_purge({$result.id})" class="group_purge" onclick="return confirm('Are you sure? This will delete all releases, binaries/parts in the selected group');" >Purge</a>
 					</td>
 				</tr>
 			{/foreach}
 		</table>
+		{$pager}
 		<div style="position:relative;margin-top:5px;">
 			<div style="position:absolute;">
-				{$pager}
 			</div>
 			<div style="text-align:center;">
 				<form name="groupsearch" action="" style="margin-bottom:5px;">

www/themes/shared/templates/paginate.tpl
@@ -1,25 +1,25 @@
 {strip}
-	{if $pagelast > 1}
+	{if $pagerlast > 1}
 		<div class="pager">
 			{if $pagecurrent > 1}
 				<a title="First" href="{$pagerquerybase}1">First</a>&nbsp;&nbsp;
 				<a title="Previous" href="{$pagerquerybase}{$pagecurrent-1}">&lt;</a>&nbsp;
 			{/if}
-			{section name=pager start=1 loop=$pagelast+1}
+			{section name=pager loop=$pagerlast+1}
 				{if $smarty.section.pager.iteration == $pagecurrent}
 					<span class="current" title="Current page {$smarty.section.pager.iteration}">{$smarty.section.pager.iteration}</span>&nbsp;
 				{elseif $smarty.section.pager.iteration > $pagecurrent && $smarty.section.pager.iteration < ($pagecurrent + 10)}
 					... <a title="Goto page {$smarty.section.pager.iteration}"
 						href="{$pagerquerybase}{$smarty.section.pager.iteration}{$pagerquerysuffix}">{$smarty.section.pager.iteration}</a>&nbsp;
-				{elseif $smarty.section.pager.iteration < $pagecurrent && $smarty.section.pager.iteration >= ($pagecurrent - 10)}&nbsp;
-					<a title="Goto page {$smarty.section.pager.iteration}"
-							href="{$pagerquerybase}{$smarty.section.pager.iteration}{$pagerquerysuffix}">{$smarty.section.pager.iteration}</a>&nbsp;...&nbsp;
+				{elseif $smarty.section.pager.iteration < $pagecurrent && $smarty.section.pager.index >= ($pagecurrent - 10)}
+					&nbsp; <a title="Goto page {$smarty.section.pager.iteration}"
+						href="{$pagerquerybase}{$smarty.section.pager.iteration}{$pagerquerysuffix}">{$smarty.section.pager.iteration}</a>&nbsp;...&nbsp;
 				{/if}
 			{/section}
-			{if $pagecurrent < $pagelast}
+			{if $pagecurrent < $pagerlast}
 				&nbsp;<a title="Next" href="{$pagerquerybase}{$pagecurrent+1}">&gt;</a>
 				&nbsp;
-				&nbsp;<a title="Last" href="{$pagerquerybase}{$pagelast}">Last</a>
+				&nbsp;<a title="Last" href="{$pagerquerybase}{$pagerlast}">Last</a>
 			{/if}
 		</div>
 	{/if}

commit 5c726c08e6ba084de850afd109279bf86307ed3d

Fix group list active/inactive pages broken by commit 8b5235b8. Closes issue #2586 (mostly).

www/admin/group-list-active.php
@@ -3,7 +3,8 @@ require_once './config.php';
 
 use app\models\Groups;
 
-$pageno = $_REQUEST['pageno'] ?? 1;
+$page = new AdminPage();
+
 $page->title = $page->meta_title = 'Group List';
 
 $pageno = $_REQUEST['pageno'] ?? 1;

www/admin/group-list-inactive.php
@@ -1,9 +1,10 @@
 <?php
 require_once './config.php';
 
-use app\models\Group as Groups;
+use app\models\Groups;
 
 $page = new AdminPage();
+
 $page->title = $page->meta_title = 'Group List';
 
 $pageno = $_REQUEST['pageno'] ?? 1;