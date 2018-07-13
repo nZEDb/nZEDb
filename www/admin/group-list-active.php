5f16495b024e22d20e984299c972cee626ff745c
Bart39 <bart39@sky.com>
2018-07-13 22:59:44 +0100

    Admin grouplist page fixes

--- www/admin/group-list-active.php
+++ www/admin/group-list-active.php
@@ -9,17 +9,20 @@ $page->title = $page->meta_title = 'Group List';
 
 $pageno = $_REQUEST['pageno'] ?? 1;
 
+$groupname = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) ?
+	$_REQUEST['groupname'] : '';
+
 $conditions = ['active' => 1];
 $conditions += empty($groupname) ? [] : ['name' => ['LIKE' => '%$groupname%']];
 
 $count = Groups::find('count', ['conditions' => $conditions]);
-$grouplist = Groups::getRange($pageno,ITEMS_PER_PAGE, $_REQUEST['groupname'], 1);
+$grouplist = Groups::getRange($pageno,ITEMS_PER_PAGE, $groupname, 1);
 
 $groupsearch = empty($_REQUEST['groupname']) ? '' : '&amp;groupname=' . $_REQUEST['groupname'];
 
 $page->smarty->assign(
 	[
-		'groupname'        => $_REQUEST['groupname'],
+		'groupname'        => $groupname,
 		'pagecurrent'      => (int)$pageno,
 		'pagerlast'         => (int)($count / ITEMS_PER_PAGE) + 1,
 		'pagerquerybase'   => WWW_TOP . '/group-list-active?pageno=',
