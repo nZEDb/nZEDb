27b93b09ad8760f5d7bf642df789953ae5083712
Niel Archer <niel.archer@gmail.com>
2019-03-17 05:14:59 +0000

    Replace build/git-hooks/addHooks.sh with nzedb\build\Composer::addGitHooks() method invoked post-install or post-update composer commands.

--- nzedb/build/Composer.php
+++ nzedb/build/Composer.php
@@ -14,37 +14,102 @@
  *
  * @link      <http://www.gnu.org/licenses/>.
  * @author    niel
- * @copyright 2017 nZEDb
+ * @copyright 2017-2019 nZEDb
  */
 namespace nzedb\build;
 
-require_once dirname(__DIR__) . '/constants.php';
-
 use Composer\Script\Event;
+
 
 class Composer
 {
-	public static function postInstallCmd(Event $event)
-	{
-		$last = $output = $return = null;
-		if (!file_exists(nZEDb_CONFIGS . 'settings.php')) {
-			copy(nZEDb_CONFIGS . 'settings.example.php', nZEDb_CONFIGS . 'settings.php');
-		}
+	private static $cwd = null;
 
+
+	public static function postInstallCmd(Event $event) : void
+	{
+		self::setDirectory();
+
+		self::addGitHooks();
+
+		self::createComposerLink();
+
+		self::createVendorLink();
+	}
+
+	public static function postUpdateCmd(Event $event) : void
+	{
+		self::setDirectory();
+
+		self::addGitHooks();
+
+		self::createComposerLink();
+
+		self::createVendorLink();
+	}
+
+	/**
+	 *  IF we are in Composer's dev mode Create symlink(s) from .../.git/hooks/* to the file in
+	 *  .../build/git-hooks/*.
+	 */
+	protected static function addGitHooks(): void
+	{
 		if (getenv('COMPOSER_DEV_MODE') == 1) {
-			echo "Updating git hooks... ";
-			$last = exec('build/git-hooks/addHooks.sh', $output, $return);
-			if ($return > 0) {
-				echo PHP_EOL;
-				exit($last);
+			echo 'Updating git hooks... ';
+
+			$hooks = ['pre-commit', 'post-merge'];
+			foreach ($hooks as $hook) {
+				$link = self::$cwd . '.git/hooks/' . $hook;
+				if (!\file_exists($link) || !\is_link($link)) {
+					if (\file_exists($link)) {
+						unlink($link);
+					}
+
+					self::createSymlink(self::$cwd . 'build/git-hooks/' . $hook, $link);
+				}
 			}
-			echo "done" . PHP_EOL;
+			echo 'done,' . PHP_EOL;
 		}
 	}
 
-	public static function postUpdateCmd(Event $event)
+	protected static function createComposerLink() : void
 	{
-		echo __METHOD__ . ": Working on it!\n";
+		if (!\file_exists(self::$cwd . 'app-laravel/composer.json')) {
+			self::createSymlink(self::$cwd . 'composer.json', self::$cwd . 'app-laravel/composer.json');
+		}
+	}
+
+	protected static function createVendorLink() : void
+	{
+		if (!\file_exists(self::$cwd . 'app-laravel/vendor')) {
+			self::createSymlink(self::$cwd . 'vendor', self::$cwd . 'app-laravel/vendor');
+		}
+	}
+
+
+	private static function createSymlink(string $target, string $link, bool $verbose = false) : void
+	{
+		if ($verbose) {
+			echo "Creating symlink from '$link' to '$target'... ";
+		}
+
+		$result = \symlink($target, $link);
+
+		if (!$result) {
+			echo 'FAILED' . PHP_EOL;
+			throw new \ErrorException("Failed to create symlink from '$link' to '$target'");
+		}
+
+		if ($verbose) {
+			echo 'done.' . \PHP_EOL;
+		}
+	}
+
+	private static function setDirectory() : void
+	{
+		if (\is_null(self::$cwd)) {
+			self::$cwd = \getcwd() . \DIRECTORY_SEPARATOR;
+		}
 	}
 }
 
