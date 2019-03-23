ea821e47ae6b6f91032b0f8f5472320a2f9899ee
Niel Archer <niel.archer@gmail.com>
2019-03-23 20:44:50 +0000

    Add to PDO  options, so that LOCAL infile is permitted.

--- app-laravel/config/database.php
+++ app-laravel/config/database.php
@@ -56,6 +56,7 @@ return [
             'engine' => null,
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
+				PDO::MYSQL_ATTR_LOCAL_INFILE => true,
             ]) : [],
         ],
 
