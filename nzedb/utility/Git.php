8ba9c5140ddeb86b8cc114f136706a12c2ad7d83
Niel Archer <niel.archer@gmail.com>
2019-03-26 23:23:04 +0000

    Update nzedb\utility\Git class with functionailty from Lithium extension class of same name.

--- nzedb/utility/Git.php
+++ nzedb/utility/Git.php
@@ -4,37 +4,47 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
- *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- *
  * You should have received a copy of the GNU General Public License
  * along with this program (see LICENSE.txt in the base directory.  If
  * not, see:
  *
- * @link <http://www.gnu.org/licenses/>.
- * @author niel
+ * @link      <http://www.gnu.org/licenses/>.
+ * @author    niel
  * @copyright 2014 nZEDb
  */
 namespace nzedb\utility;
 
+use zed\Nzedb;
+use \GitRepo;
+
 /**
  * Class Git - Wrapper for various git operations.
+ *
  * @package nzedb\utility
  */
-class Git extends \GitRepo
+class Git extends GitRepo
 {
 	private $branch;
+
+	private $branches = [
+		'stable'      => ['0.x', 'Latest-testing', '\d+\.\d+\.\d+(\.\d+)?'],
+		'development' => ['dev', 'dev-test', 'dev-laravel']
+	];
+
 	private $mainBranches = ['0.x', 'dev', 'Latest-testing', 'dev-test'];
+
+	private $tagLatest = '';
 
 	public function __construct(array $options = [])
 	{
 		$defaults = [
-			'create'		=> false,
-			'initialise'	=> false,
-			'filepath'		=> nZEDb_ROOT,
+			'create'     => false,
+			'initialise' => false,
+			'filepath'   => Nzedb::BASE,
 		];
 		$options += $defaults;
 
@@ -45,7 +55,7 @@ class Git extends \GitRepo
 	/**
 	 * Return the number of commits made to repo
 	 */
-	public function commits()
+	public function commits(): int
 	{
 		$count = 0;
 		$log = explode("\n", $this->log());
@@ -54,6 +64,7 @@ class Git extends \GitRepo
 				++$count;
 			}
 		}
+
 		return $count;
 	}
 
@@ -62,14 +73,53 @@ class Git extends \GitRepo
 	 *
 	 * @return string
 	 */
-	public function describe($options = null)
+	public function describe($options = null): string
 	{
 		return $this->run("describe $options");
 	}
 
-	public function getBranch()
+	public function getBranch(): string
 	{
 		return $this->branch;
+	}
+
+	public function getBranchesDevelop(): array
+	{
+		return $this->branches['development'];
+	}
+
+	/**
+	 * Fetches the array of branch names that are considered to be core.
+	 *
+	 * @return array
+	 */
+	public function getBranchesMain(): array
+	{
+		$main = array_merge($this->getBranchesStable(), $this->getBranchesDevelop());
+
+		return $main;
+	}
+
+	public function getBranchesStable(): array
+	{
+		return $this->branches['stable'];
+	}
+
+	public function getHeadHash(): string
+	{
+		return $this->run('rev-parse HEAD');
+	}
+
+	public function getTagLatest(): string
+	{
+		if (empty($this->tagLatest)) {
+			$this->tagLatest = trim($this->describe('--tags --abbrev=0 HEAD'));
+			if (strtolower($this->tagLatest[0]) === 'v') {
+				$this->tagLatest = substr($this->tagLatest, 1);
+			}
+		}
+
+		return $this->tagLatest;
 	}
 
 	/**
@@ -78,7 +128,7 @@ class Git extends \GitRepo
 	 * @return bool
 	 * @throws \Exception
 	 */
-	public function isCommited($gitObject)
+	public function isCommited($gitObject): bool
 	{
 		$cmd = "cat-file -e $gitObject";
 
@@ -92,15 +142,29 @@ class Git extends \GitRepo
 				throw new \Exception($message);
 			}
 		}
+
 		return ($result === '');
 	}
 
-	public function log($options = null)
+	public function isStable($branch): bool
+	{
+		foreach ($this->getBranchesStable() as $pattern) {
+			if (!preg_match("#$pattern#", $branch)) {
+				continue;
+			} else {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	public function log($options = null): string
 	{
 		return $this->run("log $options");
 	}
 
-	public function mainBranches()
+	public function mainBranches(): array
 	{
 		return $this->mainBranches;
 	}
@@ -110,14 +174,9 @@ class Git extends \GitRepo
 	 *
 	 * @return string
 	 */
-	public function tag($options = null)
+	public function tag($options = null): string
 	{
 		return $this->run("tag $options");
-	}
-
-	public function tagLatest()
-	{
-		return $this->describe("--tags --abbrev=0 HEAD");
 	}
 }
 
