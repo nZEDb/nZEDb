63edf6fcedb6683cb73efd5afaf089ed1348f926
Niel Archer <niel.archer@gmail.com>
2019-03-26 23:20:42 +0000

    Adding Cake application files.

---
+++ app-cake/tests/TestCase/ApplicationTest.php
@@ -0,0 +1,84 @@
+<?php
+/**
+ * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
+ * @link          https://cakephp.org CakePHP(tm) Project
+ * @since         3.3.0
+ * @license       https://opensource.org/licenses/mit-license.php MIT License
+ */
+namespace App\Test\TestCase;
+
+use App\Application;
+use Cake\Error\Middleware\ErrorHandlerMiddleware;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\Middleware\AssetMiddleware;
+use Cake\Routing\Middleware\RoutingMiddleware;
+use Cake\TestSuite\IntegrationTestCase;
+use InvalidArgumentException;
+
+/**
+ * ApplicationTest class
+ */
+class ApplicationTest extends IntegrationTestCase
+{
+
+    /**
+     * testBootstrap
+     *
+     * @return void
+     */
+    public function testBootstrap()
+    {
+        $app = new Application(dirname(dirname(__DIR__)) . '/config');
+        $app->bootstrap();
+        $plugins = $app->getPlugins();
+
+        $this->assertCount(3, $plugins);
+        $this->assertSame('Bake', $plugins->get('Bake')->getName());
+        $this->assertSame('Migrations', $plugins->get('Migrations')->getName());
+        $this->assertSame('DebugKit', $plugins->get('DebugKit')->getName());
+    }
+
+    /**
+     * testBootstrapPluginWitoutHalt
+     *
+     * @return void
+     */
+    public function testBootstrapPluginWitoutHalt()
+    {
+        $this->expectException(InvalidArgumentException::class);
+
+        $app = $this->getMockBuilder(Application::class)
+            ->setConstructorArgs([dirname(dirname(__DIR__)) . '/config'])
+            ->setMethods(['addPlugin'])
+            ->getMock();
+
+        $app->method('addPlugin')
+            ->will($this->throwException(new InvalidArgumentException('test exception.')));
+
+        $app->bootstrap();
+    }
+
+    /**
+     * testMiddleware
+     *
+     * @return void
+     */
+    public function testMiddleware()
+    {
+        $app = new Application(dirname(dirname(__DIR__)) . '/config');
+        $middleware = new MiddlewareQueue();
+
+        $middleware = $app->middleware($middleware);
+
+        $this->assertInstanceOf(ErrorHandlerMiddleware::class, $middleware->get(0));
+        $this->assertInstanceOf(AssetMiddleware::class, $middleware->get(1));
+        $this->assertInstanceOf(RoutingMiddleware::class, $middleware->get(2));
+    }
+}
