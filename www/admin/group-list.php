135107f2ebf5fdf355cdc63ebc15455da0ffc6b7
Niel Archer <niel.archer@gmail.com>
2019-04-28 02:44:14 +0100

    Updated admin/group-list.php to work with Cake Group model

--- www/admin/group-list.php
+++ www/admin/group-list.php
@@ -1,7 +1,8 @@
 <?php
 require_once './config.php';
 
-use app\models\Groups;
+use Cake\Datasource\ConnectionManager;
+use Cake\ORM\TableRegistry;
 
 $page = new AdminPage();
 $page->title = $page->meta_title = 'Group List';
@@ -10,10 +11,34 @@ $pageno = $_REQUEST['pageno'] ?? 1;
 
 $groupname = (isset($_REQUEST['groupname']) && !empty($_REQUEST['groupname'])) ? $_REQUEST['groupname'] : '';
 
-$conditions = empty($groupname) ? [] : ['name' => ['LIKE' => "%$groupname%"]];
+$conditions = empty($groupname) ? [] : ['Groups.name LIKE' => "%$groupname%"];
 
-$count = Groups::find('count', ['conditions' => $conditions]);
-$grouplist = Groups::getRange($pageno, ITEMS_PER_PAGE, $groupname);
+$groups = TableRegistry::getTableLocator()->get('Groups');
+$query = $groups->find('all', [
+	'conditions' => $conditions,
+]);
+$count = $query->count();
+
+$query = $groups->find('all')
+	->select([
+		'id',
+		'name',
+		'backfill_target',
+		'first_record',
+		'first_record_postdate',
+		'last_record',
+		'last_record_postdate',
+		'last_updated',
+		'minfilestoformrelease',
+		'minsizetoformrelease',
+		'active',
+		'backfill',
+		'description',
+	])
+	->where($conditions)
+	->order('name')
+	->limit(ITEMS_PER_PAGE)
+	->page($pageno);
 
 $groupsearch = empty($_REQUEST['groupname']) ? '' : '&amp;groupname=' . $_REQUEST['groupname'];
 
@@ -25,8 +50,8 @@ $page->smarty->assign(
 		'pagerquerybase'   => WWW_TOP . '/group-list.php?pageno=',
 		'pagerquerysuffix' => $groupsearch,
 		'pagertotalitems'  => $count,
-		'results'          => $grouplist,
-		'tz'               => \lithium\data\Connections::config()['default']['object']->timezone(),
+		'results'          => $query->all(),
+		'tz'               => ConnectionManager::get('default')->config()['timezone'],
 	]
 );
 
