145b1c78a26591bbb4ecd3899ead7acf799a3680
Niel Archer <niel.archer@gmail.com>
2019-09-16 20:55:30 +0100

    Update code-style.

--- misc/testing/PostProcess/updateMovieImages.php
+++ misc/testing/PostProcess/updateMovieImages.php
@@ -1,19 +1,20 @@
 <?php
 require_once realpath(dirname(dirname(dirname(__DIR__))) . DIRECTORY_SEPARATOR . 'bootstrap.php');
 
+use app\models\Settings;
 use nzedb\db\DB;
 use nzedb\utility\Misc;
 
 $pdo = new DB();
 $covers = $updated = $deleted = 0;
 
-if ($argc == 1 || $argv[1] != 'true') {
+if ($argc === 1 || $argv[1] != 'true') {
 	exit($pdo->log->error("\nThis script will check all images in covers/movies and compare to db->movieinfo.\nTo run:\nphp $argv[0] true\n"));
 }
 
-$row = $pdo->queryOneRow("SELECT value FROM settings WHERE setting = 'coverspath'");
-if ($row !== false) {
-	Misc::setCoversConstant($row['value']);
+$coverspath = Settings::value('..coverspath', true);
+if ($coverspath !== null) {
+	Misc::setCoversConstant($coverspath);
 } else {
 	die("Unable to set Covers' constant!\n");
 }
@@ -28,7 +29,7 @@ foreach ($itr as $filePath) {
 			$covers++;
 		} else {
 			$run = $pdo->queryDirect('SELECT imdbid FROM movieinfo WHERE imdbid = ' . $match[1]);
-			if ($run->rowCount() == 0) {
+			if ($run->rowCount() === 0) {
 				echo $pdo->log->info($filePath . ' not found in db.');
 			}
 		}
@@ -43,7 +44,7 @@ foreach ($itr as $filePath) {
 				printf('UPDATE movieinfo SET backdrop = 1 WHERE backdrop = 0 AND imdbid = ' . $match1[1] . "\n");
 			} else {
 				$run = $pdo->queryDirect('SELECT imdbid FROM movieinfo WHERE imdbid = ' . $match1[1]);
-				if ($run->rowCount() == 0) {
+				if ($run->rowCount() === 0) {
 					echo $pdo->log->info($filePath . ' not found in db.');
 				}
 			}
@@ -53,20 +54,20 @@ foreach ($itr as $filePath) {
 
 $qry = $pdo->queryDirect('SELECT imdbid FROM movieinfo WHERE cover = 1');
 if ($qry instanceof \Traversable) {
-	foreach ($qry as $rows) {
-		if (!is_file($path2covers . $rows['imdbid'] . '-cover.jpg')) {
-			$pdo->queryDirect('UPDATE movieinfo SET cover = 0 WHERE cover = 1 AND imdbid = ' . $rows['imdbid']);
-			echo $pdo->log->info($path2covers . $rows['imdbid'] . '-cover.jpg does not exist.');
+	foreach ($qry as $row) {
+		if (!is_file($path2covers . $row['imdbid'] . '-cover.jpg')) {
+			$pdo->queryDirect('UPDATE movieinfo SET cover = 0 WHERE cover = 1 AND imdbid = ' . $row['imdbid']);
+			echo $pdo->log->info($path2covers . $row['imdbid'] . '-cover.jpg does not exist.');
 			$deleted++;
 		}
 	}
 }
 $qry1 = $pdo->queryDirect('SELECT imdbid FROM movieinfo WHERE backdrop = 1');
 if ($qry1 instanceof \Traversable) {
-	foreach ($qry1 as $rows) {
-		if (!is_file($path2covers . $rows['imdbid'] . '-backdrop.jpg')) {
-			$pdo->queryDirect('UPDATE movieinfo SET backdrop = 0 WHERE backdrop = 1 AND imdbid = ' . $rows['imdbid']);
-			echo $pdo->log->info($path2covers . $rows['imdbid'] . '-backdrop.jpg does not exist.');
+	foreach ($qry1 as $row) {
+		if (!is_file($path2covers . $row['imdbid'] . '-backdrop.jpg')) {
+			$pdo->queryDirect('UPDATE movieinfo SET backdrop = 0 WHERE backdrop = 1 AND imdbid = ' . $row['imdbid']);
+			echo $pdo->log->info($path2covers . $row['imdbid'] . '-backdrop.jpg does not exist.');
 			$deleted++;
 		}
 	}
